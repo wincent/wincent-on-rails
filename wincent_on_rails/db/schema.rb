57f43c17cf278a646307074fec7f4c0871ca8d52
Wincent Colaiuta <win@wincent.com>
2007-10-15 03:34:43 +0200

    Update script/spec
    Yet another file which should have been updated as part of the move to the
    RSpec trunk (b8c4b59).

--- wincent_on_rails/db/schema.rb
+++ wincent_on_rails/db/schema.rb
@@ -2,6 +2,107 @@
 # migrations feature of ActiveRecord to incrementally modify your database, and
 # then regenerate this schema definition.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 10) do
+
+  create_table "comments", :force => true do |t|
+    t.text     "body",                :default => "",    :null => false
+    t.integer  "user_id",             :default => 0,     :null => false
+    t.integer  "commentable_id",      :default => 0,     :null => false
+    t.string   "commentable_type",    :default => "",    :null => false
+    t.boolean  "awaiting_moderation", :default => true
+    t.boolean  "spam",                :default => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "emails", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "address",            :default => "",    :null => false
+    t.boolean  "verified",           :default => false, :null => false
+    t.string   "verification_key"
+    t.datetime "verification_limit"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "issues", :force => true do |t|
+    t.integer  "type"
+    t.string   "summary",             :default => "",    :null => false
+    t.integer  "status_id",           :default => 0,     :null => false
+    t.boolean  "public"
+    t.integer  "user_id"
+    t.text     "description"
+    t.boolean  "awaiting_moderation", :default => true
+    t.boolean  "spam",                :default => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "locales", :force => true do |t|
+    t.string   "code"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :default => "", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+
+  create_table "statuses", :force => true do |t|
+    t.string   "name",        :default => "",    :null => false
+    t.string   "description", :default => "",    :null => false
+    t.boolean  "closed",      :default => true,  :null => false
+    t.boolean  "is_default",  :default => false, :null => false
+    t.integer  "position"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "taggings", :force => true do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id",   :default => 0,  :null => false
+    t.string   "taggable_type", :default => "", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tags", :force => true do |t|
+    t.string   "name",       :default => "", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "translations", :force => true do |t|
+    t.integer  "locale_id"
+    t.string   "key"
+    t.string   "translation"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "translations", ["locale_id", "key"], :name => "index_translations_on_locale_id_and_key", :unique => true
+
+  create_table "users", :force => true do |t|
+    t.string   "login_name",      :default => "",    :null => false
+    t.string   "display_name",    :default => "",    :null => false
+    t.string   "passphrase_hash", :default => "",    :null => false
+    t.string   "passphrase_salt", :default => "",    :null => false
+    t.integer  "locale_id"
+    t.boolean  "superuser",       :default => false
+    t.boolean  "verified",        :default => false
+    t.boolean  "suspended",       :default => false
+    t.string   "session_key"
+    t.datetime "session_expiry"
+    t.datetime "deleted_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
