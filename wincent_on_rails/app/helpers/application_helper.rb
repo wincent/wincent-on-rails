fba09f23e769f8e7b38385a19814323fb6d4f99f
Wincent Colaiuta <win@wincent.com>
2007-10-15 03:28:41 +0200

    Pretty formatting for creation/update timestamps
    Add a method to the application helper for human-friendly display of
    timestamps.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- wincent_on_rails/app/helpers/application_helper.rb
+++ wincent_on_rails/app/helpers/application_helper.rb
@@ -1,3 +1,32 @@
-# Methods added to this helper will be available to all templates in the application.
+require 'time_additions'
+
 module ApplicationHelper
+
+  # Pretty formatting for model creation/update information.
+  #
+  # Examples:
+  #   - Created yesterday
+  #   - Created 4 hours ago, updated a few seconds ago
+  #
+  def timeinfo(model, precise = false)
+    # BUG: in Spanish localization will be translated as "creado" (masculine) etc for both masculine and feminine models
+    created = model.created_at
+    updated = model.updated_at
+    if precise  # always show exact date and time
+      if created == updated
+        'Created %s'.localized % created.to_s(:long)
+      else
+        'Created %s, updated %s' % [created.to_s(:long), updated.to_s(:long)]
+      end
+    else        # show human-friendly dates ("yesterday", "2 hours ago" etc)
+      created = created.distance_in_words
+      updated = updated.distance_in_words
+      if created == updated
+        'Created %s'.localized % created
+      else
+        'Created %s, updated %s'.localized % [created, updated]
+      end
+    end
+  end
+
 end
