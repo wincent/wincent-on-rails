edf298ed6f1d0e061a6f0d83980376042c93b418
Wincent Colaiuta <win@wincent.com>
2007-10-15 03:13:42 +0200

    Prepare spec helper
    Include FixtureReplacement from within the spec helper, add a custom matcher
    class for conveniently verifying validations, and remove excess comments.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- wincent_on_rails/spec/spec_helper.rb
+++ wincent_on_rails/spec/spec_helper.rb
@@ -1,23 +1,52 @@
-# This file is copied to ~/spec when you run 'ruby script/generate rspec'
-# from the project root directory.
 ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'spec/rails'
 
+include FixtureReplacement
+
 Spec::Runner.configure do |config|
   config.use_transactional_fixtures = true
   config.use_instantiated_fixtures  = false
-  config.fixture_path = RAILS_ROOT + '/spec/fixtures'
-
-  # You can declare fixtures for each behaviour like this:
-  #   describe "...." do
-  #     fixtures :table_a, :table_b
-  #
-  # Alternatively, if you prefer to declare them only once, you can
-  # do so here, like so ...
-  #
-  #   config.global_fixtures = :table_a, :table_b
-  #
-  # If you declare global fixtures, be aware that they will be declared
-  # for all of your examples, even those that don't use them.
+  config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
 end
+
+module Spec
+  module Rails
+    module Matchers
+      class FailValidationFor # :nodoc:
+        def initialize attribute
+          @attribute = attribute
+        end
+
+        def matches? model
+          @model = model
+          !@model.valid? and !@model.errors.on(@attribute).nil?
+        end
+
+        def failure_message
+          "expected to fail validation with errors on #{@attribute} but was #{self.valid}; #{self.errors}"
+        end
+
+        def negative_failure_message
+          "expected to pass validation with no errors on #{@attribute} but was #{self.valid}; #{self.errors}"
+        end
+
+        def description
+          "fail validation for attribute #{@attribute}"
+        end
+
+        def valid
+          if @model.valid? then 'valid' else 'invalid' end
+        end
+
+        def errors
+          if @model.valid? then 'no errors' else @model.errors.full_messages.to_sentence end
+        end
+      end # class FailValidationFor
+
+      def fail_validation_for attribute
+        FailValidationFor.new attribute
+      end
+    end # module Matchers
+  end # module Rails
+end # module Spec
