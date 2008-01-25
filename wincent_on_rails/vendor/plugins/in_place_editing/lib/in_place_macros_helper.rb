3fa763da9ca22d99b48619585bc47b19daccc5e6
Wincent Colaiuta <win@wincent.com>
2008-01-25 16:27:18 +0100

    Clean up in in place editor helper macros
    The code can be made to read nicer by dumping an "a = a || b" construct
    in favor of a "a ||= b" one.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- wincent_on_rails/vendor/plugins/in_place_editing/lib/in_place_macros_helper.rb
+++ wincent_on_rails/vendor/plugins/in_place_editing/lib/in_place_macros_helper.rb
@@ -66,7 +66,7 @@ module InPlaceMacrosHelper
   def in_place_editor_field(object, method, tag_options = {}, in_place_editor_options = {})
     tag = ::ActionView::Helpers::InstanceTag.new(object, method, self)
     tag_options = {:tag => "span", :id => "#{object}_#{method}_#{tag.object.id}_in_place_editor", :class => "in_place_editor_field"}.merge!(tag_options)
-    in_place_editor_options[:url] = in_place_editor_options[:url] || url_for({ :action => "set_#{object}_#{method}", :id => tag.object.id })
+    in_place_editor_options[:url] ||= url_for({ :action => "set_#{object}_#{method}", :id => tag.object.id })
     tag.to_content_tag(tag_options.delete(:tag), tag_options) +
     in_place_editor(tag_options[:id], in_place_editor_options)
   end
