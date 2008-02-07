ba69361ac02aced11b8b2cd22f160c10800976b7
Wincent Colaiuta <win@wincent.com>
2008-02-07 17:22:45 +0100

    Minor rewrapping for readability
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- wincent_on_rails/app/models/tag.rb
+++ wincent_on_rails/app/models/tag.rb
@@ -2,9 +2,9 @@ class Tag < ActiveRecord::Base
   has_many                :taggings
   has_many                :taggables, :through => :taggings
   validates_presence_of   :name
-  validates_format_of     :name, :with => /\A[a-z]+(\.[a-z]+)*\z/i, :message =>
-    'may only contain letters with words separated by periods'
-
+  validates_format_of     :name,
+    :with     => /\A[a-z]+(\.[a-z]+)*\z/i,
+    :message  => 'may only contain letters with words separated by periods'
   validates_uniqueness_of :name
 
   # returns a floating point number between 0 and 1 to denote a tag's relative popularity
