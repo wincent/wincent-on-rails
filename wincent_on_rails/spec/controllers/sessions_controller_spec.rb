e89c44d99e4b86d8b9cac920f779bf47ddd5286e
Wincent Colaiuta <win@wincent.com>
2008-02-07 12:29:03 +0100

    Fix sessions controller specs
    This was a tricky one, who to simulate a user login in the specs. The
    problem is that the testing machinery assumes one request per spec, so
    you can't do the obvious easy solution of doing a real login and then
    following it up with other specs.
    
    The next most obvious option is just calling the "current_user=" method
    but that won't work either. That method sets stuff in cookies and the
    session and those aren't set up yet prior to doing a post in your specs.
    
    So in the end we just reach in and set the "@current_user" instance
    variable directly. Note that we have to intercept the call to the
    "login_before" before filter otherwise it will proceed to clear the
    instance variable as soon as we initiate an actual request.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- wincent_on_rails/spec/controllers/sessions_controller_spec.rb
+++ wincent_on_rails/spec/controllers/sessions_controller_spec.rb
@@ -5,14 +5,28 @@ describe SessionsController do
   it_should_behave_like 'ApplicationController'
 end
 
+def login_as user
+  controller.instance_eval { @current_user = user }
+  controller.stub!(:login_before).and_return(nil)   # don't let the before filter clear the user again
+end
+
+def current_user
+  controller.send :current_user
+end
+
 describe SessionsController, 'logging in with a valid username and passphrase' do
   before do
-    User.should_receive(:authenticate).and_return(default_user)
+    @user = create_user
+    User.should_receive(:authenticate).and_return(@user)
     post 'create'
   end
 
   it 'should flash a success notice' do
     flash[:notice].should match(/logged in/i)
+  end
+
+  it 'should set the current user' do
+    current_user.should == @user
   end
 
   it 'should redirect to the home path' do
@@ -35,8 +49,9 @@ describe SessionsController, 'logging in with an invalid username or passphrase'
   end
 end
 
-describe SessionsController, 'logging out' do
+describe SessionsController, 'logging out when previously logged in' do
   before do
+    login_as create_user
     post 'destroy'
   end
 
@@ -45,7 +60,21 @@ describe SessionsController, 'logging out' do
   end
 
   it 'should set the current user to nil' do
-    assigns[:current_user].should == nil
+    current_user.should == nil
+  end
+
+  it 'should redirect to the home path' do
+    response.should redirect_to(home_path)
+  end
+end
+
+describe SessionsController, 'logging out when not previously logged in' do
+  before do
+    post 'destroy'
+  end
+
+  it 'should flash an error' do
+    flash[:error].should match(/Can't log out/i)
   end
 
   it 'should redirect to the home path' do
