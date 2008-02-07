5313bbb8540e2f1b61f33a31578c542c78126f24
Wincent Colaiuta <win@wincent.com>
2008-02-07 10:22:44 +0100

    Drop HTML caching from revisions table
    Now we just store the wikitext markup in the database and
    generate HTML on demand.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- wincent_on_rails/db/migrate/012_create_revisions.rb
+++ wincent_on_rails/db/migrate/012_create_revisions.rb
@@ -2,8 +2,7 @@ class CreateRevisions < ActiveRecord::Migration
   def self.up
     create_table :revisions do |t|
       t.integer     :article_id, :null => false
-      t.text        :wikitext
-      t.text        :html
+      t.text        :body
       t.boolean     :public
       t.timestamps
     end
