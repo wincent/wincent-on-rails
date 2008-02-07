aa385f3da1f2b525664820838a626510e55979ad
Wincent Colaiuta <win@wincent.com>
2008-02-07 10:37:11 +0100

    Fix locale specs
    Again, minimal changes to get them running.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- wincent_on_rails/spec/models/locale_spec.rb
+++ wincent_on_rails/spec/models/locale_spec.rb
@@ -1,11 +1,7 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Locale do
-  before(:each) do
-    @locale = Locale.new
-  end
-
-  it "should be valid" do
-    @locale.should be_valid
+  it 'should be valid' do
+    create_locale.should be_valid
   end
 end
