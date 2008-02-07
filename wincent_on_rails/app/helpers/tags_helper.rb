b724d1a8331f4bea504bca8ffdfa923c7de2bb3a
Wincent Colaiuta <win@wincent.com>
2008-02-07 16:15:44 +0100

    Size tag font depending on popularity
    According to "Web 2.0" doctrine you need tags and you need to change the
    font size to indicate the popularity of a tag.
    
    This commit introduces the resizing in the range of 1.0em (for impopular
    tags) through to 2.0em (for the most popular).
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- wincent_on_rails/app/helpers/tags_helper.rb
+++ wincent_on_rails/app/helpers/tags_helper.rb
@@ -1,5 +1,5 @@
 module TagsHelper
   def scaled_tag tag
-    link_to tag.name, tag_path(tag), :style => 'font-size: 10pt;'
+    link_to tag.name, tag_path(tag), :style => "font-size: #{1 + tag.normalized_taggings_count * 1}em;"
   end
 end
--- wincent_on_rails/app/models/tag.rb
+++ wincent_on_rails/app/models/tag.rb
@@ -1,6 +1,16 @@
 class Tag < ActiveRecord::Base
   has_many :taggings
   has_many :taggables, :through => :taggings
+
+  # returns a floating point number between 0 and 1 to denote a tag's relative popularity
+  def normalized_taggings_count
+    max = Tag.maximum(:taggings_count) # the Rails query cache will cache this
+    min = Tag.minimum(:taggings_count) # the Rails query cache will cache this
+    range = max - min
+    count = self.taggings_count
+    (count - min).to_f / range
+  end
+
   def to_param
     self.name
   end
