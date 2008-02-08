57f734acb5202e2e0809ef1ef07b85c6befd8be6
Wincent Colaiuta <win@wincent.com>
2008-02-08 01:36:55 +0100

    Move scaled_tag helper function into application helper
    Move the scaled_tag helper function from the tags helper to the main
    applicaton helper so that all views can benefit from it.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- wincent_on_rails/app/helpers/application_helper.rb
+++ wincent_on_rails/app/helpers/application_helper.rb
@@ -37,6 +37,13 @@ module ApplicationHelper
     end
   end
 
+  def scaled_tag tag
+    # NOTE: that we report the full taggings count here: may want to exclude taggables to which the user doesn't have access
+    link_to tag.name, tag_path(tag),
+      :style => "font-size: #{1 + tag.normalized_taggings_count * 1}em;",
+      :title => "#{item_count(tag.taggings_count)} tagged with '#{tag.name}'"
+  end
+
   # TODO: potentially move these methods into authentication.rb as well
   def logged_in_only &block
     if logged_in?
--- wincent_on_rails/app/helpers/tags_helper.rb
+++ wincent_on_rails/app/helpers/tags_helper.rb
@@ -1,8 +1,2 @@
 module TagsHelper
-  def scaled_tag tag
-    # NOTE: that we report the full taggings count here: may want to exclude taggables to which the user doesn't have access
-    link_to tag.name, tag_path(tag),
-      :style => "font-size: #{1 + tag.normalized_taggings_count * 1}em;",
-      :title => "#{item_count(tag.taggings_count)} tagged with '#{tag.name}'"
-  end
 end
