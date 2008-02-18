61b8b6fd55029d5db2401e239c80938268605c31
Wincent Colaiuta <win@wincent.com>
2008-02-18 12:37:23 +0100

    Basic tag display
    When showing a given tag, show all of its taggings in a table.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- wincent_on_rails/app/controllers/tags_controller.rb
+++ wincent_on_rails/app/controllers/tags_controller.rb
@@ -4,6 +4,14 @@ class TagsController < ApplicationController
   end
 
   def show
-    @tag = Tag.find_by_name(params[:id]) || Tag.find(params[:id])
+    @tag      = Tag.find_by_name(params[:id]) || Tag.find(params[:id])
+    @taggings = Tagging.find_all_by_tag_id(@tag.id)
   end
+
+private
+
+  def record_not_found
+    super tags_path
+  end
+
 end
--- wincent_on_rails/app/helpers/tags_helper.rb
+++ wincent_on_rails/app/helpers/tags_helper.rb
@@ -1,2 +1,12 @@
 module TagsHelper
+  def taggable_link model
+    case model
+    when Article
+      link_to model.title, wiki_path(model)
+    when Post
+      link_to model.title, blog_path(model)
+    else
+      raise 'not yet implemented'
+    end
+  end
 end
