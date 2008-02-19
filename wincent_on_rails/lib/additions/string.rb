8d444fd2ff7d059df150413f86e203979200c0ef
Wincent Colaiuta <win@wincent.com>
2008-02-19 20:04:45 +0100

    Add preprocessing of wikitext
    We automatically turn strings like "bug #12" and "issue #19" into links
    to the corresponding item on the issue tracker.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- wincent_on_rails/lib/additions/string.rb
+++ wincent_on_rails/lib/additions/string.rb
@@ -5,10 +5,18 @@ class String
   def to_wikitext
     # this is where we will add pre-filtering as well
     @@shared_wikitext_parser ||= Wikitext::Parser.new(:space_to_underscore => true)
-    @@shared_wikitext_parser.parse self
+    @@shared_wikitext_parser.parse wikitext_preprocess
   end
 
   # Convenience shortcut
   alias :w :to_wikitext
+
+private
+
+  # for now do this in pure Ruby
+  # if speed later becomes a concern can whip up a Ragel C extension to do it
+  def wikitext_preprocess
+    gsub /\b(bug|issue|request) #(\d+)/i, '[[issues/\2|\1 #\2]]'
+  end
 
 end
