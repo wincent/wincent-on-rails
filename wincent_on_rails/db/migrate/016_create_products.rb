8ef596d2c863c56f86a2ce0ca9042c84e8c16c55
Wincent Colaiuta <win@wincent.com>
2008-03-04 21:36:18 +0100

    Make sure product name is not nil
    Add a database-level constraint.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- wincent_on_rails/db/migrate/016_create_products.rb
+++ wincent_on_rails/db/migrate/016_create_products.rb
@@ -1,8 +1,7 @@
 class CreateProducts < ActiveRecord::Migration
   def self.up
     create_table :products do |t|
-      t.string :name
-
+      t.string      :name, :null => false
       t.timestamps
     end
   end
