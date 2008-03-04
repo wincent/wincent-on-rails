b023972350959652802715a45b6f90d05e86c638
Wincent Colaiuta <win@wincent.com>
2008-03-04 21:42:31 +0100

    Require product names to be unique
    Add validation and corresponding spec.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- wincent_on_rails/spec/models/product_spec.rb
+++ wincent_on_rails/spec/models/product_spec.rb
@@ -4,4 +4,10 @@ describe Product, 'validating the name' do
   it 'should require it to be present' do
     new_product(:name => nil).should fail_validation_for(:name)
   end
+
+  it 'should require it to be unique' do
+    name = String.random
+    create_product(:name => name).should be_valid
+    new_product(:name => name).should fail_validation_for(:name)
+  end
 end
