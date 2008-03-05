4b2b9cb5153873de82e3eb4559d10e5f8eab8984
Wincent Colaiuta <win@wincent.com>
2008-03-05 09:30:13 +0100

    Add basic products controller specs
    These are the shared application controller specs which all controllers
    should pass.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ wincent_on_rails/spec/controllers/products_controller_spec.rb
@@ -0,0 +1,6 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+require File.dirname(__FILE__) + '/application_spec'
+
+describe ProductsController do
+  it_should_behave_like 'ApplicationController'
+end
