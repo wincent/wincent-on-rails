a4263dffa090fd08370c9c42d68a9edd2beeeaff
Wincent Colaiuta <win@wincent.com>
2008-03-05 09:23:04 +0100

    Add permalink column to products table
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- wincent_on_rails/db/migrate/016_create_products.rb
+++ wincent_on_rails/db/migrate/016_create_products.rb
@@ -1,15 +1,19 @@
 class CreateProducts < ActiveRecord::Migration
   def self.up
     create_table :products do |t|
-      t.string      :name, :null => false
+      t.string      :name,      :null => false
+      t.string      :permalink, :null => false
       t.timestamps
     end
 
-     # database-level constraint to ensure uniqueness (validates_uniqueness_of vulnerable to races)
-     add_index  :products, :name, :unique => true
+     # database-level constraints to ensure uniqueness (validates_uniqueness_of vulnerable to races)
+     add_index  :products, :name,       :unique => true
+     add_index  :products, :permalink,  :unique => true
   end
 
   def self.down
-    drop_table :products
+    remove_index  :products, :column => :permalink
+    remove_index  :products, :column => :name
+    drop_table    :products
   end
 end
