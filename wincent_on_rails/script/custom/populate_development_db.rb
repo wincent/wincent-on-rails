4258fcc7f92735ab379573654c6adc52df60a880
Wincent Colaiuta <win@wincent.com>
2008-03-17 14:19:51 +0100

    Add script for populating the development environment database
    Mostly just random sample data.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ wincent_on_rails/script/custom/populate_development_db.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env script/runner
+# (must run from top level of application)
+
+include FixtureReplacement
+
+def chunk_of_text
+  (Array.new(rand(100) + 25)).collect { |a| String.random(rand(16) + 1) }.join(' ')
+end
+
+if ENV['RAILS_ENV'] != 'development'
+  puts "warning: RAILS_ENV is '#{ENV['RAILS_ENV']}'"
+  puts "(expected 'development')"
+end
+
+puts "Really populate development database with sample data?"
+answer = gets.chomp
+exit unless answer =~ /\Ay(es?)?\z/i
+
+user = create_user(:passphrase => 'password', :passphrase_confirmation => 'password', :superuser => true)
+user.emails.create!(:address => 'win@wincent.com')
+puts "Created superuser with email address: win@wincent.com, password: password"
+
+tags = ['foo', 'bar', 'baz', 'thing', 'this', 'that', 'the', 'other', 'xyz', 'abc.foo']
+
+create_forum
+create_forum(:description => 'talk about something')
+create_forum(:description => 'talk about something else')
+create_forum
+
+create_post(:excerpt => chunk_of_text)
+create_post(:accepts_comments => true, :excerpt => chunk_of_text)
+50.times do
+  post = create_post(:excerpt => chunk_of_text)
+  post.tag tags.rand
+end
+
+10.times { create_product }
+
+100.times { create_article(:body => chunk_of_text) }
