0ae3c7026c25c1f4ff51921a606c01c1ccef3179
Wincent Colaiuta <win@wincent.com>
2008-03-19 15:52:43 +0100

    Capistrano: make sudo work (needs a pty)
    sudo won't run unless the "pty" option is set to true.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,6 +39,9 @@ set :branch, 'origin/maint'
 set :scm, :git
 set :group_writable, :false
 
+# sudo won't run without this ("sorry, you must have a tty to run sudo")
+default_run_options[:pty] = true
+
 depend :remote, :gem, :wikitext, '>= 1.0'
 depend :remote, :gem, :haml, '>= 1.8.2'
 depend :remote, :gem, :rails, '>= 2.0.2'
