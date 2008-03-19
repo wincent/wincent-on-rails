04283e8fc13ebe8630521b244f2c519900b93393
Wincent Colaiuta <win@wincent.com>
2008-03-19 23:36:15 +0100

    Improve MediaWiki import script
    Extract timestamps from MediaWiki dump, report exceptions for failed
    record creation (basically this only ever occurs due to a duplicate
    title, it seems), and improve category-to-tag parsing by stripping off
    leading or trailing period (this would prevent saving).
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- script/custom/import_mediawiki_files.rb
+++ script/custom/import_mediawiki_files.rb
@@ -7,9 +7,10 @@ doc.root.each_element('//page') do |page|
   body = page.elements['revision'].elements['text'].text
   skip = [ /\ACategory:/, /\AHelp:/, /\AKnowledge Base:/, /\AMediaWiki:/, /\AUser talk:/, /\AUser:/ ]
   next if skip.any? { |re| title =~ re }
+  timestamp = Time.parse(page.elements['revision'].elements['timestamp'].text).to_s(:db)
   begin
     if body =~ /\A\s*#REDIRECT\s+\[\[(.+)\]\]\s*\z/
-      Article.create(:title => title, :redirect => "[[#{$~[1]}]]")
+      article = Article.create! :title => title, :redirect => "[[#{$~[1]}]]"
     else
       tags = []
       while true
@@ -18,12 +19,19 @@ doc.root.each_element('//page') do |page|
           ''
         end
       end
-      tags = tags.collect { |tag| tag.downcase.gsub(/[^a-z]+/, '.') }
+      tags = tags.collect { |tag| tag.downcase.gsub(/[^a-z]+/, '.').gsub(/(\A\.)|(\.\z)/, '') }
       pending_tags = tags.join(' ')
-      Article.create(:title => title, :body => body, :pending_tags => pending_tags)
+      article = Article.create! :title => title, :body => body, :pending_tags => pending_tags
     end
+
+    # now update the timestamps: have to go behind ActiveRecord's back to do this otherwise it will override us
+    Article.connection.execute <<-SQL
+      UPDATE  articles
+      SET     created_at = '#{timestamp}', updated_at = '#{timestamp}'
+      WHERE   id = #{article.id}
+    SQL
+
   rescue Exception => e
     puts "Rescued exception for article title '#{title}': #{e.inspect}"
   end
 end
-
