ca221dd771a91b0bd4a7128d764a5dd9da45800f
Wincent Colaiuta <win@wincent.com>
2008-03-20 19:52:22 +0100

    Add stories for logging out
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- stories/sessions.rb
+++ stories/sessions.rb
@@ -2,6 +2,21 @@
 require File.join(File.dirname(__FILE__), 'helper')
 
 steps = Spec::Story::StepGroup.new do |define|
+  define.given 'a logged-in user' do
+    passphrase = String.random
+    email = "#{String.random}@#{String.random}.com"
+    user = create_user(:passphrase => passphrase, :passphrase_confirmation => passphrase)
+    create_email(:address => email, :user => user)
+    get '/login'
+    fills_in 'email', :with => email
+    fills_in 'passphrase', :with => passphrase
+    clicks_button 'Log in'
+  end
+
+  define.given 'an anonymous user' do
+    get_via_redirect '/logout'
+  end
+
   define.given 'a user with email "$email" and passphrase "$passphrase"' do |email, passphrase|
     user = create_user(:passphrase => passphrase, :passphrase_confirmation => passphrase)
     create_email(:address => email, :user => user)
@@ -9,6 +24,10 @@ steps = Spec::Story::StepGroup.new do |define|
 
   define.when 'I go to the login form' do
     get '/login'
+  end
+
+  define.when 'I logout' do
+    get_via_redirect '/logout'
   end
 
   define.when 'I fill in the "$field" field with "$text"' do |field, text|
@@ -55,5 +74,24 @@ Story 'logging in', %{
     And 'I fill in the "passphrase" field with "passphrase1000"'
     And 'I click the "Log in" button'
     Then 'the page should show "Invalid email or passphrase"'
+  end
+end
+
+Story 'logging out', %{
+  As an account holder
+  I want to log out
+  So that nobody using my machine can impersonate me
+}, :type => RailsStory, :steps => steps do
+
+  Scenario 'logging out when logged-in' do
+    Given 'a logged-in user'
+    When 'I logout'
+    Then 'the page should show "You have logged out successfully"'
+  end
+
+  Scenario 'logging out when not logged-in' do
+    Given 'an anonymous user'
+    When 'I logout'
+    Then 'the page should show "Can\'t log out \(weren\'t logged in\)"'
   end
 end
