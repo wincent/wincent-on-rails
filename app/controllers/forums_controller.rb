1795ab2d7831f559b30ff4214c76bf78977277ba
Wincent Colaiuta <win@wincent.com>
2008-03-22 19:44:52 +0100

    Tackle N + 1 SELECT problem in forums controller
    Here we replace the old query with a new one that avoids the N + 1
    problem but I've actually left the older query, and an intermediate one,
    commented out in the method for documentation purposes.
    
    In effect the three queries are "N + 1", "N + some" and "N + none", and
    they represent a trade off between lots of simple queries and fewer
    complex ones. I'll need to benchmark this on the production server to
    decide which one performs best (here on the heavily-loaded, slow local
    machine in the development environment they are all equally slow).
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -23,7 +23,18 @@ class ForumsController < ApplicationController
 
   def show
     @paginator = Paginator.new(params, @forum.topics.count(:conditions => { :public => true }), forum_path(@forum), 20)
-    @topics = @forum.topics.find(:all, :conditions => { :public => true }, :limit => @paginator.limit, :offset => @paginator.offset)
+
+    # option 1: full "N + 1" SELECT problem caused when the view does topic.last_commenter.display_name on each topic
+    #@topics = @forum.topics.find(:all, :conditions => { :public => true }, :limit => @paginator.limit,
+    #  :offset => @paginator.offset)
+
+    # option 2: "N + some": still incurring a topic.user.display_name query for each topic which doesn't have comments yet
+    #@topics = @forum.topics.find(:all, :conditions => { :public => true }, :limit => @paginator.limit,
+    #  :offset => @paginator.offset, :include => 'last_commenter')
+
+    # option 3: "N + none": no extra queries, but two LEFT OUTER JOINS which make for a complex query
+    @topics = @forum.topics.find(:all, :conditions => { :public => true }, :limit => @paginator.limit,
+      :offset => @paginator.offset, :include => ['user', 'last_commenter'])
   end
 
 private
