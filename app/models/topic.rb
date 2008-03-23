4988c6360059b7e1a01d118ce1dd6d800d5e7e29
Wincent Colaiuta <win@wincent.com>
2008-03-23 15:11:12 +0100

    Use destroy in Forum and Topic models
    I don't actually anticipate deleting Forums (ever) or Topics
    (often), but for now going with the :destroy rather than :delete_all
    option for the dependent models.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,10 +2,12 @@ class Topic < ActiveRecord::Base
   belongs_to  :forum,     :counter_cache => true
   belongs_to  :user
   belongs_to  :last_commenter, :class_name => 'User', :foreign_key => 'last_commenter_id'
-  has_many    :comments,  :as => :commentable, :extend => Commentable, :order => 'comments.updated_at DESC',
-              :dependent => :delete_all
-
+  has_many    :comments,  :as => :commentable, :extend => Commentable, :order => 'comments.updated_at DESC', :dependent => :destroy
   acts_as_taggable
+
+  # TODO: attr_accessible here to prevent taking over posts
+  # current have some params that I want only the admin to be able to set
+  # like "public" and "accepts comments"
 
   def hit!
     Topic.increment_counter :view_count, id
