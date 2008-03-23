92ec0f5cc5705c41e5e95ff39d7cb2cde9944079
Wincent Colaiuta <win@wincent.com>
2008-03-23 15:19:59 +0100

    Use destroy in Forum model
    In 4988c63 I switched from using :delete_all to :destroy in both the
    Forum and Topic models, but this was going a little too far. It turns
    out that the most efficient pattern is to use :destroy at the Forum
    level (so it can trigger the callbacks when the associated Topic
    instances are destroyed), and use :delete_all at the Topic level (so
    that it can blow away all the comments as quickly as possible).
    
    As the Comment instances themselves have no dependent records blowing
    them away with :delete_all doesn't leave any cruft behind in the
    database.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,7 +2,7 @@ class Topic < ActiveRecord::Base
   belongs_to  :forum,     :counter_cache => true
   belongs_to  :user
   belongs_to  :last_commenter, :class_name => 'User', :foreign_key => 'last_commenter_id'
-  has_many    :comments,  :as => :commentable, :extend => Commentable, :order => 'comments.updated_at DESC', :dependent => :destroy
+  has_many    :comments,  :as => :commentable, :extend => Commentable, :order => 'comments.updated_at DESC', :dependent => :delete_all
   acts_as_taggable
 
   # TODO: attr_accessible here to prevent taking over posts
