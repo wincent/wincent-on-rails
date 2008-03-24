cd422ef9dfaa7ffad310055bc752abc4afdc9f5e
Wincent Colaiuta <win@wincent.com>
2008-03-24 18:04:14 +0100

    Fix logging broken by heartbeat controller
    Doing a "self.logger = nil" ending up silencing _all_ logging for the
    app. This alternative seems to be the only way to silence logging for
    only one controller.
    
    It's still not ideal (I'd like to see error conditions still logged,
    for instance) but it is the best compromise for now.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/controllers/heartbeat_controller.rb
+++ app/controllers/heartbeat_controller.rb
@@ -1,7 +1,9 @@
 class HeartbeatController < ApplicationController
   # we don't want Monit filling up the logs with noise
   # (4 requests, 1 for each mongrel, every 30 seconds)
-  self.logger = nil
+  def logger
+    nil
+  end
 
   # lightweight request that tests the full stack
   # (routing, controller, database/model, views, Haml)
