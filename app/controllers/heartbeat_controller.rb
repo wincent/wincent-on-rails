db9dd0b4f8051e20034814453cff521060440eae
Wincent Colaiuta <win@wincent.com>
2008-03-27 00:27:41 +0100

    Add note to heartbeat controller logger method
    Make a note about this message having to be public even though it turns
    out that it is not an accessible action.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/controllers/heartbeat_controller.rb
+++ app/controllers/heartbeat_controller.rb
@@ -1,6 +1,7 @@
 class HeartbeatController < ApplicationController
   # we don't want Monit filling up the logs with noise
   # (4 requests, 1 for each mongrel, every 30 seconds)
+  # not sure why, but this method cannot be protected or private
   def logger
     nil
   end
