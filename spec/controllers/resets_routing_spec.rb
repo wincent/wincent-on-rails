5e5822f617994f132e3a6b7edc95f67fa2ff266d
Wincent Colaiuta <win@wincent.com>
2008-04-01 15:10:19 +0200

    Resets controller routing specs
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/controllers/resets_routing_spec.rb
+++ spec/controllers/resets_routing_spec.rb
@@ -1,61 +1,59 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe ResetsController do
-  describe "route generation" do
-
+  describe 'route generation' do
     it "should map { :controller => 'resets', :action => 'index' } to /resets" do
-      route_for(:controller => "resets", :action => "index").should == "/resets"
+      route_for(:controller => 'resets', :action => 'index').should == '/resets'
     end
-  
+
     it "should map { :controller => 'resets', :action => 'new' } to /resets/new" do
-      route_for(:controller => "resets", :action => "new").should == "/resets/new"
+      route_for(:controller => 'resets', :action => 'new').should == '/resets/new'
     end
-  
-    it "should map { :controller => 'resets', :action => 'show', :id => 1 } to /resets/1" do
-      route_for(:controller => "resets", :action => "show", :id => 1).should == "/resets/1"
+
+    it "should map { :controller => 'resets', :action => 'show', :id => 'foo' } to /resets/foo" do
+      route_for(:controller => 'resets', :action => 'show', :id => 'foo').should == '/resets/foo'
     end
-  
-    it "should map { :controller => 'resets', :action => 'edit', :id => 1 } to /resets/1/edit" do
-      route_for(:controller => "resets", :action => "edit", :id => 1).should == "/resets/1/edit"
+
+    it "should map { :controller => 'resets', :action => 'edit', :id => 'foo' } to /resets/foo/edit" do
+      route_for(:controller => 'resets', :action => 'edit', :id => 'foo').should == '/resets/foo/edit'
     end
-  
-    it "should map { :controller => 'resets', :action => 'update', :id => 1} to /resets/1" do
-      route_for(:controller => "resets", :action => "update", :id => 1).should == "/resets/1"
+
+    it "should map { :controller => 'resets', :action => 'update', :id => 'foo'} to /resets/foo" do
+      route_for(:controller => 'resets', :action => 'update', :id => 'foo').should == '/resets/foo'
     end
-  
-    it "should map { :controller => 'resets', :action => 'destroy', :id => 1} to /resets/1" do
-      route_for(:controller => "resets", :action => "destroy", :id => 1).should == "/resets/1"
+
+    it "should map { :controller => 'resets', :action => 'destroy', :id => 'foo'} to /resets/foo" do
+      route_for(:controller => 'resets', :action => 'destroy', :id => 'foo').should == '/resets/foo'
     end
   end
 
-  describe "route recognition" do
-
+  describe 'route recognition' do
     it "should generate params { :controller => 'resets', action => 'index' } from GET /resets" do
-      params_from(:get, "/resets").should == {:controller => "resets", :action => "index"}
+      params_from(:get, '/resets').should == {:controller => 'resets', :action => 'index'}
     end
-  
+
     it "should generate params { :controller => 'resets', action => 'new' } from GET /resets/new" do
-      params_from(:get, "/resets/new").should == {:controller => "resets", :action => "new"}
+      params_from(:get, '/resets/new').should == {:controller => 'resets', :action => 'new'}
     end
-  
+
     it "should generate params { :controller => 'resets', action => 'create' } from POST /resets" do
-      params_from(:post, "/resets").should == {:controller => "resets", :action => "create"}
+      params_from(:post, '/resets').should == {:controller => 'resets', :action => 'create'}
     end
-  
-    it "should generate params { :controller => 'resets', action => 'show', id => '1' } from GET /resets/1" do
-      params_from(:get, "/resets/1").should == {:controller => "resets", :action => "show", :id => "1"}
+
+    it "should generate params { :controller => 'resets', action => 'show', id => 'foo' } from GET /resets/foo" do
+      params_from(:get, '/resets/foo').should == {:controller => 'resets', :action => 'show', :id => 'foo'}
     end
-  
-    it "should generate params { :controller => 'resets', action => 'edit', id => '1' } from GET /resets/1;edit" do
-      params_from(:get, "/resets/1/edit").should == {:controller => "resets", :action => "edit", :id => "1"}
+
+    it "should generate params { :controller => 'resets', action => 'edit', id => 'foo' } from GET /resets/foo;edit" do
+      params_from(:get, '/resets/foo/edit').should == {:controller => 'resets', :action => 'edit', :id => 'foo'}
     end
-  
-    it "should generate params { :controller => 'resets', action => 'update', id => '1' } from PUT /resets/1" do
-      params_from(:put, "/resets/1").should == {:controller => "resets", :action => "update", :id => "1"}
+
+    it "should generate params { :controller => 'resets', action => 'update', id => 'foo' } from PUT /resets/foo" do
+      params_from(:put, '/resets/foo').should == {:controller => 'resets', :action => 'update', :id => 'foo'}
     end
-  
-    it "should generate params { :controller => 'resets', action => 'destroy', id => '1' } from DELETE /resets/1" do
-      params_from(:delete, "/resets/1").should == {:controller => "resets", :action => "destroy", :id => "1"}
+
+    it "should generate params { :controller => 'resets', action => 'destroy', id => 'foo' } from DELETE /resets/foo" do
+      params_from(:delete, '/resets/foo').should == {:controller => 'resets', :action => 'destroy', :id => 'foo'}
     end
   end
 end
