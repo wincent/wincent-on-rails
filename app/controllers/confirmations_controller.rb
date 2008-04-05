08a02eb8a488ec1f49b2b3fcea4ea11104125c63
Wincent Colaiuta <win@wincent.com>
2008-04-05 03:00:12 +0200

    Interface for confirming email addresses
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,4 +1,36 @@
 class ConfirmationsController < ApplicationController
+  before_filter :require_user, :only => [ :new, :create ]
+
+  def new
+    @confirmation = Confirmation.new
+  end
+
+  def create
+    emails  = current_user.emails.find_all_by_verified :false
+    errors  = []
+    notices = []
+    emails.each do |e|
+      if params[:emails][e.id.to_s] == '1'
+        error_msg     = "An error occurred while sending the confirmation email to #{e.address}"
+        confirmation  = e.confirmations.create
+        begin
+          ConfirmationMailer.deliver_confirmation_message confirmation, request
+        rescue Net::SMTPFatalError
+          errors << "#{error_msg} (this looks like a permanent delivery problem; please check the address)"
+        rescue Net::SMTPServerBusy, Net::SMTPUnknownError, Net::SMTPSyntaxError, TimeoutError
+          errors << "#{error_msg} (this looks like a temporary delivery problem; you may want to try again later)"
+        rescue Exception
+          errors << "#{error_msg} (the cause of the error was unknown)"
+        else
+          notices << "A confirmation email has been sent to #{e.address}"
+        end
+      end
+    end
+    flash[:error] = errors.join('; ') if !errors.empty?
+    flash[:notice]  = notices.join('; ') if !notices.empty?
+    redirect_to user_path(current_user)
+  end
+
   def show
     @confirmation = Confirmation.find_by_secret params[:id], :include => :email
     if @confirmation.nil?
@@ -9,7 +41,7 @@ class ConfirmationsController < ApplicationController
       redirect_to login_path
     elsif @confirmation.cutoff < Time.now
       flash[:error] = 'Confirmation expiry date has already passed.'
-      redirect_to root_path
+      redirect_to root_path # redirect here to conf#new
     else
       @confirmation.update_attribute :completed_at, Time.now
       @confirmation.email.update_attribute :verified, true
