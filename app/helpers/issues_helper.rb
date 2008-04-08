148486e74f036529c42e8b8615fd32aba1731f99
Wincent Colaiuta <win@wincent.com>
2008-04-08 19:56:15 +0200

    Show scope limitations in issues index
    Provide some visual feedback to the user whenever scope limitations are
    in effect.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -1,2 +1,15 @@
 module IssuesHelper
+  def scope_info
+    scopes = []
+    if params[:product]
+      scopes << "product: #{h(params[:product])}"
+    end
+    if params[:kind]
+      scopes << "kind: #{h(params[:kind].pluralize)}"
+    end
+    if params[:status]
+      scopes << "status: #{h(params[:status])}"
+    end
+    "Currently showing only #{scopes.join(', ')}" unless scopes.empty?
+  end
 end
