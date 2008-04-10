4397337e7424494bc1bceaca745df1442c2d4d2c
Wincent Colaiuta <win@wincent.com>
2008-04-10 21:51:26 +0200

    Minor clean-up in issues helper
    Shave off a few lines by using the postfix "if" form.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -1,15 +1,9 @@
 module IssuesHelper
   def scope_info
     scopes = []
-    if params[:product]
-      scopes << "product: #{h(params[:product])}"
-    end
-    if params[:kind]
-      scopes << "kind: #{h(params[:kind].pluralize)}"
-    end
-    if params[:status]
-      scopes << "status: #{h(params[:status])}"
-    end
+    scopes << "product: #{h(params[:product])}" if params[:product]
+    scopes << "kind: #{h(params[:kind].pluralize)}" if params[:kind]
+    scopes << "status: #{h(params[:status])}" if params[:status]
     "Currently showing only issues with #{scopes.join(', ')}" unless scopes.empty?
   end
 
