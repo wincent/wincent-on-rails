0dbb2efe9db801208e5ef81831f0046cacd6249c
Wincent Colaiuta <win@wincent.com>
2008-04-10 21:38:59 +0200

    Extract AJAX "select" invocation into helper method
    The "select" invocation is pretty ugly in the view because it is so long
    and takes so many parameters (mostly because of the nested
    "remote_function" call which itself takes a lot of parameters).
    
    Extract it out into a helper method. I'll be able to use the exact same
    method for the other AJAX select controls on this page.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -12,4 +12,15 @@ module IssuesHelper
     end
     "Currently showing only issues with #{scopes.join(', ')}" unless scopes.empty?
   end
+
+  def ajax_select form, attribute, options
+    js = remote_function \
+      :update   => attribute.to_sym,
+      :url      => { :action => "update_#{attribute.to_s}".to_sym, :id => @issue.id },
+      :with     => "'#{attribute.to_s}=' + $('issue_#{attribute.to_s}').value",
+      :before   => "Element.show('#{attribute.to_s}_spinner')",
+      :complete => "Element.hide('#{attribute.to_s}_spinner')",
+      :failure  => "alert('HTTP Error ' + request.status)"
+    form.select attribute, options, {}, :onchange => js
+  end
 end
