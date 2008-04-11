1bf09c67420f1810a407d4538cc225aea3912742
Wincent Colaiuta <win@wincent.com>
2008-04-11 21:31:27 +0200

    Open up the support and issues controllers again
    Now that we have private bugs correctly identified on import we can
    relax the access controls on the support and issues controllers.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,8 +1,5 @@
 class IssuesController < ApplicationController
-  # TODO: before deployment uncomment this next line
-  # some issues are sensitive and I want an opportunity to mark them as private before going live
-  before_filter     :require_admin
-  #before_filter     :require_admin, :except => [:create, :index, :new, :show]
+  before_filter     :require_admin, :except => [:create, :index, :new, :show]
   before_filter     :find_product, :only => [:index]
   before_filter     :find_issue, :except => [:create, :index, :new]
   before_filter     :find_prev_next, :only => [:show]
