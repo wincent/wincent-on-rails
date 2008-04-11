7e10b279aa4743036084c83efe396cc54863fb12
Wincent Colaiuta <win@wincent.com>
2008-04-11 11:20:11 +0200

    Fix spelling error in code comment
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -28,7 +28,7 @@ class Topic < ActiveRecord::Base
   end
 
   def visible_comments
-    # can't use the Commentable association mixin methods here becuse we need to specify an :include clause
+    # can't use the Commentable association mixin methods here because we need to specify an :include clause
     conditions = { :public => true, :awaiting_moderation => false, :spam => false, :commentable_id => self.id,
       :commentable_type => 'Topic' }
     Comment.find :all, :conditions => conditions, :include => 'user', :order => 'comments.created_at'
