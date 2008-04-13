5a3e8ca7b74101c48f5bc4eb115900e8b11a2b68
Wincent Colaiuta <win@wincent.com>
2008-04-13 13:26:19 +0200

    Sync support controller index action with issues controller
    Make the support controller have the same access control behaviour as
    the issues controller. It might be nice to try and remove this
    duplication but there are only two or three repeated lines so at this
    stage the alarm bells aren't yet ringing.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/controllers/support_controller.rb
+++ app/controllers/support_controller.rb
@@ -2,7 +2,8 @@ class SupportController < ApplicationController
   acts_as_sortable  :by => [:kind, :id, :product_id, :summary, :updated_at], :default => :updated_at, :descending => true
 
   def index
-    @paginator    = Paginator.new(params, Issue.count(:conditions => { :public => true }), issues_path)
-    @issues       = Issue.find :all, sort_options.merge({:offset => @paginator.offset, :limit => @paginator.limit})
+    options       = default_access_options # defined in ApplicationController
+    @paginator    = Paginator.new params, Issue.count(:conditions => options), issues_path
+    @issues       = Issue.find :all, sort_options.merge({:offset => @paginator.offset, :limit => @paginator.limit, :conditions => options})
   end
 end
