10b239bf6081c64a999fa475f8c52a1426063cd1
Wincent Colaiuta <win@wincent.com>
2008-04-15 00:13:26 +0200

    Fix view spec failures for products#index
    I must admit to seriously questioning the value of mock-heavy view specs
    like this. They are brittle, they break whenever you evolve the code
    (not merely retrospective changes, but forward-looking additions as
    well), and they represent a whole lot of "busy work" which I am not sure
    is justified.
    
    So I've fixed the breakage but I think I need a new way of approaching
    these view tests in the future.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/views/products/index.html.haml_spec.rb
+++ spec/views/products/index.html.haml_spec.rb
@@ -6,8 +6,12 @@ describe '/products/index.html.haml' do
   before do
     product_1 = mock_model(Product)
     product_1.should_receive(:name).and_return('Synergy')
+    product_1.should_receive(:icon_path).at_least(1).and_return(nil)
+    product_1.should_receive(:description).at_least(1).and_return('An iTunes controller')
     product_2 = mock_model(Product)
     product_2.should_receive(:name).and_return('Synergy Advance')
+    product_2.should_receive(:icon_path).at_least(1).and_return('/system/products/icons/synergy-advance.png')
+    product_2.should_receive(:description).at_least(1).and_return('An improved iTunes accessory')
     assigns[:products] = [product_1, product_2]
     render '/products/index.html.haml'
   end
