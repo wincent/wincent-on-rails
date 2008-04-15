36ec1e0fc3533b42936a068d3bd8e26c081d652f
Wincent Colaiuta <win@wincent.com>
2008-04-16 00:49:54 +0200

    Add specs for dashboard#show view
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ spec/views/dashboard/show.html.haml_spec.rb
@@ -0,0 +1,37 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe '/dashboard/show viewed as admin user' do
+  include DashboardHelper
+
+  before do
+    template.stub!(:current_user).and_return(new_user)
+    template.should_receive(:admin?).and_return(true)
+    assigns[:issues] = []
+    assigns[:topics] = []
+    assigns[:comments] = []
+    render '/dashboard/show'
+  end
+
+  it 'should provide a link to the admin dashboard' do
+    response.should have_tag('.links') do
+      with_tag 'a[href=?]', admin_dashboard_path
+    end
+  end
+end
+
+describe '/dashboard/show viewed as normal user' do
+  include DashboardHelper
+
+  before do
+    template.stub!(:current_user).and_return(new_user)
+    template.should_receive(:admin?).and_return(false)
+    assigns[:issues] = []
+    assigns[:topics] = []
+    assigns[:comments] = []
+    render '/dashboard/show'
+  end
+
+  it 'should not provide a link to the admin dashboard' do
+    response.should_not have_tag('a[href=?]', admin_dashboard_path)
+  end
+end
