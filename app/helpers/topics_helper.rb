9bad29ebc63dc7cb52e8008b3339e89f0666f300
Wincent Colaiuta <win@wincent.com>
2008-04-18 18:37:43 +0200

    Extract button code into topics helper and use it
    These one-button forms are fairly ugly in the views, so extract them
    into the topics helper. The other benefit here is that the same helpers
    can then be used to put the buttons on both the index and edit pages.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,5 +1,33 @@
 module TopicsHelper
-  def timeinfo_for_topic topic
-    topic.created_at.distance_in_words
+  def button_to_destroy_topic topic
+    haml_tag :form, { :id => "topic_#{topic.id}_destroy_form", :style => 'display:inline;' } do
+      button = submit_to_remote 'button', 'destroy',
+        :url      => forum_topic_path(topic.forum, topic),
+        :method   => :delete,
+        :failure  => "alert('Failed to delete')",
+        :confirm  => 'Really delete?'
+      concat button
+    end
+  end
+
+  def button_to_moderate_topic_as_spam topic
+    haml_tag :form, { :id => "topic_#{topic.id}_spam_form", :style => 'display:inline;' } do
+      button = submit_to_remote 'button', 'spam',
+        :url => forum_topic_path(topic.forum, topic),
+        :method => :put,
+        :failure => "alert('Failed to mark as spam')",
+        :confirm => 'Really mark as spam?'
+      concat button
+    end
+  end
+
+  def button_to_moderate_topic_as_ham topic
+    haml_tag :form, { :id => "topic_#{topic.id}_ham_form", :style => 'display:inline;' } do
+      button = submit_to_remote 'button', 'ham',
+        :url => forum_topic_path(topic.forum, topic),
+        :method => :put,
+        :failure => "alert('Failed to mark as ham')"
+      concat button
+    end
   end
 end # module TopicsHelper
