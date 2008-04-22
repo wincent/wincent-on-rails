f4dcd86be6026dbcba785701e2754fdb192e764c
Wincent Colaiuta <win@wincent.com>
2008-04-22 18:24:48 +0200

    Add links to Comments#edit template
    These are various links for administrator convenience: destroy, mark
    as spam, mark as ham, return to list of comments awaiting moderation
    and return to admin dashboard.
    
    These links are especially useful in the light of the "new comment
    alert" emails that get sent to the administrator: the alerts contain a
    link back to the Comments#edit URL and so it makes sense to provide
    buttons on that page for moderating them.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ spec/views/comments/edit.html.haml_spec.rb
@@ -0,0 +1,47 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe '/comments/edit' do
+  include CommentsHelper
+
+  before do
+    assigns[:comment] = @comment = create_comment
+  end
+
+  def do_render
+    render '/comments/edit'
+  end
+
+  it 'should have a div for the comment' do
+    do_render
+    response.should have_tag("\#comment_#{@comment.id}")
+  end
+
+  it 'should have a destroy button' do
+    template.should_receive(:button_to_destroy_comment).with(@comment)
+    do_render
+  end
+
+  it 'should have a spam button' do
+    template.should_receive(:button_to_moderate_comment_as_spam).with(@comment)
+    do_render
+  end
+
+  it 'should have a ham button' do
+    template.should_receive(:button_to_moderate_comment_as_ham).with(@comment)
+    do_render
+  end
+
+  it 'should have a link back to the list of comments awaiting moderation' do
+    do_render
+    response.should have_tag('.links') do
+      with_tag 'a[href=?]', comments_path
+    end
+  end
+
+  it 'should have a link back to the administrator dashboard' do
+    do_render
+    response.should have_tag('.links') do
+      with_tag 'a[href=?]', admin_dashboard_path
+    end
+  end
+end
