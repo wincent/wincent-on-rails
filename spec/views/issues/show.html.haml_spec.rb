64bad8d37cc2d87548341db08e0444e50ad5c0b3
Wincent Colaiuta <win@wincent.com>
2008-04-23 19:02:25 +0200

    Add "edit" links to Issues#show template
    These are visible only to the admin user.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/views/issues/show.html.haml_spec.rb
+++ spec/views/issues/show.html.haml_spec.rb
@@ -17,3 +17,43 @@ describe '/issues/show' do
     end
   end
 end
+
+describe '/issues/show viewed by an administrator' do
+  include IssuesHelper
+
+  before do
+    assigns[:issue]     = @issue = create_issue
+    assigns[:comments]  = []
+    assigns[:comment]   = Comment.new
+    template.should_receive(:admin?).at_least(:once).and_return(true)
+    render '/issues/show'
+  end
+
+  it 'should show an edit link' do
+    response.should have_tag('a[href=?]', edit_issue_path(@issue))
+  end
+
+  it 'should show a destroy link' do
+    response.should have_text(/destroy/) # not sure how best to test this, so this is a cheap stand-in for now
+  end
+end
+
+describe '/issues/show viewed by a normal user' do
+  include IssuesHelper
+
+  before do
+    assigns[:issue]     = @issue = create_issue
+    assigns[:comments]  = []
+    assigns[:comment]   = Comment.new
+    template.should_receive(:admin?).at_least(:once).and_return(false)
+    render '/issues/show'
+  end
+
+  it 'should not show an edit link' do
+    response.should_not have_tag('a[href=?]', edit_issue_path(@issue))
+  end
+
+  it 'should not show a destroy link' do
+    response.should_not have_text(/destroy/) # not sure how best to test this, so this is a cheap stand-in for now
+  end
+end
