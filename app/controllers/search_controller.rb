eef76cafdfc4300b5bbcaac2e4336bd9b985b6f5
Wincent Colaiuta <win@wincent.com>
2008-04-25 19:33:07 +0200

    Add user interface for showing full-text search results
    Now we have a working search form, results page, and the ability to skip
    through multiple pages of search results.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,6 @@
 class SearchController < ApplicationController
+  def create
+    @offset = params[:offset].to_i
+    @models = Needle.find_using_query_string((params[:query] || ''), :offset => @offset)
+  end
 end
