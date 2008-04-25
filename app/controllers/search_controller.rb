c6d99cfb4a388636696fcb970f74866b6f204741
Wincent Colaiuta <win@wincent.com>
2008-04-25 19:50:52 +0200

    Add specs for search controller
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,7 @@
 class SearchController < ApplicationController
+  # This is somewhat of an abuse of RESTful resource routing:
+  # both the "index" and "new" actions render the search form
+  # and the "create" action is used to display the results.
   def create
     @offset = params[:offset].to_i
     @models = Needle.find_using_query_string((params[:query] || ''), :offset => @offset)
---
+++ spec/controllers/search_controller_spec.rb
@@ -0,0 +1,40 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+require File.dirname(__FILE__) + '/application_spec'
+
+describe SearchController do
+  it_should_behave_like 'ApplicationController'
+end
+
+describe SearchController, 'create action' do
+  it 'should assign the offset for use by the view' do
+    post :create, :offset => '5'
+    assigns[:offset].should == 5
+  end
+
+  it 'should find using the query string' do
+    Needle.should_receive(:find_using_query_string).with('foo', :offset => 0)
+    post :create, :query => 'foo'
+  end
+
+  it 'should should pass a nil query parameter as an empty string' do
+    # this is low cost and prevents an exception being thrown in the face of bad input (possible attack)
+    Needle.should_receive(:find_using_query_string).with('', :offset => 0)
+    post :create
+  end
+
+  it 'should use the offset parameter when performing the query' do
+    Needle.should_receive(:find_using_query_string).with('foo', :offset => 5)
+    post :create, :query => 'foo', :offset => '5'
+  end
+
+  it 'should treat a missing offset value as 0' do
+    Needle.should_receive(:find_using_query_string).with('foo', :offset => 0)
+    post :create, :query => 'foo', :offset => nil
+  end
+
+  it 'should assign the found models for use by the view' do
+    Needle.stub!(:find_using_query_string).and_return(:search_results)
+    post :create
+    assigns[:models].should == :search_results
+  end
+end
