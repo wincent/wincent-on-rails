c02517640927995d1099ac1470c792f379a4ae62
Wincent Colaiuta <win@wincent.com>
2008-04-25 12:45:24 +0200

    Add specs for "search/form" partial
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ spec/views/search/_form.html.haml_spec.rb
@@ -0,0 +1,36 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe '/search/_form' do
+  def do_render
+    render '/search/_form'
+  end
+
+  it 'should use the "search" style sheet' do
+    template.should_receive(:stylesheet_link_tag).with('search')
+    do_render
+  end
+
+  it 'should have an "issue tracker search" link' do
+    do_render
+    response.should have_tag('div.links') do
+      with_tag 'a[href=?]', issues_search_path
+    end
+  end
+
+  it 'should have an "tag search" link' do
+    do_render
+    response.should have_tag('div.links') do
+      with_tag 'a[href=?]', search_tags_path
+    end
+  end
+
+  it 'should show the search form' do
+    do_render
+    response.should have_tag('form[action=?]', '/search') do
+      with_tag 'input[name=?]', 'query'
+      with_tag 'input[type=?]', 'text'
+      with_tag 'input[value=?]', 'Search'
+      with_tag 'input[type=?]', 'submit'
+    end
+  end
+end
