f90f51d5ecc477fba1a6011734f6c138a3538393
Wincent Colaiuta <win@wincent.com>
2008-04-26 16:38:54 +0200

    Extract "classifiable" specs from comments specs into separate file
    These will subsequently be expanded and used for other models as well.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ spec/lib/shared_classifiable_spec.rb
@@ -0,0 +1,64 @@
+require File.join(File.dirname(__FILE__), '..', 'spec_helper')
+require 'classifiable.rb'
+
+describe Classifiable, '"moderate_as_spam!" method', :shared => true do
+  it 'should turn off the "awaiting moderation" flag' do
+    lambda { @object.moderate_as_spam! }.should change(@object, :awaiting_moderation).from(true).to(false)
+  end
+
+  it 'should change the "awaiting moderation" flag in the database' do
+    @object.moderate_as_spam!
+    @object.class.find(@object.id).awaiting_moderation.should == @object.awaiting_moderation
+  end
+
+  it 'should turn on the "spam" flag' do
+    lambda { @object.moderate_as_spam! }.should change(@object, :spam).from(false).to(true)
+  end
+
+  it 'should change the "spam" flag in the database' do
+    @object.moderate_as_spam!
+    @object.class.find(@object.id).spam.should == @object.spam
+  end
+
+  it 'should not alter the comment "updated at" timestamp' do
+    lambda { @object.moderate_as_spam! }.should_not change(@object, :updated_at)
+  end
+
+  it 'should not change the "updated at" timestamp in the database' do
+    # as seems to be usual with ActiveRecord round-tripping, we lose precision and so must do a "to_s"
+    @object.moderate_as_spam!
+    @object.class.find(@object.id).updated_at.to_s.should == @object.updated_at.to_s
+  end
+end
+
+describe Classifiable, '"moderate_as_ham!" method', :shared => true do
+  it 'should turn off the "awaiting moderation" flag' do
+    lambda { @object.moderate_as_ham! }.should change(@object, :awaiting_moderation).from(true).to(false)
+  end
+
+  it 'should change the "awaiting moderation" flag in the database' do
+    @object.moderate_as_ham!
+    @object.class.find(@object.id).awaiting_moderation.should == @object.awaiting_moderation
+  end
+
+  it 'should turn off the "spam" flag' do
+    # the spam flag starts as "off" by default anyway
+    @object.moderate_as_ham!
+    @object.spam.should == false
+  end
+
+  it 'should change the "spam" flag in the database' do
+    @object.moderate_as_ham!
+    @object.class.find(@object.id).spam.should == @object.spam
+  end
+
+  it 'should not alter the comment "updated at" timestamp' do
+    lambda { @object.moderate_as_ham! }.should_not change(@object, :updated_at)
+  end
+
+  it 'should not change the "updated at" timestamp in the database' do
+    # as seems to be usual with ActiveRecord round-tripping, we lose precision and so must do a "to_s"
+    @object.moderate_as_ham!
+    @object.class.find(@object.id).updated_at.to_s.should == @object.updated_at.to_s
+  end
+end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,4 +1,5 @@
 require File.dirname(__FILE__) + '/../spec_helper'
+require File.join(File.dirname(__FILE__), '..', 'lib', 'shared_classifiable_spec')
 
 describe Comment do
   it 'should be valid' do
@@ -6,74 +7,20 @@ describe Comment do
   end
 end
 
-describe Comment, '"moderate_as_spam!" method' do
+describe Comment, 'acting as classifiable ("moderate_as_spam!" method)' do
   before do
-    @comment = create_comment :awaiting_moderation => true
+    @object = create_comment :awaiting_moderation => true
   end
 
-  it 'should turn off the "awaiting moderation" flag' do
-    lambda { @comment.moderate_as_spam! }.should change(@comment, :awaiting_moderation).from(true).to(false)
-  end
-
-  it 'should change the "awaiting moderation" flag in the database' do
-    @comment.moderate_as_spam!
-    Comment.find(@comment.id).awaiting_moderation.should == @comment.awaiting_moderation
-  end
-
-  it 'should turn on the "spam" flag' do
-    lambda { @comment.moderate_as_spam! }.should change(@comment, :spam).from(false).to(true)
-  end
-
-  it 'should change the "spam" flag in the database' do
-    @comment.moderate_as_spam!
-    Comment.find(@comment.id).spam.should == @comment.spam
-  end
-
-  it 'should not alter the comment "updated at" timestamp' do
-    lambda { @comment.moderate_as_spam! }.should_not change(@comment, :updated_at)
-  end
-
-  it 'should not change the "updated at" timestamp in the database' do
-    # as seems to be usual with ActiveRecord round-tripping, we lose precision and so must do a "to_s"
-    @comment.moderate_as_spam!
-    Comment.find(@comment.id).updated_at.to_s.should == @comment.updated_at.to_s
-  end
+  it_should_behave_like 'Classifiable "moderate_as_spam!" method'
 end
 
-describe Comment, '"moderate_as_ham!" method' do
+describe Comment, 'acting as classifiable ("moderate_as_ham!" method)' do
   before do
-    @comment = create_comment :awaiting_moderation => true
+    @object = create_comment :awaiting_moderation => true
   end
 
-  it 'should turn off the "awaiting moderation" flag' do
-    lambda { @comment.moderate_as_ham! }.should change(@comment, :awaiting_moderation).from(true).to(false)
-  end
-
-  it 'should change the "awaiting moderation" flag in the database' do
-    @comment.moderate_as_ham!
-    Comment.find(@comment.id).awaiting_moderation.should == @comment.awaiting_moderation
-  end
-
-  it 'should turn off the "spam" flag' do
-    # the spam flag starts as "off" by default anyway
-    @comment.moderate_as_ham!
-    @comment.spam.should == false
-  end
-
-  it 'should change the "spam" flag in the database' do
-    @comment.moderate_as_ham!
-    Comment.find(@comment.id).spam.should == @comment.spam
-  end
-
-  it 'should not alter the comment "updated at" timestamp' do
-    lambda { @comment.moderate_as_ham! }.should_not change(@comment, :updated_at)
-  end
-
-  it 'should not change the "updated at" timestamp in the database' do
-    # as seems to be usual with ActiveRecord round-tripping, we lose precision and so must do a "to_s"
-    @comment.moderate_as_ham!
-    Comment.find(@comment.id).updated_at.to_s.should == @comment.updated_at.to_s
-  end
+  it_should_behave_like 'Classifiable "moderate_as_ham!" method'
 end
 
 describe Comment, '"send_new_comment_alert" method' do
