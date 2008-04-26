74c641ddd448a09098e1b73d30ebc733ec871f42
Wincent Colaiuta <win@wincent.com>
2008-04-26 16:02:18 +0200

    Make full-text indexing spam and moderation-savvy
    Don't index models that are spam or awaiting moderation, and if changing
    the spam/moderation status then update the index accordingly.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- lib/active_record/acts/searchable.rb
+++ lib/active_record/acts/searchable.rb
@@ -59,6 +59,8 @@ module ActiveRecord
         #
         #     I tested this with a 10,000 word article and it took less than a second in development mode.
         def create_needles
+          return if self.respond_to?(:awaiting_moderation?) && self.awaiting_moderation?
+          return if self.respond_to?(:spam?) && self.spam?
           model_class     = self.class.to_s
           model_id        = self.id
           model_public    = self.public ? 'TRUE' : 'FALSE'
--- lib/classifiable.rb
+++ lib/classifiable.rb
@@ -10,10 +10,15 @@ module Classifiable
 protected
 
   # BUG: this kind of modification won't trigger any cache sweepers, which means that feeds might get out of date
+  # may need to manually trigger sweepers
   def moderate! is_spam
     # we don't want moderating a model to mark it as updated, so use update_all
     self.awaiting_moderation  = false
     self.spam                 = is_spam
     self.class.update_all ['awaiting_moderation = FALSE, spam = ?', is_spam], ['id = ?', self.id]
+
+    # I don't really like intertwining the classifiable and searchable functionality,
+    # but seems to be a necessary evil for now
+    update_needles if private_methods.include?('update_needles')
   end
 end # module Classifiable
