1a30f2fe9e956e002636242895b9928db05f25a1
Wincent Colaiuta <win@wincent.com>
2008-04-27 16:15:10 +0200

    Switch navbar search field to full-text search
    Drop tag search in favor of full-text search.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ spec/views/layouts/application.haml_spec.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe '/layouts/application' do
+  it 'should have a search form' do
+    render '/layouts/application'
+    response.should have_tag('form[action=?]', search_index_path) do
+      with_tag 'input[name=query][type=text]'
+    end
+  end
+end
