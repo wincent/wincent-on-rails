93f88cb2a8217065555ea457075bc2152adc4bd0
Wincent Colaiuta <win@wincent.com>
2008-09-11 22:02:49 +0200

    Include comment "in context" link in notification emails
    For comments not awaiting moderation, include a "Show comment in
    context" link as a convenience in outgoing notification emails.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/models/comment_mailer.rb
+++ app/models/comment_mailer.rb
@@ -4,9 +4,10 @@ class CommentMailer < ActionMailer::Base
     url_options[:port] = APP_CONFIG['port'] if APP_CONFIG['port'] != 80
     subject     "new comment alert from #{APP_CONFIG['host']}"
     body({
-      :comment        => comment,
-      :comment_url    => edit_comment_url(comment, url_options),
-      :moderation_url => admin_dashboard_url(url_options)
+      :comment          => comment,
+      :comment_url      => comment_url(comment, url_options),
+      :edit_comment_url => edit_comment_url(comment, url_options),
+      :moderation_url   => admin_dashboard_url(url_options)
       })
     recipients  APP_CONFIG['admin_email']
     from        APP_CONFIG['admin_email']
