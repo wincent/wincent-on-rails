3f39f3a7580d44e78a7bc141e67f3cceaad6e791
Wincent Colaiuta <win@wincent.com>
2008-11-05 18:04:51 +0100

    Restore application.js accidentally deleted in Rails 2.1.2 upgrade
    Not the first time I've made this mistake, is it?
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,20 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+function getCookie(name) {
+  var cookies = document.cookie.split('; ');
+  for (var i = 0; i < cookies.length; i++) {
+    var c = cookies[i];
+    var pos = c.indexOf('=');
+    var n = c.substring(0, pos);
+    if (n == name)
+      return c.substring(pos + 1);
+  }
+  return null;
+}
+
+function setUpLoginLogoutLinks()
+{
+  var user = getCookie('user_id');
+  if (user == null || user == '')
+    $('logout').hide();
+  else
+    $('login').hide();
+}
