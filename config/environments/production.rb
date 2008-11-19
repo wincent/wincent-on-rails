8a5a83ce288a468de7cc48279aa46b25c79e699c
Wincent Colaiuta <win@wincent.com>
2008-11-19 12:28:56 +0100

    Fix Rails 2.2.0 breakage (cache_template_loading has been removed)
    This is not only a no-op now, it's actually been removed so will
    prevent the app from starting at all.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,6 @@ config.cache_classes = false # BUG: temporary kludge for Rails 2.2.0 breakage
 # Full error reports are disabled and caching is turned on
 config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
-config.action_view.cache_template_loading            = true
 
 # Use a different cache store in production
 # config.cache_store = :mem_cache_store
