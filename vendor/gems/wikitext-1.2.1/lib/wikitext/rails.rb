c032fb1a92e2d2c2074eac7dff764ce9a6388236
Wincent Colaiuta <win@wincent.com>
2009-01-04 06:21:32 +0100

    Fix recent Rails breakage of template engines
    Yet another undocumented API change. As far as I'm concerned, Rails 3
    and the promised API stability that will come from the merge with Merb
    can't come soon enough.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- vendor/gems/wikitext-1.2.1/lib/wikitext/rails.rb
+++ vendor/gems/wikitext-1.2.1/lib/wikitext/rails.rb
@@ -1,4 +1,4 @@
-# Copyright 2008 Wincent Colaiuta
+# Copyright 2008-2009 Wincent Colaiuta
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -16,33 +16,10 @@ require 'wikitext/string'
 
 module Wikitext
   class TemplateHandler
-    def initialize view; end
-
-    # fix Rails 2.2.0 breakage
-    # TODO: add specs
-    def self.call template; end
-
-    # fix Rails 2.1 breakage
-    # TODO: add specs to catch this kind of breakage in the future
-    def compilable?
-      false
-    end
-
-    def render template
-      template.source.w
-    end
-
-    # in a subsequent wikitext release will work out how to dynamically check for this at runtime
-    def pre_rails_2_1_render text, locals = {}
-      text.w
+    def self.call template
+      'template.source.w'
     end
   end
 end
 
-if ActionView::Template.respond_to? :register_template_handler  # Rails 2.1.0_RC1 and above
-  ActionView::Template.register_template_handler :wikitext, Wikitext::TemplateHandler
-elsif ActionView::Base.respond_to? :register_template_handler   # Rails 2.0.2
-  ActionView::Base.register_template_handler :wikitext, Wikitext::TemplateHandler
-else
-  raise "Incompatible Rails API version (can't find register_template_handler method)"
-end
+ActionView::Template.register_template_handler :wikitext, Wikitext::TemplateHandler
