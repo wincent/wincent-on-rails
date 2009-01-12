0a6ac1dae7c0b272431271b6eba43e2e4304345e
Wincent Colaiuta <win@wincent.com>
2009-01-12 20:50:50 +0100

    Revise limit in MEDIUMTEXT migrations
    Turns out that 16,777,215 doesn't work as we thought it did. If you use
    that value then you wind up with LONGTEXT columns, not MEDIUMTEXT ones.
    I am guessing that this is because the tables are set up to use UTF-8
    encoding, so MySQL bumps the size up to 16,777,215 (characters) times
    by 3 (maximum bytes per character) to arrive at a total size which
    pushes into the LONGTEXT size range.
    
    Problem caught because Rails doesn't properly handle LONGTEXT columns,
    which in turn caused a bunch of specs to break.
    
    Seeing as we haven't deployed this yet, just revise the migrations
    themselves rather than adding new ones on top.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- db/migrate/20090112174654_change_post_body_type.rb
+++ db/migrate/20090112174654_change_post_body_type.rb
@@ -1,6 +1,12 @@
 class ChangePostBodyType < ActiveRecord::Migration
   def self.up
-    change_column :posts, :body, :text, :limit => 16777215
+    # We don't use the expected value of 16777215 here or
+    # MySQL will actually create LONGTEXT columns instead
+    # of MEDIUMTEXT ones (it's probably multiplying the
+    # limit value by three because the tables use UTF-8
+    # encoding, and the byte size is therefore rolling over
+    # into the LONGTEXT size range).
+    change_column :posts, :body, :text, :limit => 262143
   end
 
   def self.down
