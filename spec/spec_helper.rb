wincent/wincent-on-rails

commit b39c68ce904df0d682947181e83b2536eecdca4f

Redirect to HTTPS URL if HTTP request gets through
Although the nginx front end automatically redirects "https://" URLs
to "http://" it is still possible that someone could guess the port
number of an upstream mongrel and try connecting directly to it without
using SSL.

Set up an application-level before filter that redirects the user to
the equivalent HTTPS URL if this happens.

Signed-off-by: Wincent Colaiuta <win@wincent.com>

app/controllers/application.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   #helper                    :all # include all helpers, all the time
   filter_parameter_logging  'passphrase'
-  before_filter             :login_before
+  before_filter             :ensure_correct_protocol, :login_before
   protect_from_forgery      :secret => APP_CONFIG['forgery_secret']
   rescue_from               ActiveRecord::RecordNotFound, :with => :record_not_found
 
@@ -51,6 +51,22 @@ protected
 
   def default_access_options
     'awaiting_moderation = FALSE AND ' + default_access_options_including_awaiting_moderation
+  end
+
+  # nginx will rewrite HTTP URLs to HTTPs automatically
+  # but still need to catch improper direct access to the mongrels
+  # (if somebody guesses their port numbers, they can connect via HTTP)
+  def ensure_correct_protocol
+    if not request.ssl?
+      url = 'https://' + request.host
+      url << ":#{APP_CONFIG['port']}" if APP_CONFIG['port'] != 443
+      url << request.request_uri
+      redirect_to url
+      flash.keep
+      false
+    else
+      true
+    end
   end
 
   # uncomment this method to test what remote users will see when there are errors in production mode

commit b2bde10454316e6d6976bb3cc404f09d72d1b0ce

Fix spec breakage caused by b39c68c
Another "nasty" change required in the move to SSL; many specs broke
because of the way Rails sets up fake request objects for testing.
These ActionController::TestRequest instances will basically always
return false for "ssl?" and thanks to the before filter introduced
in b39c68c, that means that we'll always redirect and this breaks
many specs.

As you can see the fix is "nasty" because it requires two ugly
changes:

- first, a monkey patch to the TestRequest class to make it always
  return true for "ssl?"; it's not possible to actually have it
  inspect itself to see whether it should return true because the
  instances are wildly inconsistent (sometimes ":protocol =>
  'https'" is set in the parameters instance variable, sometimes in
  @path_parameters, sometimes in @symbolized_path_parameters, and
  sometimes not at all).

- second, change our nested_comments_path method into
  nested_comments_url; this is a "nasty" change because it requires
  us to change the application for what is essentially a problem in
  the tests.

Signed-off-by: Wincent Colaiuta <win@wincent.com>

app/controllers/application.rb
@@ -12,14 +12,14 @@ protected
 
   # URL to the comment nested in the context of its parent (resources), including an anchor.
   # NOTE: this method is dog slow if called in an "N + 1 SELECT" situation
-  def nested_comment_path comment
+  def nested_comment_url comment
     commentable = comment.commentable
     anchor      = "comment_#{comment.id}"
     case commentable
     when Article, Issue, Post
-      send "#{commentable.class.to_s.downcase}_path", commentable, :anchor => anchor
+      send "#{commentable.class.to_s.downcase}_url", commentable, :anchor => anchor
     when Topic
-      forum_topic_path commentable.forum, commentable, :anchor => anchor
+      forum_topic_url commentable.forum, commentable, :anchor => anchor
     end
   end
 

app/controllers/comments_controller.rb
@@ -20,7 +20,7 @@ class CommentsController < ApplicationController
     else # anonymous user
       @comment = Comment.find params[:id], :conditions => { :public => true, :spam => false, :awaiting_moderation => false }
     end
-    redirect_to nested_comment_path(@comment)
+    redirect_to nested_comment_url(@comment)
   end
 
   def create
@@ -92,7 +92,7 @@ class CommentsController < ApplicationController
         @comment.public = params[:comment][:public] if params[:comment] && params[:comment].key?(:public)
         if @comment.update_attributes params[:comment]
           flash[:notice] = 'Successfully updated'
-          redirect_to (@comment.awaiting_moderation ? comments_path : nested_comment_path(@comment))
+          redirect_to (@comment.awaiting_moderation ? comments_path : nested_comment_url(@comment))
         else
           flash[:error] = 'Update failed'
           render :action => 'edit'

spec/controllers/comments_controller_spec.rb
@@ -46,7 +46,7 @@ describe CommentsController, 'GET /comments/:id/edit logged in as normal user' d
   it 'should deny access to the "edit" action' do
     login_as_normal_user
     get :edit, :id => @comment.id, :protocol => 'https'
-    response.should redirect_to(login_path)
+    response.should redirect_to(login_url)
     flash[:notice].should =~ /requires administrator privileges/
   end
 end
@@ -60,7 +60,7 @@ describe CommentsController, 'GET /comments/:id/edit as an anonymous visitor' do
   # but the effort is minimal, so it doesn't hurt to err on the safe side
   it 'should deny access to the "edit" action' do
     get :edit, :id => @comment.id, :protocol => 'https'
-    response.should redirect_to(login_path)
+    response.should redirect_to(login_url)
     flash[:notice].should =~ /requires administrator privileges/
   end
 end
@@ -102,7 +102,7 @@ describe CommentsController, 'PUT /comments/:id logged in as admin' do
     @comment.stub!(:save).and_return(true)
     Comment.stub!(:find).and_return(@comment)
     do_put
-    response.should redirect_to(controller.send(:nested_comment_path, @comment))
+    response.should redirect_to(controller.send(:nested_comment_url, @comment))
   end
 
   it 'should redirect to the list of comments awaiting moderation on success for comments that are awaiting moderation' do
@@ -110,7 +110,7 @@ describe CommentsController, 'PUT /comments/:id logged in as admin' do
     @comment.stub!(:save).and_return(true)
     Comment.stub!(:find).and_return(@comment)
     do_put
-    response.should redirect_to(comments_path)
+    response.should redirect_to(comments_url)
   end
 
   it 'should show an error on failure' do

spec/controllers/issues_controller_spec.rb
@@ -144,7 +144,7 @@ describe IssuesController, 'PUT /issues/:id (html format)' do
     @issue.stub!(:save).and_return(true)
     Issue.stub!(:find).and_return(@issue)
     do_put
-    response.should redirect_to(issue_path(@issue))
+    response.should redirect_to(issue_url(@issue))
   end
 
   it 'should redirect to the list of issues awaiting moderation on success for comments that are awaiting moderation' do
@@ -152,7 +152,7 @@ describe IssuesController, 'PUT /issues/:id (html format)' do
     @issue.stub!(:save).and_return(true)
     Issue.stub!(:find).and_return(@issue)
     do_put
-    response.should redirect_to(admin_issues_path)
+    response.should redirect_to(admin_issues_url)
   end
 
   it 'should show an error on failure' do
@@ -218,7 +218,7 @@ describe IssuesController, 'admin-only methods' do
   # TODO: write custom matchers or helper methods in spec helper for expressing this pattern (of redirects for non-admin users)
   it 'should deny access to the "set_issue_summary" method for non-admin users' do
     get :set_issue_summary, :protocol => 'https'
-    response.should redirect_to(login_path)
+    response.should redirect_to(login_url)
     flash[:notice].should =~ /requires administrator privileges/
   end
 end

spec/controllers/sessions_controller_spec.rb
@@ -25,7 +25,7 @@ describe SessionsController, 'logging in with a valid username and passphrase' d
   end
 
   it 'should redirect to the user dashboard' do
-    response.should redirect_to(dashboard_path)
+    response.should redirect_to(dashboard_url)
   end
 end
 
@@ -59,7 +59,7 @@ describe SessionsController, 'logging out when previously logged in' do
   end
 
   it 'should redirect to the home path' do
-    response.should redirect_to(root_path)
+    response.should redirect_to(root_url)
   end
 end
 
@@ -73,7 +73,7 @@ describe SessionsController, 'logging out when not previously logged in' do
   end
 
   it 'should redirect to the home path' do
-    response.should redirect_to(root_path)
+    response.should redirect_to(root_url)
   end
 end
 
@@ -85,24 +85,24 @@ describe SessionsController, 'redirecting after login' do
 
   it 'should redirect to the user dashboard if no original uri supplied' do
     post 'create', :protocol => 'https'
-    response.should redirect_to(dashboard_path)
+    response.should redirect_to(dashboard_url)
   end
 
   it 'should redirect to if original uri supplied via session' do
     session[:original_uri] = '/comments'
     post 'create', :protocol => 'https'
-    response.should redirect_to('/comments')
+    response.should redirect_to(comments_url)
   end
 
   it 'should redirect to if original uri supplied via params' do
     post 'create', :original_uri => '/comments', :protocol => 'https'
-    response.should redirect_to('/comments')
+    response.should redirect_to(comments_url)
   end
 
   # was a bug were failing because even blank "original_uri" would redirect (to the root rather than the dashboard)
   it 'should redirect to the user dashboard if original uri is blank' do
     session[:original_uri] = ''
     post 'create', :protocol => 'https'
-    response.should redirect_to(dashboard_path)
+    response.should redirect_to(dashboard_url)
   end
 end

spec/spec_helper.rb
@@ -30,3 +30,14 @@ end
 
 # custom matchers
 require File.join(File.dirname(__FILE__), 'matchers', 'validation')
+
+# nasty hack to get specs passing
+# the fake requests created by Rails always return false for "ssl?"
+# so our before filter always redirects
+module ActionController
+  class TestRequest
+    def ssl?
+      true
+    end
+  end
+end