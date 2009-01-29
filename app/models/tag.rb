d74696c575e49a32de2674a7d86391fd246605fd
Wincent Colaiuta <win@wincent.com>
2009-01-29 11:04:41 +0100

    Add support for "scoping" to Tag model
    Provide methods that allow caller to "drill down" when browsing the
    tag cloud or looking at tag search results. Given all objects tagged
    with "foo", we check to see what other tags exist for those objects
    and could be used to narrow down the search.
    
    See:
    
      https://rails.wincent.com/issues/1209
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -25,4 +25,68 @@ class Tag < ActiveRecord::Base
   def to_param
     name
   end
+
+  # Given tag_names "foo", "bar" etc, find all items tagged with those
+  # tags and identify what other tags could be used to narrow the search.
+  def self.tags_reachable_from_tag_names *tag_names
+    count = tag_names.length
+    return [] if count < 1 or count > 5 # sanity limit: no more than 5 joins
+    raise ArgumentError if tag_names.include?(nil)
+    query = Array.new(count, 'name = ?').join ' OR '
+    tags = Tag.find :all, :conditions => [query, *tag_names]
+    tags_reachable_from_tags *tags
+  end
+
+  # Given actual tag objects, find all items tagged with those tags and
+  # identify what other tags could be used to narrow the search.
+  def self.tags_reachable_from_tags *tags
+    # Goal here is to produce a query string that looks like this
+    # (when "one-level deep", ie. looking at "foo"):
+    #
+    #   SELECT t2.tag_id AS id, tags.name, tags.taggings_count
+    #   FROM tags, taggings AS t1
+    #   JOIN taggings AS t2
+    #   WHERE t1.tag_id = 38
+    #   AND t2.taggable_id = t1.taggable_id
+    #   AND t2.taggable_type = t1.taggable_type
+    #   AND tags.id = t2.tag_id
+    #   AND t2.tag_id NOT IN (38)
+    #   GROUP BY t2.tag_id;
+    #
+    # Here is a sample when goal is "two levels deep"
+    # (ie. looking at "foo bar"):
+    #
+    #   SELECT t3.tag_id AS id, tags.name, tags.taggings_count
+    #   FROM tags, taggings AS t1
+    #   JOIN taggings AS t2
+    #   JOIN taggings AS t3
+    #   WHERE t1.tag_id = 38
+    #   AND t2.tag_id = 40
+    #   AND t2.taggable_id = t1.taggable_id
+    #   AND t2.taggable_type = t1.taggable_type
+    #   AND t3.taggable_id = t1.taggable_id
+    #   AND t3.taggable_type = t1.taggable_type
+    #   AND tags.id = t3.tag_id
+    #   AND t3.tag_id NOT IN (38, 40)
+    #   GROUP BY t3.tag_id;
+    count = tags.length
+    return [] if count < 1 or count > 5 # sanity limit: no more than 5 joins
+    tag_ids = tags.collect(&:id)        # will raise if any tag is nil
+    query = ["SELECT t#{count + 1}.tag_id AS id, tags.name, tags.taggings_count"]
+    joins = ['taggings AS t1']
+    ands  = []
+    count.times do |i|
+      joins << "taggings AS t#{i + 2}"
+      ands.unshift "t#{count - i}.tag_id = #{tag_ids[count - i - 1]}"
+      ands  << "t#{i + 2}.taggable_id = t1.taggable_id"
+      ands  << "t#{i + 2}.taggable_type = t1.taggable_type"
+    end
+    ands << "tags.id = t#{count + 1}.tag_id"
+    ands << "t#{count + 1}.tag_id NOT IN (" + tag_ids.join(', ') + ')'
+    query << "FROM tags, " + joins.join(' JOIN ')
+    query << "WHERE " + ands.join(' AND ')
+    query << "GROUP BY t#{count + 1}.tag_id"
+    query = query.join ' '
+    Tag.find_by_sql query
+  end
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -76,3 +76,150 @@ describe Tag, 'name normalization' do
     tag.name.should == name.downcase
   end
 end
+
+describe Tag, 'tags_reachable_from_tag_names method' do
+  before do
+    create_article      # no tags
+    create_issue        # no tags
+    create_article.tag  'nginx'
+    create_article.tag  'nginx updates'
+    create_article.tag  'nginx updates'
+    create_article.tag  'nginx security updates'
+    create_article.tag  'nginx'
+    create_article.tag  'nginx security'
+    create_article.tag  'nginx mac.os.x'
+    create_post.tag     'nginx'
+    create_post.tag     'ruby'
+    create_post.tag     'nginx'
+    create_post.tag     'nginx updates'
+    create_post.tag     'nginx ssl'
+    create_post.tag     'nginx'
+    create_issue.tag    'security'
+    create_issue.tag    'crash'
+    create_issue.tag    'security crash'
+    @nginx    = Tag.find_by_name 'nginx'
+    @updates  = Tag.find_by_name 'updates'
+    @security = Tag.find_by_name 'security'
+    @macosx   = Tag.find_by_name 'mac.os.x'
+    @ruby     = Tag.find_by_name 'ruby'
+    @ssl      = Tag.find_by_name 'ssl'
+    @crash    = Tag.find_by_name 'crash'
+  end
+
+  it 'should return empty array for no tags' do
+    Tag.tags_reachable_from_tag_names.should == []
+  end
+
+  it 'should return empty array for more than 5 tags' do
+    Tag.tags_reachable_from_tag_names('foo', 'bar', 'baz', 'bing', 'bong', 'bang').should == []
+  end
+
+  it 'should return empty array for non-existent tag (singular)' do
+    Tag.tags_reachable_from_tag_names('gibberish').should == []
+  end
+
+  it 'should complain if passed nil tag name' do
+    lambda { Tag.tags_reachable_from_tag_names(nil) }.should raise_error
+  end
+
+  it 'should return empty array for non-existent tags (plural)' do
+    Tag.tags_reachable_from_tag_names('gibberish', 'splinkel').should == []
+  end
+
+  it 'should return tag objects for "top-level" search' do
+    expected = [@updates, @security, @macosx, @ssl]
+    Tag.tags_reachable_from_tag_names('nginx').should =~ expected
+    Tag.tags_reachable_from_tag_names('ssl').should == [@nginx]
+    Tag.tags_reachable_from_tag_names('crash').should == [@security]
+  end
+
+  it 'should return tag objects for "second level" search' do
+    Tag.tags_reachable_from_tag_names('nginx', 'security').should == [@updates]
+  end
+
+  it 'should exclude starting tags from returned results' do
+    # tested implicitly above, but test explicitly here anyway
+    Tag.tags_reachable_from_tag_names('nginx').should_not include(@nginx)
+  end
+
+  it 'should return empty array when no more results' do
+    Tag.tags_reachable_from_tag_names('nginx', 'security', 'updates').should == []
+  end
+end
+
+describe Tag, 'tags_reachable_from_tags method' do
+  before do
+    create_article      # no tags
+    create_issue        # no tags
+    create_article.tag  'nginx'
+    create_article.tag  'nginx updates'
+    create_article.tag  'nginx updates'
+    create_article.tag  'nginx security updates'
+    create_article.tag  'nginx'
+    create_article.tag  'nginx security'
+    create_article.tag  'nginx mac.os.x'
+    create_post.tag     'nginx'
+    create_post.tag     'ruby'
+    create_post.tag     'nginx'
+    create_post.tag     'nginx updates'
+    create_post.tag     'nginx ssl'
+    create_post.tag     'nginx'
+    create_issue.tag    'security'
+    create_issue.tag    'crash'
+    create_issue.tag    'security crash'
+    @nginx    = Tag.find_by_name 'nginx'
+    @updates  = Tag.find_by_name 'updates'
+    @security = Tag.find_by_name 'security'
+    @macosx   = Tag.find_by_name 'mac.os.x'
+    @ruby     = Tag.find_by_name 'ruby'
+    @ssl      = Tag.find_by_name 'ssl'
+    @crash    = Tag.find_by_name 'crash'
+  end
+
+  it 'should return empty array for no tags' do
+    Tag.tags_reachable_from_tags.should == []
+  end
+
+  it 'should return empty array for more than 5 tags' do
+    Tag.tags_reachable_from_tags(create_tag, create_tag, create_tag, create_tag, create_tag, create_tag).should == []
+  end
+
+  it 'should return empty array for non-existent tag (singular)' do
+    fake_tag    = new_tag
+    fake_tag.id = 78321
+    Tag.tags_reachable_from_tags(fake_tag).should == []
+  end
+
+  it 'should complain if passed nil tag' do
+    lambda { Tag.tags_reachable_from_tags(nil) }.should raise_error
+  end
+
+  it 'should return empty array for non-existent tags (plural)' do
+    fake1     = new_tag
+    fake1.id  = 98712
+    fake2     = new_tag
+    fake2.id  = 34510
+    Tag.tags_reachable_from_tags(fake1, fake2).should == []
+  end
+
+  it 'should return tag objects for "top-level" search' do
+    expected = [@updates, @security, @macosx, @ssl]
+    Tag.tags_reachable_from_tags(@nginx).should =~ expected
+    Tag.tags_reachable_from_tags(@ssl).should == [@nginx]
+    Tag.tags_reachable_from_tags(@crash).should == [@security]
+  end
+
+  it 'should return tag objects for "second level" search' do
+    Tag.tags_reachable_from_tags(@nginx, @security).should == [@updates]
+  end
+
+  it 'should exclude starting tags from returned results' do
+    # tested implicitly above, but test explicitly here anyway
+    Tag.tags_reachable_from_tags(@nginx).should_not include(@nginx)
+  end
+
+  it 'should return empty array when no more results' do
+    Tag.tags_reachable_from_tags(@nginx, @security, @updates).should == []
+  end
+end
+
