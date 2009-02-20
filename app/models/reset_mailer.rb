3b0ea5cad796c083a1a08b0e453cedcabe6c1978
Wincent Colaiuta <win@wincent.com>
2009-02-20 23:55:36 +0100

    Create Message records for outgoing email resets
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/models/reset_mailer.rb
+++ app/models/reset_mailer.rb
@@ -2,16 +2,22 @@ class ResetMailer < ActionMailer::Base
   def reset_message reset
     # TODO: allow user to set default email address
     email = reset.user.emails.first(:conditions => 'deleted_at IS NULL') || (raise ActiveRecord::RecordNotFound)
-    subject     'wincent.com forgotten passphrase helper'
+    subject(subject_header = 'wincent.com forgotten passphrase helper')
     body({
       :address          => email.address,
       :reset_url        => edit_reset_url(reset), # will provide user with a form to change the passphrase
       :cutoff           => reset.cutoff.utc
       })
-    recipients  email.address
-    bcc         APP_CONFIG['admin_email']
-    from        APP_CONFIG['admin_email']
-    sent_on     Time.now
-    headers     {}
+    recipients(to_header = email.address)
+    bcc APP_CONFIG['admin_email']
+    from(from_header = APP_CONFIG['admin_email'])
+    sent_on Time.now
+    headers 'Message-ID' => (message_id_header = TMail.new_message_id 'wincent.com')
+    Message.create  :related => reset,
+                    :message_id_header => message_id_header,
+                    :to_header => to_header,
+                    :from_header => from_header,
+                    :subject_header => subject_header,
+                    :incoming => false
   end
 end
--- spec/models/reset_mailer_spec.rb
+++ spec/models/reset_mailer_spec.rb
@@ -45,4 +45,14 @@ describe ResetMailer, 'reset' do
   it 'should mention the cutoff date in UTC time' do
     @mail.body.should match(/#{@reset.cutoff.utc.to_s}/)
   end
+
+  it 'should include "@wincent.com" in the Message-ID header' do
+    @mail.header['message-id'].to_s.should =~ %r{@wincent.com}
+  end
+
+  it 'should create a corresponding Message object' do
+    message = Message.find_by_message_id_header(@mail.header['message-id'].to_s)
+    message.related.should == @reset
+  end
+
 end
