315dcf593a8d117c3be78203b4c712a92c632a01
Wincent Colaiuta <win@wincent.com>
2009-02-21 19:31:27 +0100

    Specs for mangled "To" headers
    I found the message which made the incoming mailing
    processing choke; it was a mangled "To" header which
    TMail returns as "nil" instead of an Array.
    
    So I've added a copy of the raw message text itself
    and use that in the spec.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/models/support_mailer_spec.rb
+++ spec/models/support_mailer_spec.rb
@@ -1,25 +1,19 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
+def sample_email name
+  path = File.dirname(__FILE__) + '/../fixtures/mail/' + name
+  IO.read(path)
+end
+
 describe SupportMailer do
   it 'should process incoming support requests'
 end
 
 describe SupportMailer, 'regressions' do
-  before do
-    @mail = TMail::Mail.new
-    @mail.from      = 'foo@example.com'
-    @mail.to        = 'bar@example.com'
-    @mail.subject   = '$$$$'
-    @mail.body      = 'Hello, world'
-  end
-
-  it 'should handle incoming mails without "To" headers' do
-    pending
-    # doesn't work: bails with a very unhelpful stack trace:
-    #    undefined method `index' for #<TMail::Mail:0x21880b0>
-    #   ./spec/models/support_mailer_spec.rb:9:
-    @mail.to = nil
-    SupportMailer.receive @mail
+  it 'should handle incoming mails without valid "To" headers' do
+    # this one wasn't missing the "To" header, it was just horribly mangled:
+    #   "@@TO_NAME" <@@TO_EMAIL>
+    lambda { SupportMailer.receive sample_email('dodgy-to-header') }.should_not raise_error
   end
 
   it 'should handle incoming mails without "From" headers'
