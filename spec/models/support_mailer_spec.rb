94a2e021f7a280bf242151d53b5c11194a229132
Wincent Colaiuta <win@wincent.com>
2009-02-21 18:59:07 +0100

    Add pending specs for commit 43caf42
    Would be nice to put in some specs here to stop the issue
    that was fixed in 43caf42 from regressing, but for some
    unknown reason, the basic pattern doesn't work at all.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/models/support_mailer_spec.rb
+++ spec/models/support_mailer_spec.rb
@@ -3,3 +3,24 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe SupportMailer do
   it 'should process incoming support requests'
 end
+
+describe SupportMailer, 'regressions' do
+  before do
+    @mail = TMail::Mail.new
+    @mail.from      = 'foo@example.com'
+    @mail.to        = 'bar@example.com'
+    @mail.subject   = '$$$$'
+    @mail.body      = 'Hello, world'
+  end
+
+  it 'should handle incoming mails without "To" headers' do
+    pending
+    # doesn't work: bails with a very unhelpful stack trace:
+    #    undefined method `index' for #<TMail::Mail:0x21880b0>
+    #   ./spec/models/support_mailer_spec.rb:9:
+    @mail.to = nil
+    SupportMailer.receive @mail
+  end
+
+  it 'should handle incoming mails without "From" headers'
+end
