eca061e8159863f07a278cf945a4a14d499ac9e4
Wincent Colaiuta <win@wincent.com>
2009-02-22 00:55:53 +0100

    Fix broken pagination in issues moderation index
    Fixes:
    
      https://wincent.com/issues/1100
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/controllers/admin/issues_controller.rb
+++ app/controllers/admin/issues_controller.rb
@@ -1,10 +1,11 @@
 class Admin::IssuesController < ApplicationController
   before_filter :require_admin
 
+  # TODO: compact interface for mass-deleting spam issues using checkboxes
   def index
-    @paginator  = Paginator.new params, Issue.count(:conditions => { :awaiting_moderation => true }), admin_issues_url
+    @paginator  = Paginator.new params, Issue.count(:conditions => { :awaiting_moderation => true }), admin_issues_url, 20
     @issues     = Issue.find :all, :offset => @paginator.offset, :conditions => { :awaiting_moderation => true },
-      :order => 'created_at DESC'
+      :order => 'created_at DESC', :limit => @paginator.limit
   end
 
   # AJAX (format: js) only.
---
+++ spec/controllers/admin/issues_controller_spec.rb
@@ -0,0 +1,39 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+require File.dirname(__FILE__) + '/../application_controller_spec'
+
+describe Admin::IssuesController do
+  it_should_behave_like 'ApplicationController'
+end
+
+describe Admin::IssuesController, 'index action' do
+  before do
+    login_as_admin
+  end
+
+  def do_get
+    get 'index', :protocol => 'https'
+  end
+
+  it 'should run the "require_admin" before_filter' do
+    controller.should_receive(:require_admin)
+    do_get
+  end
+
+  it 'should render the index template' do
+    do_get
+    response.should render_template('index')
+  end
+
+  it 'should be successful' do
+    do_get
+    response.should be_success
+  end
+
+  # was a bug: https://wincent.com/issues/1100
+  it 'should paginate in groups of 20' do
+    paginator = Paginator.new({}, 100, 'foo', 20)
+    Paginator.should_receive(:new).with(anything(), anything(), anything(), 20).and_return(paginator)
+    Issue.should_receive(:find).with(anything(), hash_including(:limit => paginator.limit))
+    do_get
+  end
+end
