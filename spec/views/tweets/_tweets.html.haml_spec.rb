1288ef7b539438e825eddb04b919455fe96272a5
Wincent Colaiuta <win@wincent.com>
2009-02-27 22:29:57 +0100

    Move rendering of tweets list out into a partial
    In this way it can be used in other places as well (for
    example, to put a list of recent tweets in the sidebar
    next to the blog).
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ spec/views/tweets/_tweets.html.haml_spec.rb
@@ -0,0 +1,40 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+
+describe '/tweets/index.html.haml' do
+  include TweetsHelper
+
+  def do_render
+    assigns[:tweets] = @tweets
+    render '/tweets/_tweets.html.haml'
+  end
+
+  before do
+    @tweets = [
+      create_tweet(:body => "''foo''"),
+      create_tweet(:body => "'''bar'''")
+    ]
+  end
+
+  it 'should display a div for each tweet' do
+    do_render
+    response.should have_tag('div.tweet', 2)
+  end
+
+  it 'should display the HTML body of each tweet' do
+    do_render
+    response.body.should =~ %r{<em>foo</em>}
+    response.body.should =~ %r{<strong>bar</strong>}
+  end
+
+  it 'should show the time information for each tweet' do
+    template.should_receive(:timeinfo).with(@tweets[0])
+    template.should_receive(:timeinfo).with(@tweets[1])
+    do_render
+  end
+
+  it 'should show a permalink for each tweet' do
+    do_render
+    response.should have_tag('a[href=?]', tweet_url(@tweets[0]), 'permalink')
+    response.should have_tag('a[href=?]', tweet_url(@tweets[1]), 'permalink')
+  end
+end
--- spec/views/tweets/index.html.haml_spec.rb
+++ spec/views/tweets/index.html.haml_spec.rb
@@ -4,16 +4,12 @@ describe '/tweets/index.html.haml' do
   include TweetsHelper
 
   def do_render
-    assigns[:tweets] = @tweets
     assigns[:paginator] = @paginator
+    assigns[:tweets] = [ create_tweet, create_tweet ]
     render '/tweets/index.html.haml'
   end
 
   before do
-    @tweets = [
-      create_tweet(:body => "''foo''"),
-      create_tweet(:body => "'''bar'''")
-    ]
     @paginator = RestfulPaginator.new({}, 2, tweets_url, 20)
   end
 
@@ -28,26 +24,8 @@ describe '/tweets/index.html.haml' do
     do_render
   end
 
-  it 'should display a div for each tweet' do
+  it 'should render the "tweets" partial' do
+    template.should_receive(:render).with('tweets.html.haml')
     do_render
-    response.should have_tag('div.tweet', 2)
-  end
-
-  it 'should display the HTML body of each tweet' do
-    do_render
-    response.body.should =~ %r{<em>foo</em>}
-    response.body.should =~ %r{<strong>bar</strong>}
-  end
-
-  it 'should show the time information for each tweet' do
-    template.should_receive(:timeinfo).with(@tweets[0])
-    template.should_receive(:timeinfo).with(@tweets[1])
-    do_render
-  end
-
-  it 'should show a permalink for each tweet' do
-    do_render
-    response.should have_tag('a[href=?]', tweet_url(@tweets[0]), 'permalink')
-    response.should have_tag('a[href=?]', tweet_url(@tweets[1]), 'permalink')
   end
 end
