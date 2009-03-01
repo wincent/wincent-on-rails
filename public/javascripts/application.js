b091d3de90851fb719fcb6ced7bf2e77cbaaf63d
Wincent Colaiuta <win@wincent.com>
2009-03-01 14:28:29 +0100

    Add GNU Affero General Public License to application.js
    "application.js" is starting to acquire more and more
    functionality, and seeing as it is distributed in source
    form it is probably time to think about what terms use of
    the source should be permitted.
    
    So release it under the GNU Affero General Public License
    and include a copy of the license on the server.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,24 @@
+/*
+   Copyright 2008-2009 Wincent Colaiuta.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public
+   License along with this program.  If not, see
+   <http://www.gnu.org/licenses/>.
+
+   A copy of the GNU Affero General Public License is also available
+   at <https://wincent.com/javascripts/agpl-3.0.txt>
+ */
+
 function getCookie(name) {
   var cookies = document.cookie.split('; ');
   for (var i = 0; i < cookies.length; i++) {
