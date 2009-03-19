a7f31f593aa14dff967930afbc0ca31b41b778ad
Wincent Colaiuta <win@wincent.com>
2009-03-20 00:47:10 +0100

    Cucumber: suppress warnings about parentheses
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- features/step_definitions/issues.rb
+++ features/step_definitions/issues.rb
@@ -3,11 +3,11 @@ Given /^an issue with summary "(.*)"$/ do |summary|
 end
 
 When /^I look at the issue with summary "(.*)"$/ do |summary|
-  visit issue_path(Issue.find_by_summary summary)
+  visit issue_path(Issue.find_by_summary(summary))
 end
 
 When /^I edit the issue with summary "(.*)"$/ do |summary|
-  visit edit_issue_path(Issue.find_by_summary summary)
+  visit edit_issue_path(Issue.find_by_summary(summary))
 end
 
 When /^I change the summary to "(.*)"$/ do |summary|
