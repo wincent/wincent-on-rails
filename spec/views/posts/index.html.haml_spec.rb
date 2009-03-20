78ff30573af9d7b17f4591a2b86f2a6053d264c0
Wincent Colaiuta <win@wincent.com>
2009-03-20 11:22:18 +0100

    Show tweet feed link with icon on blog index page
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/views/posts/index.html.haml_spec.rb
+++ spec/views/posts/index.html.haml_spec.rb
@@ -1,17 +1,25 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
-describe "/posts/index.html.haml" do
+describe '/posts/index.html.haml' do
   include PostsHelper
 
   before do
     assigns[:tweets] = [create_tweet]
-    assigns[:posts] = [create_post]
+    assigns[:posts]  = [create_post]
+  end
+
+  def do_render
+    render '/posts/index.html.haml'
   end
 
   # was a bug
-  it "should not have nested <p> tags" do
-    render "/posts/index.html.haml"
+  it 'should not have nested <p> tags' do
+    do_render
     response.should_not have_text(/<p>\w*<p>/)
   end
-end
 
+  it 'should have a link to the tweets Atom feed' do
+    do_render
+    response.should have_tag('a[href=?]', tweets_url(:format => :atom))
+  end
+end
