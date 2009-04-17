3111f57edfd098094277511e292a82d0436071fd
Wincent Colaiuta <win@wincent.com>
2009-04-17 17:14:27 +0200

    Correct description of link model
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,12 +1,14 @@
 # Simple class with two purposes:
 #
 #   - providing links to other sites and counting the number of click-throughs.
-#   - providing internal and external links using convenient slugs (for example "link/google" to redirect to google.com,
-#     or link/menu-crash to link to a "hot" bug in the issue tracker)
+#   - providing internal and external links using convenient slugs (for example
+#     /links/google to redirect to google.com, or /links/menu-crash to link to
+#     a "hot" bug in the issue tracker)
 #
-# This is a simple implementation that just counts the absolute number of clicks; it does not record
-# the date and time of the click (for example), although this could be easily added later on if desired
-# with a Clickthrough model.
+# This is a simple implementation that just counts the absolute number of
+# clicks; it does not record the date and time of the click (for example),
+# although this could be easily added later on if desired with a Clickthrough
+# model.
 class Link < ActiveRecord::Base
   validates_presence_of   :uri
   validates_uniqueness_of :uri
