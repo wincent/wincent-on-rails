f764e48b097147ad2baf623f6ffb42d2295c7e14
Wincent Colaiuta <win@wincent.com>
2009-04-20 11:43:29 +0200

    Switch to new edit-in-place for admin/posts#index
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,7 +1,7 @@
 class Admin::PostsController < ApplicationController
-  before_filter     :require_admin
-  in_place_edit_for :post, :title
-  in_place_edit_for :post, :permalink
+  before_filter           :require_admin
+  before_filter           :find_post, :only => [:show, :update]
+  uses_dynamic_javascript :only => :index
 
   # TODO: really need a published_at field, I think
   # TODO: add "public", "accepts comments" and "comments_count" columns here
@@ -10,5 +10,36 @@ class Admin::PostsController < ApplicationController
   def index
     # TODO: combine sortability with pagination?
     @posts = Post.find(:all, sort_options)
+  end
+
+  def show
+    respond_to do |format|
+      format.js { # AJAX updates
+        render :json => @post.to_json(:only => [:permalink, :title])
+      }
+    end
+  end
+
+  def update
+    respond_to do |format|
+      format.js { # an AJAX update
+        if @post.update_attributes params[:post]
+          # don't use admin_post_url here because we want to force the use of a
+          # numeric id; url_for will keep us in admin namespace here
+          redirect_to url_for(:controller => 'posts', :action => 'show',
+            :id => @post.id, :protocol => 'https')
+        else
+          render :text => '', :status => 422
+        end
+      }
+    end
+  end
+
+protected
+
+  def find_post
+    # unlike in the non-admin posts controller, find using numeric id
+    # (for compatibility with the in-place-editing AJAX)
+    @post = Post.find params[:id]
   end
 end
