bb223e426474c6f4899dc002a116c2020991286a
Wincent Colaiuta <win@wincent.com>
2009-04-20 23:55:18 +0200

    Remove unused admin/issues#update
    The inline AJAX uses issues#update to mark records as spam;
    there is no need for this redundant method in admin/issues.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/controllers/admin/issues_controller.rb
+++ app/controllers/admin/issues_controller.rb
@@ -7,18 +7,4 @@ class Admin::IssuesController < ApplicationController
     @issues     = Issue.find :all, :offset => @paginator.offset, :conditions => { :awaiting_moderation => true },
       :order => 'created_at DESC', :limit => @paginator.limit
   end
-
-  # AJAX (format: js) only.
-  def update
-    @issue = Issue.find params[:id]
-    if params[:button] == 'ham'
-      @issue.moderate_as_ham!
-      render :update do |page|
-        page.visual_effect :highlight, "issue_#{@issue.id}", :duration => 1.5
-        page.visual_effect :fade, "issue_#{@issue.id}_ham_form"
-      end
-    else
-      raise 'unrecognized AJAX action'
-    end
-  end
 end
