77fa1f976db075ec25562ec1c16de8b1d1bafd3f
Wincent Colaiuta <win@wincent.com>
2009-04-20 11:16:07 +0200

    Switch to new edit-in-place for admin/tags#index
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -1,9 +1,41 @@
 class Admin::TagsController < ApplicationController
   before_filter     :require_admin
-  in_place_edit_for :tag, :name
+  before_filter     :find_tag, :only => [:show, :update]
   acts_as_sortable  :by => [:name, :taggings_count], :default => :name
+  uses_dynamic_javascript :only => :index
 
   def index
     @tags = Tag.find :all, sort_options
+  end
+
+  def show
+    respond_to do |format|
+      format.js { # AJAX updates
+          render :json => @tag.to_json(:only => [:name])
+      }
+    end
+  end
+
+  def update
+    respond_to do |format|
+      format.js { # an AJAX update
+        if @tag.update_attributes params[:tag]
+          # don't use admin_tag_url here because we want to force the use of a
+          # numeric id
+          redirect_to url_for(:controller => 'tags', :action => 'show',
+            :id => @tag.id, :protocol => 'https')
+        else
+          render :text => '', :status => 422
+        end
+      }
+    end
+  end
+
+protected
+
+  def find_tag
+    # unlike in the non-admin tags controller, find using numeric id
+    # (for compatibility with the in-place-editing AJAX)
+    @tag = Tag.find params[:id]
   end
 end
