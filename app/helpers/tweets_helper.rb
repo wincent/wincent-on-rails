f35f320f77130e1c43ee5b73a7100d1fbff2b838
Wincent Colaiuta <win@wincent.com>
2009-04-21 22:33:32 +0200

    jQuery live preview for tweet#new and tweet#edit
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -11,21 +11,36 @@ module TweetsHelper
   end
 
   def link_to_update_preview
-    link_to_remote 'update', common_options, :class => 'update_link'
+    onclick = inline_js do
+      <<-JS
+        jQuery('\#spinner').show();
+        jQuery.ajax({
+          'url': '#{tweets_url}.js',
+          'type': 'post',
+          'dataType': 'html',
+          'data': 'body=' + encodeURIComponent(jQuery('\#tweet_body').val()),
+          'success': function(html) { jQuery('\#preview').html(html); },
+          'error': function() { alert('an error occurred updating the preview'); },
+          'complete': function() { jQuery('\#spinner').hide(); }
+        });
+        return false;
+      JS
+    end
+    %Q{<a href="#" class="update_link" onclick="#{onclick}">update</a>}
   end
 
   def observe_body
-    observe_field 'tweet_body', common_options.merge({ :frequency => 5.0 })
-  end
-
-  def common_options
-    {
-      :url => tweets_url,
-      :method => 'post',
-      :update => 'preview',
-      :with => "'body=' + encodeURIComponent($('tweet_body').value)",
-      :before => "Element.show('spinner')",
-      :complete => "Element.hide('spinner')"
-    }
+    javascript_tag <<-JS
+      observe_field({
+        'field': jQuery('\#tweet_body'),
+        'fieldName': 'body',
+        'interval': 5,
+        'url': '#{tweets_url}.js',
+        'before': function() { jQuery('\#spinner').show(); },
+        'success': function(html) { jQuery('\#preview').html(html); },
+        'error': function(html) { alert('an error occurred updating the preview'); },
+        'complete': function() { jQuery('\#spinner').hide(); },
+      });
+    JS
   end
 end
