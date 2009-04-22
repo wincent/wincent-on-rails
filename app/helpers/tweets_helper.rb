e655c1fb130d53a77ae652e2a9b8062d96a4d0d0
Wincent Colaiuta <win@wincent.com>
2009-04-22 23:51:09 +0200

    Migrate link_to_update_preview from helpers/tweets_helper to JS
    This is the first step -- making the link unobtrusive -- and the
    next step will be refactoring so that the code need not be
    repeated for other controllers.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -9,28 +9,4 @@ module TweetsHelper
   def character_count tweet
     pluralizing_count tweet.rendered_length, 'character'
   end
-
-  def link_to_update_preview
-    onclick = inline_js do
-      <<-JS
-        $('\#spinner').show();
-        $.ajax({
-          'url': '#{tweets_url}.js',
-          'type': 'post',
-          'dataType': 'html',
-          'data': 'body=' + encodeURIComponent($('\#tweet_body').val()),
-          'success': function(html) {
-            $('\#preview').html(html);
-            clearAJAXFlash();
-          },
-          'error': function(req) {
-            insertAJAXFlash('error', req.responseText);
-          },
-          'complete': function() { $('\#spinner').hide(); }
-        });
-        return false;
-      JS
-    end
-    %Q{<a href="#" class="update_link" onclick="#{onclick}">update</a>}
-  end
 end
