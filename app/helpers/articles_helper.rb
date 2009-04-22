b840b1820dd891aaf399be5280a3d448c1916df0
Wincent Colaiuta <win@wincent.com>
2009-04-23 00:08:32 +0200

    Use setup_preview_link in articles#new and articles#edit
    This removes some code duplication at the same time as making the
    JS unobtrusive.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -5,29 +5,4 @@ module ArticlesHelper
     text << @article.body
     text.join("\n\n").w :base_heading_level => 2
   end
-
-  def link_to_update_preview
-    onclick = inline_js do
-      <<-JS
-        $('\#spinner').show();
-        $.ajax({
-          'url': '#{articles_url}.js',
-          'type': 'post',
-          'dataType': 'html',
-          'data': 'title=' + encodeURIComponent($('\#article_title').val()) +
-            '&body=' + encodeURIComponent($('\#article_body').val()),
-          'success': function(html) {
-            $('\#preview').html(html);
-            clearAJAXFlash();
-          },
-          'error': function(req) {
-            insertAJAXFlash('error', req.responseText);
-          },
-          'complete': function() { $('\#spinner').hide(); }
-        });
-        return false;
-      JS
-    end
-    %Q{<a href="#" class="update_link" onclick="#{onclick}">update</a>}
-  end
 end
