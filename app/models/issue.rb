wincent/wincent-on-rails

commit a917d2cb50c863b376909ebebefcb2d77ed27011

Make selection matching in ajax_select case-insensitive
When trying to find the current selection in ajax_select we were
calling toLowerCase() on the static text (so for example, "Feature
request" would become "feature request") and this matched correctly
against the supplied options hash (produced with the
"underscores_to_spaces" helper method).

But this didn't work for the products pop-up because the options
hash used mixed-case names like "WOCommon" and so forth.

There are three ways to fix this:

- stop calling "humanize" in the issues#show view, thus keeping
  "feature request" as is rather than turning it into "Feature
  request"; this in turn would allow us to drop the toLowerCase()
  call entirely
- do case-insensitive matching (ie. call toLowerCase on both sides)
- use "humanized" text in the pop-ups as well

Of these the second solution is the one I went with in the
interests of a quick hot-fix. But in the longer term (for the next
deployment) I am going to go with the first solution. The third
solution was never an attractive one as such text looks ugly and
I don't like the inconsitency between different pop-ups (status
and kind being "humanized" but product being left alone).

Fixes:

  https://wincent.com/issues/1288

Signed-off-by: Wincent Colaiuta <win@wincent.com>

public/javascripts/ajax.js
@@ -133,14 +133,14 @@ function ajax_select(selector, class_name, attribute_name, options, include_blan
   var model_id = $(selector).attr('id'); // issue_22
   var record_id = model_id.match(/_(\d+)$/)[1]; // 22
   var field_id = $('#' + model_id + '_' + attribute_name); // issue_22_status
-  var field_text = field_id.text(); // eg. New, Open, Closed (first letter capitalized by humanize method)
+  var field_text = field_id.text(); // eg. New, Open, Closed
   var new_contents = '<select id="' + class_name + '_' + attribute_name + '" name="' + class_name + '[' + attribute_name + ']">';
   if (include_blank) {
     new_contents = new_contents + '<option value=""></option>';
   }
   var selection_found = false;
   for (var i = 0; i < options.length; i++) {
-    if (field_text.toLowerCase() == options[i][0]) {
+    if (field_text.toLowerCase() == options[i][0].toLowerCase()) {
       new_contents = new_contents + '<option value="' + options[i][1] + '" selected="selected">' + options[i][0] + '</option>';
       selection_found = true;
     } else {

commit 2dd638a232c8cb1525fb277bfa8063dc987bcca4

Make ajax_select() totally case sensitive
Commit a917d2c introduced a quick hot-fix for the broken product
pop-up menus in issues#show.

Here we make an alternative, slightly more invasive fix, intended
for deployment in the long term. In effect we have gone from the
buggy starting point:

- only one side of the equality comparison treated as case-insensitive

To the quick workaround:

- both sides treated case-insentively

To the robust final solution:

- both sides treated as case-sensitive

Signed-off-by: Wincent Colaiuta <win@wincent.com>

app/models/issue.rb
@@ -42,12 +42,12 @@ class Issue < ActiveRecord::Base
 
   # We expose this for use in the controller layer.
   def self.string_for_status status
-    STATUS_MAP[status].to_s.humanize
+    STATUS_MAP[status].to_s.gsub('_', ' ')
   end
 
   # We expose this for use in the controller layer.
   def self.string_for_kind kind
-    KIND_MAP[kind].to_s.humanize
+    KIND_MAP[kind].to_s.gsub('_', ' ')
   end
 
   def status_string

public/javascripts/ajax.js
@@ -140,7 +140,7 @@ function ajax_select(selector, class_name, attribute_name, options, include_blan
   }
   var selection_found = false;
   for (var i = 0; i < options.length; i++) {
-    if (field_text.toLowerCase() == options[i][0].toLowerCase()) {
+    if (field_text == options[i][0]) {
       new_contents = new_contents + '<option value="' + options[i][1] + '" selected="selected">' + options[i][0] + '</option>';
       selection_found = true;
     } else {

spec/models/issue_spec.rb
@@ -109,8 +109,8 @@ describe Issue, "annotations" do
     @issue.save
     body = @issue.comments.first.body
     body.should =~ /Kind.*changed:/
-    body.should =~ /From:.*Bug/
-    body.should =~ /To:.*Feature request/
+    body.should =~ /From:.*bug/
+    body.should =~ /To:.*feature request/
   end
 
   it 'should add an annotation for status changes' do
@@ -119,8 +119,8 @@ describe Issue, "annotations" do
     @issue.save
     body = @issue.comments.first.body
     body.should =~ /Status.*changed:/
-    body.should =~ /From:.*Open/
-    body.should =~ /To:.*Closed/
+    body.should =~ /From:.*open/
+    body.should =~ /To:.*closed/
   end
 
   it 'should add an annotation for public changes' do