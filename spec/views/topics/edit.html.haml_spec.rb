1650aeaecd567fadb52694e019f80b723174f0db
Wincent Colaiuta <win@wincent.com>
2009-04-30 18:46:38 +0200

    Add view specs for topics#edit
    topics#edit wasn't affected by the bugs recently fixed
    in comments#edit and issues#edit (wherein the "ham"
    button was shown even for records not awaiting moderation)
    but it was missing specs to confirm this, so add them.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ spec/views/topics/edit.html.haml_spec.rb
@@ -0,0 +1,28 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe '/topics/edit' do
+  include TopicsHelper
+
+  before do
+    assigns[:forum] = @forum = create_forum
+    assigns[:topic] = @topic = create_topic(:forum => @forum)
+  end
+
+  def do_render
+    render '/topics/edit'
+  end
+
+  it 'should have a ham button if the topic is awaiting moderation' do
+    assigns[:topic] = @topic = create_topic(:awaiting_moderation => true,
+      :forum => @forum)
+    template.should_receive(:button_to_moderate_topic_as_ham).with(@topic)
+    do_render
+  end
+
+  it 'should not have a ham button if the issue is not awaiting moderation' do
+    assigns[:topic] = @topic = create_topic(:awaiting_moderation => false,
+      :forum => @forum)
+    template.should_not_receive(:button_to_moderate_topic_as_ham)
+    do_render
+  end
+end
