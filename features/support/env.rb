c13dc88b82c3bb1b132a4016d207b08822ca5b9d
Wincent Colaiuta <win@wincent.com>
2009-04-30 19:03:29 +0200

    Update to Cucumber 0.3.1
    These changes are the result of running "script/generate cucumber"
    after updating the system-installed Cucumber Gem to version 0.3.1.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,13 +1,17 @@
-ENV["RAILS_ENV"] = "test"
+ENV['RAILS_ENV'] = 'test'
 require File.expand_path(File.dirname(__FILE__) + '/../../config/environment')
 require 'cucumber/rails/world'
 require 'cucumber/formatters/unicode'
 require 'webrat'
+require 'webrat/core/matchers'
 require 'cucumber/rails/rspec'
+require 'json'
+
+Cucumber::Rails.use_transactional_fixtures
+Cucumber::Rails.bypass_rescue # Comment out this line if you want Rails own error handling
+                              # (e.g. rescue_action_in_public / rescue_responses / rescue_from)
 
 include FixtureReplacement
-
-require 'json'
 
 # evidently, response.request doesn't exist when running under Selenium
 # undefined method `request' for #<Webrat::SeleniumResponse:0x36fad58> (NoMethodError)
--- features/support/paths.rb
+++ features/support/paths.rb
@@ -1,12 +1,17 @@
-def path_to(page_name)
-  case page_name
-  when /the homepage/i
-    root_path
-  when /the login page/
-    login_path
-  when /the wiki index/
-    articles_path
-  else
-    raise "Can't find mapping from \"#{page_name}\" to a path."
+module NavigationHelpers
+  def path_to(page_name)
+    case page_name
+    when /the homepage/i
+      root_path
+    when /the login page/
+      login_path
+    when /the wiki index/
+      articles_path
+    else
+      raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
+        "Now, go and add a mapping in #{__FILE__}"
+    end
   end
 end
+
+World(NavigationHelpers)
