2895ff5eea5004635fb5edf9107ef8e908b92c6f
Wincent Colaiuta <win@wincent.com>
2009-05-06 00:49:53 +0200

    Require admin privileges to access attachments#new
    Only admins will be able to create "stand-alone" attachments
    through the attachments#new action; normal users will be
    limited to adding attachments to issues (via an AJAX post
    to attachments#create).
    
    This is for security reasons: we don't want anonymous users
    uploading files and using the server as a free distribution
    service. Subsequent commits will flesh out the security
    model.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,4 +1,7 @@
 class AttachmentsController < ApplicationController
+  before_filter :require_admin
+
+  # Admin only.
   def new
   end
 end
--- spec/controllers/attachments_controller_spec.rb
+++ spec/controllers/attachments_controller_spec.rb
@@ -2,9 +2,19 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe AttachmentsController do
   describe "GET 'new'" do
-    it 'should be successful' do
+    def do_get admin = false
+      login_as_admin if admin
       get 'new', :protocol => 'https'
+    end
+
+    it 'should be successful' do
+      do_get :as_admin
       response.should be_success
+    end
+
+    it 'should redirect non-admin users' do
+      do_get
+      response.should redirect_to(login_path)
     end
   end
 end
