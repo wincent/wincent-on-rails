339a4dc2f244e3c38a6899e042a65bc854fb68a7
Wincent Colaiuta <win@wincent.com>
2009-06-14 12:50:18 +0200

    Teach nested_comment_path method about tweets
    Necessary prelude to the full enabling of tweet comments.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ protected
     commentable = comment.commentable
     anchor      = "comment_#{comment.id}"
     case commentable
-    when Article, Issue, Post
+    when Article, Issue, Post, Tweet
       send "#{commentable.class.to_s.downcase}_path", commentable, :anchor => anchor
     when Topic
       forum_topic_path commentable.forum, commentable, :anchor => anchor
