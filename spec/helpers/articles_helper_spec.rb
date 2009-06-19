02f65d12698655fe66a00072256d9cb1cc9e3d35
Wincent Colaiuta <win@wincent.com>
2009-06-19 13:14:01 +0200

    Replace "title_and_body_html" helper method with partial rendering
    For consistency with the tweets and posts controllers and corresponding
    views, use the preview partial rather than a special helper method.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/helpers/articles_helper_spec.rb
+++ spec/helpers/articles_helper_spec.rb
@@ -2,13 +2,3 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe ArticlesHelper do
 end
-
-describe ArticlesHelper, 'body_html method (removed with Rails 2.2.0)' do
-  include ArticlesHelper
-
-  # the body_html method is no longer needed as of Rail 2.2.0 due to a behaviour change
-  # but we retain a specs here to catch any further behaviour changes in the future
-  it 'should use the empty string as an article body on new records' do
-    Article.new.body.should == ''
-  end
-end
