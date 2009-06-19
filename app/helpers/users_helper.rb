584f4e7ab368575031181b0cb93962b412c429a0
Wincent Colaiuta <win@wincent.com>
2009-06-19 12:11:33 +0200

    Remove unused "editable" helper method
    This is currently used nowhere in the app, and has perhaps never been
    used. In any case the implementation is not correct seeing as it relies
    on the "@user" instance variable being set when it would make more sense
    to take an explicit "model" parameter and get the user from the model.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,4 @@
 module UsersHelper
-  # similar helpers will work for any model that has a "user" (or owner) association
-  def editable &block
-    if logged_in? && @user == current_user || admin?
-      yield
-    end
-  end
-
   def email_status email
     statuses = [email.verified? ? 'verified' : 'unverified']
     statuses << 'deleted' if email.deleted?
