a551b0d898c7d5a0278ce5374f2c92a04c868fca
Wincent Colaiuta <win@wincent.com>
2009-06-19 12:06:16 +0200

    Use explicit parameters in article helpers
    For the same reasons as noted in f90de3f, use explicit parameters
    instead of implicitly passing the instance variable.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,8 +1,8 @@
 module ArticlesHelper
-  def title_and_body_html
+  def title_and_body_html article
     text = []
-    text << (@article.title.blank? ? '' : "= #{@article.title} =")
-    text << @article.body
+    text << (article.title.blank? ? '' : "= #{article.title} =")
+    text << article.body
     text.join("\n\n").w :base_heading_level => 2
   end
 end
