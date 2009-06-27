a4494ee26d8baf873fe63b5eb429eeaf44cbab63
Wincent Colaiuta <win@wincent.com>
2009-06-27 12:43:16 +0200

    Add products#show action
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,15 +1,28 @@
 class ProductsController < ApplicationController
   before_filter :require_admin, :except => [ :index, :show ]
+  before_filter :get_product, :only => :show
 
   def index
     @products = Product.find :all
   end
 
+  def show
+    render
+    # TODO: Atom feed will be for product update notices
+  end
+
+  # admin-only
   def new
     @product = Product.new
   end
 
+  # admin-only
   def create
     @product = Product.create params[:product]
+  end
+
+private
+  def get_product
+    @product = Product.find_by_permalink! params[:id]
   end
 end
