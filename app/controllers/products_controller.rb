4b498ace5704b654842c7e44c092e95805b2eaa2
Wincent Colaiuta <win@wincent.com>
2009-06-27 13:46:04 +0200

    Add products#update action
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   before_filter :require_admin, :except => [ :index, :show ]
-  before_filter :get_product, :only => [ :edit, :show ]
+  before_filter :get_product, :only => [ :edit, :show, :update ]
 
   def index
     @products = Product.find :all
@@ -24,6 +24,17 @@ class ProductsController < ApplicationController
   # admin-only
   def create
     @product = Product.create params[:product]
+  end
+
+  # admin-only
+  def update
+    if @product.update_attributes params[:product]
+      flash[:notice] = 'Successfully updated'
+      redirect_to @product
+    else
+      flash[:error] = 'Update failed'
+      render :action => 'edit'
+    end
   end
 
 private
