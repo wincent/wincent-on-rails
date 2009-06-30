dd6b6c2044cffead22630dd15e0d97d56662516e
Wincent Colaiuta <win@wincent.com>
2009-06-30 19:54:18 +0200

    Add "maxified" versions of some JavaScript
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ public/javascripts/ajax.max.js
@@ -0,0 +1,297 @@
+// Copyright 2009 Wincent Colaiuta. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+var global_spinner_counter = 0;
+
+function escapeHTML(html) {
+  return html.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g,
+    '&gt;').replace(/"/g, '&quot;');
+}
+
+function insertAJAXFlash(css_class, msg) {
+  $('#ajax-flash').html('<div class="' + css_class + '">' + escapeHTML(msg) +
+    '</div>').show();
+}
+
+function clearAJAXFlash() {
+  $('#ajax-flash').fadeOut('slow', function() { $('#ajax-flash').hide(); });
+}
+
+function ajax_comment_form(url) {
+  var comment_div = $('#comment-form');
+  var anchor = comment_div.find('a');
+  anchor.attr('href', '#comment-form');
+  var click = function() {
+    anchor.unbind('click');
+    var spinner_id = 'spinner_' + global_spinner_counter++;
+    comment_div.append('<img alt="Spinner" id="' + spinner_id + '" class="spinner" src="/images/spinner.gif" />');
+    var spinner = comment_div.find('#' + spinner_id);
+    spinner.show();
+    $.ajax({
+      'url': url,
+      'type': 'get',
+      'dataType': 'html',
+      'success': function(html) {
+        clearAJAXFlash();
+        comment_div.html(html);
+      },
+      'error': function(req) {
+        insertAJAXFlash('error', req.responseText);
+        spinner.remove();
+        anchor.click(click);
+      }
+    });
+  }
+  anchor.click(click);
+}
+
+function edit_in_place(selector, class_name, attribute_name, url) {
+  var model = $(selector); // could be many
+  model.each(function(i) {
+    var model_id = $(this).attr('id'); // issue_22
+    var record_id = model_id.match(/_(\d+)$/)[1]; // 22
+    var field_id = $('#' + model_id + '_' + attribute_name); // issue_22_summary
+    function highlight() { field_id.addClass('highlight'); }
+    function unhighlight() { field_id.removeClass('highlight'); }
+    function clickFunction() {
+      var field_text = field_id.text();
+      field_id.unbind('mouseenter mouseleave');
+      unhighlight();
+      field_id.attr('title', 'Click outside to abort editing');
+      field_id.html('<form action="javascript:void(0)" style="display:inline;"><input type="text" value="' +
+        escapeHTML(field_text) +
+        '"></form>');
+      field_id.find('input')[0].select();
+      field_id.unbind('dblclick');
+      field_id.find('input').blur(function() {
+        field_id.text(field_text);
+        field_id.dblclick(clickFunction);
+        field_id.hover(highlight, unhighlight);
+      });
+      field_id.find('form').submit(function() {
+        var value = field_id.find('input').val();
+        field_id.text('saving...');
+        $.ajax({
+          'url': url + record_id,
+          'type': 'post',
+          'dataType': 'json',
+          'data': '_method=put&' + class_name + '[' + attribute_name + ']=' +
+            encodeURIComponent(value),
+          'success': function(json) {
+            field_id.text(json[class_name][attribute_name]);
+            field_id.removeClass('ajax_error');
+            clearAJAXFlash();
+          },
+          'error': function(req) {
+            field_id.text(value);
+            field_id.addClass('ajax_error');
+            insertAJAXFlash('error', req.responseText);
+          },
+          'complete': function() {
+            field_id.hover(highlight, unhighlight);
+            field_id.dblclick(clickFunction);
+          }
+        });
+      });
+    }
+    field_id.attr('title', 'Double-click to edit');
+    field_id.dblclick(clickFunction);
+    field_id.hover(highlight, unhighlight);
+  });
+}
+
+function ajax_check_box(selector, class_name, attribute_name, url) {
+  var model_id = $(selector).attr('id'); // issue_22
+  var record_id = model_id.match(/_(\d+)$/)[1]; // 22
+  var field_id = $('#' + model_id + '_' + attribute_name); // issue_22_public
+  var field_text = field_id.text(); // eg. true, false
+  var spinner_id = 'spinner_' + global_spinner_counter++;
+  var new_contents = '<input id="' + class_name + '_' + attribute_name +
+    '" name="' + class_name + '[' + attribute_name + ']" type="checkbox">' +
+    '<img alt="Spinner" id="' + spinner_id + '" class="spinner" src="/images/spinner.gif" />';
+  field_id.html(new_contents);
+  var check_box_id = field_id.find('input');
+  if (field_text == 'true') {
+    check_box_id.attr('checked', 'checked');
+    }
+  var old_attr = check_box_id.attr('checked');
+  check_box_id.change(function() {
+    var spinner = field_id.find('#' + spinner_id);
+    spinner.show();
+    $.ajax({
+      'url': url + record_id,
+      'type': 'post',
+      'dataType': 'json',
+      'data': '_method=put&' + class_name + '[' + attribute_name + ']=' +
+        check_box_id.attr('checked'),
+      'success': function(json) {
+        old_attr = check_box_id.attr('checked');
+        clearAJAXFlash();
+      },
+      'error': function(req) {
+        check_box_id.attr('checked', old_attr);
+        insertAJAXFlash('error', req.responseText);
+      },
+      'complete': function() {
+        spinner.hide();
+      }
+    });
+  });
+}
+
+function ajax_select(selector, class_name, attribute_name, options, include_blank, url) {
+  var model_id = $(selector).attr('id'); // issue_22
+  var record_id = model_id.match(/_(\d+)$/)[1]; // 22
+  var field_id = $('#' + model_id + '_' + attribute_name); // issue_22_status
+  var field_text = field_id.text(); // eg. New, Open, Closed
+  var spinner_id = 'spinner_' + global_spinner_counter++;
+  var new_contents = '<select id="' + class_name + '_' + attribute_name + '" name="' + class_name + '[' + attribute_name + ']">';
+  if (include_blank) {
+    new_contents = new_contents + '<option value=""></option>';
+  }
+  var selection_found = false;
+  for (var i = 0; i < options.length; i++) {
+    if (field_text == options[i][0]) {
+      new_contents = new_contents + '<option value="' + options[i][1] + '" selected="selected">' + options[i][0] + '</option>';
+      selection_found = true;
+    } else {
+      new_contents = new_contents + '<option value="' + options[i][1] + '">' + options[i][0] + '</option>';
+    }
+  }
+  new_contents = new_contents + '</select><img alt="Spinner" id="' + spinner_id + '" class= "spinner" src="/images/spinner.gif" />';
+  field_id.html(new_contents);
+  var select_id = field_id.find('select');
+  if (!selection_found) {
+    if (!include_blank) {
+      alert('failed to find selection');
+      return; // programmer error! bail
+    } else {
+      // if none of the options match, eg "no product", must be the "blank" option
+      select_id.find('option:first').attr('selected', 'selected');
+    }
+  }
+  var old_val = field_id.find('select option:selected').text();
+  select_id.change(function() {
+    var spinner = field_id.find('#' + spinner_id);
+    spinner.show();
+    $.ajax({
+      'url': url + record_id,
+      'type': 'post',
+      'dataType': 'json',
+      'data': '_method=put&' + class_name + '[' + attribute_name + ']=' +
+        select_id.val(),
+      'success': function(json) {
+        old_val = field_id.find('select option:selected').text();
+        clearAJAXFlash();
+      },
+      'error': function(req) {
+        select_id.val(old_val);
+        insertAJAXFlash('error', req.responseText);
+      },
+      'complete': function() {
+        spinner.hide();
+      }
+    });
+  });
+}
+
+function setup_preview_link(options) {
+  $('#preview_link').append('<a href="#"><img src="/images/update.png" alt="refresh" /></a>' +
+    '<img id="preview_spinner" src="/images/spinner.gif" alt="spinner" />');
+  $('#preview_link a').click(function() {
+    $('#preview_spinner').show();
+    var data = [];
+    var max = options['include'].length;
+    for (var i = 0; i < max; i++) {
+      var included = $('#' + options['kind'] + '_' +
+        options['include'][i]).val();
+      data.push(options['include'][i] +'=' + encodeURIComponent(included));
+    }
+    $.ajax({
+      'url': options['url'],
+      'type': 'post',
+      'dataType': 'html',
+      'data': data.join('&'),
+      'success': function(html) {
+        $('#preview').html(html);
+        clearAJAXFlash();
+      },
+      'error': function(req) {
+        insertAJAXFlash('error', req.responseText);
+      },
+      'complete': function() { $('#preview_spinner').hide(); }
+    });
+    return false;
+  });
+}
+
+function observe_field(options) {
+  // defaults
+  var field = options['field'] ||
+    $('#' + options['kind'] + '_' + options['fieldName']);
+  var interval = (options['interval'] || 30) * 1000;
+  var before = options['before'] || function() { $('#preview_spinner').show(); };
+  var success = options['success'] || function(html) {
+    $('#preview').html(html);
+    clearAJAXFlash();
+  };
+  var error = options['error'] || function(req) {
+    insertAJAXFlash('error', req.responseText);
+  };
+  var complete = options['complete'] || function() { $('#preview_spinner').hide(); };
+
+  if (typeof window.observed_field_contents == 'undefined')
+    window.observed_field_contents = {};
+  window.observed_field_contents[field.attr('id')] = field.val();
+  setInterval(function() {
+    var new_content = field.val();
+    var old_content =
+      window.observed_field_contents[field.attr('id')];
+    if (new_content != old_content) {
+      before();
+      var data = options['fieldName'] + '=' + encodeURIComponent(new_content);
+      if (options['include']) {
+        var max = options['include'].length;
+        for (var i = 0; i < max; i++) {
+          var included = $('#' + options['kind'] + '_' +
+            options['include'][i]).val();
+          data += '&' + options['include'][i] +'=' +
+            encodeURIComponent(included);
+        }
+      }
+      $.ajax({
+        'url': options['url'],
+        'type': 'post',
+        'dataType': 'html',
+        'data': data,
+        'success': function(html) { success(html); },
+        'error': function(req) { error(req); },
+        'complete': function() {
+          complete();
+          // regardless of success/failure, only try to submit once
+          window.observed_field_contents[field.attr('id')] = new_content;
+        }
+      });
+    }
+  }, interval);
+}
---
+++ public/javascripts/application.max.js
@@ -0,0 +1,237 @@
+// Copyright 2008-2009 Wincent Colaiuta. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+function getCookie(name) {
+  var cookies = document.cookie.split('; ');
+  for (var i = 0; i < cookies.length; i++) {
+    var c = cookies[i];
+    var pos = c.indexOf('=');
+    var n = c.substring(0, pos);
+    if (n == name)
+      return c.substring(pos + 1);
+  }
+  return null;
+}
+
+// name (String), value (String or null), expiry (seconds or null)
+function setCookie(name, value, expiry) {
+  var cookieString = escape(name) + '=' + escape(value || '') + ';'
+  if (expiry) {
+    var d = new Date();
+    d.setTime(d.getTime() + (1000 * parseInt(expiry)));
+    cookieString = cookieString + ' expires=' + d.toGMTString() + ';'
+  }
+  cookieString = cookieString + ' path=/;'
+  return document.cookie = cookieString;
+}
+
+function deleteCookie(name) {
+  // set value to empty and expiry to yesterday
+  return setCookie(name, null, -86400);
+}
+
+function setUpLoginLogoutLinks()
+{
+  var user = getCookie('user_id');
+  if (user == null || user == '')
+    $('#logout').hide();
+  else
+    $('#login').hide();
+}
+
+function insertFlash(css, msg) {
+  if (msg) {
+    $('#cacheable-flash').append('<div id="' + css + '">' + msg + '</div>');
+  }
+}
+
+function displayCacheableFlash() {
+  var flash = getCookie('flash');
+  if (flash) {
+    flash = unescape(flash).replace(/\+/g, ' ');
+    flash = eval('flash = ' + flash + ';');
+    insertFlash('error', flash.error);
+    insertFlash('notice', flash.notice);
+    deleteCookie('flash');
+  }
+}
+
+function relativizeDates()
+{
+  $('.relative-date').each(function(i) {
+    var result  = this.innerHTML;
+    var now     = new Date;
+    var then    = new Date(result);
+    var dist    = now.getTime() - then.getTime();
+    var months  = new Array('January', 'February',
+        'March', 'April', 'May', 'June', 'July',
+        'August', 'September', 'October', 'November',
+        'December');
+    var seconds = Math.round((dist / 1000));
+    if (seconds < 0) {
+      result = 'in the future';
+    } else if (seconds == 0) {
+      result = 'now';
+    } else if (seconds < 60) {
+      result = 'a few seconds ago';
+    } else if (seconds < 120) {
+      result = 'a minute ago';
+    } else if (seconds < 180) {
+      result = 'a couple of minutes ago';
+    } else if (seconds < 300) { // 5 minutes
+      result = 'a few minutes ago';
+    } else if (seconds < 3600) { // 60 minutes
+      result = Math.round(seconds / 60) + ' minutes ago';
+    } else if (seconds < 7200) {
+      result = 'an hour ago';
+    } else if (seconds < 86400) { // 24 hours
+      result = Math.round(seconds / 3600) + ' hours ago';
+    } else {
+      var days = Math.round(seconds / 86400);
+      if (days == 1) {
+        result = 'yesterday';
+      } else if (days <= 7) {
+        result = days + ' days ago';
+      } else {
+        var weeks = Math.round(days / 7);
+        if (weeks == 1) {
+          result = 'a week ago';
+        } else if (weeks <= 6) {
+          result = weeks + ' weeks ago';
+        } else {
+          // %d %B %Y
+          result = then.getDate() + ' ' + months[then.getMonth()] + ' ' + then.getFullYear();
+        }
+      }
+    }
+    this.title = this.innerHTML;
+    this.innerHTML = result;
+  });
+}
+
+function syntaxHighlight(text, rules)
+{
+  if (!rules)
+    return text;
+
+  var output    = '';
+  while (text != '') {
+    // iterate through rules, leftmost match wins
+    // in case of tie, first matching rule wins (earlier rules have priority)
+    var leftmost  = null;
+    var apply     = null;
+    for (var rule in rules) {
+      var match = text.match(rules[rule]);
+      if (match && ((leftmost && match.index < leftmost) || !leftmost)) {
+        leftmost    = match.index;
+        match.rule  = rule;
+        apply       = match;
+        if (leftmost == 0)
+          break;
+      }
+    }
+    if (apply) {
+      var start_span = '<span class="' + apply.rule + '-syntax">';
+      var end_span = '</span>';
+      if (apply.rule.match(/^skip-/)) {
+        start_span = '';
+        end_span = '';
+      }
+      output = output + text.substring(0, apply.index) + start_span + apply[0] + end_span;
+      text = text.substring(apply.index + apply[0].length);
+    }
+    else {
+      output = output + text;
+      break;
+    }
+  }
+  return output;
+}
+
+function stylePreBlocks()
+{
+  var styles = {};
+  styles['c-syntax'] = {
+    'preprocessor':     /^\s*#.*/m, // without m (multi-line) mode, ^ anchors at start of string
+    'comment':          /\/\/.*|\/\*([^\*]|\*(?!\/))*\*\//,
+    'string-literal':   /("(<a .+?a>|[^"\\]|\\.)*"|'([^'\\]|\\.)?')/,
+    'keyword':          /\b(_Bool|bool|char|const|int|long|short|struct|typedef|unsigned|void)\b/,
+    'statement':        /\b(break|case|continue|default|do|else|for|goto|if|return|sizeof|static|switch|while)\b/,
+    'boolean':          /\b(true|false)\b/,
+    'numeric-literal':  /\b(0[xX][a-fA-F0-9]+|\d+(\.\d+f?)?)\b/
+  };
+
+  // Objective-C is based on C with some overrides
+  styles['objc-syntax'] = $.extend({}, styles['c-syntax'], {
+    'keyword': /\b(_Bool|BOOL|bool|char|const|int|long|short|struct|typedef|unsigned|void)\b/,
+    'boolean': /\b(YES|NO|true|false)\b/
+  });
+
+  styles['ruby-syntax'] = {
+    // could almost make these default rules
+    'skip-anchor':      /<a .+?a>/,  // don't mangle HTML tags ("a" tags)
+    'skip-entities':    /&\w+;/,     // or entities
+    'comment':          /#.*/,
+    'string-literal':   /("(<a .+?a>|[^"\\]|\\.)*"|'(<a .+?a>|[^'\\]|\\.)*')/,
+    'keyword':          /\b(begin|break|catch|class|continue|def|else|end|for|if|include|load|module|raise|redo|require|rescue|retry|then|throw|unless|until|while|yield)\b/,
+    'statement':        /\b(private|protected|public)\b/,
+    'boolean':          /\b(true|false)\b/,
+    'constant':         /\b[A-Z][a-zA-Z0-9_]*\b/,
+    'namespace':        /::/, // no CSS for this; just to stop false positives for the symbol rule
+    'identifier':       /(\$|@{1,2})[a-z_][a-zA-Z0-9_]*\b/, // or to be zealous, add: |\b[a-z][a-zA-Z0-9_]*\b
+    'symbol':           /:[a-zA-Z_][a-zA-Z0-9_]*\b/,
+    'numeric-literal':  /\b(0[xX][a-fA-F0-9]+(_[a-fA-F0-9]+)*|0[bB][0-1]+(_[0-1]+)*|[0-9]+(_[0-9]+)*(\.[0-9]+(_[0-9]+)*)?)\b/
+  };
+
+  // syntax coloring for a transcript of a shell session
+  // for syntax coloring of a shell script see "bash-syntax"
+  styles['shell-syntax'] = {
+    'output':           /^[^#\$\n].*/m, // no CSS
+    'constant':         /(\\\n|.)+/m    // the command
+  };
+
+  $("pre[class$='syntax']").each(function(i) {
+    // syntax-highlighting
+    var content = syntaxHighlight(this.innerHTML, styles[$(this).attr('class')]);
+
+    // line-numbering
+    var span = '<span class="line-number"></span>';
+    var lines = content.split('\n');
+    this.innerHTML = span + lines.join('\n' + span);
+  });
+}
+
+function setUpSearchLink() {
+  $('#search-link').click(function () {
+    $('#search_box').toggle().focus();
+    return false;
+  });
+}
+
+$(document).ready(function() {
+  setUpLoginLogoutLinks();
+  setUpSearchLink();
+  displayCacheableFlash();
+  relativizeDates();
+  stylePreBlocks();
+});
