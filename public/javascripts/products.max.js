e2e0d8a80723522c56ab25347ace555c6a6ffa94
Wincent Colaiuta <win@wincent.com>
2009-07-01 11:20:20 +0200

    Initial version of products JavaScript (lightbox implementation)
    At this stage we just superimpose an "expand widget".
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ public/javascripts/products.max.js
@@ -0,0 +1,53 @@
+// Copyright 2009 Wincent Colaiuta. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+/* set up lightbox */
+function lightbox(thumbnail) {
+  /* add "expand" widget */
+  var link = thumbnail.parent();
+  link.wrap('<div class="lightbox-wrapper"></div>');
+  link.prepend('<img class="widget" src="/images/dashboard-expand.png" />');
+
+  /* start preloading image on mouseover */
+
+  /* show lightbox on click */
+  link.click(function() {
+    return false;
+  });
+
+  /*
+   * on click, show spinner and load image (if image not loaded yet)
+   * background will be dark black, semi-transparent
+   * will probably have rounded corners
+   * then zoom out to show image
+   * show close box
+   * clicking on image also closes box
+   * show "title" attribute as caption (white text on transparent dark background?)
+   */
+}
+
+$(document).ready(function() {
+  $('.lightbox').each(function () {
+    lightbox($(this))
+  });
+});
