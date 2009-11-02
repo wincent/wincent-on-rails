875599d9b8228c4f131ef01c23134cfa01678d62
Wincent Colaiuta <win@wincent.com>
2009-11-02 16:51:47 +0100

    Updates for Cucumber 0.4.3
    Seeing as the same files get overwritten each time we update Cucumber,
    at least try to streamline the process by putting custom additions at
    the end of the files.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -1,8 +1,21 @@
 module NavigationHelpers
+  # Maps a name to a path. Used by the
+  #
+  #   When /^I go to (.+)$/ do |page_name|
+  #
+  # step definition in webrat_steps.rb
+  #
   def path_to(page_name)
     case page_name
-    when /the homepage/i
-      root_path
+    
+    when /the home\s?page/
+      '/'
+    
+    # Add more mappings here.
+    # Here is a more fancy example:
+    #
+    #   when /^(.*)'s profile page$/i
+    #     user_profile_path(User.find_by_login($1))
     when /the login page/
       login_path
     when /the wiki index/
--- features/support/webrat.rb
+++ features/support/webrat.rb
@@ -1,7 +1,7 @@
 # truncate all tables (Selenium doesn't use transactional fixtures)
 Wincent::Test::truncate_all_tables
 
-Cucumber::Rails.use_transactional_fixtures
+Cucumber::Rails::World.use_transactional_fixtures = true
 
 Webrat.configure do |config|
   config.mode = :rails
