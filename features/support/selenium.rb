8960a815a7faebb3ad6f24764e03a67e43f37957
Wincent Colaiuta <win@wincent.com>
2009-11-07 17:09:02 +0100

    Fixes for Selenium breakage
    Due to bugs in Firefox _and_ Selenium under Snow Leopard, the old
    Selenium configuration was broken.
    
    As a temporary fix, switch to Safari.
    
    At the same time, disable database truncation, which was also broken in
    the last Cucumber update applied here (to 0.4.3).
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- features/support/selenium.rb
+++ features/support/selenium.rb
@@ -1,6 +1,13 @@
 Webrat.configure do |config|
   config.mode = :selenium
   config.application_port = 3000 # 3001 is the default
+  #config.application_environment = :test
+
+  # forced to switch to Safari due to bugs in Selenium:
+  #   http://jira.openqa.org/browse/SRC-743
+  # and Firefox:
+  #   https://bugzilla.mozilla.org/show_bug.cgi?id=513747
+  config.selenium_browser_key = '*safari'
 
   # connect to already-running Selenium RC instance
   config.selenium_server_address = '127.0.0.1'
@@ -9,5 +16,5 @@ end
 
 Before do
   # Selenium can't use transactional fixtures
-  Wincent::Test::truncate_all_tables
+  #Wincent::Test::truncate_all_tables
 end
