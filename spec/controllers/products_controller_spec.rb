a699c9da708e54be9aa327103a578e8174a396e0
Wincent Colaiuta <win@wincent.com>
2009-11-08 14:24:50 +0100

    Update products controller specs for index redirect
    We are temporarily redirecting to the products index from the old site,
    so update the specs to match.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -14,24 +14,33 @@ describe ProductsController, 'index action' do
     get :index, :protocol => 'https'
   end
 
-  it 'should be successful' do
+  # until all (or at least most) products are set up will just redirect
+  it 'should redirect to the old site' do
     do_get
-    response.should be_success
+    response.should redirect_to('http://wincent.com/a/products/')
   end
 
-  it 'should find all products' do
-    Product.should_receive(:categorized_products)
-    do_get
-  end
+  # temporarily disabled while redirect is in effect
+  if false
+    it 'should be successful' do
+      do_get
+      response.should be_success
+    end
 
-  it 'should assign found products for the view' do
-    Product.stub!(:categorized_products).and_return(@products)
-    do_get
-    assigns[:products].should == @products
-  end
+    it 'should find all products' do
+      Product.should_receive(:categorized_products)
+      do_get
+    end
 
-  it 'should render the index template' do
-    do_get
-    response.should render_template('index')
+    it 'should assign found products for the view' do
+      Product.stub!(:categorized_products).and_return(@products)
+      do_get
+      assigns[:products].should == @products
+    end
+
+    it 'should render the index template' do
+      do_get
+      response.should render_template('index')
+    end
   end
 end
