be5e7d0b7ee49f167f2063c53283739e217904c2
Wincent Colaiuta <win@wincent.com>
2009-11-09 11:24:00 +0100

    Add "markup_type" attribute to Page model
    For most pages we want the precise control offered by HTML markup, but
    for others Wikitext is sufficient and much more convenient.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,13 +1,28 @@
 class Page < ActiveRecord::Base
-  belongs_to            :product
-  validates_presence_of :title
-  validates_presence_of :permalink
-  validates_format_of   :permalink, :with => /\A[a-zA-Z0-9\-]+\z/,
-                        :message => 'must only contain letters, numbers and hyphens'
-  attr_accessible       :title, :permalink, :body, :front
+  module MarkupType
+    HTML      = 0
+    WIKITEXT  = 1
+  end
+
+  belongs_to              :product
+  validates_presence_of   :title
+  validates_presence_of   :permalink
+  validates_format_of     :permalink, :with => /\A[a-zA-Z0-9\-]+\z/,
+                          :message => 'must only contain letters, numbers and hyphens'
+  validates_inclusion_of  :markup_type,
+                          :in => [ MarkupType::HTML, MarkupType::WIKITEXT ]
+  attr_accessible         :title, :permalink, :body, :markup_type, :front
   # TODO: acts_as_searchable :attributes => [:body, :title] (will require HTML tokenization)
 
   def to_param
     self.permalink
+  end
+
+  def html?
+    self.markup_type == MarkupType::HTML
+  end
+
+  def wikitext?
+    self.markup_type == MarkupType::WIKITEXT
   end
 end
--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -8,6 +8,12 @@ describe Page do
   it 'should be valid (saved records)' do
     create_page.should be_valid
   end
+
+  describe 'markup type' do
+    it 'should default to HTML' do
+      Page.new.markup_type.should == Page::MarkupType::HTML
+    end
+  end
 end
 
 describe Page, 'validation' do
@@ -22,6 +28,12 @@ describe Page, 'validation' do
   it 'should allow only letters, numbers and hyphens in the permalink' do
     new_page(:permalink => '%__%').should fail_validation_for(:permalink)
   end
+
+  it 'should allow only known markup types (HTML and Wikitext)' do
+    new_page(:markup_type => 943).should fail_validation_for(:markup_type)
+    new_page(:markup_type => Page::MarkupType::HTML).should_not fail_validation_for(:markup_type)
+    new_page(:markup_type => Page::MarkupType::WIKITEXT).should_not fail_validation_for(:markup_type)
+  end
 end
 
 describe Page, 'accessible attributes' do
@@ -35,6 +47,10 @@ describe Page, 'accessible attributes' do
 
   it 'should allow mass-assignment of the body attribute' do
     create_page.should allow_mass_assignment_of(:body => "<p>baz</p>\n")
+  end
+
+  it 'should allow mass-assignment of the markup_type attribute' do
+    create_page.should allow_mass_assignment_of(:markup_type => Page::MarkupType::WIKITEXT)
   end
 
   it 'should allow mass-assignment of the front attribute' do
