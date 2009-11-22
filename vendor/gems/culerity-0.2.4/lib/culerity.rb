4e82b16a31a01ad724fe7cb44b58c84fa711a9ab
Wincent Colaiuta <win@wincent.com>
2009-11-22 23:22:17 +0100

    Update to Culerity 0.2.4
    This is the "real" 0.2.4, not the post-0.2.3 custom HEAD build that was
    installed as "0.2.4" in commit 07d6354.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- vendor/gems/culerity-0.2.4/lib/culerity.rb
+++ vendor/gems/culerity-0.2.4/lib/culerity.rb
@@ -1,6 +1,12 @@
 require File.dirname(__FILE__) + '/culerity/remote_object_proxy'
 require File.dirname(__FILE__) + '/culerity/remote_browser_proxy'
 
+Symbol.class_eval do
+  def to_proc
+    Proc.new{|object| object.send(self)}
+  end
+end unless :symbol.respond_to?(:to_proc)
+
 module Culerity
 
   module ServerCommands
@@ -16,44 +22,21 @@ module Culerity
 
   def self.run_server
     IO.popen("jruby #{__FILE__}", 'r+').extend(ServerCommands)
-
-    # open the two pipes that were created below
-    # while(!File.exists?("tmp/culerity_in.pipe")) 
-    #   sleep(1)
-    # end
-    # pipe_in = open("tmp/culerity_in.pipe", "w+")
-    # pipe_out = open("tmp/culerity_out.pipe", "r+")
-    
-    
-    
-    # store celerity pid in tmp/culerity_celerity.pid
-    # store server pid in tmp/culerity_rails_server.pid
-    
-    # open named pipes to communicate with celerity_server + return them
   end
   
-  def self.run_rails
-    unless File.exists?("tmp/culerity_rails_server.pid")
+  def self.run_rails(options = {})
+    if defined?(Rails) && !File.exists?("tmp/culerity_rails_server.pid")
       puts "WARNING: Speed up execution by running 'rake culerity:rails:start'"
-      port        = 3001
-      environment = 'culerity_development'
-      puts "Launched rails on :#{port}..."
-      return IO.popen("script/server -e #{environment} -p #{port}", 'r+')
+      port        = options[:port] || 3001
+      environment = options[:environment] || 'culerity_development'
+      rails_server = IO.popen("script/server -e #{environment} -p #{port}", 'r+')
+      sleep 5
+      rails_server
     end
   end
 end
 
 if __FILE__ == $0
-  # `rm tmp/culerity_in.pipe`
-  # `mkfifo tmp/culerity_in.pipe`
-  # `rm tmp/culerity_out.pipe`
-  # `mkfifo tmp/culerity_out.pipe`
-  # 
-  # pipe_in = open("tmp/culerity_in.pipe", "r+")
-  # p pipe_in
-  # p STDIN
-  # pipe_out = open("tmp/culerity_out.pipe", "w+")
-  # 
   require File.dirname(__FILE__) + '/culerity/celerity_server'
   Culerity::CelerityServer.new STDIN, STDOUT
 end
