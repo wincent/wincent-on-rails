3b81dd9a4fcb7de312fe8f206a7012bef7b34aaf
Wincent Colaiuta <win@wincent.com>
2009-11-28 19:17:28 +0100

    Cucumber: make "I am" optional were appropriate
    This allows us to say things like:
    
      When logged in
    
    Instead of:
    
      When I am logged in
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- features/step_definitions/sessions.rb
+++ features/step_definitions/sessions.rb
@@ -9,7 +9,7 @@ When /^I log in as an admin user$/ do
   click_button 'Log in'
 end
 
-When /^I am logged in as an admin user$/ do
+When /^(?:I am |)logged in as an admin user$/ do
   When 'I log in as an admin user'
 end
 
@@ -23,7 +23,7 @@ When /^I log in$/ do
   click_button 'Log in'
 end
 
-When /^I am logged in$/ do
+When /^(?:I am |)logged in$/ do
   When 'I log in'
 end
 
@@ -31,11 +31,11 @@ When /^I log out$/ do
   When 'I go to /logout'
 end
 
-When /^I am logged out$/ do
+When /^(?:I am |)logged out$/ do
   When 'I log out'
   And 'I go to /' # get rid of "Can't log out" flash, if present
 end
 
-When /^I am not logged in$/ do
+When /^(?:I am |)not logged in$/ do
   When 'I am logged out'
 end
