341bf3cd2436dc639b032c869743f388181c8e87
Wincent Colaiuta <win@wincent.com>
2010-01-04 18:47:23 +0100

    Update to Capybara 0.2.0
    Includes a modification to the cucumber "env_additions.rb" file to fix
    breakage caused by Aslak Hellesøy's removal of DatabaseCleaner from the
    Capybara set-up.
    
    Breakage introduced here:
    
      http://github.com/jnicklas/capybara/commit/bb6051078a4ac645f607fd0003b8280f33e8c353
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- features/support/env_additions.rb
+++ features/support/env_additions.rb
@@ -1,6 +1,11 @@
 # custom additions to "env.rb" file
 require 'fixture_replacement'
 include FixtureReplacement
+
 require 'capybara/rails'
 require 'capybara/cucumber'
 Capybara.javascript_driver = :culerity
+
+require 'database_cleaner'
+require 'database_cleaner/cucumber'
+DatabaseCleaner.strategy = :truncation
