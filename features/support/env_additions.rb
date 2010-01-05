42e650ccb0f373ae863e271121c1fee59e5c2493
Wincent Colaiuta <win@wincent.com>
2010-01-05 16:00:18 +0100

    Update to cucumber 0.6.0 and cucumber-rails 0.2.3
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- features/support/env_additions.rb
+++ features/support/env_additions.rb
@@ -1,11 +1,4 @@
 # custom additions to "env.rb" file
 require 'fixture_replacement'
 include FixtureReplacement
-
-require 'capybara/rails'
-require 'capybara/cucumber'
 Capybara.javascript_driver = :culerity
-
-require 'database_cleaner'
-require 'database_cleaner/cucumber'
-DatabaseCleaner.strategy = :truncation
