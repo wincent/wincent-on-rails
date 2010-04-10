1860ba3b1fd9f04317d2c3f309b663123f93a9a7
Wincent Colaiuta <win@wincent.com>
2010-04-10 22:41:49 +0200

    Refactor lightbox JS in preparation for movie support
    Initial shuffling around of code in preparation for the addition of
    movie support. This preserves existing behaviour for image lightboxes
    while laying some groundwork for the not-yet-working movie lightboxes.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- public/javascripts/products.max.js
+++ public/javascripts/products.max.js
@@ -1,4 +1,4 @@
-// Copyright 2009 Wincent Colaiuta. All rights reserved.
+// Copyright 2009-2010 Wincent Colaiuta. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
@@ -23,87 +23,126 @@
 
 var global_pending_lightbox_image = null;
 
-/* set up lightbox */
+// set up lightbox
 function lightbox(thumbnail) {
 
-  /* add "expand" widget */
+  // add "expand" widget
   var link = $(thumbnail).parent();
   link.wrap('<div class="lightbox-wrapper"></div>');
   link.prepend('<img class="widget expand" alt="expand" title="Click to enlarge" src="/images/dashboard-expand.png" />');
 
-  /* start preloading image on mouseenter */
-  link.mouseenter(function() {
-    /* will store the preloaded image as a property in the thumbnail DOM element */
-    if (thumbnail.fullsized)
-        return; /* already been here for this image */
-    var image = $('<img />').attr('src', link.attr('href'));
-    image.thumbnail = $(thumbnail); /* keep reference to "parent" thumbnail */
-    image.attr('title', image.thumbnail.attr('title'));
-    $(image).load(function() {
-      image.loaded = true;
-      if (global_pending_lightbox_image == image) {
-        show_image(image);
-      }
+  // for images, start preloading on mouseenter
+  var href = link.attr('href');
+  var is_image = !href.match(/\.mov(\?.*)?$/);
+  if (is_image) {
+    link.mouseenter(function() {
+      // will store the preloaded image as a property in the thumbnail DOM element
+      if (thumbnail.fullsized)
+          return; // already been here for this image
+      var image = $('<img />').attr('src', href);
+      image.thumbnail = $(thumbnail); // keep reference to "parent" thumbnail
+      image.attr('title', image.thumbnail.attr('title'));
+      $(image).load(function() {
+        image.loaded = true;
+        if (global_pending_lightbox_image == image) {
+          show_image(image);
+        }
+      });
+      thumbnail.fullsized = image;
     });
-    thumbnail.fullsized = image;
-  });
+  }
+  else {
+    // movie dimensions come from query string
+    // or if no query string, from image thumbnail
+    var width = link.width();
+    var height = link.height() + 16; // plus 16 pixels for controls
+    var movie = $('<object/>', {
+      classid: "clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B",
+      codebase: "http://www.apple.com/qtactivex/qtplugin.cab",
+      width: width,
+      height: height
+    })
+    .append($('<param/>', { name: 'src', value: href }))
+    .append($('<param/>', { name: 'autoplay', value: 'true' }))
+    .append($('<embed/>', {
+      src: href,
+      width: width,
+      height: height,
+      type: 'quicktime/video',
+      autoplay: 'true',
+      pluginspage: 'http://www.apple.com/quicktime/download/'
+    }));
+    movie.thumbnail = $(thumbnail); // keep reference to "parent" thumbnail
+    thumbnail.fullsized = movie;
+  }
 
-  /* show lightbox on click */
+  // show lightbox on click
   var click = function(e) {
     disable_expand_widgets();
-    if (e.data.tag.fullsized.loaded)
-      show_image(e.data.tag.fullsized);
-    else {
-      show_spinner();
-      global_pending_lightbox_image = e.data.tag.fullsized;
+    if (is_image) {
+      if (e.data.tag.fullsized.loaded)
+        show_image(e.data.tag.fullsized);
+      else {
+        show_spinner();
+        global_pending_lightbox_image = e.data.tag.fullsized;
+      }
     }
+    else
+      show_movie(e.data.tag.fullsized);
     return false;
   }
   link.bind('click', { tag: thumbnail }, click);
 
+  function show_movie() {
+    show_lightbox(movie);
+  }
+
   function show_image(image) {
-    /* if spinner on screen, hide it */
+    show_lightbox(image);
+  }
+
+  function show_lightbox(content) {
+    // if spinner on screen, hide it
     $('#lightbox-spinner-frame').hide();
     if ($('#lightbox-image-frame').length == 0) {
-      /* add frame to DOM if not present already */
+      // add frame to DOM if not present already
       $('#content').prepend(
         $('<div id="lightbox-image-frame">' +
           '<a href="#" title="Click to dismiss" onclick="return false;">' +
           '<img class="widget close" src="/images/dashboard-close.png" />' +
           '</a>' +
           '<div id="lightbox-caption"></div>' +
-          '</div>').append(image).click(function() {
+          '</div>').append(content).click(function() {
             $('#lightbox-image-frame').fadeOut('def');
             enable_expand_widgets();
           })
       );
     }
     else {
-      /* frame was already present, just have to swap in new image */
+      // frame was already present, just have to swap in new content
       $('#lightbox-image-frame').find('img').not('.widget').remove();
-      $('#lightbox-image-frame').append(image);
+      $('#lightbox-image-frame').append(content);
     }
 
-    /* update caption */
-    $('#lightbox-caption').html(image.attr('title'));
+    // update caption
+    $('#lightbox-caption').html(content.attr('title'));
 
-    /* position lightbox relative to thumbnail before fading it in:
-     * - center horizontally relative to middle of document
-     * - center vertically relative to middle of thumbnail
-     */
-    var thumbnail_offset = image.thumbnail.offset();
-    var anchor_top = thumbnail_offset.top + image.thumbnail.height() / 2;
-    var image_top = anchor_top - (image[0].height / 2);
-    if (image_top + image[0].height + 20 > $(document).height()) /* allow 20px padding */
-      image_top = $(document).height() - (image[0].height + 85);
-    if (image_top < 25)
-      image_top = 25;
-    var left = ($(document).width() / 2) - (image[0].width / 2);
+    // position lightbox relative to thumbnail before fading it in:
+    // - center horizontally relative to middle of document
+    // - center vertically relative to middle of thumbnail
+    var thumbnail_offset = content.thumbnail.offset();
+    var anchor_top = thumbnail_offset.top + content.thumbnail.height() / 2;
+    var content_top = anchor_top - (content[0].height / 2);
+    if (content_top + content[0].height + 20 > $(document).height()) // allow 20px padding
+      content_top = $(document).height() - (content[0].height + 85);
+    if (content_top < 25)
+      content_top = 25;
+    var left = ($(document).width() / 2) - (content[0].width / 2);
     if (left < 25)
       left = 25;
-    $('#lightbox-image-frame').css('top', image_top + 'px').css('left', left + 'px').fadeIn('def');
+    $('#lightbox-image-frame').css('top', content_top + 'px').css('left', left + 'px').fadeIn('def');
 
-    /* if possible, scroll so that lightbox is centered within viewport */
+    // if possible, scroll so that lightbox is centered within viewport
     var w_width = window.innerWidth;
     var w_height = window.innerHeight;
     var w_origin_x = window.pageXOffset;
@@ -113,8 +152,8 @@ function lightbox(thumbnail) {
         typeof w_origin_x == 'undefined' ||
         typeof w_origin_y == 'undefined')
       return;
-    var lightbox_center_x = left + image[0].width / 2;
-    var lightbox_center_y = image_top + image[0].height / 2;
+    var lightbox_center_x = left + content[0].width / 2;
+    var lightbox_center_y = content_top + content[0].height / 2;
     var viewport_center_x = w_origin_x + w_width / 2;
     var viewport_center_y = w_origin_y + w_height / 2;
     w_origin_x -= viewport_center_x - lightbox_center_x;
@@ -135,7 +174,7 @@ function lightbox(thumbnail) {
 
   function show_spinner() {
     if ($('#lightbox-spinner-frame').length == 0)
-      /* don't add spinner more than once */
+      // don't add spinner more than once
       $(document.body).append($('<div id="lightbox-spinner-frame">' +
         '<img id="lightbox-spinner" alt="spinner" src="/images/spinner-large.gif" />' +
         '</div>'));
