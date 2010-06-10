f913179433a039217ac88e67c67f3b72745806fa
Wincent Colaiuta <win@wincent.com>
2010-06-10 13:27:57 +0200

    Haml: explicitly set up to emit HTML5
    It appears that Haml is already automatically emitting HTML5 under Rails
    3, but be explicit about it anyway, by dropping the
    
      <?xml version="1.0" encoding="UTF-8"?>
    
    tag, which is not needed in normal (text/html) HTML5 documents, and also
    switching the Haml "format" option to :html5.
    
    See
    
      http://dev.w3.org/html5/html4-differences/
    
    for info on the differences between HTML4 and HTML5, as well as the XML
    version of HTML5 documents.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- config/initializers/gems.rb
+++ config/initializers/gems.rb
@@ -2,5 +2,6 @@ require 'wikitext/rails'
 
 Wikitext::Parser.shared_parser.img_prefix = '/system/images/'
 Haml::Template::options[:ugly] = true
+Haml::Tempalte::options[:format] = :html5
 Sass::Plugin.options[:style] = :compact
 Sass::Plugin.options[:template_location] = "#{Rails.root}/app/views/sass"
