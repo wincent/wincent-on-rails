05461165cdb59ec1aef82f615c41e64b3ec8c1b3
Wincent Colaiuta <win@wincent.com>
2010-06-14 09:09:08 +0200

    JsController: return 404 for non-existent templates
    Previously a request for a non-existent template would cause an HTTP 500
    status code (internal server error due to do ActionView::MissingTemplate
    exception).
    
    We don't want to produce exception notifications for this kind of thing,
    so instead check for the existence of the template and return a 404 if
    not found.
    
    We have 3 alternatives here for how to render the 404.
    
      (1) render :file => "#{Rails.root}/public/404.html", :status => 404
    
      (2) raise ActiveRecord::RecordNotFound
    
      (3) render :text => '', :status => 404
    
    Here the third option is the most appropriate, just like it is for
    missing feeds, because this controller should supposedly only be serving
    legitimate machine-originated requests (ie. other pages on the site
    requesting JavaScript) rather than end-user-originated requests.
    
    For this reason the second option is completely inappropriate (it ends
    up redirecting to the main index and showing a flash error), and the
    first option is better but still goes too far (no need to show an entire
    404 error page).
    
    Tested using:
    
      curl -k -i -H 'Accept: */*' https://localhost:3002/js/issues/create.js
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/controllers/js_controller.rb
+++ app/controllers/js_controller.rb
@@ -1,18 +1,30 @@
 class JsController < ApplicationController
+  before_filter :get_template_path_from_params
+
   # don't embed JS in default application layout (HTML)
   layout false
 
   def show
     respond_to do |format|
-      format.js { render template_path_from_params }
+      format.js {
+        if template_exists?
+          render @template
+        else
+          render :text => '', :status => 404
+        end
+      }
     end
   end
 
 private
 
-  def template_path_from_params
+  def get_template_path_from_params
     # no need to sanitize as router ensures param is of format:
     #   ([a-z_]+/)+[a-z_]+\.js
-    "js/#{params[:delegated]}.erb"
+    @template = "js/#{params[:delegated]}.erb"
+  end
+
+  def template_exists?
+    (Rails.root + 'app' + 'views' + @template).exist?
   end
 end
