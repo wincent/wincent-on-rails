5f3eb955226113b91d3be9a817e0b7ec03a73bf4
Wincent Colaiuta <win@wincent.com>
2010-06-15 23:47:46 +0200

    Fix for sessions spec
    As noted in the comments here, getting a working products#index action
    is a very high priority right now.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/acceptance/sessions_spec.rb
+++ spec/acceptance/sessions_spec.rb
@@ -1,6 +1,14 @@
 require File.dirname(__FILE__) + '/acceptance_helper'
 
 feature 'Logging in to the site' do
+  before :each do
+    # BUG: routing assumes that /products/synergy exists
+    # if we visit "/" without creating the product first we get a
+    # 404 and an infinite redirection loop (back to "/")
+    # getting a real products#index action finished is a high priority
+    Product.make! :permalink => 'synergy'
+  end
+
   scenario 'dynamic "log in"/"log out" links (with no JavaScript)' do
     visit '/'
     page.should have_content('log in')
