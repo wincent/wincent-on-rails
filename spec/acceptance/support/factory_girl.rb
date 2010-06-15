17ad26b654bb3cae412b4ed1dd5d25192d561b34
Wincent Colaiuta <win@wincent.com>
2010-06-15 20:15:18 +0200

    Extend Factory Girl wrapper methods
    Add "stub" and "attributes_for" wrappers, and refactor to avoid
    repetition now that we have 4 wrapper methods.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/acceptance/support/factory_girl.rb
+++ spec/acceptance/support/factory_girl.rb
@@ -1,13 +1,38 @@
 # Convenience methods added to invoke Factory Girl factories by sending
 # messages directly to ActiveRecord classes.
+#
+# We use these rather than the one provided by Factory Girl itself
+# (factory_girl/syntax/make) because we want wrappers for all four methods,
+# not just "create", and we also want the option of customising the returned
+# instance via a block.
 class ActiveRecord::Base
-  def self.make attributes = {}, &block
-    instance = Factory.build to_s.underscore.to_sym, attributes
-    block_given? ? yield(instance) : instance
+  module FactoryGirl
+    module Forwarder
+      def self.delegate method, model, attributes, &block
+        instance = Factory.send method, model.to_s.underscore.to_sym, attributes
+        yield instance if block_given?
+        instance
+      end
+    end
   end
 
+  # Wrapper for Factory.build
+  def self.make attributes = {}, &block
+    FactoryGirl::Forwarder.delegate :build, self, attributes, &block
+  end
+
+  # Wrapper for Factory.create
   def self.make! attributes = {}, &block
-    instance = Factory.create to_s.underscore.to_sym, attributes
-    block_given? ? yield(instance) : instance
+    FactoryGirl::Forwarder.delegate :create, self, attributes, &block
+  end
+
+  # Wrapper for Factory.attributes_for
+  def self.valid_attributes attributes = {}, &block
+    FactoryGirl::Forwarder.delegate :attributes_for, self, attributes, &block
+  end
+
+  # Wrapper for Factory.stub
+  def self.stub attributes = {}, &block
+    FactoryGirl::Forwarder.delegate :stub, self, attributes, &block
   end
 end
