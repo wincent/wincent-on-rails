a7f8ba385ecd954765042d451e1e930f461fcca7
Wincent Colaiuta <win@wincent.com>
2010-06-21 11:10:27 +0200

    Capybara: use CSS selectors by default
    Can switch to XPath only for specific cases where CSS won't cut it.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/acceptance/support/capybara.rb
+++ spec/acceptance/support/capybara.rb
@@ -2,6 +2,10 @@ require 'capybara/rails'
 
 Capybara.javascript_driver = :culerity
 
+# Capybara might make this the default soon
+# See: http://groups.google.com/group/ruby-capybara/browse_thread/thread/c336f52ee28f9910
+Capybara.default_selector = :css
+
 RSpec.configure do |config|
   config.include Capybara
 end
