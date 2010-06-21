b97d0d353c117755b5177f0485907ea484de367a
Wincent Colaiuta <win@wincent.com>
2010-06-22 00:10:10 +0200

    Add regression spec for bug fixed in 76968b7
    As noted, unforunately one of the scenarios has to be marked as pending
    for now, as it exposes a bug in Capybara/Culerity.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ spec/acceptance/regressions/76968b7_spec.rb
@@ -0,0 +1,25 @@
+require File.expand_path('../acceptance_helper', File.dirname(__FILE__))
+
+# Bug fixed in commit 76968b7.
+feature 'wiki article comments' do
+  before :each do
+    Article.make! :title => 'has <strange> stuff'
+                         #/wiki/has_%3Cstrange%3E_stuff
+    @article_path     = '/wiki/has_%3Cstrange%3E_stuff'
+    @comment_path     = '/wiki/has_%3Cstrange%3E_stuff/comments'
+    @new_comment_path = '/wiki/has_%3Cstrange%3E_stuff/comments/new'
+  end
+
+  scenario 'a title with "strange" characters (AJAX)', :js => true do
+    pending # Capybara gets a 404 when using js (Culerity) here
+    visit @article_path
+    page.should_not have_css("form[action='#{@comment_path}']")
+    click 'add a comment' # form pulled down via AJAX
+    page.should have_css("form[action='#{@comment_path}']")
+  end
+
+  scenario 'a title with "strange" characters (no JavaScript)' do
+    visit @new_comment_path
+    page.should have_css("form[action='#{@comment_path}']")
+  end
+end
