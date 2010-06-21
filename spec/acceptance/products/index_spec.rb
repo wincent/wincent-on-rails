efffa819b4b5b0bd3e029dcc471eaa81c873d2e6
Wincent Colaiuta <win@wincent.com>
2010-06-21 11:10:12 +0200

    Add spec for hidden products on front page
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/acceptance/products/index_spec.rb
+++ spec/acceptance/products/index_spec.rb
@@ -8,4 +8,12 @@ feature 'Products index' do
     page.should have_content(p1.name)
     page.should have_content(p2.name)
   end
+
+  scenario 'visiting the index with a hidden product' do
+    p1 = Product.make! :hide_from_front_page => true
+    p2 = Product.make! :hide_from_front_page => false
+    visit '/products'
+    page.should_not have_content(p1.name)
+    page.should have_content(p2.name)
+  end
 end
