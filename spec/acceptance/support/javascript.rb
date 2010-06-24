7f881f24611f83836f3aad57efce8f9cf47a6a10
Wincent Colaiuta <win@wincent.com>
2010-06-24 12:24:24 +0200

    Use RSpec 2.0.0 beta 13 "example.options"
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/acceptance/support/javascript.rb
+++ spec/acceptance/support/javascript.rb
@@ -9,13 +9,13 @@
 #  end
 RSpec.configure do |config|
   config.before :each do
-    if self.running_example.instance_variable_get(:@options)[:js]
+    if example.options[:js]
       Capybara.current_driver = :culerity
     end
   end
 
   config.after :each do
-    if self.running_example.instance_variable_get(:@options)[:js]
+    if example.options[:js]
       Capybara.use_default_driver
     end
   end
