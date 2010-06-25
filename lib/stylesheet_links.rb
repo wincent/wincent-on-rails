24033c5b9ee09b955a7c93e06dd47d3edda1a25a
Wincent Colaiuta <win@wincent.com>
2010-06-25 21:20:51 +0200

    Add methods for automatic insertion of stylesheet tags
    This gives us a declarative DSL to be used in controllers to declare
    which actions should have controller-specific stylesheet tags inserted
    into the "head" of their layout.
    
    This saves us repeating the same stylesheet code on all the views for a
    controller.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ lib/stylesheet_links.rb
@@ -0,0 +1,47 @@
+module ActionController
+  module StylesheetLinks
+    def self.included base
+      base.extend(ClassMethods)
+    end
+
+    module ClassMethods
+      def uses_stylesheet_links options = {}
+        unless respond_to? :uses_stylesheet_links?
+          class_eval <<-END
+            def self.uses_stylesheet_links?; true; end
+          END
+        end
+
+        if options[:only]
+          unless respond_to? :included_stylesheet_link_actions
+            class_eval <<-END
+              @@included_stylesheet_link_actions = []
+              def self.included_stylesheet_link_actions
+                @@included_stylesheet_link_actions
+              end
+            END
+          end
+          [options[:only]].flatten.each do |inclusion|
+            included_stylesheet_link_actions << inclusion
+          end
+        end
+
+        if options[:except]
+          unless respond_to? :excluded_stylesheet_link_actions
+            class_eval <<-END
+              @@excluded_stylesheet_link_actions = []
+              def self.excluded_stylesheet_link_actions
+                @@excluded_stylesheet_link_actions
+              end
+            END
+          end
+          [options[:except]].flatten.each do |exclusion|
+            excluded_stylesheet_link_actions << exclusion
+          end
+        end
+      end
+    end
+  end # module StylesheetLinks
+end # module ActionController
+
+ActionController::Base.class_eval { include ActionController::StylesheetLinks }
