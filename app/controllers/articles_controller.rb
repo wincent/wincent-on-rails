b52e6204b44e8f36f6ab4c2f28edb75951a7b5a0
Wincent Colaiuta <win@wincent.com>
2010-06-27 13:09:03 +0200

    Add missing uses_stylesheet_links omitted from previous commit
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,7 @@ class ArticlesController < ApplicationController
   before_filter     :get_article,     :only => [ :show, :edit, :update ]
   caches_page       :index, :show,    :if => Proc.new { |c| c.send(:is_atom?) }
   cache_sweeper     :article_sweeper, :only => [ :create, :update, :destroy ]
+  uses_stylesheet_links
 
   def index
     respond_to do |format|
