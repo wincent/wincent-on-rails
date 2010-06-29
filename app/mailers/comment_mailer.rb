8eb72338c9f3efccfa66a7cbb6001842486c40c9
Wincent Colaiuta <win@wincent.com>
2010-06-29 13:09:59 +0200

    Use mail method in comment mailer
    It already works without this, but this represents "the way" to set up
    mailers in Rails 3 (all the documentation suggests this).
    
    The reason it works anyway is that when you send:
    
      CommentMailer.new_comment_alert
    
    Note that you are sending a class method which doesn't exist. Rails
    catches this, calls the instance method, and creates the Mail instance
    if you didn't do it explicitly.
    
    Best to create it explicitly, though, because it seems that the
    "default" settings don't apply if you rely on Rails to create the
    instance.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -1,20 +1,23 @@
 class CommentMailer < ActionMailer::Base
   def new_comment_alert comment
-    subject(subject_header = "new comment alert from #{APP_CONFIG['host']}")
+    subject_header    = "new comment alert from #{APP_CONFIG['host']}"
+    to_header         = APP_CONFIG['admin_email']
+    from_header       = APP_CONFIG['support_email']
     @comment          = comment
     @comment_url      = comment_url(comment)
     @edit_comment_url = edit_comment_url(comment)
     @moderation_url   = admin_dashboard_url
-    recipients(to_header = APP_CONFIG['admin_email'])
-    from(from_header = APP_CONFIG['support_email'])
-    sent_on     Time.now
     headers 'Message-ID' => (message_id_header = SupportMailer.new_message_id),
             'return-path' => from_header
-    Message.create  :related => comment,
-                    :message_id_header => message_id_header,
-                    :to_header => to_header,
-                    :from_header => from_header,
-                    :subject_header => subject_header,
-                    :incoming => false
+    Message.create  :related            => comment,
+                    :message_id_header  => message_id_header,
+                    :to_header          => to_header,
+                    :from_header        => from_header,
+                    :subject_header     => subject_header,
+                    :incoming           => false
+    mail  :subject  => subject_header,
+          :to       => to_header,
+          :from     => from_header,
+          :date     => Time.now
   end
 end
