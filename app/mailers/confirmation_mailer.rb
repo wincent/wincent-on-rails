836a45854c3f9d9846b93dbcc7e91205b451a97f
Wincent Colaiuta <win@wincent.com>
2010-06-29 23:27:36 +0200

    Silence more Rails 3 deprecation warnings
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/mailers/confirmation_mailer.rb
+++ app/mailers/confirmation_mailer.rb
@@ -1,11 +1,11 @@
 class ConfirmationMailer < ActionMailer::Base
   def confirmation_message confirmation
     subject(subject_header = 'wincent.com requests that you confirm your email address')
-    body({
-      :address          => confirmation.email.address,
-      :confirmation_url => confirmation_url(confirmation),
-      :cutoff           => confirmation.cutoff.utc
-      })
+
+    @address          = confirmation.email.address
+    @confirmation_url = confirmation_url(confirmation)
+    @cutoff           = confirmation.cutoff.utc
+
     recipients(to_header = confirmation.email.address)
     bcc APP_CONFIG['admin_email']
     from(from_header = APP_CONFIG['support_email'])
