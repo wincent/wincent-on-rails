57723c4c17a325ad145a620595b4738fde85a43d
Wincent Colaiuta <win@wincent.com>
2010-06-29 23:34:05 +0200

    Refactor confirmation mailer using new API
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/mailers/confirmation_mailer.rb
+++ app/mailers/confirmation_mailer.rb
@@ -1,22 +1,24 @@
 class ConfirmationMailer < ActionMailer::Base
+  default :return_path => APP_CONFIG['support_email']
+
   def confirmation_message confirmation
-    subject(subject_header = 'wincent.com requests that you confirm your email address')
+    message = Message.create \
+      :related            => confirmation,
+      :message_id_header  => SupportMailer.new_message_id,
+      :to_header          => confirmation.email.address,
+      :from_header        => APP_CONFIG['support_email'],
+      :subject_header     => 'wincent.com requests that you confirm your email address',
+      :incoming           => false
 
     @address          = confirmation.email.address
     @confirmation_url = confirmation_url(confirmation)
     @cutoff           = confirmation.cutoff.utc
 
-    recipients(to_header = confirmation.email.address)
-    bcc APP_CONFIG['admin_email']
-    from(from_header = APP_CONFIG['support_email'])
-    sent_on Time.now
-    headers 'Message-ID' => (message_id_header = SupportMailer.new_message_id),
-            'return-path' => from_header
-    Message.create  :related => confirmation,
-                    :message_id_header => message_id_header,
-                    :to_header => to_header,
-                    :from_header => from_header,
-                    :subject_header => subject_header,
-                    :incoming => false
+    mail  :subject    => message.subject_header,
+          :to         => message.to_header,
+          :bcc        => APP_CONFIG['admin_email'],
+          :from       => message.from_header,
+          :date       => Time.now,
+          :message_id => message.message_id_header
   end
 end
