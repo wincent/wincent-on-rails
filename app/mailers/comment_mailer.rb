b282c05ea4b40df62d574f504ff93016837d01a6
Wincent Colaiuta <win@wincent.com>
2010-06-29 19:17:56 +0200

    Refactor comments mailer to avoid temporary variables
    This is the same refactoring previously done to the issues mailer.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -2,26 +2,23 @@ class CommentMailer < ActionMailer::Base
   default :return_path => APP_CONFIG['support_email']
 
   def new_comment_alert comment
-    subject_header        = "new comment alert from #{APP_CONFIG['host']}"
-    to_header             = APP_CONFIG['admin_email']
-    from_header           = APP_CONFIG['support_email']
-    headers['Message-Id'] = message_id_header = SupportMailer.new_message_id
+    message = Message.create \
+      :related            => comment,
+      :message_id_header  => SupportMailer.new_message_id,
+      :to_header          => APP_CONFIG['admin_email'],
+      :from_header        => APP_CONFIG['support_email'],
+      :subject_header     => "new comment alert from #{APP_CONFIG['host']}",
+      :incoming           => false
 
     @comment          = comment
     @comment_url      = comment_url(comment)
     @edit_comment_url = edit_comment_url(comment)
     @moderation_url   = admin_dashboard_url
 
-    Message.create  :related            => comment,
-                    :message_id_header  => message_id_header,
-                    :to_header          => to_header,
-                    :from_header        => from_header,
-                    :subject_header     => subject_header,
-                    :incoming           => false
-
-    mail  :subject  => subject_header,
-          :to       => to_header,
-          :from     => from_header,
-          :date     => Time.now
+    mail  :subject    => message.subject_header,
+          :to         => message.to_header,
+          :from       => message.from_header,
+          :date       => Time.now,
+          :message_id => message.message_id_header
   end
 end
