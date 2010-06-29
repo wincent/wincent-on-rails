dcba24bb6daf69f741162e917fa32faf2eabc6d5
Wincent Colaiuta <win@wincent.com>
2010-06-29 18:56:58 +0200

    Refactor issue mailer to eliminate temporary variables
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/mailers/issue_mailer.rb
+++ app/mailers/issue_mailer.rb
@@ -2,25 +2,23 @@ class IssueMailer < ActionMailer::Base
   default :return_path => APP_CONFIG['support_email']
 
   def new_issue_alert issue
-    subject_header        = "new issue alert from #{APP_CONFIG['host']}"
-    to_header             = APP_CONFIG['admin_email']
-    from_header           = APP_CONFIG['support_email']
-    headers['Message-Id'] = message_id_header = SupportMailer.new_message_id
+    message = Message.create \
+      :related            => issue,
+      :message_id_header  => SupportMailer.new_message_id,
+      :to_header          => APP_CONFIG['admin_email'],
+      :from_header        => APP_CONFIG['support_email'],
+      :subject_header     => "new issue alert from #{APP_CONFIG['host']}",
+      :incoming           => false
+
+    headers['Message-Id'] = message.message_id_header
 
     @issue          = issue
     @issue_url      = edit_issue_url(issue)
     @moderation_url = admin_dashboard_url
 
-    Message.create  :related => issue,
-                    :message_id_header => message_id_header,
-                    :to_header => to_header,
-                    :from_header => from_header,
-                    :subject_header => subject_header,
-                    :incoming => false
-
-    mail  :subject  => subject_header,
-          :to       => to_header,
-          :from     => from_header,
+    mail  :subject  => message.subject_header,
+          :to       => message.to_header,
+          :from     => message.from_header,
           :date     => Time.now
   end
 end
