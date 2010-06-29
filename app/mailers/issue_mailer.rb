50a24590585ea473b27d6bff7b316edd6ee136c2
Wincent Colaiuta <win@wincent.com>
2010-06-29 18:52:24 +0200

    Update issue mailer to use new API
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/mailers/issue_mailer.rb
+++ app/mailers/issue_mailer.rb
@@ -1,19 +1,26 @@
 class IssueMailer < ActionMailer::Base
+  default :return_path => APP_CONFIG['support_email']
+
   def new_issue_alert issue
-    subject(subject_header = "new issue alert from #{APP_CONFIG['host']}")
+    subject_header        = "new issue alert from #{APP_CONFIG['host']}"
+    to_header             = APP_CONFIG['admin_email']
+    from_header           = APP_CONFIG['support_email']
+    headers['Message-Id'] = message_id_header = SupportMailer.new_message_id
+
     @issue          = issue
     @issue_url      = edit_issue_url(issue)
     @moderation_url = admin_dashboard_url
-    recipients(to_header = APP_CONFIG['admin_email'])
-    from(from_header = APP_CONFIG['support_email'])
-    sent_on Time.now
-    headers 'Message-ID' => (message_id_header = SupportMailer.new_message_id),
-            'return-path' => from_header
+
     Message.create  :related => issue,
                     :message_id_header => message_id_header,
                     :to_header => to_header,
                     :from_header => from_header,
                     :subject_header => subject_header,
                     :incoming => false
+
+    mail  :subject  => subject_header,
+          :to       => to_header,
+          :from     => from_header,
+          :date     => Time.now
   end
 end
