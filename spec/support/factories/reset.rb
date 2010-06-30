84f2091769605edf28ebd6655d151b44c2f2514b
Wincent Colaiuta <win@wincent.com>
2010-06-30 17:02:36 +0200

    Add reset factory
    This works, but I think we've discovered some code-smell here due to the
    denormalized (duplicated) fields here (storing both "email_id" and
    "user_id", when the user is already accessible via "email.user").
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ spec/support/factories/reset.rb
@@ -0,0 +1,12 @@
+Factory.define (:reset) do |r|
+  r.association :email
+
+  r.after_build do |reset|
+    # BUG: unnecessarily denormalized here
+    # (we store user needlessly, seeing as we store email already)
+    reset.user = reset.email.user
+
+    # not a real column in the database, just for verification purposes
+    reset.email_address = reset.email.address
+  end
+end
