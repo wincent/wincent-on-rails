ae3a0d85268d9a37d0aebabd4a77df088a775238
Wincent Colaiuta <win@wincent.com>
2010-06-30 20:27:45 +0200

    Add lorem_ipsum generator
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -19,6 +19,19 @@ Sham.random_last_name do |n|
   names[rand(length)]
 end
 
+Sham.lorem_ipsum do |n|
+  text = <<-TEXT
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+    est laborum.
+  TEXT
+  text.gsub(/\s+/, ' ').strip
+end
+
 # Convenience methods added to invoke Factory Girl factories by sending
 # messages directly to ActiveRecord classes.
 #
