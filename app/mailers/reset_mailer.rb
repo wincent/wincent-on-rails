4705b75331dd7565b0539cd1933f29c10e0a4bc1
Wincent Colaiuta <win@wincent.com>
2010-06-30 19:38:21 +0200

    Convert reset mailer to use new Rails 3 API
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/mailers/reset_mailer.rb
+++ app/mailers/reset_mailer.rb
@@ -1,21 +1,24 @@
 class ResetMailer < ActionMailer::Base
+  default :return_path => APP_CONFIG['support_email']
+
   def reset_message reset
-    address = reset.email.address
-    subject(subject_header = 'wincent.com forgotten passphrase helper')
-    @address          = address
-    @reset_url        = edit_reset_url(reset) # will provide user with a form to change the passphrase
-    @cutoff           = reset.cutoff.utc
-    recipients(to_header = address)
-    bcc APP_CONFIG['admin_email']
-    from(from_header = APP_CONFIG['support_email'])
-    sent_on Time.now
-    headers 'Message-ID' => (message_id_header = SupportMailer.new_message_id),
-            'return-path' => from_header
-    Message.create  :related => reset,
-                    :message_id_header => message_id_header,
-                    :to_header => to_header,
-                    :from_header => from_header,
-                    :subject_header => subject_header,
-                    :incoming => false
+    message = Message.create \
+      :related            => reset,
+      :message_id_header  => SupportMailer.new_message_id,
+      :to_header          => reset.email.address,
+      :from_header        => APP_CONFIG['support_email'],
+      :subject_header     => 'wincent.com forgotten passphrase helper',
+      :incoming           => false
+
+    @address    = message.to_header
+    @reset_url  = edit_reset_url(reset) # shows form to change passphrase
+    @cutoff     = reset.cutoff.utc
+
+    mail  :subject    => message.subject_header,
+          :to         => message.to_header,
+          :bcc        => APP_CONFIG['admin_email'],
+          :from       => message.from_header,
+          :date       => Time.now,
+          :message_id => message.message_id_header
   end
 end
