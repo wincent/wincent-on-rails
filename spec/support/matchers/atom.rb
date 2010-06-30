f846203dc385d9d5a8c2b334ada6afe3598a8066
Wincent Colaiuta <win@wincent.com>
2010-06-30 22:20:16 +0200

    Fix custom matcher breakage caused by the move to RSpec 2
    Matchers must now live under RSpec::Matchers instead of the previous
    namespace, Spec::Rails::Matchers.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/support/matchers/atom.rb
+++ spec/support/matchers/atom.rb
@@ -2,76 +2,74 @@ require 'mkdtemp'
 require 'pathname'
 
 # https://wincent.com/wiki/Testing_validity_of_Rails_feeds_with_RSpec
-module Spec
-  module Rails
-    module Matchers
-      module Atom
-        # don't really like hardcoding this path,
-        # but "whereis" behaves differently on Mac OS X and Linux,
-        # making it hard to use
-        def self.java_path
-          '/usr/bin/java'
-        end
-
-        def self.has_java?
-          File.exist? java_path
-        end
-
-        def self.jing_path
-          Pathname.new(__FILE__).dirname + 'jing' + 'bin' + 'jing.jar'
-        end
-
-        def self.schema_path
-          # RNG schema created from RNC schema with:
-          # java -jar trang.jar atom.rnc atom.rng
-          Pathname.new(__FILE__).dirname + 'atom-latest.rng'
-        end
-
-        # writes the contents of the rendered feed to a temporary file, returning the full path to the file
-        # (necessary because Jing can only read from files, not from the standard input)
-        def self.write_to_temp_file rendered
-          temp_dir  = Pathname.new(Dir.mkdtemp)
-          temp_file = temp_dir + 'feed.atom'
-          temp_file.open 'w' do |f|
-            f.write rendered
-          end
-          temp_file
-        end
-      end # module Atom
-
-      def can_validate_feeds?
-        Spec::Rails::Matchers::Atom.has_java?
+module RSpec
+  module Matchers
+    module Atom
+      # don't really like hardcoding this path,
+      # but "whereis" behaves differently on Mac OS X and Linux,
+      # making it hard to use
+      def self.java_path
+        '/usr/bin/java'
       end
 
-      # allows us to do:
-      # render('foo').should be_valid_atom
-      def be_valid_atom
-        BeValidAtom.new
+      def self.has_java?
+        File.exist? java_path
       end
 
-      class BeValidAtom
-        def matches? string
-          # java -jar jing.jar atom.rng feed.atom
-          java    = Spec::Rails::Matchers::Atom.java_path
-          jing    = Spec::Rails::Matchers::Atom.jing_path
-          schema  = Spec::Rails::Matchers::Atom.schema_path
-          @path   = Spec::Rails::Matchers::Atom.write_to_temp_file string
-          `#{java} -jar #{jing} #{schema} #{@path}`
-          $?.exitstatus == 0
-        end
+      def self.jing_path
+        Pathname.new(__FILE__).dirname + 'jing' + 'bin' + 'jing.jar'
+      end
 
-        def failure_message
-          "expected feed #{@path} to pass Atom validation but it failed"
-        end
+      def self.schema_path
+        # RNG schema created from RNC schema with:
+        # java -jar trang.jar atom.rnc atom.rng
+        Pathname.new(__FILE__).dirname + 'atom-latest.rng'
+      end
 
-        def negative_failure_message
-          "expected feed #{@path} to fail Atom validation but it passed"
+      # writes the contents of the rendered feed to a temporary file, returning the full path to the file
+      # (necessary because Jing can only read from files, not from the standard input)
+      def self.write_to_temp_file rendered
+        temp_dir  = Pathname.new(Dir.mkdtemp)
+        temp_file = temp_dir + 'feed.atom'
+        temp_file.open 'w' do |f|
+          f.write rendered
         end
+        temp_file
+      end
+    end # module Atom
 
-        def description
-          'pass Atom validation'
-        end
-      end # class BeValidAtom
-    end # module Matchers
-  end # module Rails
-end # module Spec
+    def can_validate_feeds?
+      Spec::Rails::Matchers::Atom.has_java?
+    end
+
+    # allows us to do:
+    # render('foo').should be_valid_atom
+    def be_valid_atom
+      BeValidAtom.new
+    end
+
+    class BeValidAtom
+      def matches? string
+        # java -jar jing.jar atom.rng feed.atom
+        java    = Spec::Rails::Matchers::Atom.java_path
+        jing    = Spec::Rails::Matchers::Atom.jing_path
+        schema  = Spec::Rails::Matchers::Atom.schema_path
+        @path   = Spec::Rails::Matchers::Atom.write_to_temp_file string
+        `#{java} -jar #{jing} #{schema} #{@path}`
+        $?.exitstatus == 0
+      end
+
+      def failure_message
+        "expected feed #{@path} to pass Atom validation but it failed"
+      end
+
+      def negative_failure_message
+        "expected feed #{@path} to fail Atom validation but it passed"
+      end
+
+      def description
+        'pass Atom validation'
+      end
+    end # class BeValidAtom
+  end # module Matchers
+end # module RSpec
--- spec/support/matchers/validation.rb
+++ spec/support/matchers/validation.rb
@@ -1,40 +1,38 @@
-module Spec
-  module Rails
-    module Matchers
-      class FailValidationFor # :nodoc:
-        def initialize attribute
-          @attribute = attribute
-        end
-
-        def matches? model
-          @model = model
-          !@model.valid? and !@model.errors.on(@attribute).nil?
-        end
-
-        def failure_message
-          "expected to fail validation with errors on #{@attribute} but was #{self.valid}; #{self.errors}"
-        end
-
-        def negative_failure_message
-          "expected to pass validation with no errors on #{@attribute} but was #{self.valid}; #{self.errors}"
-        end
-
-        def description
-          "fail validation for attribute #{@attribute}"
-        end
-
-        def valid
-          if @model.valid? then 'valid' else 'invalid' end
-        end
-
-        def errors
-          if @model.valid? then 'no errors' else @model.errors.full_messages.to_sentence end
-        end
-      end # class FailValidationFor
-
-      def fail_validation_for attribute
-        FailValidationFor.new attribute
+module RSpec
+  module Matchers
+    class FailValidationFor # :nodoc:
+      def initialize attribute
+        @attribute = attribute
       end
-    end # module Matchers
-  end # module Rails
-end # module Spec
+
+      def matches? model
+        @model = model
+        !@model.valid? and !@model.errors.on(@attribute).nil?
+      end
+
+      def failure_message
+        "expected to fail validation with errors on #{@attribute} but was #{self.valid}; #{self.errors}"
+      end
+
+      def negative_failure_message
+        "expected to pass validation with no errors on #{@attribute} but was #{self.valid}; #{self.errors}"
+      end
+
+      def description
+        "fail validation for attribute #{@attribute}"
+      end
+
+      def valid
+        if @model.valid? then 'valid' else 'invalid' end
+      end
+
+      def errors
+        if @model.valid? then 'no errors' else @model.errors.full_messages.to_sentence end
+      end
+    end # class FailValidationFor
+
+    def fail_validation_for attribute
+      FailValidationFor.new attribute
+    end
+  end # module Matchers
+end # module RSpec
