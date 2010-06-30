f0841f344ef0c0e7b1403cfa96cd0ee37428fbbf
Wincent Colaiuta <win@wincent.com>
2010-06-30 23:56:29 +0200

    Add validations to monitorship model
    We already have database-level constraints, but we should provide
    validations as well because it is preferable to return "false" from a
    "save()" call rather than throwing an exception.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/models/monitorship.rb
+++ app/models/monitorship.rb
@@ -1,6 +1,10 @@
 class Monitorship < ActiveRecord::Base
   belongs_to      :user
   belongs_to      :monitorable, :polymorphic => true
+
+  validates_presence_of :user
+  validates_presence_of :monitorable
+
   attr_accessor   :nothing
   attr_accessible :nothing
 end
