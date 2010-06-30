425f11caddf05c2d72fb61f3764385e3ddd99765
Wincent Colaiuta <win@wincent.com>
2010-06-30 21:10:07 +0200

    Convert topic mailer to new API
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/mailers/topic_mailer.rb
+++ app/mailers/topic_mailer.rb
@@ -1,19 +1,23 @@
 class TopicMailer < ActionMailer::Base
+  default :return_path => APP_CONFIG['support_email']
+
   def new_topic_alert topic
-    subject(subject_header = "new topic alert from #{APP_CONFIG['host']}")
+    message = Message.create \
+      :related            => topic,
+      :message_id_header  => SupportMailer.new_message_id,
+      :to_header          => APP_CONFIG['admin_email'],
+      :from_header        => APP_CONFIG['support_email'],
+      :subject_header     => "new topic alert from #{APP_CONFIG['host']}",
+      :incoming           => false
+
     @topic          = topic
     @topic_url      = edit_forum_topic_url(topic.forum, topic)
     @moderation_url = admin_dashboard_url
-    recipients(to_header = APP_CONFIG['admin_email'])
-    from(from_header = APP_CONFIG['support_email'])
-    sent_on Time.now
-    headers 'Message-ID' => (message_id_header = SupportMailer.new_message_id),
-            'return-path' => from_header
-    Message.create  :related => topic,
-                    :message_id_header => message_id_header,
-                    :to_header => to_header,
-                    :from_header => from_header,
-                    :subject_header => subject_header,
-                    :incoming => false
+
+    mail  :subject    => message.subject_header,
+          :to         => message.to_header,
+          :from       => message.from_header,
+          :date       => Time.now,
+          :message_id => message.message_id_header
   end
 end
