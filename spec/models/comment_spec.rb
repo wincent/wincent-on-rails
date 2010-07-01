41e8ba0ebee6cd8b1165464fc2df0d71510a223a
Wincent Colaiuta <win@wincent.com>
2010-07-01 19:18:35 +0200

    Update comment specs to use Factory Girl
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path('../spec_helper', File.dirname(__FILE__))
 
 describe Comment do
   it 'should be valid' do
-    create_comment.should be_valid
+    Comment.make!.should be_valid
   end
 
   # we test a value larger than the default MySQL TEXT size (65535)
@@ -10,7 +10,7 @@ describe Comment do
     # make sure the long body survives the round-trip from the db
     length = 128 * 1024
     long_body = 'x' * length
-    comment = create_comment :body => long_body
+    comment = Comment.make! :body => long_body
     comment.body.length.should == length
     comment.reload
     comment.body.length.should == length
@@ -19,7 +19,7 @@ end
 
 describe Comment, 'acting as classifiable ("moderate_as_ham!" method)' do
   before do
-    @object = create_comment :awaiting_moderation => true
+    @object = Comment.make! :awaiting_moderation => true
   end
 
   it_should_behave_like 'ActiveRecord::Acts::Classifiable "moderate_as_ham!" method'
@@ -27,18 +27,18 @@ end
 
 describe Comment, 'validating the body' do
   it 'should require it to be present' do
-    new_comment(:body => nil).should fail_validation_for(:body)
+    Comment.make(:body => nil).should fail_validation_for(:body)
   end
 
   it 'should complain if longer than 128k' do
     long_body = 'x' * (128 * 1024 + 100)
-    new_comment(:body => long_body).should fail_validation_for(:body)
+    Comment.make(:body => long_body).should fail_validation_for(:body)
   end
 end
 
 describe Comment, '"send_new_comment_alert" method' do
   before do
-    @comment = new_comment :user => (create_user :superuser => false)
+    @comment = Comment.make :user => (User.make! :superuser => false)
   end
 
   it 'should fire after saving new records' do
@@ -58,13 +58,13 @@ describe Comment, '"send_new_comment_alert" method' do
   end
 
   it 'should deliver a new comment alert for anonymous comments' do
-    comment = new_comment :user => nil
+    comment = Comment.make :user => nil
     CommentMailer.should_receive(:deliver_new_comment_alert).with(comment)
     comment.save
   end
 
   it 'should not send comment alerts for superuser comments' do
-    comment = new_comment :user => (create_user :superuser => true)
+    comment = Comment.make :user => (User.make! :superuser => true)
     CommentMailer.should_not_receive(:deliver_new_comment_alert)
     comment.save
   end
