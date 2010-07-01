1c47b3a16ac16a53d0bd79d94a4a6d6e8a07f7ea
Wincent Colaiuta <win@wincent.com>
2010-07-01 21:10:58 +0200

    Update user specs (FixtureReplacement -> Factory Girl)
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,34 +24,34 @@ end
 
 describe User, 'setting a passphrase' do
   it 'should generate a new salt' do
-    u = new_user
+    u = User.make
     old_salt = u.passphrase_salt
-    u.passphrase = FR::random_string
+    u.passphrase = Sham.random
     u.passphrase_salt.should_not == old_salt
   end
 
   it 'should generate a new hash' do
-    u = new_user
+    u = User.make
     old_hash = u.passphrase_hash
-    u.passphrase = FR::random_string
+    u.passphrase = Sham.random
     u.passphrase_hash.should_not == old_hash
   end
 end
 
 describe User, 'authenticating' do
   before do
-    @email      = "#{FR::random_string}@example.com"
-    @passphrase = FR::random_string
-    @user       = create_user :passphrase => @passphrase, :passphrase_confirmation => @passphrase
+    @email      = "#{Sham.random}@example.com"
+    @passphrase = Sham.random
+    @user       = User.make! :passphrase => @passphrase, :passphrase_confirmation => @passphrase
     @user.emails.create :address => @email
   end
 
   it 'should return nil if invalid email address' do
-    User.authenticate(FR::random_string, @passphrase).should be_nil
+    User.authenticate(Sham.random, @passphrase).should be_nil
   end
 
   it 'should return nil if invalid passphrase' do
-    User.authenticate(@email, FR::random_string).should be_nil
+    User.authenticate(@email, Sham.random).should be_nil
   end
 
   it 'should return the user if valid email and passphrase' do
@@ -61,86 +61,86 @@ end
 
 describe User, 'accessible attributes' do
   it 'should allow mass-assignment to the display name' do
-    new_user.should allow_mass_assignment_of(:display_name => FR::random_string)
+    User.make.should allow_mass_assignment_of(:display_name => Sham.random)
   end
 
   it 'should allow mass-assignment to the passphrase (and confirmation)' do
     # have to test these two together otherwise validation fails
-    passphrase = FR::random_string
-    new_user.should allow_mass_assignment_of(:passphrase => passphrase, :passphrase_confirmation => passphrase)
+    passphrase = Sham.random
+    User.make.should allow_mass_assignment_of(:passphrase => passphrase, :passphrase_confirmation => passphrase)
   end
 
   it 'should allow mass-assignment to the old passphrase' do
-    new_user.should allow_mass_assignment_of(:old_passphrase => FR::random_string)
+    User.make.should allow_mass_assignment_of(:old_passphrase => Sham.random)
   end
 end
 
 describe User, 'protected attributes' do
   it 'should deny mass-assignment to the passphrase hash' do
-    new_user.should_not allow_mass_assignment_of(:passphrase_hash => FR::random_string)
+    User.make.should_not allow_mass_assignment_of(:passphrase_hash => Sham.random)
   end
 
   it 'should deny mass-assignment to the passphrase salt' do
-    new_user.should_not allow_mass_assignment_of(:passphrase_salt => FR::random_string)
+    User.make.should_not allow_mass_assignment_of(:passphrase_salt => Sham.random)
   end
 
   it 'should deny mass-assignment to the superuser flag' do
-    new_user.should_not allow_mass_assignment_of(:superuser => true)
+    User.make.should_not allow_mass_assignment_of(:superuser => true)
   end
 
   it 'should deny mass-assignment to the verified flag' do
-    new_user(:verified => false).should_not allow_mass_assignment_of(:verified => true)
+    User.make(:verified => false).should_not allow_mass_assignment_of(:verified => true)
   end
 
   it 'should deny mass-assignment to the suspended flag' do
-    new_user.should_not allow_mass_assignment_of(:suspended => true)
+    User.make.should_not allow_mass_assignment_of(:suspended => true)
   end
 
   it 'should deny mass-assignment to the session key' do
-    new_user.should_not allow_mass_assignment_of(:session_key => FR::random_string)
+    User.make.should_not allow_mass_assignment_of(:session_key => Sham.random)
   end
 
   it 'should deny mass-assignment to the session expiry' do
-    new_user.should_not allow_mass_assignment_of(:session_expiry => Time.now)
+    User.make.should_not allow_mass_assignment_of(:session_expiry => Time.now)
   end
 
   it 'should deny mass-assignment to the deleted at field' do
-    new_user.should_not allow_mass_assignment_of(:deleted_at => Time.now)
+    User.make.should_not allow_mass_assignment_of(:deleted_at => Time.now)
   end
 end
 
 describe User, 'validating the display name' do
   it 'should require it to be unique' do
-    name = FR::random_string
-    create_user(:display_name => name).should be_valid
-    new_user(:display_name => name).should fail_validation_for(:display_name)
+    name = Sham.random
+    User.make!(:display_name => name).should be_valid
+    User.make(:display_name => name).should fail_validation_for(:display_name)
   end
 
   it 'should require it to be at least 3 characters long' do
-    new_user(:display_name => FR::random_string(2)).should fail_validation_for(:display_name)
+    User.make(:display_name => Sham.random[0..1]).should fail_validation_for(:display_name)
   end
 
   it 'should require it to begin with at least 2 letters' do
-    new_user(:display_name => '12345678').should fail_validation_for(:display_name)
-    new_user(:display_name => '__foobar').should fail_validation_for(:display_name)
+    User.make(:display_name => '12345678').should fail_validation_for(:display_name)
+    User.make(:display_name => '__foobar').should fail_validation_for(:display_name)
   end
 
   it 'should disallow trailing spaces' do
-    new_user(:display_name => 'foobar ').should fail_validation_for(:display_name)
+    User.make(:display_name => 'foobar ').should fail_validation_for(:display_name)
   end
 
   it 'should disallow consecutive spaces' do
-    new_user(:display_name => 'foo  bar').should fail_validation_for(:display_name)
+    User.make(:display_name => 'foo  bar').should fail_validation_for(:display_name)
   end
 
   it 'should allow letters, numbers and non-consecutive spaces' do
-    new_user(:display_name => 'foo bar baz9').should be_valid
+    User.make(:display_name => 'foo bar baz9').should be_valid
   end
 
   it 'should disallow all other characters' do
-    new_user(:display_name => 'foo/bar').should fail_validation_for(:display_name)
-    new_user(:display_name => 'foo$bar').should fail_validation_for(:display_name)
-    new_user(:display_name => 'foo#bar').should fail_validation_for(:display_name)
+    User.make(:display_name => 'foo/bar').should fail_validation_for(:display_name)
+    User.make(:display_name => 'foo$bar').should fail_validation_for(:display_name)
+    User.make(:display_name => 'foo#bar').should fail_validation_for(:display_name)
   end
 end
 
@@ -152,31 +152,31 @@ describe User, 'validating the passphrase' do
     # passphrase set on it)
     # for full discussion see:
     #   http://github.com/smtlaissezfaire/fixturereplacement/issues#issue/10
-    user = User.new :display_name => FR::random_string, :passphrase => nil, :passphrase_confirmation => nil
+    user = User.new :display_name => Sham.random, :passphrase => nil, :passphrase_confirmation => nil
     user.should fail_validation_for(:passphrase)
   end
 
   it 'should not require it to be present on non-new records' do
-    u = create_user
+    u = User.make!
     u.passphrase = nil
     u.should be_valid
   end
 
   it 'should require it to be at least 8 characters long' do
-    passphrase = FR::random_string(7)
-    new_user(:passphrase => passphrase, :passphrase_confirmation => passphrase).should fail_validation_for(:passphrase)
+    passphrase = Sham.random[0..6]
+    User.make(:passphrase => passphrase, :passphrase_confirmation => passphrase).should fail_validation_for(:passphrase)
   end
 
   it 'should require it to be confirmed' do
-    user = new_user(:passphrase => FR::random_string, :passphrase_confirmation => FR::random_string)
+    user = User.make(:passphrase => Sham.random, :passphrase_confirmation => Sham.random)
     user.should fail_validation_for(:passphrase)
   end
 
   it 'should require the old password in order to set a new password' do
-    passphrase = FR::random_string
-    u = create_user(:passphrase => passphrase, :passphrase_confirmation => passphrase)
+    passphrase = Sham.random
+    u = User.make!(:passphrase => passphrase, :passphrase_confirmation => passphrase)
     u.should be_valid
-    new_passphrase = FR::random_string
+    new_passphrase = Sham.random
     u.update_attributes(:passphrase => new_passphrase, :passphrase_confirmation => new_passphrase)
     u.should fail_validation_for(:old_passphrase)
   end
