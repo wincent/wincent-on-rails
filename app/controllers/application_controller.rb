816518a8abbd5cbb9c60e02d9df332fe25aadf73
Wincent Colaiuta <win@wincent.com>
2010-07-01 10:40:09 +0200

    Move rescue_action_in_public into middleware
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -109,44 +109,6 @@ protected
     cookies[:flash] = flash_hash.to_json unless flash_hash.blank?
   end
 
-  def rescue_action_in_public exception
-    # From vendor/rails/actionpack/lib/action_controller/rescue.rb:
-    #
-    #  'ActionController::RoutingError'             => :not_found,
-    #  'ActionController::UnknownAction'            => :not_found,
-    #  'ActiveRecord::RecordNotFound'               => :not_found,
-    #  'ActiveRecord::StaleObjectError'             => :conflict,
-    #  'ActiveRecord::RecordInvalid'                => :unprocessable_entity,
-    #  'ActiveRecord::RecordNotSaved'               => :unprocessable_entity,
-    #  'ActionController::MethodNotAllowed'         => :method_not_allowed,
-    #  'ActionController::NotImplemented'           => :not_implemented,
-    #  'ActionController::InvalidAuthenticityToken' => :unprocessable_entity
-    #
-    # All other exceptions will result in a 500 (internal server error)
-    case exception
-    when ActionController::RoutingError
-      # requests like /foo (unknown controller, 404)
-    when ActionController::UnknownAction
-      # requests like /misc/foo (known controller, unknown action, 404)
-    when ActiveRecord::RecordNotFound
-      # strictly speaking, should never get here (RecordNotFound is handled
-      # by the record_not_found method above), but if we did the default 404
-      # page would be fine
-    when ActiveRecord::StaleObjectError,
-         ActiveRecord::RecordInvalid,
-         ActiveRecord::RecordNotSaved,
-         ActionController::MethodNotAllowed,
-         ActionController::NotImplemented,
-         ActionController::InvalidAuthenticityToken
-      # default handling is fine for all of these
-    else
-      # Internal Server Error (500): these are the ones we want to know about
-      # TODO: may later want to rate limit these
-      ExceptionMailer.deliver_exception_report exception, self, request
-    end
-    super exception
-  end
-
   # uncomment this method to test what remote users will see
   # when there are errors (must be in production mode)
   #def local_request?
--- lib/exception_reporter.rb
+++ lib/exception_reporter.rb
@@ -7,6 +7,46 @@ class ExceptionReporter
     status, headers, body = @app.call env
     [status, headers, body]
   rescue Exception => e
-    raise e
+    rescue_action_in_public e
+  end
+
+private
+
+  def rescue_action_in_public exception
+    # From vendor/rails/actionpack/lib/action_controller/rescue.rb:
+    #
+    #  'ActionController::RoutingError'             => :not_found,
+    #  'ActionController::UnknownAction'            => :not_found,
+    #  'ActiveRecord::RecordNotFound'               => :not_found,
+    #  'ActiveRecord::StaleObjectError'             => :conflict,
+    #  'ActiveRecord::RecordInvalid'                => :unprocessable_entity,
+    #  'ActiveRecord::RecordNotSaved'               => :unprocessable_entity,
+    #  'ActionController::MethodNotAllowed'         => :method_not_allowed,
+    #  'ActionController::NotImplemented'           => :not_implemented,
+    #  'ActionController::InvalidAuthenticityToken' => :unprocessable_entity
+    #
+    # All other exceptions will result in a 500 (internal server error)
+    case exception
+    when ActionController::RoutingError
+      # requests like /foo (unknown controller, 404)
+    when ActionController::UnknownAction
+      # requests like /misc/foo (known controller, unknown action, 404)
+    when ActiveRecord::RecordNotFound
+      # strictly speaking, should never get here (RecordNotFound is handled
+      # by the record_not_found method above), but if we did the default 404
+      # page would be fine
+    when ActiveRecord::StaleObjectError,
+         ActiveRecord::RecordInvalid,
+         ActiveRecord::RecordNotSaved,
+         ActionController::MethodNotAllowed,
+         ActionController::NotImplemented,
+         ActionController::InvalidAuthenticityToken
+      # default handling is fine for all of these
+    else
+      # Internal Server Error (500): these are the ones we want to know about
+      # TODO: may later want to rate limit these
+      ExceptionMailer.deliver_exception_report exception, self, request
+    end
+    raise exception
   end
 end
