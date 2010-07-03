e854b33f0cf02989fc023d01cfbd50f8cd1c576d
Wincent Colaiuta <win@wincent.com>
2010-07-03 20:52:48 +0200

    Make 'it' strings briefer by dropping 'should'
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,69 +1,69 @@
 require File.expand_path('../spec_helper', File.dirname(__FILE__))
 
 describe Tag, 'name validation' do
-  it 'should require a name to be present' do
+  it 'requires a name to be present' do
     Tag.make(:name => nil).should fail_validation_for(:name)
   end
 
-  it 'should require name to be unique' do
+  it 'requires name to be unique' do
     name = Sham.random
     Tag.make!(:name => name)
     Tag.make(:name => name).should fail_validation_for(:name)
   end
 
-  it 'should compare names in a case-insensitive manner' do
+  it 'compares names in a case-insensitive manner' do
     name = Sham.random
     Tag.make!(:name => name.upcase)
     Tag.make(:name => name.downcase).should fail_validation_for(:name)
   end
 
-  it 'should accept names containing only letters' do
+  it 'accepts names containing only letters' do
     Tag.make!(:name => 'foobar').should be_valid
   end
 
-  it 'should accept names consisting of multiple words separated by a period' do
+  it 'accepts names consisting of multiple words separated by a period' do
     Tag.make!(:name => 'foo.bar').should be_valid
     Tag.make!(:name => 'foo.bar.baz').should be_valid
   end
 
-  it 'should accept names containing numbers' do
+  it 'accepts names containing numbers' do
     Tag.make(:name => 'foo100').should be_valid
   end
 
-  it 'should reject names containing spaces' do
+  it 'rejects names containing spaces' do
     Tag.make(:name => 'foo bar').should fail_validation_for(:name)
     Tag.make(:name => 'foo bar baz').should fail_validation_for(:name)
   end
 
-  it 'should reject names starting with leading periods' do
+  it 'rejects names starting with leading periods' do
     Tag.make(:name => '.foo').should fail_validation_for(:name)
     Tag.make(:name => '..foo').should fail_validation_for(:name)
   end
 
-  it 'should reject names ending with trailing periods' do
+  it 'rejects names ending with trailing periods' do
     Tag.make(:name => 'foo.').should fail_validation_for(:name)
     Tag.make(:name => 'foo..').should fail_validation_for(:name)
   end
 
-  it 'should reject names containing consecutive periods' do
+  it 'rejects names containing consecutive periods' do
     Tag.make(:name => 'foo..bar').should fail_validation_for(:name)
     Tag.make(:name => 'foo...bar').should fail_validation_for(:name)
   end
 
-  it 'should reject names containing other punctuation' do
+  it 'rejects names containing other punctuation' do
     Tag.make(:name => 'foo,bar').should fail_validation_for(:name)
     Tag.make(:name => 'foo-bar').should fail_validation_for(:name)
   end
 end
 
 describe Tag, 'name normalization' do
-  it 'should normalize names to lowercase upon creation' do
+  it 'normalizes names to lowercase upon creation' do
     name = Sham.random.upcase
     tag = Tag.make(:name => name)
     tag.name.should == name.downcase
   end
 
-  it 'should normalize names when updating attributes via the accessor' do
+  it 'normalizes names when updating attributes via the accessor' do
     name = Sham.random
     tag = Tag.make!
     tag.name = name.upcase
@@ -100,43 +100,43 @@ describe Tag, 'tags_reachable_from_tag_names method' do
     @crash    = Tag.find_by_name 'crash'
   end
 
-  it 'should return empty array for no tags' do
+  it 'returns empty array for no tags' do
     Tag.tags_reachable_from_tag_names.should == []
   end
 
-  it 'should return empty array for more than 5 tags' do
+  it 'returns empty array for more than 5 tags' do
     Tag.tags_reachable_from_tag_names('foo', 'bar', 'baz', 'bing', 'bong', 'bang').should == []
   end
 
-  it 'should return empty array for non-existent tag (singular)' do
+  it 'returns empty array for non-existent tag (singular)' do
     Tag.tags_reachable_from_tag_names('gibberish').should == []
   end
 
-  it 'should complain if passed nil tag name' do
+  it 'complains if passed nil tag name' do
     lambda { Tag.tags_reachable_from_tag_names(nil) }.should raise_error
   end
 
-  it 'should return empty array for non-existent tags (plural)' do
+  it 'returns empty array for non-existent tags (plural)' do
     Tag.tags_reachable_from_tag_names('gibberish', 'splinkel').should == []
   end
 
-  it 'should return tag objects for "top-level" search' do
+  it 'returns tag objects for "top-level" search' do
     expected = [@updates, @security, @macosx, @ssl]
     Tag.tags_reachable_from_tag_names('nginx').should =~ expected
     Tag.tags_reachable_from_tag_names('ssl').should == [@nginx]
     Tag.tags_reachable_from_tag_names('crash').should == [@security]
   end
 
-  it 'should return tag objects for "second level" search' do
+  it 'returns tag objects for "second level" search' do
     Tag.tags_reachable_from_tag_names('nginx', 'security').should == [@updates]
   end
 
-  it 'should exclude starting tags from returned results' do
+  it 'excludes starting tags from returned results' do
     # tested implicitly above, but test explicitly here anyway
     Tag.tags_reachable_from_tag_names('nginx').should_not include(@nginx)
   end
 
-  it 'should return empty array when no more results' do
+  it 'returns empty array when no more results' do
     Tag.tags_reachable_from_tag_names('nginx', 'security', 'updates').should == []
   end
 end
@@ -170,25 +170,25 @@ describe Tag, 'tags_reachable_from_tags method' do
     @crash    = Tag.find_by_name 'crash'
   end
 
-  it 'should return empty array for no tags' do
+  it 'returns empty array for no tags' do
     Tag.tags_reachable_from_tags.should == []
   end
 
-  it 'should return empty array for more than 5 tags' do
+  it 'returns empty array for more than 5 tags' do
     Tag.tags_reachable_from_tags(Tag.make!, Tag.make!, Tag.make!, Tag.make!, Tag.make!, Tag.make!).should == []
   end
 
-  it 'should return empty array for non-existent tag (singular)' do
+  it 'returns empty array for non-existent tag (singular)' do
     fake_tag    = Tag.make
     fake_tag.id = 78321
     Tag.tags_reachable_from_tags(fake_tag).should == []
   end
 
-  it 'should complain if passed nil tag' do
+  it 'complains if passed nil tag' do
     lambda { Tag.tags_reachable_from_tags(nil) }.should raise_error
   end
 
-  it 'should return empty array for non-existent tags (plural)' do
+  it 'returns empty array for non-existent tags (plural)' do
     fake1     = Tag.make
     fake1.id  = 98712
     fake2     = Tag.make
@@ -196,28 +196,28 @@ describe Tag, 'tags_reachable_from_tags method' do
     Tag.tags_reachable_from_tags(fake1, fake2).should == []
   end
 
-  it 'should return tag objects for "top-level" search' do
+  it 'returns tag objects for "top-level" search' do
     expected = [@updates, @security, @macosx, @ssl]
     Tag.tags_reachable_from_tags(@nginx).should =~ expected
     Tag.tags_reachable_from_tags(@ssl).should == [@nginx]
     Tag.tags_reachable_from_tags(@crash).should == [@security]
   end
 
-  it 'should return tag objects for "second level" search' do
+  it 'returns tag objects for "second level" search' do
     Tag.tags_reachable_from_tags(@nginx, @security).should == [@updates]
   end
 
-  it 'should accept an array rather than a list of parameters' do
+  it 'accepts an array rather than a list of parameters' do
     # this makes things easier for callers
     Tag.tags_reachable_from_tags([@nginx, @security]).should == [@updates]
   end
 
-  it 'should exclude starting tags from returned results' do
+  it 'excludes starting tags from returned results' do
     # tested implicitly above, but test explicitly here anyway
     Tag.tags_reachable_from_tags(@nginx).should_not include(@nginx)
   end
 
-  it 'should return empty array when no more results' do
+  it 'returns empty array when no more results' do
     Tag.tags_reachable_from_tags(@nginx, @security, @updates).should == []
   end
 end
