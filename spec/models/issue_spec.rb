d4a67ab0dfe781863884b296a377e852eb6df2d4
Wincent Colaiuta <win@wincent.com>
2010-07-04 01:07:54 +0200

    Unbreak issue specs
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/models/issue_spec.rb
+++ spec/models/issue_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path('../spec_helper', File.dirname(__FILE__))
 
 describe Issue do
   before do
-    @issue = create_issue
+    @issue = Issue.make!
   end
 
   # we test a value larger than the default MySQL TEXT size (65535)
@@ -10,7 +10,7 @@ describe Issue do
     # make sure the long description survives the round-trip from the db
     length = 128 * 1024
     long_description = 'x' * length
-    issue = create_issue :description => long_description
+    issue = Issue.make! :description => long_description
     issue.description.length.should == length
     issue.reload
     issue.description.length.should == length
@@ -19,13 +19,13 @@ end
 
 describe Issue, 'creation' do
   it 'should default to accepting comments' do
-    new_issue.accepts_comments.should == true
+    Issue.make.accepts_comments.should == true
   end
 end
 
 describe Issue, 'acting as commentable' do
   before do
-    @commentable = create_issue
+    @commentable = Issue.make!
   end
 
   it_should_behave_like 'Commentable'
@@ -34,8 +34,8 @@ end
 
 describe Issue, 'acting as taggable' do
   before do
-    @object     = create_issue
-    @new_object = new_issue
+    @object     = Issue.make!
+    @new_object = Issue.make
   end
 
   it_should_behave_like 'ActiveRecord::Acts::Taggable'
@@ -44,27 +44,27 @@ end
 # :summary, :description, :public, :product_id, :kind
 describe Issue, 'accessible attributes' do
   it 'should allow mass-assignment to the summary' do
-    new_issue.should allow_mass_assignment_of(:summary => FR::random_string)
+    Issue.make.should allow_mass_assignment_of(:summary => Sham.random)
   end
 
   it 'should allow mass-assignment to the description' do
-    new_issue.should allow_mass_assignment_of(:description => FR::random_string)
+    Issue.make.should allow_mass_assignment_of(:description => Sham.random)
   end
 
   it 'should allow mass-assignment to the public attribute' do
-    new_issue(:public => false).should allow_mass_assignment_of(:public => true)
+    Issue.make(:public => false).should allow_mass_assignment_of(:public => true)
   end
 
   it 'should allow mass-assignment to the product_id' do
-    new_issue.should allow_mass_assignment_of(:product_id => create_product.id)
+    Issue.make.should allow_mass_assignment_of(:product_id => Product.make!.id)
   end
 
   it 'should allow mass-assignment to the kind' do
-    new_issue.should allow_mass_assignment_of(:kind => Issue::KIND[:feedback])
+    Issue.make.should allow_mass_assignment_of(:kind => Issue::KIND[:feedback])
   end
 
   it 'should allow mass-assignment to the "status" attribute' do
-    issue = new_issue :status => Issue::STATUS[:closed]
+    issue = Issue.make :status => Issue::STATUS[:closed]
     issue.should allow_mass_assignment_of(:status => Issue::STATUS[:open])
   end
 end
@@ -72,25 +72,25 @@ end
 describe Issue, 'protected attributes' do
   # don't want non-admin users being able to create or assign tags
   it 'should not allow mass-assignment to the "pending tags" attribute' do
-    new_issue.should_not allow_mass_assignment_of(:pending_tags => 'foo bar baz')
+    Issue.make.should_not allow_mass_assignment_of(:pending_tags => 'foo bar baz')
   end
 end
 
 describe Issue, 'validating the description' do
   it 'should not require it to be present' do
-    new_issue(:description => '').should_not fail_validation_for(:description)
+    Issue.make(:description => '').should_not fail_validation_for(:description)
   end
 
   it 'should complain if longer than 128k' do
     long_description = 'x' * (128 * 1024 + 100)
-    issue = new_issue(:description => long_description)
+    issue = Issue.make(:description => long_description)
     issue.should fail_validation_for(:description)
   end
 end
 
 describe Issue, "annotations" do
   before do
-    @issue = create_issue
+    @issue = Issue.make!
   end
 
   it 'should not add an annotation to new records' do
@@ -99,7 +99,7 @@ describe Issue, "annotations" do
 
   it 'should add an annotation for summary changes' do
     old = @issue.summary
-    new = FR::random_string
+    new = Sham.random
     @issue.summary = new
     @issue.save
     body = @issue.comments.first.body
@@ -109,7 +109,7 @@ describe Issue, "annotations" do
   end
 
   it 'should add an annotation for kind changes' do
-    @issue = create_issue :kind => Issue::KIND[:bug]
+    @issue = Issue.make! :kind => Issue::KIND[:bug]
     @issue.kind = Issue::KIND[:feature_request]
     @issue.save
     body = @issue.comments.first.body
@@ -119,7 +119,7 @@ describe Issue, "annotations" do
   end
 
   it 'should add an annotation for status changes' do
-    @issue = create_issue :status => Issue::STATUS[:open]
+    @issue = Issue.make! :status => Issue::STATUS[:open]
     @issue.status = Issue::STATUS[:closed]
     @issue.save
     body = @issue.comments.first.body
@@ -129,7 +129,7 @@ describe Issue, "annotations" do
   end
 
   it 'should add an annotation for public changes' do
-    @issue = create_issue :public => true
+    @issue = Issue.make! :public => true
     @issue.public = false
     @issue.save
     body = @issue.comments.first.body
@@ -139,8 +139,8 @@ describe Issue, "annotations" do
   end
 
   it 'should add an annotation for product changes' do
-    old, new = create_product, create_product
-    @issue = create_issue :product_id => old.id
+    old, new = Product.make!, Product.make!
+    @issue = Issue.make! :product_id => old.id
     @issue.product_id = new.id
     @issue.save
     body = @issue.comments.first.body
@@ -166,7 +166,7 @@ describe Issue, "annotations" do
 
     # summary: old -> new
     old_summary = @issue.summary
-    @issue.summary = new_summary = FR::random_string
+    @issue.summary = new_summary = Sham.random
     @issue.save
     body = @issue.comments.first.body
     body.should =~ /Summary.*changed:/
@@ -179,7 +179,7 @@ describe Issue, "annotations" do
 
   it 'should create anonymous annotations for changes made outside of controller actions' do
     # although in practice we never want to make changes outside of the controller
-    @issue.summary = FR::random_string
+    @issue.summary = Sham.random
     @issue.save
     @issue.comments.first.user_id.should be_nil
   end
@@ -187,45 +187,50 @@ end
 
 describe Issue, '"send_new_issue_alert" method' do
   before do
-    @issue = new_issue :user => (create_user :superuser => false)
+    @issue = Issue.make :user => (User.make! :superuser => false)
   end
 
   it 'should fire after saving new records' do
-    @issue.should_receive(:send_new_issue_alert)
+    mock(@issue).send_new_issue_alert
     @issue.save
   end
 
   it 'should not fire after saving an existing record' do
     @issue.save
-    @issue.should_not_receive(:send_new_issue_alert)
+    do_not_allow(@issue).send_new_issue_alert
     @issue.save
   end
 
   it 'should deliver a new issue alert for normal user issues' do
-    IssueMailer.should_receive(:deliver_new_issue_alert).with(@issue)
+    # BUG: stub! et al don't work on ActionMailer subclasses
+    mock(IssueMailer).new_issue_alert(@issue).stub!.deliver
     @issue.save
   end
 
   it 'should deliver a new issue alert for anonymous issues' do
-    issue = new_issue :user => nil
-    IssueMailer.should_receive(:deliver_new_issue_alert).with(issue)
+    issue = Issue.make :user => nil
+    # BUG: stub! et al don't work on ActionMailer subclasses
+    mock(IssueMailer).new_issue_alert(issue).stub!.deliver
     issue.save
   end
 
   it 'should not send issue alerts for superuser issues' do
-    issue = new_issue :user => (create_user :superuser => true)
-    IssueMailer.should_not_receive(:deliver_new_issue_alert)
+    issue = Issue.make :user => (User.make! :superuser => true)
+    do_not_allow(IssueMailer).new_issue_alert
     issue.save
   end
 
   it 'should rescue exceptions rather than dying' do
-    IssueMailer.should_receive(:deliver_new_issue_alert).and_raise('fatal error!')
+    # BUG: stub! et al don't work on ActionMailer subclasses
+    mock(IssueMailer).new_issue_alert(@issue) { raise 'fatal error!' }
     lambda { @issue.save }.should_not raise_error
   end
 
   it 'should log an error message on failure' do
-    IssueMailer.stub!(:deliver_new_issue_alert).and_raise('fatal error!')
-    @issue.logger.should_receive(:error)
+    # BUG: stub! et al don't work on ActionMailer subclasses
+    stub(IssueMailer).new_issue_alert(@issue) { raise 'fatal error!' }
+    pending 'RR chokes on ActionMailer subclasses'
+    mock(@issue.logger).error(/fatal error!/)
     @issue.save
   end
 end
