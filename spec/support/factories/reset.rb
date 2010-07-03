2c6eeb0434213e3998402fcb44704d9b2b47e473
Wincent Colaiuta <win@wincent.com>
2010-07-03 03:02:48 +0200

    Remove Reset factory kludge
    Setting the email_address attribute was never really needed to produce a
    valid record using the factory.
    
    Reset.make produces an unsaved record, so the validation in question
    does not run.
    
    Reset.make! produces a saved record, but as this is the first time it
    gets saved the validation doesn't run here either (it only runs on
    updates of existing, saved records).
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/support/factories/reset.rb
+++ spec/support/factories/reset.rb
@@ -1,19 +1,3 @@
 Factory.define :reset do |r|
   r.association :email
-  r.email_address :fix_up_in_after_build
-
-  r.after_build do |reset|
-    # this kludge is needed because:
-    #   (1) this is not a real column in the database, but we need to set it up
-    #       for validation purposes
-    #   (2) if we set it up unconditionally then we'll break specs which use
-    #       the factory like this:
-    #         Reset.make :email_address => nil
-    # the only downside of this kludge is that the "valid_attributes" method
-    # will spuriously report ":email_address => :fix_up_in_after_build", but
-    # this seems an acceptable trade-off for clean specs
-    if reset.email_address == :fix_up_in_after_build
-      reset.email_address = reset.email.address
-    end
-  end
 end
