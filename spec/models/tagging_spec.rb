49a1b648e6ed0a4bbd4d527ce57bf8e0c7adbc5a
Wincent Colaiuta <win@wincent.com>
2010-07-03 20:16:22 +0200

    Merge subgroups in tagging specs
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/models/tagging_spec.rb
+++ spec/models/tagging_spec.rb
@@ -4,31 +4,31 @@ describe Tagging do
   it 'should be valid' do
     Tagging.make.should be_valid
   end
-end
 
-describe Tagging, 'grouped_taggables_for_tag method' do
-  it 'should return last-updated models first' do
-    @old_post = Post.make!
-    @new_post = Post.make!
-    @old_post.tag('foo')
-    @new_post.tag('foo')
-    Post.update_all ['created_at = ?, updated_at = ?', 6.days.ago, 5.days.ago],
-      ["id = #{@old_post.id}"]
-    @tag = Tag.find_by_name 'foo'
-    groups = Tagging.grouped_taggables_for_tag(@tag, nil)
-    groups.first.taggables.should == [@new_post, @old_post]
+  describe 'grouped_taggables_for_tag method' do
+    it 'should return last-updated models first' do
+      @old_post = Post.make!
+      @new_post = Post.make!
+      @old_post.tag('foo')
+      @new_post.tag('foo')
+      Post.update_all ['created_at = ?, updated_at = ?', 6.days.ago, 5.days.ago],
+        ["id = #{@old_post.id}"]
+      @tag = Tag.find_by_name 'foo'
+      groups = Tagging.grouped_taggables_for_tag(@tag, nil)
+      groups.first.taggables.should == [@new_post, @old_post]
+    end
   end
-end
 
-describe Tagging, 'grouped_taggables_for_tag_names' do
-  it 'should return last-updated models first' do
-    @old_post = Post.make!
-    @new_post = Post.make!
-    @old_post.tag('foo', 'bar')
-    @new_post.tag('foo', 'bar')
-    Post.update_all ['created_at = ?, updated_at = ?', 6.days.ago, 5.days.ago],
-      ["id = #{@old_post.id}"]
-    groups = Tagging.grouped_taggables_for_tag_names(['foo', 'bar'], nil)
-    groups[1].first.taggables.should == [@new_post, @old_post]
+  describe 'grouped_taggables_for_tag_names method' do
+    it 'should return last-updated models first' do
+      @old_post = Post.make!
+      @new_post = Post.make!
+      @old_post.tag('foo', 'bar')
+      @new_post.tag('foo', 'bar')
+      Post.update_all ['created_at = ?, updated_at = ?', 6.days.ago, 5.days.ago],
+        ["id = #{@old_post.id}"]
+      groups = Tagging.grouped_taggables_for_tag_names(['foo', 'bar'], nil)
+      groups[1].first.taggables.should == [@new_post, @old_post]
+    end
   end
 end
