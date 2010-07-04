3de77ac0d5c74d708fb24ac4ffea26854eefdbe0
Wincent Colaiuta <win@wincent.com>
2010-07-04 12:14:09 +0200

    Unbreak tags helper specs
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/helpers/tags_helper_spec.rb
+++ spec/helpers/tags_helper_spec.rb
@@ -1,23 +1,23 @@
 require File.expand_path('../spec_helper', File.dirname(__FILE__))
 
 describe TagsHelper, 'taggable_link method' do
-  it 'should work with article models' do
-    article = create_article
+  it 'works with article models' do
+    article = Article.make!
     helper.taggable_link(article).should == link_to(article.title, article_path(article))
   end
 
-  it 'should work with issue models' do
-    issue = create_issue
+  it 'works with issue models' do
+    issue = Issue.make!
     helper.taggable_link(issue).should == link_to(issue.summary, issue_path(issue))
   end
 
-  it 'should work with post models' do
-    post = create_post
+  it 'works with post models' do
+    post = Post.make!
     helper.taggable_link(post).should == link_to(post.title, post_path(post))
   end
 
-  it 'should work with topic models' do
-    topic = create_topic
+  it 'works with topic models' do
+    topic = Topic.make!
     helper.taggable_link(topic).should == link_to(topic.title, forum_topic_path(topic.forum, topic))
   end
 end
