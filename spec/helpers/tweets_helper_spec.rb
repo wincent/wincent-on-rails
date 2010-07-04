7372fca1ad8d1a9d842daf5f8167b4da6ae4ab74
Wincent Colaiuta <win@wincent.com>
2010-07-04 12:11:43 +0200

    Unbreak tweet helper specs
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/helpers/tweets_helper_spec.rb
+++ spec/helpers/tweets_helper_spec.rb
@@ -2,20 +2,20 @@ require File.expand_path('../spec_helper', File.dirname(__FILE__))
 
 describe TweetsHelper, '"character_count" method' do
   # character_count calls pluralizing_count, defined in ApplicationHelper
-  helper.extend ApplicationHelper
+  include ApplicationHelper
 
   it 'should pluralize 0-character tweets' do
-    tweet = new_tweet :body => ''
-    helper.character_count(tweet).should == '0 characters'
+    tweet = Tweet.make :body => ''
+    character_count(tweet).should == '0 characters'
   end
 
   it 'should pluralize 1-character tweets' do
-    tweet = new_tweet :body => 'x'
-    helper.character_count(tweet).should == '1 character'
+    tweet = Tweet.make :body => 'x'
+    character_count(tweet).should == '1 character'
   end
 
   it 'should pluralize multi-character tweets' do
-    tweet = new_tweet :body => 'foo bar'
-    helper.character_count(tweet).should == '7 characters'
+    tweet = Tweet.make :body => 'foo bar'
+    character_count(tweet).should == '7 characters'
   end
 end
