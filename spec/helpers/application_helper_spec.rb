8f4f0afc896e1209c473637c565a9615cd8d5089
Wincent Colaiuta <win@wincent.com>
2010-07-04 12:07:23 +0200

    Unbreak application helper specs
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,35 +1,33 @@
 require File.expand_path('../spec_helper', File.dirname(__FILE__))
 
-describe ApplicationHelper do
-end
-
 describe ApplicationHelper, 'timeinfo method' do
   before do
-    @model = mock('model', :created_at => 2.days.ago, :updated_at => 3.days.ago)
+    @model = stub!.created_at { 2.days.ago }.subject
+    stub(@model).updated_at { 3.days.ago }
   end
 
   it 'should get the creation date' do
-    @model.should_receive(:created_at).and_return(Time.now)
+    mock(@model).created_at { Time.now }
     helper.timeinfo @model
   end
 
   it 'should get update date' do
-    @model.should_receive(:updated_at).and_return(Time.now)
+    mock(@model).updated_at { Time.now }
     helper.timeinfo @model
   end
 
   it 'should return just the creation date if update and creation date are the same (exact match)' do
     date = 2.days.ago
-    @model.should_receive(:created_at).and_return(date)
-    @model.should_receive(:updated_at).and_return(date)
+    mock(@model).created_at { date }
+    mock(@model).updated_at { date }
     helper.timeinfo(@model).should =~ /#{Regexp.escape date.to_s}/
   end
 
   it 'should return just the creation date if update and creation date are the same (fuzzy match)' do
     earlier_date  = (2.days + 2.hours).ago
     later_date    = (2.days + 1.hour).ago
-    @model.should_receive(:created_at).and_return(earlier_date)
-    @model.should_receive(:updated_at).and_return(later_date)
+    mock(@model).created_at { earlier_date }
+    mock(@model).updated_at { later_date }
     earlier_date.distance_in_words.should == later_date.distance_in_words # check our assumption about fuzzy equality
     helper.timeinfo(@model).should =~ /#{Regexp.escape earlier_date.to_s}/
   end
@@ -37,8 +35,8 @@ describe ApplicationHelper, 'timeinfo method' do
   it 'should return both creation and edit date if different' do
     earlier_date  = 3.hours.ago
     later_date    = 1.hour.ago
-    @model.should_receive(:created_at).and_return(earlier_date)
-    @model.should_receive(:updated_at).and_return(later_date)
+    mock(@model).created_at { earlier_date }
+    mock(@model).updated_at { later_date }
     earlier_date.distance_in_words.should_not == later_date.distance_in_words # check our assumption about inequality
     info = helper.timeinfo(@model)
     info.should =~ /Created.+#{Regexp.escape earlier_date.to_s}/
@@ -49,8 +47,8 @@ describe ApplicationHelper, 'timeinfo method' do
     # for some model types, it might sound better to say "edited" rather than updated
     earlier_date  = 3.hours.ago
     later_date    = 1.hour.ago
-    @model.should_receive(:created_at).and_return(earlier_date)
-    @model.should_receive(:updated_at).and_return(later_date)
+    mock(@model).created_at { earlier_date }
+    mock(@model).updated_at { later_date }
     info = helper.timeinfo(@model, :updated_string => 'edited')
     info.should =~ /Created.+#{Regexp.escape earlier_date.to_s}/
     info.should =~ /edited.+#{Regexp.escape later_date.to_s}/
@@ -59,8 +57,8 @@ describe ApplicationHelper, 'timeinfo method' do
   it 'should not show the "updated" date at all if "updated_string" is set to false' do
     earlier_date  = 3.hours.ago
     later_date    = 1.hour.ago
-    @model.should_receive(:created_at).and_return(earlier_date)
-    @model.should_receive(:updated_at).and_return(later_date)
+    mock(@model).created_at { earlier_date }
+    mock(@model).updated_at { later_date }
     info = helper.timeinfo @model, :updated_string => false
     info.should =~ /#{Regexp.escape earlier_date.to_s}/
     info.should_not =~ /#{Regexp.escape later_date.to_s}/
@@ -69,14 +67,14 @@ end
 
 describe ApplicationHelper, 'product_options method' do
   it 'should find all products' do
-    Product.should_receive(:categorized)
+    mock(Product).categorized { Hash.new }
     helper.product_options
   end
 
   it 'should return an array of name/id pairs' do
     Product.delete_all
-    product1 = create_product :name => 'foo'
-    product2 = create_product :name => 'bar'
+    product1 = Product.make! :name => 'foo'
+    product2 = Product.make! :name => 'bar'
     helper.product_options.should == [['', [["foo", product1.id], ["bar", product2.id]]]]
   end
 end
@@ -101,14 +99,15 @@ end
 describe ApplicationHelper, '"button_to_destroy_issue" method' do
   include ApplicationHelper
   before do
-    @issue = create_issue
+    @issue = Issue.make!
   end
 
-  # NOTE: rather than testing an internal implementation detail like this, a better way to keep DRY might be to
-  # test the button_to_destroy_model method as a "shared" behaviour, and then test the button_to_destroy_issue method
+  # NOTE: rather than testing an internal implementation detail like this, a
+  # better way to keep DRY might be to test the button_to_destroy_model method
+  # as a "shared" behaviour, and then test the button_to_destroy_issue method
   # using "it_should_behave_like"
   it 'should call the "button_to_destroy_model" method' do
-    should_receive(:button_to_destroy_model).with(@issue, issue_path(@issue))
+    mock(self).button_to_destroy_model(@issue, issue_path(@issue))
     button_to_destroy_issue @issue
   end
 end
@@ -116,11 +115,11 @@ end
 describe ApplicationHelper, '"button_to_moderate_issue_as_ham" method' do
   include ApplicationHelper
   before do
-    @issue = create_issue
+    @issue = Issue.make!
   end
 
   it 'should call the "button_to_moderate_model_as_ham" method' do
-    should_receive(:button_to_moderate_model_as_ham).with(@issue, issue_path(@issue))
+    mock(self).button_to_moderate_model_as_ham(@issue, issue_path(@issue))
     button_to_moderate_issue_as_ham @issue
   end
 end
@@ -129,34 +128,34 @@ describe ApplicationHelper, '"link_to_commentable" method' do
 
   # was a bug
   it 'should adequately escape HTML special characters (Issue summaries)' do
-    issue = create_issue :summary => '<em>foo</em>'
+    issue = Issue.make! :summary => '<em>foo</em>'
     helper.link_to_commentable(issue).should_not =~ /<em>/
   end
 end
 
 describe ApplicationHelper, '"tweet_title" method' do
   it 'should strip HTML tags' do
-    tweet = new_tweet :body => "foo ''bar''"
+    tweet = Tweet.make :body => "foo ''bar''"
     helper.tweet_title(tweet).should =~ /foo bar/
   end
 
   it 'should compress whitespace' do
-    tweet = new_tweet :body => "foo    bar   \n   baz"
+    tweet = Tweet.make :body => "foo    bar   \n   baz"
     helper.tweet_title(tweet).should =~ /foo bar baz/
   end
 
   it 'should remove leading whitespace' do
-    tweet = new_tweet :body => "  foo\n  bar"
+    tweet = Tweet.make :body => "  foo\n  bar"
     helper.tweet_title(tweet).should =~ /\Afoo bar/
   end
 
   it 'should remove trailing whitespace' do
-    tweet = new_tweet :body => "foo  \nbar  "
+    tweet = Tweet.make :body => "foo  \nbar  "
     helper.tweet_title(tweet).should =~ /foo bar\z/
   end
 
   it 'should truncate long text to 80 characters' do
-    tweet = new_tweet :body => 'x' * 100
+    tweet = Tweet.make :body => 'x' * 100
     helper.tweet_title(tweet).length.should == 80
   end
 end
