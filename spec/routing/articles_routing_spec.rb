9b9d09c0266e26b815e6eac25d6b8bcee8f19525
Wincent Colaiuta <win@wincent.com>
2010-07-04 23:29:16 +0200

    More article routing spec clean-up
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/routing/articles_routing_spec.rb
+++ spec/routing/articles_routing_spec.rb
@@ -14,35 +14,30 @@ describe ArticlesController do
       articles_path.should == '/wiki'
     end
 
-    example 'GET /wiki/page/2' do
-      get('/wiki/page/2').should map_to(:controller => 'articles', :action => 'index', :page => '2')
-      paginated_articles_path(:page => 2).should == '/wiki/page/2'
-    end
-
     example 'GET /wiki/new' do
       get('/wiki/new').should   map_to({ :controller => 'articles', :action => 'new' })
       get('/wiki/new').should map_from({ :controller => 'articles', :action => 'new' })
       new_article_path.should == '/wiki/new'
     end
 
-    example 'GET /wiki/Rails_3.0_upgrade_notes' do
+    example 'GET /wiki/:id' do
       get('/wiki/Rails_3.0_upgrade_notes').should   map_to({ :controller => 'articles', :action => 'show', :id => 'Rails_3.0_upgrade_notes' })
       get('/wiki/Rails_3.0_upgrade_notes').should map_from({ :controller => 'articles', :action => 'show', :id => 'Rails_3.0_upgrade_notes' })
       article_path(@article).should == '/wiki/Rails_3.0_upgrade_notes'
     end
 
-    example 'GET /wiki/Rails_3.0_upgrade_notes/edit' do
+    example 'GET /wiki/:id/edit' do
       get('/wiki/Rails_3.0_upgrade_notes/edit').should   map_to({ :controller => 'articles', :action => 'edit', :id => 'Rails_3.0_upgrade_notes' })
       get('/wiki/Rails_3.0_upgrade_notes/edit').should map_from({ :controller => 'articles', :action => 'edit', :id => 'Rails_3.0_upgrade_notes' })
       edit_article_path(@article).should == '/wiki/Rails_3.0_upgrade_notes/edit'
     end
 
-    example 'PUT /wiki/Rails_3.0_upgrade_notes' do
+    example 'PUT /wiki/:id' do
       put('/wiki/Rails_3.0_upgrade_notes').should   map_to({ :controller => 'articles', :action => 'update', :id => 'Rails_3.0_upgrade_notes' })
       put('/wiki/Rails_3.0_upgrade_notes').should map_from({ :controller => 'articles', :action => 'update', :id => 'Rails_3.0_upgrade_notes' })
     end
 
-    example 'DELETE /wiki/Rails_3.0_upgrade_notes' do
+    example 'DELETE /wiki/:id' do
       delete('/wiki/Rails_3.0_upgrade_notes').should   map_to({ :controller => 'articles', :action => 'destroy', :id => 'Rails_3.0_upgrade_notes' })
       delete('/wiki/Rails_3.0_upgrade_notes').should map_from({ :controller => 'articles', :action => 'destroy', :id => 'Rails_3.0_upgrade_notes' })
     end
@@ -51,70 +46,33 @@ describe ArticlesController do
       post('/wiki').should   map_to({ :controller => 'articles', :action => 'create' })
       post('/wiki').should map_from({ :controller => 'articles', :action => 'create' })
     end
-  end
 
-  describe "route recognition" do
-    it "should generate params { :controller => 'articles', :action => 'index', :protocol => 'https' } from GET /wiki" do
-      params_from(:get, "/wiki").should == {:controller => "articles", :action => "index", :protocol => 'https'}
+    describe 'RESTful index pagination' do
+      example 'GET /wiki/page/2' do
+        get('/wiki/page/2').should map_to(:controller => 'articles', :action => 'index', :page => '2')
+        paginated_articles_path(:page => 2).should == '/wiki/page/2'
+      end
+
+      # note how we can still have an article titled "Page"
+      example 'GET /wiki/page' do
+        get('/wiki/page').should   map_to(:controller => 'articles', :action => 'show', :id => 'page')
+        get('/wiki/page').should map_from(:controller => 'articles', :action => 'show', :id => 'page')
+      end
     end
 
-    # Rails 2.3.0 RC1 BUG: trailing slash on resources declared using ":as" raises routing error
-    # See: http://rails.lighthouseapp.com:80/projects/8994/tickets/2039
-    it "should generate params { :controller => 'articles', :action => 'index', :protocol => 'https' } from GET /wiki/" do
-      params_from(:get, '/wiki/').should == {:controller => "articles", :action => "index", :protocol => 'https'}
-    end
+    describe 'regressions' do
+      it 'handles trailing slashes on resources declared using ":as"' do
+        # bug appeared in Rails 2.3.0 RC1; see:
+        #   http://rails.lighthouseapp.com/projects/8994/tickets/2039
+        get('/wiki/').should map_to({ :controller => 'articles', :action => 'index' })
+      end
 
-    it "should generate params { :controller => 'articles', :action => 'new', :protocol => 'https' } from GET /wiki/new" do
-      params_from(:get, "/wiki/new").should == {:controller => "articles", :action => "new", :protocol => 'https'}
-    end
-
-    it "should generate params { :controller => 'articles', :action => 'create', :protocol => 'https' } from POST /wiki" do
-      params_from(:post, "/wiki").should == {:controller => "articles", :action => "create", :protocol => 'https'}
-    end
-
-    it "should generate params { :controller => 'articles', :action => 'show', :id => 'foo', :protocol => 'https' } from GET /wiki/foo" do
-      params_from(:get, "/wiki/foo").should == {:controller => "articles", :action => "show", :id => "foo", :protocol => 'https'}
-    end
-
-    it "should generate params { :controller => 'articles', :action => 'edit', :id => 'foo', :protocol => 'https' } from GET /wiki/foo/edit" do
-      params_from(:get, "/wiki/foo/edit").should == {:controller => "articles", :action => "edit", :id => "foo", :protocol => 'https'}
-    end
-
-    it "should generate params { :controller => 'articles', :action => 'update', :id => 'foo', :protocol => 'https' } from PUT /wiki/foo" do
-      params_from(:put, "/wiki/foo").should == {:controller => "articles", :action => "update", :id => "foo", :protocol => 'https'}
-    end
-
-    it "should generate params { :controller => 'articles', :action => 'destroy', :id => 'foo', :protocol => 'https' } from DELETE /wiki/foo" do
-      params_from(:delete, "/wiki/foo").should == {:controller => "articles", :action => "destroy", :id => "foo", :protocol => 'https'}
-    end
-
-    # test index pagination
-    it "should generate params { :controller => 'articles', :action => 'index', :page => '2', :protocol => 'https' } from GET /wiki/page/2" do
-      params_from(:get, '/wiki/page/2').should == {:controller => 'articles', :action => 'index', :page => '2', :protocol => 'https'}
-    end
-
-    # note how we can still have an article named "page" if we want
-    it "should generate params { :controller => 'articles', :action => 'show', :if => 'page', :protocol => 'https' } from GET /wiki/page" do
-      params_from(:get, '/wiki/page').should == {:controller => 'articles', :action => 'show', :id => 'page', :protocol => 'https'}
-    end
-
-    # see: https://wincent.com/issues/1410
-    it 'should handle comment creation on articles with periods in the title' do
-      expected = {  :controller => 'comments',
-                    :action => 'create',
-                    :article_id => 'foo.bar',
-                    :protocol => 'https' }
-      params_from(:post, '/wiki/foo.bar/comments').should == expected
-    end
-
-  # same bug: requirements (in this case the protocol) don't trickle down when
-  # using nested routes
-    it 'should handle comment creation on articles' do
-      expected = {  :controller => 'comments',
-                    :action => 'create',
-                    :article_id => 'foo',
-                    :protocol => 'https' }
-      params_from(:post, '/wiki/foo/comments').should == expected
+      it 'handles comment creation on articles with periods in the title' do
+        # see: https://wincent.com/issues/1410
+        post('/wiki/foo.bar/comments').should map_to({ :controller => 'comments',
+                                                       :action => 'create',
+                                                       :article_id => 'foo.bar' })
+      end
     end
   end
 end
