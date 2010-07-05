d26cf6b1c7917e85941fc0d202a85f7dc0992c01
Wincent Colaiuta <win@wincent.com>
2010-07-05 19:16:40 +0200

    Bring tag route specs up to date?
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/routing/tags_routing_spec.rb
+++ spec/routing/tags_routing_spec.rb
@@ -1,79 +1,57 @@
 require File.expand_path('../spec_helper', File.dirname(__FILE__))
 
 describe TagsController do
-  describe 'route generation' do
-    it "should map { controller: 'tags', action: 'index', protocol: 'https' } to /tags" do
-      route_for(:controller => "tags", :action => "index", :protocol => 'https').should == "/tags"
+  describe 'routing' do
+    it { get('/tags').should map('tags#index') }
+    it { get('/tags/new').should map('tags#new') }
+    it { get('/tags/foo.bar').should map('tags#show', :id => 'foo.bar') }
+    it { get('/tags/foo.bar/edit').should map('tags#edit', :id => 'foo.bar') }
+    it { put('/tags/foo.bar').should map('tags#update', :id => 'foo.bar') }
+    it { delete('/tags/foo.bar').should map('tags#destroy', :id => 'foo.bar') }
+    it { post('/tags').should map('tags#create') }
+
+    it 'rejects invalid tag names' do
+      pending 'requires change to RSpec be_routable matcher'
+      # be_routeable feeds params into routes.recognize like so:
+      #   routes.recognize(path, method)
+      # where path is determined using
+      #   params.values.first
+      # and method is determined using
+      #   params.keys.first
+      # ie. it expects a hash of format { :get => 'path' }
+      # but we are handing it a hash of { :method => :get, :path => 'path' }
+
+      # only letters, numbers and periods allowed
+      get('/tags/foo.2').should be_routable
+      get('/tags/foo-bar').should_not be_routable
     end
 
-    it "should map { controller: 'tags', action: 'new', protocol: 'https' } to /tags/new" do
-      route_for(:controller => "tags", :action => "new", :protocol => 'https').should == "/tags/new"
+    describe 'non-RESTful routes' do
+      it { get('/tags/search').should map('tags#search') }
     end
 
-    it "should map { controller: 'tags', action: 'show', id: 'foo', protocol: 'https' } to /tags/foo" do
-      route_for(:controller => "tags", :action => "show", :id => 'foo', :protocol => 'https').should == "/tags/foo"
-    end
+    describe 'helpers' do
+      before do
+        # we use an tag with a "tricky" id (containing a period, which is
+        # usually a format separator) to test the routes
+        @tag = Tag.stub :name => 'foo.bar'
+      end
 
-    # tag name including period
-    it "should map { controller: 'tags', action: 'show', id: 'foo.bar', protocol: 'https' } to /tags/foo.bar" do
-      route_for(:controller => "tags", :action => "show", :id => 'foo.bar', :protocol => 'https').should == "/tags/foo.bar"
-    end
+      describe 'tags_path' do
+        it { tags_path.should == '/tags' }
+      end
 
-    # tag name including number
-    it "should map { controller: 'tags', action: 'show', id: 'wopen3', protocol: 'https' } to /tags/wopen3" do
-      route_for(:controller => "tags", :action => "show", :id => 'wopen3', :protocol => 'https').should == "/tags/wopen3"
-    end
+      describe 'new_tag_path' do
+        it { new_tag_path.should == '/tags/new' }
+      end
 
-    it "should map { controller: 'tags', action: 'edit', id: 'foo', protocol: 'https' } to /tags/foo/edit" do
-      route_for(:controller => "tags", :action => "edit", :id => 'foo', :protocol => 'https').should == "/tags/foo/edit"
-    end
+      describe 'tag_path' do
+        it { tag_path(@tag).should == '/tags/foo.bar' }
+      end
 
-    it "should map { controller: 'tags', action: 'update', id: 'foo', protocol: 'https' } to /tags/foo" do
-      route_for(:controller => "tags", :action => "update", :id => 'foo', :protocol => 'https').should == { :path => '/tags/foo', :method => 'put' }
-    end
-
-    it "should map { controller: 'tags', action: 'destroy', id: 'foo', protocol: 'https' } to /tags/foo" do
-      route_for(:controller => "tags", :action => "destroy", :id => 'foo', :protocol => 'https').should == { :path => '/tags/foo', :method => 'delete' }
-    end
-  end
-
-  describe "route recognition" do
-    it "should generate params { controller: 'tags', action: 'index', protocol: 'https' } from GET /tags" do
-      params_from(:get, "/tags").should == {:controller => "tags", :action => "index", :protocol => 'https'}
-    end
-
-    it "should generate params { controller: 'tags', action: 'new', protocol: 'https' } from GET /tags/new" do
-      params_from(:get, "/tags/new").should == {:controller => "tags", :action => "new", :protocol => 'https'}
-    end
-
-    it "should generate params { controller: 'tags', action: 'create', protocol: 'https' } from POST /tags" do
-      params_from(:post, "/tags").should == {:controller => "tags", :action => "create", :protocol => 'https'}
-    end
-
-    it "should generate params { controller: 'tags', action: 'show', id: 'foo', protocol: 'https' } from GET /tags/foo" do
-      params_from(:get, "/tags/foo").should == {:controller => "tags", :action => "show", :id => "foo", :protocol => 'https'}
-    end
-
-    # tag name including period
-    it "should generate params { controller: 'tags', action: 'show', id: 'foo.bar', protocol: 'https' } from GET /tags/foo.bar" do
-      params_from(:get, "/tags/foo.bar").should == {:controller => "tags", :action => "show", :id => "foo.bar", :protocol => 'https'}
-    end
-
-    # tag name including number
-    it "should generate params { controller: 'tags', action: 'show', id: 'wopen3', protocol: 'https' } from GET /tags/wopen3" do
-      params_from(:get, "/tags/wopen3").should == {:controller => "tags", :action => "show", :id => "wopen3", :protocol => 'https'}
-    end
-
-    it "should generate params { controller: 'tags', action: 'edit', id: 'foo', protocol: 'https' } from GET /tags/foo/edit" do
-      params_from(:get, "/tags/foo/edit").should == {:controller => "tags", :action => "edit", :id => "foo", :protocol => 'https'}
-    end
-
-    it "should generate params { controller: 'tags', action: 'update', id: 'foo', protocol: 'https' } from PUT /tags/foo" do
-      params_from(:put, "/tags/foo").should == {:controller => "tags", :action => "update", :id => "foo", :protocol => 'https'}
-    end
-
-    it "should generate params { controller: 'tags', action: 'destroy', id: 'foo', protocol: 'https' } from DELETE /tags/foo" do
-      params_from(:delete, "/tags/foo").should == {:controller => "tags", :action => "destroy", :id => "foo", :protocol => 'https'}
+      describe 'edit_tag_path' do
+        it { edit_tag_path(@tag).should == '/tags/foo.bar/edit' }
+      end
     end
   end
 end
