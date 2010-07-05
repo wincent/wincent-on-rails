009ffd8448c2dcb445b29baeb93e1b998d59ebef
Wincent Colaiuta <win@wincent.com>
2010-07-05 10:46:41 +0200

    Use shorter 'controller#action' syntax in article routing specs
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/routing/articles_routing_spec.rb
+++ spec/routing/articles_routing_spec.rb
@@ -3,41 +3,41 @@ require File.expand_path('../spec_helper', File.dirname(__FILE__))
 describe ArticlesController do
   describe 'routing' do
     example 'GET /wiki' do
-      get('/wiki').should map({ :controller => 'articles', :action => 'index' })
+      get('/wiki').should map('articles#index')
     end
 
     example 'GET /wiki/new' do
-      get('/wiki/new').should map({ :controller => 'articles', :action => 'new' })
+      get('/wiki/new').should map('articles#new')
     end
 
     example 'GET /wiki/:id' do
-      get('/wiki/Rails_3.0_upgrade_notes').should map({ :controller => 'articles', :action => 'show', :id => 'Rails_3.0_upgrade_notes' })
+      get('/wiki/Rails_3.0_upgrade_notes').should map('articles#show', :id => 'Rails_3.0_upgrade_notes')
     end
 
     example 'GET /wiki/:id/edit' do
-      get('/wiki/Rails_3.0_upgrade_notes/edit').should map({ :controller => 'articles', :action => 'edit', :id => 'Rails_3.0_upgrade_notes' })
+      get('/wiki/Rails_3.0_upgrade_notes/edit').should map('articles#edit', :id => 'Rails_3.0_upgrade_notes')
     end
 
     example 'PUT /wiki/:id' do
-      put('/wiki/Rails_3.0_upgrade_notes').should map({ :controller => 'articles', :action => 'update', :id => 'Rails_3.0_upgrade_notes' })
+      put('/wiki/Rails_3.0_upgrade_notes').should map('articles#update', :id => 'Rails_3.0_upgrade_notes')
     end
 
     example 'DELETE /wiki/:id' do
-      delete('/wiki/Rails_3.0_upgrade_notes').should map({ :controller => 'articles', :action => 'destroy', :id => 'Rails_3.0_upgrade_notes' })
+      delete('/wiki/Rails_3.0_upgrade_notes').should map('articles#destroy', :id => 'Rails_3.0_upgrade_notes')
     end
 
     example 'POST /wiki' do
-      post('/wiki').should map({ :controller => 'articles', :action => 'create' })
+      post('/wiki').should map('articles#create')
     end
 
-    describe 'RESTful index pagination' do
+    describe 'index pagination' do
       example 'GET /wiki/page/2' do
-        get('/wiki/page/2').should map_to(:controller => 'articles', :action => 'index', :page => '2')
+        get('/wiki/page/2').should map_to('articles#index', :page => '2')
       end
 
       # note how we can still have an article titled "Page"
       example 'GET /wiki/page' do
-        get('/wiki/page').should map(:controller => 'articles', :action => 'show', :id => 'page')
+        get('/wiki/page').should map('articles#show', :id => 'page')
       end
     end
 
@@ -45,12 +45,12 @@ describe ArticlesController do
       it 'handles trailing slashes on resources declared using ":as"' do
         # bug appeared in Rails 2.3.0 RC1; see:
         #   http://rails.lighthouseapp.com/projects/8994/tickets/2039
-        get('/wiki/').should map_to(:controller => 'articles', :action => 'index')
+        get('/wiki/').should map_to('articles#index')
       end
 
       it 'handles comment creation on articles with periods in the title' do
         # see: https://wincent.com/issues/1410
-        post('/wiki/foo.bar/comments').should map_to(:controller => 'comments', :action => 'create', :article_id => 'foo.bar')
+        post('/wiki/foo.bar/comments').should map_to('comments#create', :article_id => 'foo.bar')
       end
     end
 
