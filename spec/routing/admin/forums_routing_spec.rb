12906d0c0179b61ff0d21d6e17f901fbfeb12e82
Wincent Colaiuta <win@wincent.com>
2010-07-05 22:41:17 +0200

    Finish admin forums routing specs
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/routing/admin/forums_routing_spec.rb
+++ spec/routing/admin/forums_routing_spec.rb
@@ -2,13 +2,29 @@ require File.expand_path('../../spec_helper', File.dirname(__FILE__))
 
 describe Admin::ForumsController do
   describe 'routing' do
-    # NOTE: controller currently only implements #index, #show and #update
+    # controller only implements #index, #show and #update
     specify { get('/admin/forums').should map('admin/forums#index') }
-    specify { get('/admin/forums/new').should map('admin/forums#new') }
     specify { get('/admin/forums/foo').should map('admin/forums#show', :id => 'foo') }
-    specify { get('/admin/forums/foo/edit').should map('admin/forums#edit', :id => 'foo') }
     specify { put('/admin/forums/foo').should map('admin/forums#update', :id => 'foo') }
-    specify { delete('/admin/forums/foo').should map('admin/forums#destroy', :id => 'foo') }
-    specify { post('/admin/forums').should map('admin/forums#create') }
+
+    # the remaining RESTful actions aren't recognized
+    pending { get('/admin/forums/new').should_not be_recognized }
+    pending { get('/admin/forums/foo/edit').should_not be_recognized }
+    pending { delete('/admin/forums/foo').should_not be_recognized }
+    pending { post('/admin/forums').should_not be_recognized }
+
+    describe 'helpers' do
+      before do
+        @forum = Forum.stub :permalink => 'foo'
+      end
+
+      describe 'admin_forums_path' do
+        it { admin_forums_path.should == '/admin/forums' }
+      end
+
+      describe 'admin_forum_path' do
+        it { admin_forum_path(@forum).should == '/admin/forums/foo' }
+      end
+    end
   end
 end
