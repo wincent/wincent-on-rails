fc10cd08b8d68be01f12c117b712f141a87718fe
Wincent Colaiuta <win@wincent.com>
2010-07-05 18:09:23 +0200

    Unbreak link routing specs
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/routing/links_routing_spec.rb
+++ spec/routing/links_routing_spec.rb
@@ -1,71 +1,42 @@
 require File.expand_path('../spec_helper', File.dirname(__FILE__))
 
 describe LinksController do
-  describe "route generation" do
-    it "should map { :controller => 'links', :action => 'index', :protocol => 'https' } to /links" do
-      route_for(:controller => "links", :action => "index", :protocol => 'https').should == "/links"
-    end
+  describe 'routing' do
+    it { get('/links').should map('links#index') }
+    it { get('/links/new').should map('links#new') }
+    it { get('/links/foo').should map('links#show', :id => 'foo') }
+    it { get('/links/foo/edit').should map('links#edit', :id => 'foo') }
+    it { put('/links/foo').should map('links#update', :id => 'foo') }
+    it { delete('/links/foo').should map('links#destroy', :id => 'foo') }
+    it { post('/links').should map('links#create') }
 
-    it "should map { :controller => 'links', :action => 'new', :protocol => 'https' } to /links/new" do
-      route_for(:controller => "links", :action => "new", :protocol => 'https').should == "/links/new"
-    end
+    # shortcut
+    it { get('l/foo').should map_to('links#show', :id => 'foo') }
 
-    it "should map { :controller => 'links', :action => 'show', :id => '1', :protocol => 'https' } to /links/1" do
-      route_for(:controller => "links", :action => "show", :id => '1', :protocol => 'https').should == "/links/1"
-    end
+    describe 'helpers' do
+      before do
+        @link = Link.stub :permalink => 'foo'
+      end
 
-    it "should map { :controller => 'links', :action => 'edit', :id => '1', :protocol => 'https' } to /links/1/edit" do
-      route_for(:controller => "links", :action => "edit", :id => '1', :protocol => 'https').should == "/links/1/edit"
-    end
+      describe 'links_path' do
+        it { links_path.should == '/links' }
+      end
 
-    it "should map { :controller => 'links', :action => 'update', :id => '1', :protocol => 'https'} to /links/1" do
-      route_for(:controller => "links", :action => "update", :id => '1', :protocol => 'https').should == { :path => '/links/1', :method => 'put' }
-    end
+      describe 'new_link_path' do
+        it { new_link_path.should == '/links/new' }
+      end
 
-    it "should map { :controller => 'links', :action => 'destroy', :id => '1', :protocol => 'https'} to /links/1" do
-      route_for(:controller => "links", :action => "destroy", :id => '1', :protocol => 'https').should == { :path => '/links/1', :method => 'delete' }
-    end
-  end
+      describe 'link_path' do
+        it { link_path(@link).should == '/links/foo' }
+      end
 
-  describe "route recognition" do
-    it "should generate params { :controller => 'links', action => 'index', :protocol => 'https' } from GET /links" do
-      params_from(:get, "/links").should == {:controller => "links", :action => "index", :protocol => 'https'}
-    end
+      describe 'edit_link_path' do
+        it { edit_link_path(@link).should == '/links/foo/edit' }
+      end
 
-    it "should generate params { :controller => 'links', action => 'new', :protocol => 'https' } from GET /links/new" do
-      params_from(:get, "/links/new").should == {:controller => "links", :action => "new", :protocol => 'https'}
-    end
-
-    it "should generate params { :controller => 'links', action => 'create', :protocol => 'https' } from POST /links" do
-      params_from(:post, "/links").should == {:controller => "links", :action => "create", :protocol => 'https'}
-    end
-
-    it "should generate params { :controller => 'links', action => 'show', id => '1', :protocol => 'https' } from GET /links/1" do
-      params_from(:get, "/links/1").should == {:controller => "links", :action => "show", :id => "1", :protocol => 'https'}
-    end
-
-    it "should generate params { :controller => 'links', action => 'show', id => 'foo', :protocol => 'https' } from GET /links/foo (permalink)" do
-      params_from(:get, "/links/foo").should == {:controller => "links", :action => "show", :id => "foo", :protocol => 'https'}
-    end
-
-    it "should generate params { :controller => 'links', action => 'show', id => '1', :protocol => 'https' } from GET /l/1 (shortcut)" do
-      params_from(:get, "/l/1").should == {:controller => "links", :action => "show", :id => "1", :protocol => 'https'}
-    end
-
-    it "should generate params { :controller => 'links', action => 'show', id => 'foo', :protocol => 'https' } from GET /l/foo (shortcut, permalink)" do
-      params_from(:get, "/l/foo").should == {:controller => "links", :action => "show", :id => "foo", :protocol => 'https'}
-    end
-
-    it "should generate params { :controller => 'links', action => 'edit', id => '1', :protocol => 'https' } from GET /links/1/edit" do
-      params_from(:get, "/links/1/edit").should == {:controller => "links", :action => "edit", :id => "1", :protocol => 'https'}
-    end
-
-    it "should generate params { :controller => 'links', action => 'update', id => '1', :protocol => 'https' } from PUT /links/1" do
-      params_from(:put, "/links/1").should == {:controller => "links", :action => "update", :id => "1", :protocol => 'https'}
-    end
-
-    it "should generate params { :controller => 'links', action => 'destroy', id => '1', :protocol => 'https' } from DELETE /links/1" do
-      params_from(:delete, "/links/1").should == {:controller => "links", :action => "destroy", :id => "1", :protocol => 'https'}
+      describe 'edit_link_path' do
+        it { edit_link_path(@link).should == '/links/foo/edit' }
+      end
     end
   end
 end
