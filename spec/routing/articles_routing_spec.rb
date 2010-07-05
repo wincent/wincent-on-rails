e18497d3f9d1ebca09815e72688101fe37e9dbdc
Wincent Colaiuta <win@wincent.com>
2010-07-05 08:45:17 +0200

    Separate URL helper specs into a separate block
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/routing/articles_routing_spec.rb
+++ spec/routing/articles_routing_spec.rb
@@ -2,30 +2,20 @@ require File.expand_path('../spec_helper', File.dirname(__FILE__))
 
 describe ArticlesController do
   describe 'routing' do
-    before do
-      # we use an article with "tricky" id (containing a period, which is
-      # usually a format separator) to test the routes
-      @article = Article.make! :title => 'Rails 3.0 upgrade notes'
-    end
-
     example 'GET /wiki' do
       get('/wiki').should map({ :controller => 'articles', :action => 'index' })
-      articles_path.should == '/wiki'
     end
 
     example 'GET /wiki/new' do
       get('/wiki/new').should map({ :controller => 'articles', :action => 'new' })
-      new_article_path.should == '/wiki/new'
     end
 
     example 'GET /wiki/:id' do
       get('/wiki/Rails_3.0_upgrade_notes').should map({ :controller => 'articles', :action => 'show', :id => 'Rails_3.0_upgrade_notes' })
-      article_path(@article).should == '/wiki/Rails_3.0_upgrade_notes'
     end
 
     example 'GET /wiki/:id/edit' do
       get('/wiki/Rails_3.0_upgrade_notes/edit').should map({ :controller => 'articles', :action => 'edit', :id => 'Rails_3.0_upgrade_notes' })
-      edit_article_path(@article).should == '/wiki/Rails_3.0_upgrade_notes/edit'
     end
 
     example 'PUT /wiki/:id' do
@@ -43,7 +33,6 @@ describe ArticlesController do
     describe 'RESTful index pagination' do
       example 'GET /wiki/page/2' do
         get('/wiki/page/2').should map_to(:controller => 'articles', :action => 'index', :page => '2')
-        paginated_articles_path(:page => 2).should == '/wiki/page/2'
       end
 
       # note how we can still have an article titled "Page"
@@ -56,15 +45,27 @@ describe ArticlesController do
       it 'handles trailing slashes on resources declared using ":as"' do
         # bug appeared in Rails 2.3.0 RC1; see:
         #   http://rails.lighthouseapp.com/projects/8994/tickets/2039
-        get('/wiki/').should map_to({ :controller => 'articles', :action => 'index' })
+        get('/wiki/').should map_to(:controller => 'articles', :action => 'index')
       end
 
       it 'handles comment creation on articles with periods in the title' do
         # see: https://wincent.com/issues/1410
-        post('/wiki/foo.bar/comments').should map_to({ :controller => 'comments',
-                                                       :action => 'create',
-                                                       :article_id => 'foo.bar' })
+        post('/wiki/foo.bar/comments').should map_to(:controller => 'comments', :action => 'create', :article_id => 'foo.bar')
       end
+    end
+
+    describe 'helpers' do
+      before do
+        # we use an article with a "tricky" id (containing a period, which is
+        # usually a format separator) to test the routes
+        @article = Article.make! :title => 'Rails 3.0 upgrade notes'
+      end
+
+      it { articles_path.should == '/wiki' }
+      it { new_article_path.should == '/wiki/new' }
+      it { article_path(@article).should == '/wiki/Rails_3.0_upgrade_notes' }
+      it { edit_article_path(@article).should == '/wiki/Rails_3.0_upgrade_notes/edit' }
+      it { paginated_articles_path(:page => 2).should == '/wiki/page/2' }
     end
   end
 end
