4afb3b0b6393ea8c042a0ffd08862584b9c7c95b
Wincent Colaiuta <win@wincent.com>
2010-07-06 18:50:25 +0200

    Flesh out tweets#show specs
    This is probably going too far, but I wanted to see how far I could test
    view logic without the spec getting too crudded up.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/views/tweets/show.html.haml_spec.rb
+++ spec/views/tweets/show.html.haml_spec.rb
@@ -3,54 +3,78 @@ require File.expand_path('../../spec_helper', File.dirname(__FILE__))
 describe 'tweets/show.html.haml' do
   helper ApplicationHelper, TweetsHelper
 
-  def do_render
-    assign :tweet, @tweet
-    assign :comments, @tweet.comments.published
-    render
-  end
-
   before do
     @tweet = Tweet.make! :body => "''hello''"
+    @comments = @tweet.comments.published
+    @comment = Comment.new
   end
 
   it 'includes "ajax.js"' do
     mock(view).javascript_include_tag('ajax')
-    do_render
+    render
   end
 
   it 'shows breadcrumbs' do
     mock(view).breadcrumbs(/Twitter/, /Tweet #/)
-    do_render
+    render
   end
 
   it 'shows the tweet number' do
-    do_render
+    render
     rendered.should contain("Tweet ##{@tweet.id}")
   end
 
   it 'shows the time information for the tweet' do
     mock(view).timeinfo(@tweet)
-    do_render
+    render
   end
 
   it 'shows a by-line' do
-    do_render
+    render
     rendered.should contain("by #{APP_CONFIG['admin_name']}")
   end
 
   it 'shows the tweet body as HTML' do
-    do_render
+    render
     rendered.should have_selector('em', :content => 'hello')
   end
 
   it 'renders the "shared/tags" partial' do
-    pending "can't mock this because do_render() will call render() first"
+    mock.proxy(view).render(anything, anything) # initial render call
     mock(view).render('shared/tags', anything)
-    do_render
+    render
   end
 
   it 'has a link to the tweets index' do
-    do_render
-    rendered.should have_selector('.links a', :href => tweets_path)
+    render
+    rendered.should have_selector('.links a', :href => '/twitter')
+  end
+
+  context 'commenting open' do
+    it 'displays the comment form' do
+      render
+      rendered.should have_selector('#comment-form')
+    end
+
+    it 'has a submit button' do
+      render
+      rendered.should have_selector('#comment-form a', :href => new_tweet_comment_path(@tweet))
+    end
+  end
+
+  context 'commenting closed' do
+    before do
+      @comment = nil
+    end
+
+    it 'does not display a comment form' do
+      render
+      rendered.should_not have_selector('#comment-form')
+    end
+
+    it 'provides a link to the forums' do
+      render
+      rendered.should have_selector('a', :href => '/forums')
+    end
   end
 end
