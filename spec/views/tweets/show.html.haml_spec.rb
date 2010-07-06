384c50778185937730b34bb82e9d12c09237bd80
Wincent Colaiuta <win@wincent.com>
2010-07-06 11:32:33 +0200

    Unbreak tweets#show view specs
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/views/tweets/show.html.haml_spec.rb
+++ spec/views/tweets/show.html.haml_spec.rb
@@ -1,37 +1,43 @@
 require File.expand_path('../../spec_helper', File.dirname(__FILE__))
 
-describe '/tweets/show.html.haml' do
-  include TweetsHelper
-
+describe 'tweets/show.html.haml' do
   def do_render
-    assigns[:tweet]     = @tweet
-    assigns[:comments]  = @tweet.comments.published
-    render '/tweets/show.html.haml'
+    assign :tweet, @tweet
+    assign :comments, @tweet.comments.published
+    render
   end
 
   before do
-    @tweet = create_tweet :body => "''hello''"
+    @tweet = Tweet.make! :body => "''hello''"
+    view.extend TweetsHelper
+    view.extend ApplicationHelper
+  end
+
+  it 'shows breadcrumbs' do
+    mock(view).breadcrumbs(/Twitter/, /Tweet #/)
+    do_render
   end
 
   it 'should show the tweet body as HTML' do
     do_render
-    response.body.should =~ %r{<em>hello</em>}
+    rendered.should match(%r{<em>hello</em>})
   end
 
   it 'should show the time information for the tweet' do
-    template.should_receive(:timeinfo).with(@tweet)
+    mock(view).timeinfo(@tweet)
     do_render
   end
 
   it 'should have an link to the tweets index' do
+    pending 'need replacement for have_tag matcher' # was a wrapper for assert_select, no longer in RSpec
     do_render
-    response.should have_tag('.links') do
+    rendered.should have_tag('.links') do
       with_tag 'a[href=?]', tweets_path
     end
   end
 
   it 'should show the tweet number' do
     do_render
-    response.body.should =~ /Tweet ##{@tweet.id}/
+    rendered.should contain(/Tweet ##{@tweet.id}/)
   end
 end
