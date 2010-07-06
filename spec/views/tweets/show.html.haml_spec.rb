3ab967365474c8951fa3b07f86ded9deaaceb56e
Wincent Colaiuta <win@wincent.com>
2010-07-06 14:35:34 +0200

    Flesh out and tidy up tweets#show view specs
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/views/tweets/show.html.haml_spec.rb
+++ spec/views/tweets/show.html.haml_spec.rb
@@ -13,28 +13,44 @@ describe 'tweets/show.html.haml' do
     view.extend ApplicationHelper
   end
 
+  it 'includes "ajax.js"' do
+    mock(view).javascript_include_tag('ajax')
+    do_render
+  end
+
   it 'shows breadcrumbs' do
     mock(view).breadcrumbs(/Twitter/, /Tweet #/)
     do_render
   end
 
-  it 'should show the tweet body as HTML' do
+  it 'shows the tweet number' do
     do_render
-    rendered.should have_selector('em', :content => 'hello')
+    rendered.should contain("Tweet ##{@tweet.id}")
   end
 
-  it 'should show the time information for the tweet' do
+  it 'shows the time information for the tweet' do
     mock(view).timeinfo(@tweet)
     do_render
   end
 
-  it 'should have a link to the tweets index' do
+  it 'shows a by-line' do
     do_render
-    rendered.should have_selector('.links a', :href => tweets_path)
+    rendered.should contain("by #{APP_CONFIG['admin_name']}")
   end
 
-  it 'should show the tweet number' do
+  it 'shows the tweet body as HTML' do
     do_render
-    rendered.should contain(/Tweet ##{@tweet.id}/)
+    rendered.should have_selector('em', :content => 'hello')
+  end
+
+  it 'renders the "shared/tags" partial' do
+    pending "can't mock this because do_render() will call render() first"
+    mock(view).render('shared/tags', anything)
+    do_render
+  end
+
+  it 'has a link to the tweets index' do
+    do_render
+    rendered.should have_selector('.links a', :href => tweets_path)
   end
 end
