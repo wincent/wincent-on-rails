1af06220684a656588d117e0297c06455b20499a
Wincent Colaiuta <win@wincent.com>
2010-07-06 19:23:05 +0200

    Unbreak tweets#new view specs
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/views/tweets/new.html.haml_spec.rb
+++ spec/views/tweets/new.html.haml_spec.rb
@@ -1,53 +1,48 @@
 require File.expand_path('../../spec_helper', File.dirname(__FILE__))
 
-describe '/tweets/new.html.haml' do
-  include TweetsHelper
-
-  def do_render
-    assigns[:tweet] = @tweet
-    render '/tweets/new.html.haml'
-  end
+describe 'tweets/new.html.haml' do
+  helper ApplicationHelper, TweetsHelper
 
   before do
-    @tweet = new_tweet
+    @tweet = Tweet.new
   end
 
-  it 'should include the "tweets" CSS file' do
-    template.should_receive(:stylesheet_link_tag).with('tweets')
-    do_render
+  it 'includes "ajax.js"' do
+    mock(view).javascript_include_tag('ajax')
+    render
   end
 
-  it 'should display error messages' do
-    template.should_receive(:error_messages_for).with(:tweet)
-    do_render
+  it 'displays error messages' do
+    mock.proxy(view).render(anything, anything) # initial render call
+    mock(view).render('shared/error_messages', anything)
+    stub(view).render(anything) # preview partial
+    render
   end
 
-  it 'should have a form for the tweet' do
-    do_render
-    response.should have_tag("form[action=?][method=post]", tweets_path) do
-      with_tag('textarea[name=?]', 'tweet[body]')
-    end
+  it 'has a form for the tweet' do
+    render
+    rendered.should have_selector('form[method=post]', :action => '/twitter')
   end
 
-  it 'should provide a link to the wikitext cheatsheet' do
-    template.should_receive(:wikitext_cheatsheet)
-    do_render
+  it 'provides a link to the wikitext cheatsheet' do
+    mock(view).wikitext_cheatsheet
+    render
   end
 
-  it 'should have a preview div' do
-    do_render
-    response.should have_tag('#preview')
+  it 'has a preview div' do
+    render
+    rendered.should have_selector('#preview')
   end
 
-  it 'should render the preview partial' do
-    template.should_receive(:render).with('preview.html.haml')
-    do_render
+  it 'renders the preview partial' do
+    stub.proxy(view).render(anything, anything) # initial render call
+    stub(view).render('shared/error_messages', anything)
+    mock(view).render('preview.html.haml')
+    render
   end
 
-  it 'should have an link to the tweets index' do
-    do_render
-    response.should have_tag('.links') do
-      with_tag 'a[href=?]', tweets_path
-    end
+  it 'has a link to the tweets index' do
+    render
+    rendered.should have_selector('.links a', :href => '/twitter')
   end
 end
