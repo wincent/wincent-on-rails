69a8285637848e25355142b9e8be6faa3b8579d5
Wincent Colaiuta <win@wincent.com>
2010-07-08 09:12:29 +0200

    Update controller spec helpers for Factory Girl and RR
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/support/controller_spec_helpers.rb
+++ spec/support/controller_spec_helpers.rb
@@ -1,16 +1,16 @@
 module ControllerSpecHelpers
   def login_as user
     controller.instance_eval { @current_user = user }
-    controller.stub!(:login_before) # don't let the before filter clear the user again
+    stub(controller).login_before # don't let the before filter clear the user again
   end
 
   def login_as_admin
-    controller.instance_eval { @current_user = create_user :superuser => true }
-    controller.stub!(:login_before) # don't let the before filter clear the user again
+    controller.instance_eval { @current_user = User.make! :superuser => true }
+    stub(controller).login_before # don't let the before filter clear the user again
   end
 
   def login_as_normal_user
-    login_as create_user
+    login_as User.make!
   end
 
   def cookie_flash
