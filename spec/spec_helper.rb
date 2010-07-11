e9e66f51ca78d444507aa8d31f9e0c6fa8915568
Wincent Colaiuta <win@wincent.com>
2010-07-11 21:23:32 +0200

    Drop user-stupidity guard
    This no longer seems to be necessary, as with the current Bundler set-up
    if I try running "spec" things will bail anyway with:
    
      /Library/Ruby/Site/1.8/rubygems.rb:334:in `bin_path': can't find
      executable spec for rspec-2.0.0.beta.17 (Gem::Exception)
        from /usr/bin/spec:19
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,11 +3,6 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'factory_girl/syntax/sham'
 
-# guard against user stupidity
-if Object.const_defined?(:Spec) && Spec::VERSION::MAJOR == 1
-  raise "RSpec 1.x is loaded: did you run 'spec' instead of 'rspec'?"
-end
-
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
