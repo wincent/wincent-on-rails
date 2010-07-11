ba4088370cd69fd01213704f06b45d6fa3d811df
Wincent Colaiuta <win@wincent.com>
2010-07-12 00:25:36 +0200

    Unbreak links controller specs
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -7,26 +7,25 @@ end
 
 describe LinksController, 'show action with permalink' do
   before do
-    @permalink  = 'foo'
-    @link       = create_link :permalink => @permalink
+    @link = Link.make! :permalink => 'foo'
   end
 
   # was a bug (I'd forgotten to use the "find_link" before filter)
   it 'should find the link by the permalink' do
-    Link.should_receive(:find_by_permalink).and_return(@link)
-    get :show, :id => @permalink, :protocol => 'https'
+    mock(Link).find_by_permalink('foo') { @link }
+    get :show, :id => 'foo'
   end
 end
 
 describe LinksController, 'show action with raw id' do
   before do
-    @link = create_link
+    @link = Link.make!
   end
 
   # was a bug (I'd forgotten to use the "find_link" before filter)
   it 'should find the link by falling back to a find by id' do
-    Link.should_receive(:find_by_permalink).and_return(nil) # fail on first try, but...
-    Link.should_receive(:find).and_return(@link)            # succeed on fallback
-    get :show, :id => @link.id, :protocol => 'https'
+    stub(Link).find_by_permalink(@link.id) { nil }  # fail on first try, but...
+    mock(Link).find(@link.id) { @link }             # succeed on fallback
+    get :show, :id => @link.id
   end
 end
