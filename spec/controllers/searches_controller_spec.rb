78a169b8fe7c1809e811ed509e00fb7c23205b38
Wincent Colaiuta <win@wincent.com>
2010-07-12 14:19:14 +0200

    Unbreak search controller specs
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -5,36 +5,36 @@ describe SearchesController do
   it_should_behave_like 'ApplicationController parameter filtering'
 end
 
-describe SearchesController, 'create action' do
-  it 'should assign the offset for use by the view' do
+describe SearchesController, '#create' do
+  it 'assigns the offset for use by the view' do
     post :create, :offset => '5', :protocol => 'https'
     assigns[:offset].should == 5
   end
 
-  it 'should find using the query string' do
-    Needle.should_receive(:find_using_query_string).with('foo', :user => nil, :offset => 0)
-    post :create, :query => 'foo', :protocol => 'https'
+  it 'finds using the query string' do
+    mock(Needle).find_using_query_string('foo', :user => nil, :offset => 0)
+    post :create, :query => 'foo'
   end
 
-  it 'should should pass a nil query parameter as an empty string' do
+  it 'passes a nil query parameter as an empty string' do
     # this is low cost and prevents an exception being thrown in the face of bad input (possible attack)
-    Needle.should_receive(:find_using_query_string).with('', :user => nil, :offset => 0)
-    post :create, :protocol => 'https'
+    mock(Needle).find_using_query_string('', :user => nil, :offset => 0)
+    post :create
   end
 
-  it 'should use the offset parameter when performing the query' do
-    Needle.should_receive(:find_using_query_string).with('foo', :user => nil, :offset => 5)
-    post :create, :query => 'foo', :offset => '5', :protocol => 'https'
+  it 'uses the offset parameter when performing the query' do
+    mock(Needle).find_using_query_string('foo', :user => nil, :offset => 5)
+    post :create, :query => 'foo', :offset => '5'
   end
 
-  it 'should treat a missing offset value as 0' do
-    Needle.should_receive(:find_using_query_string).with('foo', :user => nil, :offset => 0)
-    post :create, :query => 'foo', :offset => nil, :protocol => 'https'
+  it 'treats a missing offset value as 0' do
+    mock(Needle).find_using_query_string('foo', :user => nil, :offset => 0)
+    post :create, :query => 'foo', :offset => nil
   end
 
-  it 'should assign the found models for use by the view' do
-    Needle.stub!(:find_using_query_string).and_return(:search_results)
-    post :create, :protocol => 'https'
+  it 'assigns the found models for use by the view' do
+    stub(Needle).find_using_query_string(anything, anything) { :search_results }
+    post :create
     assigns[:models].should == :search_results
   end
 end
