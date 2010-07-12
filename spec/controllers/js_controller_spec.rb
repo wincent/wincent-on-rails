ec9c330e656a123d9004df68348f45da9c81d4da
Wincent Colaiuta <win@wincent.com>
2010-07-12 15:43:44 +0200

    Triage JsController specs
    Unfortunately there is a bug in Rails which prevents us from doing any
    JsController testing at all; it reports "No route matches" errors
    despite the fact that the controller works in the browser, and the
    separate routing specs also pass.
    
    The likely cause is an inability of the Rails functional test machinery
    to target actions that are mapped using the "match" operator. This seems
    to be a new bug to Rails 3, either the new routing DSL or something else
    in the framework, as in previous versions of Rail there was no problem
    at all in routing to such actions.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/controllers/js_controller_spec.rb
+++ spec/controllers/js_controller_spec.rb
@@ -3,74 +3,46 @@ require File.expand_path('../spec_helper', File.dirname(__FILE__))
 describe JsController do
   it_should_behave_like 'ApplicationController protected methods'
   it_should_behave_like 'ApplicationController parameter filtering'
-end
 
-describe JsController, 'GET /js/:delegated' do
-  def do_get
-    delegated = [@delegating_controller, @delegated_action]
-    delegated.unshift @namespace if @namespace
-    get :show, :protocol => 'https', :delegated => delegated.join('/')
-  end
+  describe '#show' do
+    before do
+      @namespace = nil
+      @delegating_controller = 'issues'
+      @delegated_action = 'edit'
+    end
 
-  before do
-    @namespace = nil
-    @delegating_controller = 'issues'
-    @delegated_action = 'edit'
-  end
+    def do_get
+      delegated = [@delegating_controller, @delegated_action]
+      delegated.unshift @namespace if @namespace
+      pending 'spurious "No route matches" errors'
+      # No route matches {:controller=>"js", :delegated=>"issues/edit.js", :action=>"show"}
+      get :show, :delegated => delegated.join('/') + '.js'
+    end
 
-  it 'should be successful' do
-    do_get
-    response.should be_success
-  end
+    it 'is successful' do
+      do_get
+      response.should be_success
+    end
 
-  it 'should render the "js/issues/show.js.erb" template' do
-    do_get
-    response.should render_template('js/issues/edit.js.erb')
-  end
+    it 'renders the "js/issues/show.js.erb" template' do
+      do_get
+      response.should render_template('js/issues/edit.js.erb')
+    end
 
-  it 'should render templates in the admin namespace' do
-    @namespace = 'admin'
-    do_get
-    response.should render_template('js/admin/issues/edit.js.erb')
-  end
+    it 'renders templates in the admin namespace' do
+      @namespace = 'admin'
+      do_get
+      response.should render_template('js/admin/issues/edit.js.erb')
+    end
 
-  it 'should not use a layout' do
-    do_get
-    controller.active_layout.should be_nil
-  end
+    it 'does not use a layout' do
+      do_get
+      controller.active_layout.should be_nil
+    end
 
-  it 'should not page-cache the output' do
-    controller.should_not_receive(:cache_page)
-    do_get
-  end
-
-  it 'should complain if delegating namespace parameter is invalidly formatted' do
-    @namespace = '99999'
-    lambda { do_get }.should raise_error(ActionController::RoutingError)
-  end
-
-  it 'should complain if delegating controller parameter is invalidly formatted' do
-    @delegating_controller = '99999'
-    lambda { do_get }.should raise_error(ActionController::RoutingError)
-  end
-
-  it 'should complain if delegated action parameter is invalidly formatted' do
-    @delegated_action = 'foo-bar'
-    lambda { do_get }.should raise_error(ActionController::RoutingError)
-  end
-
-  it 'should complain if both controller and action not supplied' do
-    lambda {
-      get :show, :protocol => 'https', :delegated => 'mycontroller'
-    }.should raise_error(ActionController::RoutingError)
-    lambda {
-      get :show, :protocol => 'https', :delegated => 'mycontroller/'
-    }.should raise_error(ActionController::RoutingError)
-  end
-
-  it 'should complain about maliciously formatted parameters (..)' do
-    lambda {
-      get :show, :protocol => 'https', :delegated => '../../../../etc/passwd'
-    }.should raise_error(ActionController::RoutingError)
+    it 'does not page-cache the output' do
+      do_not_allow(controller).cache_page
+      do_get
+    end
   end
 end
--- spec/routing/js_routing_spec.rb
+++ spec/routing/js_routing_spec.rb
@@ -4,5 +4,17 @@ describe JsController do
   describe 'routing' do
     specify { get('/js/issues/show.js').should map_to('js#show', :delegated => 'issues/show.js') }
     specify { get('/js/admin/issues/edit.js').should map_to('js#show', :delegated => 'admin/issues/edit.js') }
+
+    # numbers in :delegated param
+    specify { get('/js/99999/edit.js').should_not be_recognized }
+
+    # missing .js extension
+    specify { get('/js/issues/edit').should_not be_recognized }
+
+    # missing either action or controller
+    specify { get('/js/issues.js').should_not be_recognized }
+
+    # maliciously formatted params
+    specify { get('/js/../../../../etc/passwd').should_not be_recognized }
   end
 end
