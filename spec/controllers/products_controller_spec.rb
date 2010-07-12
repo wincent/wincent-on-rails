e498752edbbd3fa2adff04cdd1e3f71885428e71
Wincent Colaiuta <win@wincent.com>
2010-07-12 14:33:17 +0200

    Unbreak products controller specs
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -5,42 +5,29 @@ describe ProductsController do
   it_should_behave_like 'ApplicationController parameter filtering'
 end
 
-describe ProductsController, 'index action' do
+describe ProductsController, '#index' do
   before do
-    @products = [mock_model(Product)]
+    @products = [ Product.make! :category => 'Excellent' ]
   end
 
-  def do_get
-    get :index, :protocol => 'https'
+  it 'is successful' do
+    get :index
+    response.should be_success
   end
 
-  # until all (or at least most) products are set up will just redirect
-  it 'should redirect to the old site' do
-    do_get
-    response.should redirect_to('http://wincent.com/a/products/')
+  it 'finds all products' do
+    mock(Product).front_page { @products }
+    get :index
   end
 
-  # temporarily disabled while redirect is in effect
-  if false
-    it 'should be successful' do
-      do_get
-      response.should be_success
-    end
+  it 'assigns found products, grouped by category' do
+    stub(Product).front_page { @products }
+    get :index
+    assigns[:products].should == { 'Excellent' => @products }
+  end
 
-    it 'should find all products' do
-      Product.should_receive(:front_page)
-      do_get
-    end
-
-    it 'should assign found products for the view' do
-      Product.stub!(:front_page).and_return(@products)
-      do_get
-      assigns[:products].should == @products
-    end
-
-    it 'should render the index template' do
-      do_get
-      response.should render_template('index')
-    end
+  it 'renders the index template' do
+    get :index
+    response.should render_template('index')
   end
 end
