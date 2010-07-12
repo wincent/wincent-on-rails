5bb4c54151ba7bb98a1870cea4b0fb96739b9786
Wincent Colaiuta <win@wincent.com>
2010-07-12 15:16:28 +0200

    Unbreak posts controller specs
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,31 +1,31 @@
 require File.expand_path('../spec_helper', File.dirname(__FILE__))
 require 'hpricot'
 
-describe PostsController, 'GET /blog.atom' do
+describe PostsController, '#index (Atom)' do
   render_views # so that we can test layouts as well
 
   def do_get
-    get :index, :format => 'atom', :protocol => 'https'
+    get :index, :format => 'atom'
   end
 
   # make sure we don't get bitten by bugs like:
   # https://wincent.com/issues/1227
-  it 'should produce valid atom when there are no posts' do
+  it 'produces valid atom when there are no posts' do
     pending unless can_validate_feeds?
     Post.destroy_all
     do_get
     response.body.should be_valid_atom
   end
 
-  it 'should produce valid atom when there are multiple posts' do
+  it 'produces valid atom when there are multiple posts' do
     pending unless can_validate_feeds?
-    10.times { create_post }
+    10.times { Post.make! }
     do_get
     response.body.should be_valid_atom
   end
 
   # Rails 2.3.0 RC1 BUG: http://rails.lighthouseapp.com/projects/8994/tickets/2043
-  it 'should produce entry links to HTML-formatted records' do
+  it 'produces entry links to HTML-formatted records' do
     do_get
     doc = Hpricot.XML(response.body)
     (doc/:entry).collect do |entry|
@@ -37,28 +37,28 @@ describe PostsController, 'GET /blog.atom' do
   end
 end
 
-describe PostsController, 'GET /blog/:permalink.atom' do
+describe PostsController, '#show (Atom)' do
   render_views # so that we can test layouts as well
 
   before do
-    @post = create_post :permalink => 'hello'
+    @post = Post.make! :permalink => 'hello'
   end
 
   def do_get
-    get :show, :id => 'hello', :format => 'atom', :protocol => 'https'
+    get :show, :id => 'hello', :format => 'atom'
   end
 
   # make sure we don't get bitten by bugs like:
   # https://wincent.com/issues/1227
-  it 'should produce valid atom when there are no comments' do
+  it 'produces valid atom when there are no comments' do
     pending unless can_validate_feeds?
     do_get
     response.body.should be_valid_atom
   end
 
-  it 'should produce valid atom when there are multiple posts' do
+  it 'produces valid atom when there are multiple posts' do
     pending unless can_validate_feeds?
-    10.times { create_comment :commentable => @post }
+    10.times { Comment.make! :commentable => @post }
     do_get
     response.body.should be_valid_atom
   end
