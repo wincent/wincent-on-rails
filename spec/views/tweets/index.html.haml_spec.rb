b4dea7ac9ca45fd45c4a8f835e56969aedaf5846
Wincent Colaiuta <win@wincent.com>
2010-07-13 00:10:26 +0200

    Unbreak tweets#index view specs (HTML format)
    Can't say I am happy with the stubbing that is necessary in the "before"
    block in order to get this working:
    
      stub(view).render('tweets.html.haml')
      stub.proxy(view).render({:template => 'tweets/index.html.haml'}, {})
    
    First we must stub the call to render the partial, because we test that
    separately in another file, we don't want errors in that partial
    bubbling up into this view's specs, and we don't want to have to do the
    additional set-up required by the partial.
    
    But then we have to stub and proxy the call to "render" of the template
    being tested itself, which is ugly due to the format of the parameters.
    I would like to find a better way.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/views/tweets/index.html.haml_spec.rb
+++ spec/views/tweets/index.html.haml_spec.rb
@@ -1,31 +1,31 @@
 require File.expand_path('../../spec_helper', File.dirname(__FILE__))
 
-describe '/tweets/index.html.haml' do
-  include TweetsHelper
-
-  def do_render
-    assigns[:paginator] = @paginator
-    assigns[:tweets] = [ create_tweet, create_tweet ]
-    render '/tweets/index.html.haml'
-  end
-
+describe 'tweets/index.html.haml' do
   before do
     @paginator = RestfulPaginator.new({}, 2, tweets_path, 20)
+    assigns[:paginator] = @paginator
+    assigns[:tweets] = [ Tweet.make!, Tweet.make! ]
+    stub(view).render('tweets.html.haml')
+    stub.proxy(view).render({:template => 'tweets/index.html.haml'}, {})
   end
 
-  it 'should include an Atom feed link' do
-    template.should_receive(:atom_link)
-    do_render
+  it 'includes an Atom feed link' do
+    mock(view).atom_link('/twitter.atom')
+    render
   end
 
-  it 'should include pagination links (top and bottom)' do
-    @paginator = mock(RestfulPaginator)
-    @paginator.should_receive(:pagination_links).twice
-    do_render
+  it 'includes breadcrumbs' do
+    mock(view).breadcrumbs 'Twitter'
+    render
   end
 
-  it 'should render the "tweets" partial' do
-    template.should_receive(:render).with('tweets.html.haml')
-    do_render
+  it 'includes pagination links (top and bottom)' do
+    mock(@paginator).pagination_links.twice
+    render
+  end
+
+  it 'renders the "tweets" partial' do
+    mock(view).render('tweets.html.haml')
+    render
   end
 end
