e523939488ff6d52f0841f804542a97e2819b0d6
Wincent Colaiuta <win@wincent.com>
2010-07-13 22:56:40 +0200

    Unbreak pages#edit view spec
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/views/pages/edit.html.haml_spec.rb
+++ spec/views/pages/edit.html.haml_spec.rb
@@ -1,21 +1,19 @@
 require File.expand_path('../../spec_helper', File.dirname(__FILE__))
 
-describe "/pages/edit.html.haml" do
-  include PagesHelper
-
-  before(:each) do
-    assigns[:product] = @product = create_product
-    assigns[:page] = @page = create_page(:product_id => @product.id)
+describe 'pages/edit' do
+  before do
+    @product = Product.make!
+    @page = Page.make! :product_id => @product.id
   end
 
   it "renders the edit page form" do
     render
 
-    response.should have_tag("form[action=#{product_page_path(@product, @page)}][method=post]") do
-      with_tag('input#page_title[name=?]', "page[title]")
-      with_tag('input#page_permalink[name=?]', "page[permalink]")
-      with_tag('textarea#page_body[name=?]', "page[body]")
-      with_tag('input#page_front[name=?]', "page[front]")
+    rendered.should have_selector('form[method=post]', :action => product_page_path(@product, @page)) do |form|
+      form.should have_selector('input#page_title', :name => 'page[title]')
+      form.should have_selector('input#page_permalink', :name => 'page[permalink]')
+      form.should have_selector('textarea#page_body', :name => 'page[body]')
+      form.should have_selector('input#page_front', :name => 'page[front]')
     end
   end
 end
