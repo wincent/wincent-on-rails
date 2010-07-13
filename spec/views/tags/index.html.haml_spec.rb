3f8e07b326a812d33e8d90ada2ef7dcbc816689d
Wincent Colaiuta <win@wincent.com>
2010-07-13 14:49:02 +0200

    Unbreak tags#index view spec
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/views/tags/index.html.haml_spec.rb
+++ spec/views/tags/index.html.haml_spec.rb
@@ -1,18 +1,12 @@
 require File.expand_path('../../spec_helper', File.dirname(__FILE__))
 
-describe '/tags/index' do
+describe 'tags/index' do
   before do
-    assigns[:tags] = [create_tag]
-  end
-
-  def do_render
-    render '/tags/index'
+    @tags = [Tag.make!]
   end
 
   it 'should have a "tag search" link' do
-    do_render
-    response.should have_tag('.links') do
-      with_tag 'a[href=?]', search_tags_path
-    end
+    render
+    rendered.should have_selector('.links a[href="/tags/search"]')
   end
 end
