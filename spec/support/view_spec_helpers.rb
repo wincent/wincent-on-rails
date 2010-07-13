dccd60ad6043680f6d5b2fa90d6ecb06052fdcef
Wincent Colaiuta <win@wincent.com>
2010-07-13 09:54:06 +0200

    Match view helper code to upstream patch version
    Use the same code as was submitted in upstream patch:
    
      http://github.com/wincent/rspec-rails/commit/10c3b979e577b8d7ab1efbd77e25cc441cb1d7bc
    
    The old code did work, but for some reason broke the Cucumber specs. The
    new code does not.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/support/view_spec_helpers.rb
+++ spec/support/view_spec_helpers.rb
@@ -2,10 +2,16 @@ module ViewSpecHelpers
   extend ActiveSupport::Concern
 
   module ClassMethods
-    def _default_helpers
-      helpers = [ApplicationHelper]
-      helpers << (example.example_group.top_level_description.split('/').first.camelize + 'Helper').constantize
+    def _default_helper
+      base = metadata[:behaviour][:description].split('/').first
+      (base.camelize + 'Helper').constantize if base
     rescue NameError
+      nil
+    end
+
+    def _default_helpers
+      helpers = [_default_helper].compact
+      helpers << ApplicationHelper if Object.const_defined?('ApplicationHelper')
       helpers
     end
   end # ClassMethods
