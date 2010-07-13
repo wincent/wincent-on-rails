da099cbb83d5439297792196eb2b8cc77d72a1e7
Wincent Colaiuta <win@wincent.com>
2010-07-13 15:45:27 +0200

    Unbreak searches/_issues view specs
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/views/searches/_issue.html.haml_spec.rb
+++ spec/views/searches/_issue.html.haml_spec.rb
@@ -1,69 +1,66 @@
 require File.expand_path('../../spec_helper', File.dirname(__FILE__))
 
-describe '/search/_issue' do
+describe 'searches/_issue' do
   before do
-    @issue          = create_issue
+    @issue          = Issue.make! :description => "can't print"
     @result_number  = 47
   end
 
   def do_render
-    render :partial => '/search/issue', :locals => { :model => @issue, :result_number => @result_number }
+    render :partial => 'searches/issue', :locals => { :model => @issue, :result_number => @result_number }
   end
 
-  it 'should show the result number' do
+  it 'shows the result number' do
     do_render
-    response.should have_text(/#{@result_number.to_s}/)
+    rendered.should contain(/#{@result_number.to_s}/)
   end
 
-  it 'should use the issue summary as link text' do
+  it 'uses the issue summary as link text' do
     do_render
-    response.should have_tag('a', @issue.summary)
+    rendered.should have_selector('a', :content => @issue.summary)
   end
 
   # was a bug
-  it 'should escape HTML special characters in the issue summary' do
-    @issue = create_issue :summary => '<em>foo</em>'
+  it 'escapes HTML special characters in the issue summary' do
+    @issue = Issue.make! :summary => '<em>foo</em>'
     do_render
-    response.should_not have_text(%r{<em>foo</em>})
+    rendered.should_not have_selector('em', :content => 'foo')
   end
 
-  it 'should link to the issue' do
+  it 'links to the issue' do
     do_render
-    response.should have_tag('a[href=?]', issue_path(@issue))
+    rendered.should have_selector("a[href='#{issue_path @issue}']")
   end
 
-  it 'should show the timeinfo for the issue' do
-    template.should_receive(:timeinfo).with(@issue)
-    do_render
-  end
-
-  it 'should show the issue kind' do
-    @issue.should_receive(:kind_string).and_return('le bug')
-    do_render
-    response.should have_text(/le bug/)
-  end
-
-  it 'should show the issue status' do
-    @issue.should_receive(:status_string).and_return('cerrado')
-    do_render
-    response.should have_text(/cerrado/)
-  end
-
-  it 'should get the issue description' do
-    # actually receives it twice (first time to check if blank)
-    @issue.should_receive(:description).at_least(:once).and_return('foo')
+  it 'shows the timeinfo for the issue' do
+    mock(view).timeinfo(@issue)
     do_render
   end
 
-  it 'should truncate the issue description to 240 characters' do
-    template.should_receive(:truncate).with(@issue.description, :length => 240, :safe => true).and_return('foo')
+  it 'shows the issue kind' do
+    mock(@issue).kind_string { 'le bug' }
+    do_render
+    rendered.should contain(/le bug/)
+  end
+
+  it 'shows the issue status' do
+    mock(@issue).status_string { 'cerrado' }
+    do_render
+    rendered.should contain(/cerrado/)
+  end
+
+  it 'gets the issue description' do
+    do_render
+    rendered.should contain("can't print")
+  end
+
+  it 'truncates the issue description to 240 characters' do
+    mock(view).truncate(@issue.description, :length => 240, :safe => true)
     do_render
   end
 
-  it 'should pass the truncated issue description through the wikitext translator' do
-    description = 'foo'
-    description.should_receive(:w).and_return('foo')
-    template.stub!(:truncate).and_return(description)
+  it 'passes the truncated issue description through the wikitext translator' do
+    stub(view).truncate.with_any_args { mock('description').w(:base_heading_level => 2) }
     do_render
   end
 end
