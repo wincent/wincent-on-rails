fd8cef4bbc3dd7042f871a8279de46782f96318e
Wincent Colaiuta <win@wincent.com>
2010-07-13 19:45:25 +0200

    Unbreak searches#create view specs
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/views/searches/create.html.haml_spec.rb
+++ spec/views/searches/create.html.haml_spec.rb
@@ -1,101 +1,99 @@
 require File.expand_path('../../spec_helper', File.dirname(__FILE__))
 
-describe '/search/create with one page of search results' do
-  before do
-    assigns[:models] = @models = [@article = create_article, @issue = create_issue, @post = create_post, @topic = create_topic]
-    assigns[:offset] = @offset = 15
-  end
+describe 'searches/create' do
+  context 'with one page of search results' do
+    before do
+      @models = [@article = Article.make!, @issue = Issue.make!, @post = Post.make!, @topic = Topic.make!]
+      @offset = 15
+    end
 
-  def do_render
-    render 'search/create'
-  end
+    it 'includes breadcrumbs' do
+      mock(view).breadcrumbs 'Search'
+      render
+    end
 
-  it 'should include the "search" style sheet' do
-    template.should_receive(:content_for).with(:css).and_yield
-    template.should_receive(:stylesheet_link_tag).with('search')
-    do_render
-  end
+    it 'renders the article partial for article results' do
+      stub.proxy(view).render
+      mock(view).render 'searches/article', :model => @article, :result_number => @offset + 1
+      render
+    end
 
-  it 'should should render the article partial for article results' do
-    template.should_receive :render, :partial => 'search/article', :locals => { :model => @article, :result_number => @offset + 1 }
-    do_render
-  end
+    it 'renders the issue partial for issue results' do
+      stub.proxy(view).render
+      mock(view).render 'searches/issue', :model => @issue, :result_number => @offset + 2
+      render
+    end
 
-  it 'should should render the issue partial for issue results' do
-    template.should_receive :render, :partial => 'search/issue', :locals => { :model => @issue, :result_number => @offset + 2 }
-    do_render
-  end
+    it 'renders the post partial for post results' do
+      stub.proxy(view).render
+      mock(view).render 'searches/post', :model => @post, :result_number => @offset + 3
+      render
+    end
 
-  it 'should should render the post partial for post results' do
-    template.should_receive :render, :partial => 'search/post', :locals => { :model => @post, :result_number => @offset + 3 }
-    do_render
-  end
+    it 'renders the topic partial for topic results' do
+      stub.proxy(view).render
+      mock(view).render 'searches/topic', :model => @topic, :result_number => @offset + 4
+      render
+    end
 
-  it 'should should render the topic partial for topic results' do
-    template.should_receive :render, :partial => 'search/topic', :locals => { :model => @topic, :result_number => @offset + 4 }
-    do_render
-  end
-
-  it 'should have a "search again" link' do
-    do_render
-    response.should have_tag('.links') do
-      with_tag 'a[href=?]', search_index_path
+    it 'has a "search again" link' do
+      render
+      rendered.should have_selector('.links a', :href=> '/search/new')
     end
   end
-end
 
-describe '/search/create with some "nil" search results' do
-  # these can occur when somebody does a Model.delete
-  # in that case, the model gets destroyed and no callbacks are fired
-  # so the needles index doesn't get updated
-  # when we try to prefetch those missing models we get nil placeholders
-  before do
-    assigns[:models] = [@issue = create_issue, nil, nil]
-    assigns[:offset] = 0
+  context 'with some "nil" search results' do
+    # these can occur when somebody does a Model.delete
+    # in that case, the model gets destroyed and no callbacks are fired
+    # so the needles index doesn't get updated
+    # when we try to prefetch those missing models we get nil placeholders
+    before do
+      @models = [@issue = Issue.make!, nil, nil]
+      @offset = 0
+    end
+
+    it 'renders the partial for existing results' do
+      stub.proxy(view).render
+      mock(view).render 'searches/issue', :model => @issue, :result_number => 1
+      render
+    end
+
+    it 'does not choke on the nil placeholders' do
+      lambda { render }.should_not raise_error
+    end
   end
 
-  def do_render
-    render 'search/create'
+  context 'with no search results' do
+    before do
+      @models = []
+      @offset = 0
+    end
+
+    it 'displays "no results"' do
+      render
+      rendered.should contain(/no results/i)
+    end
   end
 
-  it 'should should render the partial for existing results' do
-    template.should_receive :render, :partial => 'search/issue', :locals => { :model => @issue, :result_number => 1 }
-    do_render
-  end
+  context 'with multiple pages of search results' do
+    before do
+      models = []
+      60.times { models << Issue.make! }
+      @models = models
+      @offset = 20
+      # RSpec 2 BUG: undefined local variable or method `params'
+      #   http://github.com/rspec/rspec-rails/issues/126
+      #params[:query] = 'foo'
+    end
 
-  it 'should not choke on the nil placeholders' do
-    lambda { do_render }.should_not raise_error
-  end
-end
-
-
-describe '/search/create with no search results' do
-  before do
-    assigns[:models] = []
-    assigns[:offset] = 0
-  end
-
-  it 'should display "no results"' do
-    render 'search/create'
-    response.should have_text(/no results/i)
-  end
-end
-
-describe '/search/create with multiple pages of search results' do
-  before do
-    models = []
-    60.times { models << create_issue }
-    assigns[:models] = models
-    assigns[:offset] = @offset = 20
-    params[:query] = 'foo'
-  end
-
-  it 'should display a "more results" form button' do
-    render 'search/create'
-    response.should have_tag('form[action=?]', search_index_path) do
-      with_tag 'input[type=hidden][name=query][value=?]', 'foo'
-      with_tag 'input[type=hidden][name=offset][value=?]', @offset + 20
-      with_tag 'input[type=submit][value=?]', 'more results...'
+    it 'displays a "more results" form button' do
+      render
+      rendered.should have_selector('form', :action => '/search') do |form|
+        form.should have_selector('input[type=hidden][name=offset]', :value => (@offset + 20).to_s)
+        form.should have_selector('input[type=submit]', :value => 'more results...')
+        pending 'rspec-rails issue #126'
+        form.should have_selector('input[type=hidden][name=query]', :value => 'foo')
+      end
     end
   end
 end
