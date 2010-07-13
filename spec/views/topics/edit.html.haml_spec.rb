3850f11bf21ee9adcc6139dfe299f87e54805507
Wincent Colaiuta <win@wincent.com>
2010-07-13 14:49:18 +0200

    Unbreak topics#edit view specs
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/views/topics/edit.html.haml_spec.rb
+++ spec/views/topics/edit.html.haml_spec.rb
@@ -1,28 +1,20 @@
 require File.expand_path('../../spec_helper', File.dirname(__FILE__))
 
-describe '/topics/edit' do
-  include TopicsHelper
-
+describe 'topics/edit' do
   before do
-    assigns[:forum] = @forum = create_forum
-    assigns[:topic] = @topic = create_topic(:forum => @forum)
-  end
-
-  def do_render
-    render '/topics/edit'
+    @forum = Forum.make!
+    @topic = Topic.make! :forum => @forum
   end
 
   it 'should have a ham button if the topic is awaiting moderation' do
-    assigns[:topic] = @topic = create_topic(:awaiting_moderation => true,
-      :forum => @forum)
-    template.should_receive(:button_to_moderate_topic_as_ham).with(@topic)
-    do_render
+    @topic = Topic.make! :awaiting_moderation => true, :forum => @forum
+    mock(view).button_to_moderate_topic_as_ham(@topic)
+    render
   end
 
   it 'should not have a ham button if the issue is not awaiting moderation' do
-    assigns[:topic] = @topic = create_topic(:awaiting_moderation => false,
-      :forum => @forum)
-    template.should_not_receive(:button_to_moderate_topic_as_ham)
-    do_render
+    @topic = Topic.make! :awaiting_moderation => false, :forum => @forum
+    do_not_allow(view).button_to_moderate_topic_as_ham
+    render
   end
 end
