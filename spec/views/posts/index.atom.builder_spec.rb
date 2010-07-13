b2ff7cce391e6a6f665e1a8963d41585f4a51611
Wincent Colaiuta <win@wincent.com>
2010-07-13 22:38:41 +0200

    Unbreak posts#index view specs (Atom format)
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/views/posts/index.atom.builder_spec.rb
+++ spec/views/posts/index.atom.builder_spec.rb
@@ -1,14 +1,10 @@
 require File.expand_path('../../spec_helper', File.dirname(__FILE__))
 
-describe '/posts/index.atom.builder' do
-  include PostsHelper
-
-  def do_render
-    render '/posts/index.atom.builder'
-  end
-
-  it 'should handle no posts' do
-    assigns[:posts] = []
-    lambda { do_render }.should_not raise_error
+# TODO: add some worthwhile specs here, these don't even confirm that the
+# template even renders anything (had to add a "p" statement to check it)
+describe 'posts/index.atom.builder' do
+  it 'handles no posts' do
+    @posts = []
+    lambda { render }.should_not raise_error
   end
 end
