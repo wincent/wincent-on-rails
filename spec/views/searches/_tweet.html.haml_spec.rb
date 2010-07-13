71e92a5cb919d8d27df7ff7c90cbf90056376c91
Wincent Colaiuta <win@wincent.com>
2010-07-13 20:40:16 +0200

    Unbreak searches/_tweet view spec
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/views/searches/_tweet.html.haml_spec.rb
+++ spec/views/searches/_tweet.html.haml_spec.rb
@@ -1,49 +1,47 @@
 require File.expand_path('../../spec_helper', File.dirname(__FILE__))
 
-describe '/search/_tweet' do
+describe 'searches/_tweet' do
   before do
-    @tweet          = create_tweet
+    @tweet          = Tweet.make!
     @result_number  = 47
   end
 
   def do_render
-    render :partial => '/search/tweet', :locals => { :model => @tweet, :result_number => @result_number }
+    render 'searches/tweet', :model => @tweet, :result_number => @result_number
   end
 
-  it 'should show the result number' do
+  it 'shows the result number' do
     do_render
-    response.should have_text(/#{@result_number.to_s}/)
+    rendered.should contain(@result_number.to_s)
   end
 
-  it 'should use the tweet number in the link text' do
+  it 'uses the tweet number in the link text' do
     do_render
-    response.should have_tag('a', "Tweet \##{@tweet.id}")
+    rendered.should have_selector('a', :content => "Tweet \##{@tweet.id}")
   end
 
-  it 'should link to the tweet' do
+  it 'links to the tweet' do
     do_render
-    response.should have_tag('a[href=?]', tweet_path(@tweet))
+    rendered.should have_selector('a', :href => tweet_path(@tweet))
   end
 
-  it 'should show the timeinfo for the tweet' do
-    template.should_receive(:timeinfo).with(@tweet)
-    do_render
-  end
-
-  it 'should get the tweet body' do
-    @tweet.should_receive(:body).and_return('foo')
+  it 'shows the timeinfo for the tweet' do
+    mock(view).timeinfo(@tweet)
     do_render
   end
 
-  it 'should truncate the tweet body to 240 characters' do
-    template.should_receive(:truncate).with(@tweet.body, :length => 240, :safe => true).and_return('foo')
+  it 'gets the tweet body' do
+    mock(@tweet).body#).and_return('foo')
     do_render
   end
 
-  it 'should pass the truncated tweet body through the wikitext translator' do
-    body = 'foo'
-    body.should_receive(:w).and_return('foo')
-    template.stub!(:truncate).and_return(body)
+  it 'truncates the tweet body to 240 characters' do
+    mock(view).truncate(@tweet.body, :length => 240, :safe => true) # { 'foo' }
+    do_render
+  end
+
+  it 'passes the truncated tweet body through the wikitext translator' do
+    stub(view).truncate { mock('body').w :base_heading_level => 2 }
     do_render
   end
 end
