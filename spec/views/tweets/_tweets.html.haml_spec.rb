3d824fb1f00aab22b5654a12c0624ca2ab5ad49f
Wincent Colaiuta <win@wincent.com>
2010-07-13 11:25:44 +0200

    Unbreak tweets/tweets partial specs
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/views/tweets/_tweets.html.haml_spec.rb
+++ spec/views/tweets/_tweets.html.haml_spec.rb
@@ -1,40 +1,34 @@
 require File.expand_path('../../spec_helper', File.dirname(__FILE__))
 
-describe '/tweets/index.html.haml' do
-  include TweetsHelper
-
-  def do_render
-    assigns[:tweets] = @tweets
-    render '/tweets/_tweets.html.haml'
-  end
-
+describe 'tweets/_tweets.html.haml' do
   before do
     @tweets = [
-      create_tweet(:body => "''foo''"),
-      create_tweet(:body => "'''bar'''")
+      Tweet.make!(:body => "''foo''"),
+      Tweet.make!(:body => "'''bar'''")
     ]
+    assigns[:tweets] = @tweets
   end
 
   it 'should display a div for each tweet' do
-    do_render
-    response.should have_tag('div.tweet', 2)
+    render
+    rendered.should have_selector('div.tweet', :count => 2)
   end
 
   it 'should display the HTML body of each tweet' do
-    do_render
-    response.body.should =~ %r{<em>foo</em>}
-    response.body.should =~ %r{<strong>bar</strong>}
+    render
+    rendered.should =~ %r{<em>foo</em>}
+    rendered.should =~ %r{<strong>bar</strong>}
   end
 
   it 'should show the time information for each tweet' do
-    template.should_receive(:timeinfo).with(@tweets[0])
-    template.should_receive(:timeinfo).with(@tweets[1])
-    do_render
+    mock(view).timeinfo(@tweets[0])
+    mock(view).timeinfo(@tweets[1])
+    render
   end
 
   it 'should show a permalink for each tweet' do
-    do_render
-    response.should have_tag('a[href=?]', tweet_path(@tweets[0]), 'permalink')
-    response.should have_tag('a[href=?]', tweet_path(@tweets[1]), 'permalink')
+    render
+    rendered.should have_selector("a[href='/twitter/#{@tweets[0].id}']", :content => 'permalink')
+    rendered.should have_selector("a[href='/twitter/#{@tweets[1].id}']", :content => 'permalink')
   end
 end
