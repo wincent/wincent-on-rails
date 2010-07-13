3a54bb544e29721a27c3a99a203ec2081f4d20e2
Wincent Colaiuta <win@wincent.com>
2010-07-13 12:42:59 +0200

    Unbreak tweets#edit view specs
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/views/tweets/edit.html.haml_spec.rb
+++ spec/views/tweets/edit.html.haml_spec.rb
@@ -1,76 +1,65 @@
 require File.expand_path('../../spec_helper', File.dirname(__FILE__))
 
-describe '/tweets/edit.html.haml' do
-  include TweetsHelper
-
-  def do_render
-    assigns[:tweet] = @tweet
-    render '/tweets/edit.html.haml'
-  end
-
+describe 'tweets/edit.html.haml' do
   before do
-    @tweet = create_tweet :body => 'hello'
+    @tweet = Tweet.make! :body => 'hello'
+    assigns[:tweet] = @tweet
+    stub(view).render 'shared/error_messages', :model => @tweet
+    stub(view).render 'preview'
+    stub.proxy(view).render
   end
 
-  it 'should include the "tweets" CSS file' do
-    template.should_receive(:stylesheet_link_tag).with('tweets')
-    do_render
+  it 'includes "ajax.js"' do
+    mock(view).javascript_include_tag 'ajax'
+    render
   end
 
-  it 'should display error messages' do
-    template.should_receive(:error_messages_for).with(:tweet)
-    do_render
+  it 'renders the error messages partial' do
+    mock(view).render 'shared/error_messages', :model => @tweet
+    render
   end
 
-  it 'should have a form for the tweet' do
-    do_render
-    response.should have_tag("form[action=?][method=post]", tweet_path(@tweet)) do
+  it 'has a form for the tweet' do
+    render
+    rendered.should have_selector("form[action='#{tweet_path @tweet}'][method=post]") do |form|
       # real HTTP PUT is not supported, the form is just a normal POST
       # with a hidden field faking the PUT
-      with_tag('input[name=_method][value=put]')
-      with_tag('textarea[name=?]', 'tweet[body]', 'hello')
+      form.should have_selector('input[name=_method][value=put]')
+      form.should have_selector("textarea[name='tweet[body]']", :content => 'hello')
     end
   end
 
-  it 'should provide a link to the wikitext cheatsheet' do
-    template.should_receive(:wikitext_cheatsheet)
-    do_render
+  it 'provides a link to the wikitext cheatsheet' do
+    mock(view).wikitext_cheatsheet
+    render
   end
 
-  it 'should have a preview div' do
-    do_render
-    response.should have_tag('#preview')
+  it 'has a preview div' do
+    render
+    rendered.should have_selector('#preview')
   end
 
-  it 'should render the preview partial' do
-    template.should_receive(:render).with('preview.html.haml')
-    do_render
+  it 'renders the preview partial' do
+    mock(view).render 'preview'
+    render
   end
 
-  it 'should have a "show" link' do
-    do_render
-    response.should have_tag('.links') do
-      # we use Rails shortcut @tweet instead of tweet_path(@tweet)
-      # so we end up getting tweet_path(@tweet)
-      with_tag 'a[href=?]', tweet_path(@tweet)
+  it 'has a "show" link' do
+    render
+    rendered.should have_selector('.links') do |div|
+      div.should have_selector("a[href='#{tweet_path(@tweet)}']")
     end
   end
 
-  it 'should have a "destroy" link (test HTML)' do
-    do_render
-    response.should have_tag('.links') do
-      # again, using the shortcut gives us tweet_path(@tweet)
-      # instead of the habitual tweet_path(@tweet)
-      with_tag 'a[href=?]', tweet_path(@tweet), 'destroy'
+  it 'has a "destroy" link' do
+    render
+    rendered.should have_selector('.links') do |div|
+      div.should have_selector("form[action='#{tweet_path(@tweet)}'] input[name=_method][value=delete]")
     end
-    # link_to ..., :method => :delete produces some obtrusive JavaScript
-    # which unfortunately can't be easily tested
   end
 
-  it 'should have an link to the tweets index' do
-    do_render
-    response.should have_tag('.links') do
-      with_tag 'a[href=?]', tweets_path
-    end
+  it 'has an link to the tweets index' do
+    render
+    rendered.should have_selector(".links a[href='/twitter']")
   end
 end
