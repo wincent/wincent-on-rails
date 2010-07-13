3272d836b5675e06608568de8ac6f59a7acc15a5
Wincent Colaiuta <win@wincent.com>
2010-07-13 22:59:09 +0200

    Unbreak pages#new view specs
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/views/pages/new.html.haml_spec.rb
+++ spec/views/pages/new.html.haml_spec.rb
@@ -1,21 +1,19 @@
 require File.expand_path('../../spec_helper', File.dirname(__FILE__))
 
-describe "/pages/new.html.haml" do
-  include PagesHelper
-
-  before(:each) do
-    assigns[:product] = @product = create_product
-    assigns[:page] = new_page :product_id => @product.id
+describe 'pages/new' do
+  before do
+    @product = Product.make!
+    @page = Page.make :product_id => @product.id
   end
 
-  it "renders new page form" do
+  it 'renders new page form' do
     render
 
-    response.should have_tag("form[action=?][method=post]", product_pages_path(@product)) do
-      with_tag("input#page_title[name=?]", "page[title]")
-      with_tag("input#page_permalink[name=?]", "page[permalink]")
-      with_tag("textarea#page_body[name=?]", "page[body]")
-      with_tag("input#page_front[name=?]", "page[front]")
+    rendered.should have_selector("form[method=post]", :action => product_pages_path(@product)) do |form|
+      form.should have_selector('input#page_title', :name => 'page[title]')
+      form.should have_selector('input#page_permalink', :name => 'page[permalink]')
+      form.should have_selector('textarea#page_body', :name => 'page[body]')
+      form.should have_selector('input#page_front', :name => 'page[front]')
     end
   end
 end
