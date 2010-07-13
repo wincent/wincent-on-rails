e99cf62a004d2c116fd4f03680d584dd5bc29d15
Wincent Colaiuta <win@wincent.com>
2010-07-13 20:49:08 +0200

    Unbreak searches/_topic view spec
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/views/searches/_topic.html.haml_spec.rb
+++ spec/views/searches/_topic.html.haml_spec.rb
@@ -1,58 +1,55 @@
 require File.expand_path('../../spec_helper', File.dirname(__FILE__))
 
-describe '/search/_topic' do
+describe 'searches/_topic' do
   before do
-    @topic          = create_topic
+    @topic          = Topic.make!
     @result_number  = 47
   end
 
   def do_render
-    render :partial => '/search/topic', :locals => { :model => @topic, :result_number => @result_number }
+    render 'searches/topic', :model => @topic, :result_number => @result_number
   end
 
-  it 'should show the result number' do
+  it 'shows the result number' do
     do_render
-    response.should have_text(/#{@result_number.to_s}/)
+    rendered.should contain(@result_number.to_s)
   end
 
-  it 'should use the topic title as link text' do
+  it 'uses the topic title as link text' do
     do_render
-    response.should have_tag('a', @topic.title)
+    rendered.should have_selector('a', :content => @topic.title)
   end
 
   # was a bug
-  it 'should escape HTML special characters in the topic summary' do
-    @topic = create_topic :title => '<em>foo</em>'
+  it 'escapes HTML special characters in the topic summary' do
+    @topic = Topic.make! :title => '<em>foo</em>'
     do_render
-    response.should_not have_text(%r{<em>foo</em>})
+    rendered.should match('&lt;em&gt;foo&lt;/em&gt;')
+    rendered.should_not have_selector('em', :content => 'foo')
   end
 
-  it 'should link to the topic' do
+  it 'links to the topic' do
     do_render
-    # must use protected topic_path method here because in the context of these specs url_for gives crazy results
-    # (ie "/topics/show/815" instead of the correct "https://test.host/topics/815")
-    response.should have_tag('a[href=?]', template.send(:topic_path, @topic))
+    rendered.should have_selector('a', :href => topic_path(@topic))
   end
 
-  it 'should show the timeinfo for the topic' do
-    template.should_receive(:timeinfo).with(@topic)
-    do_render
-  end
-
-  it 'should get the topic body' do
-    @topic.should_receive(:body).and_return('foo')
+  it 'shows the timeinfo for the topic' do
+    mock(view).timeinfo(@topic)
     do_render
   end
 
-  it 'should truncate the topic body to 240 characters' do
-    template.should_receive(:truncate).with(@topic.body, :length => 240, :safe => true).and_return('foo')
+  it 'gets the topic body' do
+    mock(@topic).body
     do_render
   end
 
-  it 'should pass the truncated topic body through the wikitext translator' do
-    body = 'foo'
-    body.should_receive(:w).and_return('foo')
-    template.stub!(:truncate).and_return(body)
+  it 'truncates the topic body to 240 characters' do
+    mock(view).truncate @topic.body, :length => 240, :safe => true
+    do_render
+  end
+
+  it 'passes the truncated topic body through the wikitext translator' do
+    stub(view).truncate { mock('body').w :base_heading_level => 2 }
     do_render
   end
 end
