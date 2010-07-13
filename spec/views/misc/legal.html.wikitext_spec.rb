4e40be9be5d750bd3df599a6481dc3b543095eae
Wincent Colaiuta <win@wincent.com>
2010-07-13 23:05:31 +0200

    Unbreak misc#legal view spec
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/views/misc/legal.html.wikitext_spec.rb
+++ spec/views/misc/legal.html.wikitext_spec.rb
@@ -1,14 +1,10 @@
  require File.expand_path('../../spec_helper', File.dirname(__FILE__))
 
-describe '/misc/legal' do
-  def do_render
-    render '/misc/legal'
-  end
-
+describe 'misc/legal' do
   # use spec suite as a reminder to update copyright year in static page
   it 'should end copyright year range with current year' do
-    do_render
-    response.should have_text(/Copyright 1997-#{Time.now.year} Wincent Colaiuta/)
-    response.should have_text(/Copyright 2007-#{Time.now.year} Wincent Colaiuta/)
+    render
+    rendered.should contain(/Copyright 1997-#{Time.now.year} Wincent Colaiuta/)
+    rendered.should contain(/Copyright 2007-#{Time.now.year} Wincent Colaiuta/)
   end
 end
