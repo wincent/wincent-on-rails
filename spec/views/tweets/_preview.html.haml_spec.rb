abd6f882c5e6ffa411ca47121c9eaa814a0c06c0
Wincent Colaiuta <win@wincent.com>
2010-07-13 11:14:29 +0200

    Unbreak tweets/preview partial specs
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/views/tweets/_preview.html.haml_spec.rb
+++ spec/views/tweets/_preview.html.haml_spec.rb
@@ -1,60 +1,58 @@
 require File.expand_path('../../spec_helper', File.dirname(__FILE__))
 
-describe '/tweets/_preview.html.haml' do
-  include TweetsHelper
-
+describe 'tweets/_preview.html.haml' do
   def do_render
     assigns[:tweet] = @tweet
     render :partial => '/tweets/preview.html.haml'
   end
 
   before do
-    @tweet = new_tweet :body => 'hello'
+    @tweet = Tweet.make :body => 'hello'
   end
 
   it 'should display the rendered length' do
     do_render
-    response.body.should =~ /\(5 characters\)/
+    rendered.should =~ /\(5 characters\)/
   end
 
   it 'should correctly pluralize when rendered length is 0' do
-    @tweet = new_tweet :body => ''
+    @tweet = Tweet.make :body => ''
     do_render
-    response.body.should =~ /\(0 characters\)/
+    rendered.should =~ /\(0 characters\)/
   end
 
   it 'should correctly pluralize when rendered length is 1' do
-    @tweet = new_tweet :body => 'a'
+    @tweet = Tweet.make :body => 'a'
     do_render
-    response.body.should =~ /\(1 character\)/
+    rendered.should =~ /\(1 character\)/
   end
 
   it 'should display overlength count using CSS span' do
     long = 'x' * 250
-    @tweet = new_tweet :body => long
+    @tweet = Tweet.make :body => long
     do_render
-    response.should have_tag('span.overlength', /\(250 characters\)/)
+    rendered.should have_selector('span.overlength', :content => '(250 characters)')
   end
 
   it 'should not use CSS span for short messages' do
     do_render
-    response.should_not have_tag('span.overlength')
+    rendered.should_not have_selector('span.overlength')
   end
 
   it 'should show the tweet body' do
     do_render
-    response.body.should =~ /hello/
+    rendered.should =~ /hello/
   end
 
   it 'should render wikitext markup as HTML' do
-    @tweet = new_tweet :body => "'''foo'''"
+    @tweet = Tweet.make :body => "'''foo'''"
     do_render
-    response.should have_tag('strong', 'foo')
+    rendered.should have_selector('strong', :content => 'foo')
   end
 
   it 'should start with a "base heading level" of 3' do
-    @tweet = new_tweet :body => "= foo ="
+    @tweet = Tweet.make :body => "= foo ="
     do_render
-    response.should have_tag('h4', 'foo')
+    rendered.should have_selector('h4', :content => 'foo')
   end
 end
