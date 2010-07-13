5795028dd6597e57dfe486740c8a1ff63c8d5997
Wincent Colaiuta <win@wincent.com>
2010-07-13 15:21:19 +0200

    Unbreak searches#new view spec
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/views/search/new.html.haml_spec.rb
+++ spec/views/search/new.html.haml_spec.rb
@@ -1,8 +1,9 @@
 require File.expand_path('../../spec_helper', File.dirname(__FILE__))
 
-describe '/search/new' do
-  it 'should render the "form" partial' do
-    template.should_receive :render, :partial => 'search/form'
-    render '/search/new'
+describe 'searches/new' do
+  it 'renders the "form" partial' do
+    stub.proxy(view).render
+    mock(view).render 'searches/form'
+    render
   end
 end
