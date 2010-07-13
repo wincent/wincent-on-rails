b0890f556eb9da865b5d389f67d00f78a4741071
Wincent Colaiuta <win@wincent.com>
2010-07-13 20:08:45 +0200

    Remove duplicate spec file
    This one is definitely a duplicate (of the searches/new spec).
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/views/searches/index.html.haml_spec.rb
+++
@@ -1,8 +0,0 @@
-require File.expand_path('../../spec_helper', File.dirname(__FILE__))
-
-describe '/search/index' do
-  it 'should render the "form" partial' do
-    template.should_receive :render, :partial => 'search/form'
-    render '/search/index'
-  end
-end
