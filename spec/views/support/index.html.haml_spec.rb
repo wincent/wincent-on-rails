4586d187e2c035b99a56549a05e6777653f65d4c
Wincent Colaiuta <win@wincent.com>
2010-07-13 15:05:13 +0200

    Unbreak support#index view specs
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/views/support/index.html.haml_spec.rb
+++ spec/views/support/index.html.haml_spec.rb
@@ -1,58 +1,39 @@
 require File.expand_path('../../spec_helper', File.dirname(__FILE__))
 
-describe '/support/index' do
-  include SupportHelper
-
+describe 'support/index' do
   before do
-    assigns[:issues] = [create_issue]
+    @issues = [Issue.make!]
+    stub(view).render 'issues/issues'
+    stub.proxy(view).render
   end
 
-  def do_render
-    pending # url_for in lib/sortable.rb raises routing error
-    # :controller => 'support', :action => 'index'
-    # (ok, but :protocol => 'https' is suppressed, which causes failure)
-    render '/support/index'
+  it 'has a forums link' do
+    render
+    rendered.should have_selector('div.links a[href="/forums"]')
   end
 
-  it 'should have a forums link' do
-    do_render
-    response.should have_tag('div.links') do
-      with_tag 'a[href=?]', forums_path
-    end
+  it 'has a "lost license code" link' do
+    render
+    rendered.should have_selector('div.links a[href="https://secure.wincent.com/a/support/registration/"]')
   end
 
-  it 'should have a "lost license code" link' do
-    do_render
-    response.should have_tag('div.links') do
-      with_tag 'a[href=?]', 'https://secure.wincent.com/a/support/registration/'
-    end
+  it 'has an "all issues" link' do
+    render
+    rendered.should have_selector('div.links a[href="/issues"]')
   end
 
-  it 'should have an "all issues" link' do
-    do_render
-    response.should have_tag('div.links') do
-      with_tag 'a[href=?]', issues_path
-    end
+  it 'has a search link' do
+    render
+    rendered.should have_selector('div.links a[href="/issues/search"]')
   end
 
-  it 'should have a search link' do
-    do_render
-    response.should have_tag('div.links') do
-      # as noted elsewhere, url_for gives crazy results in view specs
-      # must use protected issues_search_path method
-      with_tag 'a[href=?]', template.send(:issues_search_path)
-    end
+  it 'has a "new issue" link' do
+    render
+    rendered.should have_selector('div.links a[href="/issues/new"]')
   end
 
-  it 'should have a "new issue" link' do
-    do_render
-    response.should have_tag('div.links') do
-      with_tag 'a[href=?]', new_issue_path
-    end
-  end
-
-  it 'should render the issues list partial' do
-    template.should_receive :render, :partial => 'issues/issues'
-    do_render
+  it 'renders the issues list partial' do
+    mock(view).render 'issues/issues'
+    render
   end
 end
