ddaf0d38671a6abdfd54a683bde4a93d78160f97
Wincent Colaiuta <win@wincent.com>
2010-07-13 23:28:32 +0200

    Unbreak links#new view specs
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/views/links/new.html.haml_spec.rb
+++ spec/views/links/new.html.haml_spec.rb
@@ -1,25 +1,15 @@
 require File.expand_path('../../spec_helper', File.dirname(__FILE__))
 
-describe '/links/new.html.haml' do
-  include LinksHelper
-
-  before(:each) do
-    @link = mock_model(Link)
-    @link.stub!(:new_record?).and_return(true)
-    @link.stub!(:uri).and_return(FR::random_string)
-    @link.stub!(:permalink).and_return(FR::random_string)
-    @link.stub!(:click_count).and_return(1)
-    assigns[:link] = @link
+describe 'links/new' do
+  before do
+    @link = Link.make
   end
 
-  it 'should render new form' do
-    render '/links/new.html.haml'
-
-    response.should have_tag('form[action=?][method=post]', links_path) do
-      with_tag('input#link_uri[name=?]', 'link[uri]')
-      with_tag('input#link_permalink[name=?]', 'link[permalink]')
+  it 'renders new form' do
+    render
+    rendered.should have_selector('form[method=post]', :action => links_path) do |form|
+      form.should have_selector('input#link_uri', :name => 'link[uri]')
+      form.should have_selector('input#link_permalink', :name => 'link[permalink]')
     end
   end
 end
-
-
