c503d2321b3d8d61917b970de307fffc10ada881
Wincent Colaiuta <win@wincent.com>
2010-07-13 22:26:54 +0200

    Remove CommentMailer view specs
    Seems to be an uphill battle trying to spec these things when the
    convention is to do integrated testing of the mailer and the view at the
    same time (see specs/mailers folder).
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/views/comment_mailer/new_comment_alert.erb_spec.rb
+++
@@ -1,5 +0,0 @@
-require File.expand_path('../../spec_helper', File.dirname(__FILE__))
-
-describe '/comments_mailer/new_comment_alert' do
-  # TODO: figure out how to render mailer views in specs
-end
