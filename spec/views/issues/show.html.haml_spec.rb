d1ca388ef674e367344a2def4e87e792088fe2e7
Wincent Colaiuta <win@wincent.com>
2010-07-14 18:09:42 +0200

    Unbreak issues/show view specs
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/views/issues/show.html.haml_spec.rb
+++ spec/views/issues/show.html.haml_spec.rb
@@ -1,103 +1,89 @@
 require File.expand_path('../../spec_helper', File.dirname(__FILE__))
 
-describe '/issues/show' do
-  include IssuesHelper
-
+describe 'issues/show' do
   before do
-    assigns[:issue]     = @issue = create_issue
-    assigns[:comments]  = []
-    assigns[:comment]   = Comment.new
-  end
-
-  def do_render
-    render '/issues/show'
+    @issue    = Issue.make!
+    @comments = []
+    @comment  = Comment.new
+    # BUG: http://github.com/rspec/rspec-rails/issues/119
+    # this really is a pretty horrible kludge, getting a fix is a very high
+    # priority
+    stub(view).admin? { false }
+    stub(view).logged_in? { true }
+    user = User.make!
+    stub(view).current_user { user }
   end
 
   it 'should show breadcrumbs' do
-    do_render
-    response.should have_tag('div#breadcrumbs', /#{@issue.kind_string.humanize} \##{@issue.id}/) do
-      with_tag 'a[href=?]', root_path
-      with_tag 'a[href=?]', issues_path
-    end
+    render
+    rendered.should have_selector('div#breadcrumbs', :content => "#{@issue.kind_string.humanize} \##{@issue.id}")
   end
 
   it 'should advertise an atom feed' do
-    template.should_receive(:atom_link).with(@issue)
-    do_render
-  end
-end
-
-describe '/issues/show for a private issue' do
-  include IssuesHelper
-
-  before do
-    assigns[:issue]     = @issue = create_issue(:public => false)
-    assigns[:comments]  = []
-    assigns[:comment]   = Comment.new
+    mock(view).atom_link(@issue)
+    render
   end
 
-  it 'should not advertise an atom feed' do
-    template.should_not_receive(:atom_link).with(@issue)
-    render '/issues/show'
-  end
-end
+  context 'private issue' do
+    before do
+      @issue    = Issue.make!(:public => false)
+      @comments = []
+      @comment  = Comment.new
+    end
 
-describe '/issues/show viewed by an administrator' do
-  include IssuesHelper
-
-  before do
-    assigns[:issue]     = @issue = create_issue
-    assigns[:comments]  = []
-    assigns[:comment]   = Comment.new
-    template.should_receive(:admin?).at_least(:once).and_return(true)
-    render '/issues/show'
+    it 'should not advertise an atom feed' do
+      do_not_allow(view).atom_link.with_any_args
+      render
+    end
   end
 
-  it 'should show an edit link' do
-    response.should have_tag('a[href=?]', edit_issue_path(@issue))
+  context 'viewed by an administrator' do
+    before do
+      @issue    = Issue.make!
+      @comments = []
+      @comment  = Comment.new
+      stub(view).admin? { true }
+      render
+    end
+
+    it 'should show an edit link' do
+      rendered.should have_selector('a', :href => edit_issue_path(@issue))
+    end
+
+    it 'should show a destroy link' do
+      rendered.should match(/destroy/) # not sure how best to test this, so this is a cheap stand-in for now
+    end
   end
 
-  it 'should show a destroy link' do
-    response.should have_text(/destroy/) # not sure how best to test this, so this is a cheap stand-in for now
-  end
-end
+  context 'viewed by a normal user' do
+    before do
+      @issue    = Issue.make!
+      @comments = []
+      @comment  = Comment.new
+      stub(view).admin? { false }
+      render
+    end
 
-describe '/issues/show viewed by a normal user' do
-  include IssuesHelper
+    it 'should not show an edit link' do
+      rendered.should_not have_selector('a', :href => edit_issue_path(@issue))
+    end
 
-  before do
-    assigns[:issue]     = @issue = create_issue
-    assigns[:comments]  = []
-    assigns[:comment]   = Comment.new
-    template.should_receive(:admin?).at_least(:once).and_return(false)
-    render '/issues/show'
-  end
-
-  it 'should not show an edit link' do
-    response.should_not have_tag('a[href=?]', edit_issue_path(@issue))
+    it 'should not show a destroy link' do
+      rendered.should_not contain(/destroy/) # not sure how best to test this, so this is a cheap stand-in for now
+    end
   end
 
-  it 'should not show a destroy link' do
-    response.should_not have_text(/destroy/) # not sure how best to test this, so this is a cheap stand-in for now
-  end
-end
+  context 'no description' do
+    before do
+      @issue    = Issue.make!(:description => '')
+      @comments = []
+      @comment  = Comment.new
+    end
 
-describe '/issues/show with no description' do
-  include IssuesHelper
-
-  before do
-    assigns[:issue]     = @issue = create_issue(:description => '')
-    assigns[:comments]  = []
-    assigns[:comment]   = Comment.new
-  end
-
-  def do_render
-    render '/issues/show'
-  end
-
-  it 'should show "none"' do
-    do_render
-    # this is currently a false positive (catches "Description ... Tags ... none")
-    response.should have_text(/Description.+none/m)
+    it 'should show "none"' do
+      render
+      # this is currently a false positive (catches "Description ... Tags ... none")
+      rendered.should contain(/Description.+none/m)
+    end
   end
 end
