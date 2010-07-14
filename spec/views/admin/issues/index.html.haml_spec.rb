d55dba86c0e0ddef362cea2d59709878b279cd54
Wincent Colaiuta <win@wincent.com>
2010-07-14 23:26:47 +0200

    Unbreak admin view specs
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/views/admin/issues/index.html.haml_spec.rb
+++ spec/views/admin/issues/index.html.haml_spec.rb
@@ -1,38 +1,30 @@
 require File.expand_path('../../../spec_helper', File.dirname(__FILE__))
 
-describe '/admin/issues/index' do
+describe 'admin/issues/index' do
   before do
-    @issue1, @issue2 = create_issue, create_issue
-    assigns[:issues] = [@issue1, @issue2]
+    @issue1, @issue2 = Issue.make!, Issue.make!
+    @issues = [@issue1, @issue2]
   end
 
-  def do_render
-    render '/admin/issues/index'
+  it 'has an "all issues" link' do
+    render
+    rendered.should have_selector('div.links a', :href => issues_path)
   end
 
-  it 'should have an "all issues" link' do
-    do_render
-    response.should have_tag('div.links') do
-      with_tag 'a[href=?]', issues_path
-    end
+  it 'has a "refresh" link' do
+    render
+    rendered.should have_selector('div.links a', :href => admin_issues_path)
   end
 
-  it 'should have a "refresh" link' do
-    do_render
-    response.should have_tag('div.links') do
-      with_tag 'a[href=?]', admin_issues_path
-    end
+  it 'has a "destroy" button for each issue' do
+    mock(view).button_to_destroy_issue(@issue1)
+    mock(view).button_to_destroy_issue(@issue2)
+    render
   end
 
-  it 'should have a "destroy" button for each issue' do
-    template.should_receive(:button_to_destroy_issue).with(@issue1)
-    template.should_receive(:button_to_destroy_issue).with(@issue2)
-    do_render
-  end
-
-  it 'should have a "ham" button for each issue' do
-    template.should_receive(:button_to_moderate_issue_as_ham).with(@issue1)
-    template.should_receive(:button_to_moderate_issue_as_ham).with(@issue2)
-    do_render
+  it 'has a "ham" button for each issue' do
+    mock(view).button_to_moderate_issue_as_ham(@issue1)
+    mock(view).button_to_moderate_issue_as_ham(@issue2)
+    render
   end
 end
