a8ad98bef076901e8ddb0f6740dae7acad663832
Wincent Colaiuta <win@wincent.com>
2010-07-14 21:56:43 +0200

    Unbreak issues#search view specs
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ spec/views/issues/search/create.html.haml_spec.rb
@@ -0,0 +1,46 @@
+require File.expand_path('../../../spec_helper', File.dirname(__FILE__))
+
+describe 'issues/search/create' do
+  before do
+    @issues = [Issue.make!]
+    stub(view).render 'issues/search/form'
+    stub(view).render 'issues/issues'
+    stub.proxy(view).render
+  end
+
+  it 'has an "all issues" link' do
+    render
+    rendered.should have_selector('div.links a', :href => issues_path)
+  end
+
+  it 'has a "search again" link' do
+    render
+    # this is a complex JS link, so won't try too hard to test the actual onclick attribute
+    rendered.should have_selector('div.links a', :content => 'search again')
+  end
+
+  it 'hides the search div upon initial display' do
+    render
+    rendered.should have_selector('div#issue_search', :style => 'display:none;')
+  end
+
+  it 'has a "new issue" link' do
+    render
+    rendered.should have_selector('div.links a', :href => new_issue_path)
+  end
+
+  it 'has a "support overview" link' do
+    render
+    rendered.should have_selector('div.links a', :href => support_path)
+  end
+
+  it 'renders the search form partial' do
+    mock(view).render 'issues/search/form'
+    render
+  end
+
+  it 'renders the issues list partial' do
+    mock(view).render 'issues/issues'
+    render
+  end
+end
---
+++ spec/views/issues/search/new.html.haml_spec.rb
@@ -0,0 +1,33 @@
+require File.expand_path('../../../spec_helper', File.dirname(__FILE__))
+
+describe 'issues/search/new' do
+  before do
+    stub(view).render 'issues/search/form'
+    stub.proxy(view).render
+  end
+
+  it 'has breadcrumbs' do
+    mock(view).breadcrumbs.with_any_args
+    render
+  end
+
+  it 'has an "all issues" link' do
+    render
+    rendered.should have_selector('div.links a', :href => issues_path)
+  end
+
+  it 'has a "support overview" link' do
+    render
+    rendered.should have_selector('div.links a', :href => support_path)
+  end
+
+  it 'has a "site search" link' do
+    render
+    rendered.should have_selector('div.links a', :href => new_search_path)
+  end
+
+  it 'renders the search form partial' do
+    mock(view).render 'issues/search/form'
+    render
+  end
+end
