71854540f7b66c0813ffb5af7c53f2bfb71a35a1
Wincent Colaiuta <win@wincent.com>
2010-07-14 23:19:35 +0200

    Unbreak layout specs
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/views/layouts/application.haml_spec.rb
+++ spec/views/layouts/application.haml_spec.rb
@@ -1,10 +1,10 @@
 require File.expand_path('../../spec_helper', File.dirname(__FILE__))
 
-describe '/layouts/application' do
-  it 'should have a search form' do
-    render '/layouts/application'
-    response.should have_tag('form[action=?]', search_index_path) do
-      with_tag 'input[name=query][type=text]'
+describe 'layouts/application' do
+  it 'has a search form' do
+    render
+    rendered.should have_selector('form', :action => searches_path, :method => 'post') do |form|
+      form.should have_selector('input[name=query][type=text]')
     end
   end
 end
