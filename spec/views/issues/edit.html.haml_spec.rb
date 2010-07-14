ff68fc6bb63520c464d3f600f8973394a14ac917
Wincent Colaiuta <win@wincent.com>
2010-07-14 22:57:07 +0200

    Unbreak issues#edit view specs
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/views/issues/edit.html.haml_spec.rb
+++ spec/views/issues/edit.html.haml_spec.rb
@@ -1,64 +1,53 @@
 require File.expand_path('../../spec_helper', File.dirname(__FILE__))
 
-describe '/issues/edit' do
-  include IssuesHelper
-
+describe 'issues/edit' do
   before do
-    assigns[:issue] = @issue = create_issue
-  end
-
-  def do_render
-    render '/issues/edit'
+    stub(view).render 'shared/error_messages', anything
+    stub(view).render 'issues/form', anything
+    stub.proxy(view).render
+    @issue = Issue.make!
   end
 
   it 'should have a div for the issue' do
-    do_render
-    response.should have_tag("\#issue_#{@issue.id}")
+    render
+    rendered.should have_selector("\#issue_#{@issue.id}")
   end
 
   it 'should render the form partial' do
-    form = mock('form', :null_object => true)
-    template.stub!(:form_for).and_yield(form)
-    template.should_receive :render, :partial => 'issues/form', :locals => { :f => form }
-    do_render
+    mock(view).render 'issues/form', anything
+    render
   end
 
   it 'should have a "show" link' do
-    do_render
-    response.should have_tag('.links') do
-      with_tag 'a[href=?]', issue_path(@issue)
-    end
+    render
+    rendered.should have_selector('.links a', :href => issue_path(@issue))
   end
 
   it 'should have a destroy button' do
-    template.should_receive(:button_to_destroy_issue).with(@issue)
-    do_render
+    mock(view).button_to_destroy_issue @issue
+    render
   end
 
   it 'should have a ham button if the issue is awaiting moderation' do
-    assigns[:issue] = @issue = create_issue(:awaiting_moderation => true)
-    template.should_receive(:button_to_moderate_issue_as_ham).with(@issue)
-    do_render
+    @issue = Issue.make! :awaiting_moderation => true
+    mock(view).button_to_moderate_issue_as_ham @issue
+    render
   end
 
   # was a bug
   it 'should not have a ham button if the issue is not awaiting moderation' do
-    assigns[:issue] = @issue = create_issue(:awaiting_moderation => false)
-    template.should_not_receive(:button_to_moderate_issue_as_ham)
-    do_render
+    @issue = Issue.make! :awaiting_moderation => false
+    do_not_allow(view).button_to_moderate_issue_as_ham
+    render
   end
 
   it 'should have a link back to the list of issues awaiting moderation' do
-    do_render
-    response.should have_tag('.links') do
-      with_tag 'a[href=?]', admin_issues_path
-    end
+    render
+    rendered.should have_selector('.links a', :href => admin_issues_path)
   end
 
   it 'should have a link to the list of public isues' do
-    do_render
-    response.should have_tag('.links') do
-      with_tag 'a[href=?]', issues_path
-    end
+    render
+    rendered.should have_selector('.links a', :href => issues_path)
   end
 end
