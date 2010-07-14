8398b06c30794ff18b9dc7aa9d0b051a0ac878ec
Wincent Colaiuta <win@wincent.com>
2010-07-14 23:49:01 +0200

    Unbreak dashboard#show view specs
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/views/dashboard/show.html.haml_spec.rb
+++ spec/views/dashboard/show.html.haml_spec.rb
@@ -1,37 +1,35 @@
 require File.expand_path('../../spec_helper', File.dirname(__FILE__))
 
-describe '/dashboard/show viewed as admin user' do
-  include DashboardHelper
-
+describe 'dashboard/show' do
   before do
-    template.stub!(:current_user).and_return(new_user)
-    template.should_receive(:admin?).and_return(true)
-    assigns[:issues] = []
-    assigns[:topics] = []
-    assigns[:comments] = []
-    render '/dashboard/show'
+    @issues = []
+    @topics = []
+    @comments = []
   end
 
-  it 'should provide a link to the admin dashboard' do
-    response.should have_tag('.links') do
-      with_tag 'a[href=?]', admin_dashboard_path
+  context 'viewed as admin user' do
+    before do
+      user = User.make! :superuser => true
+      stub(view).current_user { user }
+      stub(view).admin? { true }
+      render
+    end
+
+    it 'provides a link to the admin dashboard' do
+      rendered.should have_selector('.links a', :href => admin_dashboard_path)
     end
   end
-end
 
-describe '/dashboard/show viewed as normal user' do
-  include DashboardHelper
+  context 'viewed as normal user' do
+    before do
+      user = User.make! :superuser => false
+      stub(view).current_user { user }
+      stub(view).admin? { false }
+      render
+    end
 
-  before do
-    template.stub!(:current_user).and_return(new_user)
-    template.should_receive(:admin?).and_return(false)
-    assigns[:issues] = []
-    assigns[:topics] = []
-    assigns[:comments] = []
-    render '/dashboard/show'
-  end
-
-  it 'should not provide a link to the admin dashboard' do
-    response.should_not have_tag('a[href=?]', admin_dashboard_path)
+    it 'does not provide a link to the admin dashboard' do
+      rendered.should_not have_selector('a', :href => admin_dashboard_path)
+    end
   end
 end
