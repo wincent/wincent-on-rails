d15fe16382292b8bbd9a7faae6c89b78ab6dc481
Wincent Colaiuta <win@wincent.com>
2010-07-15 00:05:18 +0200

    Unbreak articles#index (Atom format) view spec
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/views/articles/index.atom.builder_spec.rb
+++ spec/views/articles/index.atom.builder_spec.rb
@@ -1,14 +1,8 @@
 require File.expand_path('../../spec_helper', File.dirname(__FILE__))
 
-describe '/articles/index.atom.builder' do
-  include ArticlesHelper
-
-  def do_render
-    render '/articles/index.atom.builder'
-  end
-
-  it 'should handle no articles' do
-    assigns[:articles] = []
-    lambda { do_render }.should_not raise_error
+describe 'articles/index.atom.builder' do
+  it 'handles no articles' do
+    @articles = []
+    lambda { render }.should_not raise_error
   end
 end
