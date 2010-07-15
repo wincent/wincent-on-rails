8e0e9ef7f41367d68aa6341b436d5312caed2164
Wincent Colaiuta <win@wincent.com>
2010-07-15 18:46:23 +0200

    Teach VimFormatter to use growlnotify
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/support/vim_formatter.rb
+++ spec/support/vim_formatter.rb
@@ -28,9 +28,14 @@ module RSpec
         def dump_pending; end
 
         def close
-          # summary_line example_count, failure_count, pending_count
-          # growlnotify here
-          super
+          summary = summary_line example_count, failure_count, pending_count
+          if failure_count > 0
+            system "growlnotify -n autotest --image ./autotest/fail.png -p Emergency -m '#{summary}' -t 'Spec failure detected'"
+          elsif pending_count > 0
+            system "growlnotify -n autotest --image ./autotest/pending.png -p High -m '#{summary}' -t 'Pending spec(s) present'"
+          else
+            system "growlnotify -n autotest --image ./autotest/pass.png -p 'Very Low' -m '#{summary}' -t 'All specs passed'"
+          end
         end
 
       private
