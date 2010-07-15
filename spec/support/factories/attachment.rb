0a7c74d26f180e6545be6dca84eeb04a87e65a9b
Wincent Colaiuta <win@wincent.com>
2010-07-15 07:44:25 +0200

    Fix attachments factory
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/support/factories/attachment.rb
+++ spec/support/factories/attachment.rb
@@ -2,4 +2,6 @@ Factory.define :attachment do |a|
   a.mime_type 'image/png'
   a.original_filename { Sham.random + '.png' }
   a.filesize { rand(1_000_000) + 4096 }
+  a.path 'ab/aaaabbbbccccddddeeeeffff01234567890123456789012345678901234567'
+  a.digest 'abaaaabbbbccccddddeeeeffff01234567890123456789012345678901234567'
 end
