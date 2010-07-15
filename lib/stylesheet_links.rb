ee0abf5d567a7a4c1e75928ea8eb77b22e30cf92
Wincent Colaiuta <win@wincent.com>
2010-07-15 12:24:32 +0200

    Use ActiveSupport::Concern to simplify StylesheetLinks module
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- lib/stylesheet_links.rb
+++ lib/stylesheet_links.rb
@@ -1,8 +1,6 @@
 module ActionController
   module StylesheetLinks
-    def self.included base
-      base.extend(ClassMethods)
-    end
+    extend ActiveSupport::Concern
 
     module ClassMethods
       def uses_stylesheet_links options = {}
