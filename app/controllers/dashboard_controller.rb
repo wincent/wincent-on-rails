b742e3929d3c2793ee81a3e7f4898970994197f7
Wincent Colaiuta <win@wincent.com>
2010-07-16 00:16:28 +0200

    New query API for dashboard controller
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,9 +5,11 @@ class DashboardController < ApplicationController
   def show
     # potentially add :awaiting_moderation => false here as well
     # (not sure, may want to actually show such items to user)
-    conditions = { :user_id => current_user }
-    @issues   = Issue.find :all, :conditions => conditions, :limit => 10, :order => 'updated_at DESC'
-    @topics   = Topic.find :all, :conditions => conditions, :include => :forum, :limit => 10, :order => 'topics.updated_at DESC'
-    @comments = Comment.find :all, :conditions => conditions, :limit => 10, :order => 'updated_at DESC'
+    @issues   = Issue.where(:user_id => current_user).
+      limit(10).order('updated_at DESC')
+    @topics   = Topic.where(:user_id => current_user).
+      limit(10).order('topics.updated_at DESC').includes(:forum)
+    @comments = Comment.where(:user_id => current_user).
+      limit(10).order('updated_at DESC')
   end
 end
