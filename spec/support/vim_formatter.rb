c7158fa3c46ab7c29012c2b2e6b9c0db0ebadfe2
Wincent Colaiuta <win@wincent.com>
2010-07-16 22:45:26 +0200

    Copy growlnotify method from GrowlFormatter
    Should probably make the VimFormatter inherit from the GrowlFormatter so
    as to avoid the duplication.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/support/vim_formatter.rb
+++ spec/support/vim_formatter.rb
@@ -30,15 +30,22 @@ module RSpec
         def close
           summary = summary_line example_count, failure_count, pending_count
           if failure_count > 0
-            system "growlnotify -n autotest --image ./autotest/fail.png -p Emergency -m '#{summary}' -t 'Spec failure detected'"
+            growlnotify "--image ./autotest/fail.png -p Emergency -m '#{summary}' -t 'Spec failure detected'"
           elsif pending_count > 0
-            system "growlnotify -n autotest --image ./autotest/pending.png -p High -m '#{summary}' -t 'Pending spec(s) present'"
+            growlnotify "--image ./autotest/pending.png -p High -m '#{summary}' -t 'Pending spec(s) present'"
           else
-            system "growlnotify -n autotest --image ./autotest/pass.png -p 'Very Low' -m '#{summary}' -t 'All specs passed'"
+            growlnotify "--image ./autotest/pass.png -p 'Very Low' -m '#{summary}' -t 'All specs passed'"
           end
         end
 
       private
+
+        def growlnotify str
+          system 'which growlnotify > /dev/null'
+          if $?.exitstatus == 0
+            system "growlnotify -n autotest #{str}"
+          end
+        end
 
         def relativize_path path
           @wd ||= Pathname.new Dir.getwd
