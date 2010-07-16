32a052376005abea536394a9f13e0ac01569749a
Wincent Colaiuta <win@wincent.com>
2010-07-16 20:31:25 +0200

    Port issue annotation feature from Cucumber to Steak
    Note that I may need to rethink my labelling practices here: I can't
    just look for "Summary", because my label tag also includes an
    additional annotation, thus forcing me to specify an ID.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/acceptance/support/helpers.rb
+++ spec/acceptance/support/helpers.rb
@@ -1,5 +1,11 @@
 module HelperMethods
-  # Put helper methods you need to be available in all tests here.
+  def log_in_as_admin
+    user = User.make! :superuser => true
+    visit '/login'
+    fill_in 'Email address', :with => user.emails.first.address
+    fill_in 'Passphrase', :with => Sham.passphrase
+    click_button 'Log in'
+  end
 end
 
 RSpec.configuration.include(HelperMethods)
