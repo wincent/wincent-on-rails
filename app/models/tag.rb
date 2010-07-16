4d422b33a96a2c3088cb0b1b024fab62ba56d04b
Wincent Colaiuta <win@wincent.com>
2010-07-16 15:48:18 +0200

    Drop unnecessary explicit receiver
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -34,7 +34,7 @@ class Tag < ActiveRecord::Base
     return [] if count < 1 or count > 5 # sanity limit: no more than 5 joins
     raise ArgumentError if tag_names.include?(nil)
     query = Array.new(count, 'name = ?').join ' OR '
-    tags = Tag.where(query, *tag_names)
+    tags = where(query, *tag_names)
     tags_reachable_from_tags *tags
   end
 
