a2119e1ef7211d67f2433fbeb8523d1dbb8f3a26
Wincent Colaiuta <win@wincent.com>
2010-07-16 21:14:08 +0200

    Remove features directory
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- features/step_definitions/wiki.rb
+++
@@ -1,7 +0,0 @@
-Given /^no articles in the wiki$/ do
-  Article.destroy_all
-end
-
-Given /^an article titled "(.*)"$/ do |title|
-  create_article :title => title
-end
--- features/support/env_additions.rb
+++
@@ -1,4 +0,0 @@
-# custom additions to "env.rb" file
-require 'fixture_replacement'
-include FixtureReplacement
-Capybara.javascript_driver = :culerity
