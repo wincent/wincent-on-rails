9c9351fb3e2fd46138fb4fce603f4a0eb13ddde9
Wincent Colaiuta <win@wincent.com>
2010-07-16 19:20:43 +0200

    New query API for admin issues controller
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/controllers/admin/issues_controller.rb
+++ app/controllers/admin/issues_controller.rb
@@ -3,11 +3,9 @@ class Admin::IssuesController < ApplicationController
 
   # TODO: compact interface for mass-deleting spam issues using checkboxes
   def index
-    @paginator  = Paginator.new params,
-      Issue.count(:conditions => { :awaiting_moderation => true }),
-      admin_issues_path, 20
-    @issues     = Issue.find :all, :offset => @paginator.offset,
-      :conditions => { :awaiting_moderation => true },
-      :order => 'created_at DESC', :limit => @paginator.limit
+    issues      = Issue.where :awaiting_moderation => true
+    @paginator  = Paginator.new params, issues.count, admin_issues_path, 20
+    @issues     = issues.order('created_at DESC').limit(@paginator.limit).
+      offset(@paginator.offset)
   end
 end
