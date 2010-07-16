9b246e0e8af4430edcc6cd478fe76881820d53f5
Wincent Colaiuta <win@wincent.com>
2010-07-16 20:02:34 +0200

    Translate wiki feature from Cucumber to Steak
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ spec/acceptance/wiki/index_spec.rb
@@ -0,0 +1,20 @@
+require File.expand_path('../acceptance_helper', File.dirname(__FILE__))
+
+feature 'the wiki index' do
+  scenario 'no articles in the wiki' do
+    visit '/wiki'
+    page.should have_content('Recently updated')
+    page.should have_content('Top tags')
+  end
+
+  scenario 'several articles in the wiki' do
+    Article.make! :title => 'foo'
+    Article.make! :title => 'bar'
+    Article.make! :title => 'baz'
+    visit '/wiki'
+    page.should have_content('Recently updated')
+    page.should have_content('foo')
+    page.should have_content('bar')
+    page.should have_content('baz')
+  end
+end
