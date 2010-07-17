4f03eac0b692949427c54412bef5ad1f3d245a39
Wincent Colaiuta <win@wincent.com>
2010-07-18 00:45:12 +0200

    New specs for resets#edit action
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/controllers/resets_controller_spec.rb
+++ spec/controllers/resets_controller_spec.rb
@@ -83,4 +83,74 @@ describe ResetsController do
       response.should redirect_to('/resets/foobar/edit')
     end
   end
+
+  describe '#edit' do
+    let(:reset) { Reset.make! }
+
+    def do_get
+      get :edit, :id => reset.secret
+    end
+
+    it 'finds and assigns the reset' do
+      do_get
+      assigns(:reset).should == reset
+    end
+
+    it 'finds and assigns the user' do
+      do_get
+      assigns(:user).should == reset.email.user
+    end
+
+    it 'renders resets/edit' do
+      do_get
+      response.should render_template('resets/edit')
+    end
+
+    it 'succeeds' do
+      do_get
+      response.should be_success
+    end
+
+    context 'no reset token found' do
+      let(:reset) { Reset.make :secret => 'non-existent' }
+
+      it 'shows a flash' do
+        do_get
+        cookie_flash['error'].should =~ /token not found/
+      end
+
+      it 'redirects to /' do
+        do_get
+        response.should redirect_to('/')
+      end
+    end
+
+    context 'token already used' do
+      let(:reset) { Reset.make! :completed_at => 3.days.ago }
+
+      it 'shows a flash' do
+        do_get
+        cookie_flash['notice'].should =~ /token already used/
+      end
+
+      it 'redirects to /login' do
+        do_get
+        response.should redirect_to('/login')
+      end
+    end
+
+    context 'expired token' do
+      let(:reset) { Reset.make! :cutoff => 1.month.ago }
+
+      it 'shows a flash' do
+        do_get
+        cookie_flash['error'].should =~ /expiry date has already passed/
+      end
+
+      it 'redirects to /' do
+        do_get
+        response.should redirect_to('/')
+      end
+    end
+  end
 end
