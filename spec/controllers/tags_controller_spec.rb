3842bcedc6e2fc91130f427ea6b1bffd96e97140
Wincent Colaiuta <win@wincent.com>
2010-07-18 20:21:36 +0200

    Specs for tags#show and tags#search actions
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -30,4 +30,71 @@ describe TagsController do
       response.should render_template('tags/index')
     end
   end
+
+  describe '#show' do
+    before do
+      @tag = Tag.make! :name => 'foo'
+    end
+
+    def do_get
+      get :show, :id => 'foo'
+    end
+
+    it 'finds and assigns the tag' do
+      do_get
+      assigns[:tag].should == @tag
+    end
+
+    it 'finds and assigns grouped taggables' do
+      mock(Tagging).grouped_taggables_for_tag @tag, anything, anything
+      do_get
+    end
+
+    it 'finds and assigns reachable tags' do
+      mock(Tag).tags_reachable_from_tags @tag
+      do_get
+    end
+  end
+
+  describe '#search' do
+    before do
+      Article.make!.tag 'foo'
+      @tag = Tag.find_by_name 'foo'
+    end
+
+    it 'trims excess tags (more than 10)' do
+      get :search, :q => '1 2 3 4 5 6 7 8 9 10 11'
+      cookie_flash['notice'].should =~ /excess tags stripped/i
+    end
+
+    it 'excludes non-existent tags' do
+      get :search, :q => 'foo bar'
+      cookie_flash['notice'].should =~ /non-existent tags excluded/i
+    end
+
+    it 'finds and assigns tags' do
+      mock.proxy(Tagging).grouped_taggables_for_tag_names ['foo'], anything, anything
+      get :search, :q => 'foo'
+      assigns[:tags][:found].should == [@tag]
+    end
+
+    it 'finds and assigns taggables' do
+      mock.proxy(Tagging).grouped_taggables_for_tag_names ['foo'], anything, anything
+      get :search, :q => 'foo'
+      assigns[:taggables].should_not be_nil # too lazy to actually spec this
+    end
+
+    it 'finds and assigns reachable tags' do
+      mock(Tag).tags_reachable_from_tags([@tag]) { :reachables }
+      get :search, :q => 'foo'
+      assigns[:reachable_tags].should == :reachables
+    end
+
+    context 'no params' do
+      it 'renders tags/search' do
+        get :search
+        response.should render_template 'tags/search'
+      end
+    end
+  end
 end
