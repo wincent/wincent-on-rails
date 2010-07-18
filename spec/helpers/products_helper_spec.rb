2e2dcd66748a53fbc0c11485e19df6bec93180de
Wincent Colaiuta <win@wincent.com>
2010-07-18 20:33:31 +0200

    Add products helper specs
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ spec/helpers/products_helper_spec.rb
@@ -0,0 +1,22 @@
+require File.expand_path('../spec_helper', File.dirname(__FILE__))
+
+describe ProductsHelper do
+  describe '#product_page_title' do
+    before do
+      @product = Product.make! :name => 'foo'
+      @page = Page.make! :title => 'bar', :product => @product
+    end
+
+    context 'product only, no page' do
+      it 'returns the product name' do
+        product_page_title(@product, nil).should == 'foo'
+      end
+    end
+
+    context 'product and page' do
+      it 'joins product name and page title' do
+        product_page_title(@product, @page).should == 'foo: bar'
+      end
+    end
+  end
+end
