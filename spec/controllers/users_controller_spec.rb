600a8c810d2d0329a63ef7ea2ae91ac6c1ed6196
Wincent Colaiuta <win@wincent.com>
2010-07-19 17:12:53 +0200

    More user controller specs
    Additional specs for #index action, and specs for the #new action.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -18,6 +18,45 @@ describe UsersController do
         do_request
         assigns[:users].to_a.should =~ User.all
       end
+
+      it 'renders users/index' do
+        do_request
+        response.should render_template('users/index')
+      end
+
+      it 'succeeds' do
+        do_request
+        response.should be_success
+      end
+    end
+  end
+
+  describe '#new' do
+    it 'assigns a new user' do
+      get :new
+      assigns[:user].should be_kind_of(User)
+      assigns[:user].should be_new_record
+    end
+
+    it 'assigns a new email' do
+      get :new
+      assigns[:email].should be_kind_of(Email)
+      assigns[:email].should be_new_record
+    end
+
+    it 'associates the email with the user' do
+      stub(User).new.mock!.emails.mock!.build
+      get :new
+    end
+
+    it 'renders users/new' do
+      get :new
+      response.should render_template('users/new')
+    end
+
+    it 'succeeds' do
+      get :new
+      response.should be_success
     end
   end
 end
