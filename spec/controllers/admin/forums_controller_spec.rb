774c76a8a9ac4c672c94b7a67e7f909d587ab94e
Wincent Colaiuta <win@wincent.com>
2010-07-19 09:11:59 +0200

    Add specs for admin/forums#show.js action
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/controllers/admin/forums_controller_spec.rb
+++ spec/controllers/admin/forums_controller_spec.rb
@@ -18,6 +18,18 @@ shared_examples_for 'require_admin' do
   end
 end
 
+shared_examples_for 'require_admin (non-HTML)' do
+  it 'returns status 403 (forbidden)' do
+    do_request
+    response.status.should == 403
+  end
+
+  it 'renders "forbidden" test' do
+    do_request
+    response.body.should match(/forbidden/i)
+  end
+end
+
 describe Admin::ForumsController do
   describe '#index' do
     def do_request
@@ -48,6 +60,50 @@ describe Admin::ForumsController do
       it 'assigns found forums' do
         do_request
         assigns[:forums] =~ [@forum1, @forum2]
+      end
+    end
+  end
+
+  describe '#show.js' do
+    before do
+      @forum = Forum.make! :permalink => 'foo'
+    end
+
+    def do_request
+      get :show, :id => @forum.id, :format => :js
+    end
+
+    it_should_behave_like 'require_admin (non-HTML)'
+
+    context 'as admin' do
+      before do
+        log_in_as_admin
+      end
+
+      it 'finds the forum' do
+        mock.proxy(Forum).find(@forum.id)
+        do_request
+      end
+
+      it 'assigns found forum' do
+        do_request
+        assigns[:forum].should == @forum
+      end
+
+      it 'renders as JSON' do
+        stub(Forum).find(@forum.id) { @forum }
+        mock(@forum).to_json(anything)
+        do_request
+      end
+
+      it 'includes only name, description and position' do
+        do_request
+        json = JSON.parse(response.body)
+        json['forum'].should == {
+          'name'        => @forum.name,
+          'description' => nil,
+          'position'    => 0
+        }
       end
     end
   end
