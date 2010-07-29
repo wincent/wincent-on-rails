cfb1fc9d2f6dd862e4492667820b116ca0a0292f
Wincent Colaiuta <win@wincent.com>
2010-07-29 13:08:47 +0200

    Merge in changes from fresh 3.0.0.rc application
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,1 +1,1 @@
-Rails.application.config.secret_token = APP_CONFIG['session_secret']
+Wincent::Application.config.secret_token = APP_CONFIG['session_secret']
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, :key => APP_CONFIG['session_key']
+Wincent::Application.config.session_store :cookie_store, :key => APP_CONFIG['session_key']
