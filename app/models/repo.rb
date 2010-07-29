b2f119d7e345d2670bd6e13c289a2972b5b3e641
Wincent Colaiuta <win@wincent.com>
2010-07-29 13:09:29 +0200

    Add missing parts of commit c12c584
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -29,6 +29,8 @@
 #   string   "permalink",                      :null => false
 #   string   "path",                           :null => false
 #   string   "description"
+#   string   "clone_url"
+#   string   "rw_clone_url"
 #   integer  "product_id"
 #   boolean  "public",      :default => false
 #   datetime "created_at"
@@ -39,5 +41,6 @@ class Repo < ActiveRecord::Base
   validates_presence_of :name, :path, :permalink
   validates_format_of :path, :with => %r{\A(/([a-z0-9._-]+))+\z}i,
     :message => 'must have format "/foo/bar/baz"'
-  attr_accessible :description, :name, :path, :permalink, :product, :public
+  attr_accessible :clone_url, :description, :name, :path, :permalink, :product,
+    :public, :rw_clone_url
 end
