27cf7f184eb8a813436515c08b05b104599d9013
Wincent Colaiuta <win@wincent.com>
2010-07-30 18:43:37 +0200

    Teach Git spec helpers to accept blocks
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/support/git_spec_helpers.rb
+++ spec/support/git_spec_helpers.rb
@@ -2,24 +2,28 @@ require 'mkdtemp'
 require 'pathname'
 
 module GitSpecHelpers
-  def scratch_repo
-    Dir.mkdtemp do
+  def scratch_repo &block
+    path = Dir.mkdtemp do
       `git init`
       `echo "foo" > file`
       `git add file`
       `git commit -m "initial import"`
     end
+    Dir.chdir(path, &block) if block_given?
+    path
   end
 
-  def bare_scratch_repo
+  def bare_scratch_repo &block
     bare = Dir.mkdtemp do
       `git init --bare`
     end
 
     # simplest way to get something into a bare repo is to push into it
-    Dir.chdir scratch_repo do
+    scratch_repo do
       `git push #{bare} master 2> /dev/null`
     end
+
+    Dir.chdir(bare, &block) if block_given?
     bare
   end
 end
