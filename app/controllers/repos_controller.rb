4347507cd126c337823bf6e03a5a1e5a96a4b514
Wincent Colaiuta <win@wincent.com>
2010-08-12 21:44:34 +0200

    Add repos#create
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -8,4 +8,15 @@ class ReposController < ApplicationController
   def new
     @repo = Repo.new
   end
+
+  def create
+    @repo = Repo.new params[:repo]
+    if @repo.save
+      flash[:notice] = 'Successfully created new repo'
+      redirect_to @repo
+    else
+      flash[:error] = 'Failed to create new repo'
+      render :action => 'new'
+    end
+  end
 end
--- spec/controllers/repos_controller_spec.rb
+++ spec/controllers/repos_controller_spec.rb
@@ -42,4 +42,61 @@ describe ReposController do
       end
     end
   end
+
+  describe '#create' do
+    it_has_behavior 'require_admin'
+
+    before do
+      @params = { 'repo' => Repo.valid_attributes.stringify_keys }
+    end
+
+    def do_request
+      post :create, @params
+    end
+
+    context 'admin user' do
+      before do
+        log_in_as_admin
+      end
+
+      context 'successful creation' do
+        before do
+          do_request
+        end
+
+        it 'creates a new repo record' do
+          assigns[:repo].should be_kind_of(Repo)
+          assigns[:repo].should_not be_new_record
+        end
+
+        it 'shows a flash' do
+          cookie_flash[:notice].should =~ /successfully created/i
+        end
+
+        it 'redirects to #show' do
+          response.should redirect_to(repo_path(assigns[:repo]))
+        end
+      end
+
+      context 'failed creation' do
+        before do
+          stub.instance_of(Repo).save { false }
+          do_request
+        end
+
+        it 'assigns a new repo instance' do
+          assigns[:repo].should be_kind_of(Repo)
+          assigns[:repo].should be_new_record
+        end
+
+        it 'shows a flash' do
+          cookie_flash[:error].should =~ /failed to create/i
+        end
+
+        it 'renders #new' do
+          response.should render_template('repos/new')
+        end
+      end
+    end
+  end
 end
