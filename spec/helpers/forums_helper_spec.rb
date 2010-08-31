74959bfb56180aad6418612e7d1b7a2ee87ae20a
Wincent Colaiuta <win@wincent.com>
2010-09-01 01:50:36 +0200

    Add a regression spec for issue #1670
    See:
    
      https://wincent.com/issues/1670
    
    This issue was caused by discrepancies in the deployment database,
    which had some columns set as NOT NULL and with no DEFAULT value,
    meaning that MySQL would provide an empty string for unset columns
    rather than nil, which was problematic for new records (where nil
    columns are actually quite normal).
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/helpers/forums_helper_spec.rb
+++ spec/helpers/forums_helper_spec.rb
@@ -1,6 +1,23 @@
 require File.expand_path('../spec_helper', File.dirname(__FILE__))
 
 describe ForumsHelper do
+  describe '#link_to_user_for_topic' do
+    include ApplicationHelper # for link_to_user method
+
+    describe 'regressions' do
+      describe 'https://wincent.com/issues/1670' do
+        it "doesn't throw a routing error due to a nil value" do
+          # this may be code smell, but this method depends on the
+          # last_active_user_id attribute, which is not a real attribute but a
+          # pseudo-attribute added by the Topic.find_topics_for_forum method.
+          topic = Topic.make! :user => User.make!
+          topics = Topic.find_topics_for_forum topic.forum
+          link_to_user_for_topic topics.first
+        end
+      end
+    end
+  end
+
   describe '#timeinfo_for_forum' do
     context 'an empty forum' do
       let (:forum) do
