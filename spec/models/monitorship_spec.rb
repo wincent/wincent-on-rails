c32b03024ac2ab7a05c3fefcf861b56623a41299
Wincent Colaiuta <win@wincent.com>
2010-09-01 19:37:39 +0200

    Monitorship model: add specs for NULL columns
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/models/monitorship_spec.rb
+++ spec/models/monitorship_spec.rb
@@ -1,5 +1,37 @@
 require File.expand_path('../spec_helper', File.dirname(__FILE__))
 
+describe Monitorship do
+  describe '#user_id' do
+    it 'defaults to nil' do
+      Monitorship.new.user_id.should be_nil
+    end
+  end
+
+  describe '#monitorable_id' do
+    it 'defaults to nil' do
+      Monitorship.new.monitorable_id.should be_nil
+    end
+  end
+
+  describe '#monitorable_type' do
+    it 'defaults to nil' do
+      Monitorship.new.monitorable_type.should be_nil
+    end
+  end
+
+  describe '#created_at' do
+    it 'defaults to nil' do
+      Monitorship.new.created_at.should be_nil
+    end
+  end
+
+  describe '#updated_at' do
+    it 'defaults to nil' do
+      Monitorship.new.updated_at.should be_nil
+    end
+  end
+end
+
 describe Monitorship, 'validation' do
   it 'should require the user association to be set' do
     Monitorship.new.should fail_validation_for(:user)
