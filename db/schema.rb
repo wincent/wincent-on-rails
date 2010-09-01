bda2c081c0db98fbd3363646be06d71ed659fecf
Wincent Colaiuta <win@wincent.com>
2010-09-02 00:07:24 +0200

    Drop sessions table from db/schema.rb
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100812201654) do
+ActiveRecord::Schema.define(:version => 20100901190551) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -225,16 +225,6 @@ ActiveRecord::Schema.define(:version => 20100812201654) do
     t.datetime "updated_at"
     t.integer  "email_id"
   end
-
-  create_table "sessions", :force => true do |t|
-    t.string   "session_id"
-    t.text     "data"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
-  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "taggings", :force => true do |t|
     t.integer  "tag_id"
