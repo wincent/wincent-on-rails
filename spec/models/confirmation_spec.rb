4bf5ce0a5d2279a1b8cc5f253a95dd2c854f3a81
Wincent Colaiuta <win@wincent.com>
2010-09-01 14:51:49 +0200

    Add confirmation model specs for NULL columns
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ spec/models/confirmation_spec.rb
@@ -0,0 +1,39 @@
+require File.expand_path('../spec_helper', File.dirname(__FILE__))
+
+describe Confirmation do
+  describe '#email_id' do
+    it 'defaults to nil' do
+      Confirmation.new.email_id.should be_nil
+    end
+  end
+
+  describe '#secret' do
+    it 'defaults to nil' do
+      Confirmation.new.secret.should be_nil
+    end
+  end
+
+  describe '#cutoff' do
+    it 'defaults to nil' do
+      Confirmation.new.cutoff.should be_nil
+    end
+  end
+
+  describe '#completed_at' do
+    it 'defaults to nil' do
+      Confirmation.new.completed_at.should be_nil
+    end
+  end
+
+  describe '#created_at' do
+    it 'defaults to nil' do
+      Confirmation.new.created_at.should be_nil
+    end
+  end
+
+  describe '#updated_at' do
+    it 'defaults to nil' do
+      Confirmation.new.updated_at.should be_nil
+    end
+  end
+end
