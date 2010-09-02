5a54ce321b4065e00f8caa77d2723572332f55ee
Wincent Colaiuta <win@wincent.com>
2010-09-03 01:34:34 +0200

    Use mem_cache_store in production
    Note that we could never share the same memcache server instance among
    different environments, even if we wanted to (for example, to perform
    testing of the cache in the test environment).
    
    So it is very important to only set it up in the production environment.
    If we ever decide to do testing or development with the cache, it must
    be "sandboxed" into a completely separate memcached server instance,
    listening on a different port.
    
    This commit seeks to fix the bug in which the memcached server isn't
    being used at all on the server.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,7 @@ Wincent::Application.configure do
   config.cache_classes                      = true
   config.consider_all_requests_local        = false
   config.action_controller.perform_caching  = true
+  config.cache_store                        = :mem_cache_store
   config.action_dispatch.x_sendfile_header  = 'X-Accel-Redirect'
   config.serve_static_assets                = false
   config.action_mailer.delivery_method      = :sendmail
