2545e27517b45874a394e8ab52b2d4828016cd2a
Wincent Colaiuta <win@wincent.com>
2010-09-06 18:35:04 +0200

    Reformat ActsAsTaggable specs
    - move test class inside describe block to avoid polluting global
      namespace
    - nest all example blocks inside the same outer containing block
    - use "let" extensively rather than instance variables
    - drop "should" from example wording
    - use "context" blocks to avoid specifying "when" clauses in example
      descriptions
    - use "expect/to" rather than "lambda" as it reads more nicely
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/lib/active_record/acts/taggable_spec.rb
+++ spec/lib/active_record/acts/taggable_spec.rb
@@ -1,170 +1,202 @@
 require File.expand_path('../../../spec_helper', File.dirname(__FILE__))
 
-# There are no plans to extract this into a separate plug-in, so piggy-back on the application's own test database.
-# (An extracted version would need to set up an in-memory SQLite database; see the acts_as_list plug-in for an example.)
-def setup_db
-  ActiveRecord::Migration.verbose = false
-  ActiveRecord::Schema.define do
-    create_table :acts_as_taggable_test_models do |t|
-      t.string :title
+describe ActiveRecord::Acts::Taggable do
+  # Create a model purely for testing purposes so as to avoid depending on a
+  # real model from the application.
+  class ActsAsTaggableTestModel < ActiveRecord::Base
+    acts_as_taggable
+  end
+
+  before :all do
+    # There are no plans to extract this into a separate plug-in, so piggy-back
+    # on the application's own test database. (An extracted version would need
+    # to set up an in-memory SQLite database; see the acts_as_list plug-in for
+    # an example.)
+    ActiveRecord::Migration.verbose = false
+    ActiveRecord::Schema.define do
+      create_table :acts_as_taggable_test_models do |t|
+        t.string :title
+      end
     end
   end
-end
 
-def teardown_db
-  ActiveRecord::Base.connection.drop_table 'acts_as_taggable_test_models'
-end
-
-# Create a model purely for testing purposes so as to avoid dependening on a real model from the application.
-class ActsAsTaggableTestModel < ActiveRecord::Base
-  acts_as_taggable
-end
-
-describe ActiveRecord::Acts::Taggable, 'shared' do
-  before(:all) do
-    setup_db
-  end
-
-  after(:all) do
+  after :all do
     ActsAsTaggableTestModel.destroy_all
-    teardown_db
+    ActiveRecord::Base.connection.drop_table 'acts_as_taggable_test_models'
   end
 
   it_has_behavior 'taggable' do
     let(:model) { ActsAsTaggableTestModel.create }
     let(:new_model) { ActsAsTaggableTestModel.new }
   end
-end
 
-describe ActiveRecord::Acts::Taggable, 'adding tag(s)' do
-  before(:all) do
-    setup_db
+  let(:model) { ActsAsTaggableTestModel.create }
+
+  describe 'adding tag(s)' do
+    context 'no parameters' do
+      it 'does nothing' do
+        expect do
+          model.tag
+        end.to_not change { model.tags.size }
+      end
+    end
+
+    context 'a string with a single tag' do
+      it 'adds a single tag' do
+        expect do
+          model.tag 'foo'
+        end.to change { model.tags.size }.by(1)
+      end
+    end
+
+    context 'multiple, space-delimited tags' do
+      it 'adds multiple tags' do
+        expect do
+          model.tag 'foo bar baz'
+        end.to change { model.tags.size }.by(3)
+      end
+    end
+
+    context 'excess whitespace' do
+      it 'ignores excess trailing or leading whitespace' do
+        expect do
+          model.tag '    foo   ,  bar  ,   baz  '
+        end.to change { model.tags.size }.by(3)
+      end
+    end
+
+    context 'repeated items' do
+      it 'adds repeated items only once' do
+        expect do
+          model.tag 'foo foo foo'
+        end.to change { model.tags.size }.by(1)
+      end
+    end
+
+    context 'tags which have already been applied' do
+      it 'has no effect' do
+        expect do
+          model.tag 'foo'
+        end.to change { model.tags.size }.by(1)
+        expect do
+          model.tag 'foo'
+        end.to change { model.tags.size }.by(0)
+      end
+    end
+
+    context 'array parameter' do
+      it 'accepts an array as a parameter' do
+        expect do
+          model.tag ['foo', 'bar']
+        end.to change { model.tags.size }.by(2)
+      end
+    end
+
+    context 'nested array' do
+      it 'handles nested arrays' do
+        expect do
+          model.tag ['foo', ['bar', 'baz abc']]
+        end.to change { model.tags.size }.by(4)
+      end
+    end
   end
 
-  before do
-    @model = ActsAsTaggableTestModel.create
+  describe 'removing tag(s)' do
+    context 'no parameters' do
+      it 'does nothing' do
+        model.tag 'foo bar baz'
+        expect do
+          model.untag
+        end.to_not change { model.tags.size }
+      end
+    end
+
+    context 'single tag' do
+      it 'removes a single tag' do
+        model.tag 'foo bar baz'
+        expect do
+          model.untag 'foo'
+        end.to change { model.tags.size }.by(-1)
+      end
+    end
+
+    context 'multiple, space-delimited tags' do
+      it 'removes' do
+        model.tag 'foo bar baz'
+        expect do
+          model.untag 'foo bar'
+        end.to change { model.tags.size }.by(-2)
+      end
+    end
+
+    context 'excess whitespace' do
+      it 'ignores excess trailing or leading whitespace' do
+        model.tag 'foo bar baz'
+        expect do
+          model.untag '    foo    ,    bar    '
+        end.to change { model.tags.size }.by(-2)
+      end
+    end
+
+    context 'repeated items' do
+      it 'handles repeated items' do
+        model.tag 'foo bar baz'
+        expect do
+          model.untag 'foo foo'
+        end.to change { model.tags.size }.by(-1)
+      end
+    end
+
+    context 'tags which have already been removed' do
+      it 'has no effect for tags which have already been removed' do
+        model.tag 'foo bar baz'
+        expect do
+          model.untag 'foo'
+        end.to change { model.tags.size }.by(-1)
+        expect do
+          model.untag 'foo'
+        end.to_not change { model.tags.size }
+      end
+    end
+
+    context 'unknown tags' do
+      it 'has no effect' do
+        model.tag 'foo bar baz'
+        expect do
+          model.untag 'abc'
+        end.to_not change { model.tags.size }
+      end
+    end
+
+    context 'array parameter' do
+      it 'accepts' do
+        model.tag 'foo bar baz'
+        expect do
+          model.untag ['foo', 'bar']
+        end.to change { model.tags.size }.by(-2)
+      end
+    end
+
+    context 'nested arrays' do
+      it 'handles nested arrays' do
+        model.tag 'foo bar baz'
+        expect do
+          model.untag ['foo', ['bar']]
+        end.to change { model.tags.size }.by(-2)
+      end
+    end
   end
 
-  it 'should do nothing if passed no parameters' do
-    lambda { @model.tag }.should_not change { @model.tags.size }
-  end
+  describe 'getting a list of tag name(s)' do
+    it 'returns an array of tag names' do
+      model.tag 'foo bar baz'
+      model.tag_names.should =~ ['bar', 'baz', 'foo']
+    end
 
-  it 'should add a single tag' do
-    lambda { @model.tag 'foo' }.should change { @model.tags.size }.by(1)
-  end
-
-  it 'should add multiple, space-delimited tags' do
-    lambda { @model.tag 'foo bar baz' }.should change { @model.tags.size }.by(3)
-  end
-
-  it 'should ignore excess trailing or leading whitespace' do
-    lambda { @model.tag '    foo   ,  bar  ,   baz  ' }.should change { @model.tags.size }.by(3)
-  end
-
-  it 'should add repeated items only once' do
-    lambda { @model.tag 'foo foo foo' }.should change { @model.tags.size }.by(1)
-  end
-
-  it 'should have no effect for tags which have already been applied' do
-    lambda { @model.tag 'foo' }.should change { @model.tags.size }.by(1)
-    lambda { @model.tag 'foo' }.should change { @model.tags.size }.by(0)
-  end
-
-  it 'should accept an array as a parameter' do
-    lambda { @model.tag ['foo', 'bar'] }.should change { @model.tags.size }.by(2)
-  end
-
-  it 'should handle nested arrays' do
-    lambda { @model.tag ['foo', ['bar', 'baz abc']] }.should change { @model.tags.size }.by(4)
-  end
-
-  after(:all) do
-    ActsAsTaggableTestModel.destroy_all
-    teardown_db
-  end
-end
-
-describe ActiveRecord::Acts::Taggable, 'removing tag(s)' do
-  before(:all) do
-    setup_db
-  end
-
-  before do
-    @model = ActsAsTaggableTestModel.create
-  end
-
-  it 'should do nothing if passed no parameters' do
-    @model.tag 'foo bar baz'
-    lambda { @model.untag }.should_not change { @model.tags.size }
-  end
-
-  it 'should remove a single tag' do
-    @model.tag 'foo bar baz'
-    lambda { @model.untag 'foo' }.should change { @model.tags.size }.by(-1)
-  end
-
-  it 'should remove multiple, space-delimited tags' do
-    @model.tag 'foo bar baz'
-    lambda { @model.untag 'foo bar' }.should change { @model.tags.size }.by(-2)
-  end
-
-  it 'should ignore excess trailing or leading whitespace' do
-    @model.tag 'foo bar baz'
-    lambda { @model.untag '    foo    ,    bar    ' }.should change { @model.tags.size }.by(-2)
-  end
-
-  it 'should handle repeated items' do
-    @model.tag 'foo bar baz'
-    lambda { @model.untag 'foo foo' }.should change { @model.tags.size }.by(-1)
-  end
-
-  it 'should have no effect for tags which have already been removed' do
-    @model.tag 'foo bar baz'
-    lambda { @model.untag 'foo' }.should change { @model.tags.size }.by(-1)
-    lambda { @model.untag 'foo' }.should_not change { @model.tags.size }
-  end
-
-  it 'should have no effect for unknown tags' do
-    @model.tag 'foo bar baz'
-    lambda { @model.untag 'abc' }.should_not change { @model.tags.size }
-  end
-
-  it 'should accept an array as a parameter' do
-    @model.tag 'foo bar baz'
-    lambda { @model.untag ['foo', 'bar'] }.should change { @model.tags.size }.by(-2)
-  end
-
-  it 'should handle nested arrays' do
-    @model.tag 'foo bar baz'
-    lambda { @model.untag ['foo', ['bar']] }.should change { @model.tags.size }.by(-2)
-  end
-
-  after(:all) do
-    ActsAsTaggableTestModel.destroy_all
-    teardown_db
-  end
-end
-
-describe ActiveRecord::Acts::Taggable, 'getting a list of tag name(s)' do
-  before(:all) do
-    setup_db
-  end
-
-  before do
-    @model = ActsAsTaggableTestModel.create
-  end
-
-  it 'should return an empty array when there are no tags' do
-    @model.tag_names.should == []
-  end
-
-  it 'should return an array of tag names when there are tags' do
-    @model.tag 'foo bar baz'
-    @model.tag_names.should =~ ['bar', 'baz', 'foo']
-  end
-
-  after(:all) do
-    ActsAsTaggableTestModel.destroy_all
-    teardown_db
+    context 'no tags' do
+      it 'returns an empty array' do
+        model.tag_names.should == []
+      end
+    end
   end
 end
