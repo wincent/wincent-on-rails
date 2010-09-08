56cfd48577e498a6f2edefd3d0aea13979f23380
Wincent Colaiuta <win@wincent.com>
2010-09-08 18:27:04 +0200

    Reformat snippet routing specs for better display on narrow terminals
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/routing/snippets_routing_spec.rb
+++ spec/routing/snippets_routing_spec.rb
@@ -2,16 +2,41 @@ require 'spec_helper'
 
 describe SnippetsController do
   describe 'routing' do
-    specify { get('/snippets').should have_routing('snippets#index') }
-    specify { get('/snippets/new').should have_routing('snippets#new') }
-    specify { get('/snippets/123').should have_routing('snippets#show', :id => '123') }
-    specify { get('/snippets/123/edit').should have_routing('snippets#edit', :id => '123') }
-    specify { put('/snippets/123').should have_routing('snippets#update', :id => '123') }
-    specify { delete('/snippets/123').should have_routing('snippets#destroy', :id => '123') }
-    specify { post('/snippets').should have_routing('snippets#create') }
+    specify do
+      get('/snippets').should have_routing('snippets#index')
+    end
+
+    specify do
+      get('/snippets/new').should have_routing('snippets#new')
+    end
+
+    specify do
+      get('/snippets/123').should have_routing('snippets#show', :id => '123')
+    end
+
+    specify do
+      get('/snippets/123/edit').
+        should have_routing('snippets#edit', :id => '123')
+    end
+
+    specify do
+      put('/snippets/123').should have_routing('snippets#update', :id => '123')
+    end
+
+    specify do
+      delete('/snippets/123').
+        should have_routing('snippets#destroy', :id => '123')
+    end
+
+    specify do
+      post('/snippets').should have_routing('snippets#create')
+    end
 
     describe 'index pagination' do
-      specify { get('/snippets/page/2').should have_routing('snippets#index', :page => '2') }
+      specify do
+        get('/snippets/page/2').
+          should have_routing('snippets#index', :page => '2')
+      end
 
       it 'rejects non-numeric :page params' do
         get('/snippets/page/foo').should_not be_recognized
@@ -20,16 +45,38 @@ describe SnippetsController do
 
     describe 'comments' do
       # only #new and #create are implemented while nested
-      # Rails BUG?: only map_to works here; map_from (and therefore also have_routing) do not
-      specify { get('/snippets/123/comments/new').should map_to('comments#new', :snippet_id => '123') }
-      specify { post('/snippets/123/comments').should map_to('comments#create', :snippet_id => '123') }
+      # Rails BUG: only map_to works here; map_from (and therefore also
+      # have_routing) do not
+      specify do
+        get('/snippets/123/comments/new').
+          should map_to('comments#new', :snippet_id => '123')
+      end
+
+      specify do
+        post('/snippets/123/comments').
+          should map_to('comments#create', :snippet_id => '123')
+      end
 
       # all other RESTful actions are no-ops
-      specify { get('/snippets/123/comments').should_not be_recognized }
-      specify { get('/snippets/123/comments/456').should_not be_recognized }
-      specify { get('/snippets/123/comments/456/edit').should_not be_recognized }
-      specify { put('/snippets/123/comments/456').should_not be_recognized }
-      specify { delete('/snippets/123/comments/456').should_not be_recognized }
+      specify do
+        get('/snippets/123/comments').should_not be_recognized
+      end
+
+      specify do
+        get('/snippets/123/comments/456').should_not be_recognized
+      end
+
+      specify do
+        get('/snippets/123/comments/456/edit').should_not be_recognized
+      end
+
+      specify do
+        put('/snippets/123/comments/456').should_not be_recognized
+      end
+
+      specify do
+        delete('/snippets/123/comments/456').should_not be_recognized
+      end
     end
 
     describe 'helpers' do
