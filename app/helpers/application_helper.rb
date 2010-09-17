404b74f20377a3112cf27617c2868558461d6de6
Wincent Colaiuta <win@wincent.com>
2010-09-17 20:13:57 +0200

    Display more info, and prettier, in commits#show
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,24 @@ module ApplicationHelper
     sha1[0..15]
   end
 
+  # Wraps the commit#author#time in a span of class "relative-date", and
+  # converts it to an ActiveSupport::TimeWithZone so that the JavaScript
+  # relativize_dates function can operate on it.
+  def commit_author_time commit
+    content_tag :span,
+      commit.author.time.in_time_zone('UTC'),
+      :class => 'relative-date'
+  end
+
+  # Wraps the commit#committer#time in a span of class "relative-date", and
+  # converts it to an ActiveSupport::TimeWithZone so that the JavaScript
+  # relativize_dates function can operate on it.
+  def commit_committer_time commit
+    content_tag :span,
+      commit.committer.time.in_time_zone('UTC'),
+      :class => 'relative-date'
+  end
+
   # Returns an appropriate CSS class to indicate whether the passed item
   # should be drawn as selected.
   def navbar_selected? item
--- app/helpers/repos_helper.rb
+++ app/helpers/repos_helper.rb
@@ -1,19 +1,2 @@
 module ReposHelper
-  # Wraps the commit#author#time in a span of class "relative-date", and
-  # converts it to an ActiveSupport::TimeWithZone so that the JavaScript
-  # relativize_dates function can operate on it.
-  def commit_author_time commit
-    content_tag :span,
-      commit.author.time.in_time_zone('UTC'),
-      :class => 'relative-date'
-  end
-
-  # Wraps the commit#committer#time in a span of class "relative-date", and
-  # converts it to an ActiveSupport::TimeWithZone so that the JavaScript
-  # relativize_dates function can operate on it.
-  def commit_committer_time commit
-    content_tag :span,
-      commit.committer.time.in_time_zone('UTC'),
-      :class => 'relative-date'
-  end
 end
