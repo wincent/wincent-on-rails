c91762538c9228d026a8aa23df0dd9a7049e6138
Wincent Colaiuta <win@wincent.com>
2010-09-19 20:15:48 +0200

    Update specs for methods moved between helpers
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -161,6 +161,21 @@ describe ApplicationHelper, '"tweet_title" method' do
 end
 
 describe ApplicationHelper do
+  describe '#commit_abbrev' do
+    it 'returns the first 16 characters of the hash' do
+      commit_abbrev('1234abcd1234abcd999999999999999999999999').
+        should == '1234abcd1234abcd'
+    end
+  end
+
+  describe '#commit_author_time' do
+    pending
+  end
+
+  describe '#commit_committer_time' do
+    pending
+  end
+
   describe '#stylesheet_link_tags' do
     describe 'regressions' do
       it 'does not pluralize the dashboard controller' do
--- spec/helpers/repos_helper_spec.rb
+++ spec/helpers/repos_helper_spec.rb
@@ -1,18 +1,4 @@
 require 'spec_helper'
 
 describe ReposHelper do
-  describe '#commit_abbrev' do
-    it 'returns the first 16 characters of the hash' do
-      commit_abbrev('1234abcd1234abcd999999999999999999999999').
-        should == '1234abcd1234abcd'
-    end
-  end
-
-  describe '#commit_author_time' do
-    pending
-  end
-
-  describe '#commit_committer_time' do
-    pending
-  end
 end
