8417c97b3a1f68313fd03f1deba2580be119f145
Wincent Colaiuta <win@wincent.com>
2010-09-22 23:40:18 +0200

    CSS is now the default selector type in Capybara
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/acceptance/support/capybara.rb
+++ spec/acceptance/support/capybara.rb
@@ -1,7 +1,3 @@
 require 'capybara/rails'
 
-# Capybara might make this the default soon
-# See: http://groups.google.com/group/ruby-capybara/browse_thread/thread/c336f52ee28f9910
-Capybara.default_selector = :css
-
 RSpec.configuration.include Capybara, :type => :acceptance
