82cec32be8ba41ad7d7fda49e450c0e79ecacbe4
Wincent Colaiuta <win@wincent.com>
2010-10-13 09:03:39 +0200

    Fix missing submit button in topics#new template
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ spec/views/topics/new.html.haml_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe 'topics/new' do
+  let(:forum) { Forum.make! }
+  let(:topic) { Topic.make :forum => forum }
+
+  context 'viewed as anonymous user' do
+    before do
+      stub(view).logged_in? { false }
+      stub(view).admin? { false }
+      @forum = forum
+      @topic = topic
+      render
+    end
+
+    # was a bug (was only showing for admin user)
+    it 'has a submit button' do
+      rendered.should have_selector('input[type=submit]')
+    end
+  end
+end
