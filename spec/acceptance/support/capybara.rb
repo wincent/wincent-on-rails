7cb4424eecd9e297cb0722ca24d0bb5e51d5643b
Wincent Colaiuta <win@wincent.com>
2010-11-12 18:39:43 +0100

    Explicitly reset Capybara sessions before scenarios
    See also:
    
      https://github.com/cavalle/steak/issues/20
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/acceptance/support/capybara.rb
+++ spec/acceptance/support/capybara.rb
@@ -1,3 +1,10 @@
 require 'capybara/rails'
 
 RSpec.configuration.include Capybara, :type => :acceptance
+
+# see: https://github.com/cavalle/steak/issues/20
+RSpec.configure do |config|
+  config.before do
+    Capybara.reset_sessions!
+  end
+end
