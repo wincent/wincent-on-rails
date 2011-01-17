3ac8f89cdc68e34cea13b520d81db9c4f1f6bb4b
Wincent Colaiuta <win@wincent.com>
2011-01-17 20:49:55 +0100

    Simplify monkey patching of ActiveRecord::Base
    Simply re-opening the class and defining our desired method is much
    simpler here; the old method is over-engineered and buys us nothing.
    After the refactoring, we are still protected against possible changes
    to the upstream API in the future (in the unlikely event that Rails
    should ever add a "flashable_error_string" method).
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- config/initializers/monkey_patches/active_record/error_extensions.rb
+++ config/initializers/monkey_patches/active_record/error_extensions.rb
@@ -1,16 +1,11 @@
-module Wincent
-  module ActiveRecord
-    module ErrorExtensions
-      def flashable_error_string
-        errors.full_messages.join(', ')
-      end
-    end
-  end
+if ActiveRecord::Base.instance_methods.include? 'flashable_error_string'
+  raise 'ActiveRecord::Base\#flashable_error_string already exists'
 end
 
-Wincent::ActiveRecord::ErrorExtensions.instance_methods.each do |m|
-  if ActiveRecord::Base.instance_methods.include? m
-    raise "ActiveRecord::Base\##{m} already exists"
-  end
-end
-ActiveRecord::Base.send(:include, Wincent::ActiveRecord::ErrorExtensions)
+module ActiveRecord
+  class Base
+    def flashable_error_string
+      errors.full_messages.join(', ')
+    end
+  end # class Base
+end # module ActiveRecord
