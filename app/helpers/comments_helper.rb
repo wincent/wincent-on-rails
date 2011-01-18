eff50121cde1d46766c477eca9457ff04a23b9ee
Wincent Colaiuta <win@wincent.com>
2011-01-18 01:04:20 +0100

    Update button_to_destroy_comment (and calling sites)
    This to match recent changes to the button_to_destroy_model method.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,6 +1,6 @@
 module CommentsHelper
   def button_to_destroy_comment comment
-    button_to_destroy_model comment, comment_path(comment)
+    button_to_destroy_model comment
   end
 
   def button_to_moderate_comment_as_ham comment
