b991fcbfaa3f4b9344c934460837c8b433c0e658
Wincent Colaiuta <win@wincent.com>
2011-01-18 01:52:33 +0100

    Don't actually need remote flag in our page destroy method
    Seeing as this is, right now, just a normal form POST, and I don't really
    see the need to make use of an AJAX-y fade out in this case.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,6 +2,6 @@ module PagesHelper
   def button_to_destroy_page page
     button_to 'destroy', product_page_path(page.product, page),
       :confirm => 'Are you sure?', :method => :delete,
-      :class => 'destructive', :remote => true
+      :class => 'destructive'
   end
 end
