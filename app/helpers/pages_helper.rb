447410a23f3c588e20470fb6ede1abc74daa5e31
Wincent Colaiuta <win@wincent.com>
2011-02-01 18:45:30 +0100

    Remove button_to_destroy_page helper
    As it is only used in one place.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,5 +1,2 @@
 module PagesHelper
-  def button_to_destroy_page page, options = {}
-    button_to_destroy_model product_page_path(page.product, page), options
-  end
 end
--- spec/helpers/pages_helper_spec.rb
+++ spec/helpers/pages_helper_spec.rb
@@ -1,12 +1,4 @@
 require 'spec_helper'
 
 describe PagesHelper do
-  describe '#button_to_destroy_page' do
-    it 'delegates to #button_to_destroy_model' do
-      product = Product.make! :permalink => 'foo'
-      page = Page.make! :product => product, :permalink => 'bar'
-      mock(helper).button_to_destroy_model page, '/products/foo/pages/bar'
-      helper.button_to_destroy_page page
-    end
-  end
 end
