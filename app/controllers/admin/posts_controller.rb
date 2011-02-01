88d0e41ce37fe4c0d2301d0d883f6b70b777cb0b
Wincent Colaiuta <win@wincent.com>
2011-02-01 13:40:49 +0100

    Drop unnecessary call to #scoped
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -8,7 +8,7 @@ class Admin::PostsController < ApplicationController
 
   def index
     # TODO: combine sortability with pagination?
-    @posts = Post.scoped.order arel_sort_options
+    @posts = Post.order arel_sort_options
   end
 
   def show
