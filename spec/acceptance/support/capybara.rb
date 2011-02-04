b1d9ba77495214a149ce00cea267ec46c09aa28a
Wincent Colaiuta <win@wincent.com>
2011-02-04 10:07:07 +0100

    Do not explicitly include Capybara in acceptance tests
    This is no longer necessary, as recent versions of Steak do this
    for us automatically.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/acceptance/support/capybara.rb
+++ spec/acceptance/support/capybara.rb
@@ -1,7 +1,5 @@
 require 'capybara/rails'
 
-RSpec.configuration.include Capybara, :type => :acceptance
-
 # see: https://github.com/cavalle/steak/issues/20
 RSpec.configure do |config|
   config.before do
