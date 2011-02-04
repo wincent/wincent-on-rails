fa44581472ac22eb116ad01a41aef61521eb1312
Wincent Colaiuta <win@wincent.com>
2011-02-04 14:12:07 +0100

    Extract common formatter code
    Signed-off-by: Wincent Colaiuta <win@wincent.com>
    
    fixup! Lay groundwork for extracting common formatter code
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/support/vim_formatter.rb
+++ spec/support/vim_formatter.rb
@@ -1,12 +1,14 @@
 require 'rspec/core/formatters/base_text_formatter'
+require 'spec/support/formatter_helpers'
 
 # Format spec results for display in the Vim quickfix window
 # Use this custom formatter like this:
-#   bin/rspec -r spec/support/vim_formatter.rb -f RSpec::Core::Formatters::VimFormatter spec
+#   bin/rspec -r spec/support/vim_formatter -f RSpec::Core::Formatters::VimFormatter spec
 module RSpec
   module Core
     module Formatters
       class VimFormatter < BaseTextFormatter
+        include FormatterHelpers
 
         # TODO: vim-side function for printing progress (if that's even possible)
 
@@ -42,30 +44,11 @@ module RSpec
           @pending_count = pending_count
         end
 
-        def close
-          super
-          summary = summary_line example_count, failure_count, pending_count
-          if failure_count > 0
-            growlnotify "--image ./autotest/fail.png -p Emergency -m '#{summary}' -t 'Spec failure detected'"
-          elsif pending_count > 0
-            growlnotify "--image ./autotest/pending.png -p High -m '#{summary}' -t 'Pending spec(s) present'"
-          else
-            growlnotify "--image ./autotest/pass.png -p 'Very Low' -m '#{summary}' -t 'All specs passed'"
-          end
-        end
-
       private
 
         def format_message msg
           # NOTE: may consider compressing all whitespace here
           msg.gsub("\n", ' ')[0,40]
-        end
-
-        def growlnotify str
-          system 'which growlnotify > /dev/null'
-          if $?.exitstatus == 0
-            system "growlnotify -n autotest #{str}"
-          end
         end
       end # class VimFormatter
     end # module Formatter
