67c131ce336762fd865ba824386416602ee7e099
Wincent Colaiuta <win@wincent.com>
2011-02-16 20:36:51 +0100

    Drop #link_to_parent helper
    This was almost identical to the #link_to_commentable helper, so
    roll them into one single method to keep things simple. Note that
    the #link_to_taggable method is also awfully similar, and there
    may be scope for DRYing that up in the future too.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -2,21 +2,4 @@ module CommentsHelper
   def button_to_moderate_comment_as_ham comment
     button_to_moderate_model_as_ham comment, comment_path(comment)
   end
-
-  def link_to_parent parent
-    case parent
-    when Article
-      link_to parent.title, article_path(parent)
-    when Issue
-      link_to "issue \##{parent.id}", issue_path(parent)
-    when Post
-      link_to parent.title, post_path(parent)
-    when Snippet
-      link_to "snippet \##{parent.id}", snippet_path(parent)
-    when Topic
-      link_to parent.title, forum_topic_path(parent.forum, parent)
-    when Tweet
-      link_to "tweet \##{parent.id}", tweet_path(parent)
-    end
-  end
 end
--- spec/helpers/comments_helper_spec.rb
+++ spec/helpers/comments_helper_spec.rb
@@ -1,13 +1,4 @@
 require 'spec_helper'
 
 describe CommentsHelper do
-  describe '#link_to_parent' do
-    context 'snippet parent' do
-      let(:parent) { Snippet.make! }
-
-      it 'returns the snippet path' do
-        link_to_parent(parent).should =~ %r{href="#{snippet_path(parent)}"}
-      end
-    end
-  end
 end
