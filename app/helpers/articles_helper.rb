c7585283b69c518a2aa5b519e9aeefc482b044a8
Wincent Colaiuta <win@wincent.com>
2011-04-17 22:03:33 -0700

    Add #redirected_from helper for custom admin logic
    For articles that we arrive at as a result of a redirection, we want to
    show not only the title, but for admins we want to show a separate edit
    link as well.
    
    Previously we made the title itself the edit link, but that was
    confusing and looked a little like a bug. By keeping it separate it
    becomes clear that the intention is not only to show the title, but
    offer admins an opportunity of fixing bad redirections should they crop
    up.
    
    See:
    
      https://wincent.com/issues/1803
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,15 @@
 module ArticlesHelper
+  def redirected_from
+    if @redirected_from
+      content_tag :p do
+        edit_link = if admin?
+          ' [' + link_to("edit", edit_article_path(@redirected_from)) + ']'
+        else
+          ''
+        end
+
+        raw "(Redirected from #{h @redirected_from.title}#{edit_link})"
+      end
+    end
+  end
 end
--- spec/helpers/articles_helper_spec.rb
+++ spec/helpers/articles_helper_spec.rb
@@ -1,4 +1,40 @@
 require 'spec_helper'
 
 describe ArticlesHelper do
+  describe '#redirected_from' do
+    context 'with no article' do
+      it 'returns nil' do
+        helper.redirected_from.should be_nil
+      end
+    end
+
+    context 'with an article' do
+      let(:article) { Article.make! :title => 'foo' }
+
+      before do
+        @redirected_from = article
+      end
+
+      context 'as a non-admin user' do
+        before do
+          stub(helper).admin? { false }
+        end
+
+        it 'reports the article title' do
+          helper.redirected_from.should == '<p>(Redirected from foo)</p>'
+        end
+      end
+
+      context 'as an admin user' do
+        before do
+          stub(helper).admin? { true }
+        end
+
+        it 'includes an edit link' do
+          expected = '<p>(Redirected from foo [<a href="/wiki/foo/edit">edit</a>])</p>'
+          helper.redirected_from.should == expected
+        end
+      end
+    end
+  end
 end
