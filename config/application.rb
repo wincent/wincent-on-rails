b1782028202fd71b57de1767177026c3bc48c8a7
Wincent Colaiuta <win@wincent.com>
2011-06-01 00:21:42 -0700

    Move observers into app/observers folder
    Rails, by convention, stores observers alongside models in the
    app/models folder.
    
    It used to do the same with mailers, but eventually those got moved out
    to app/mailers.
    
    Observers, funnily enough, haven't followed suit (and neither have
    sweepers). I know that DHH loves organization (see his RailsConf 2011
    keynote), so it is surprising that the change hasn't happened.
    
    Nevertheless, it's easy enough to set up this kind of organization, so
    go ahead and do it.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,10 @@ APP_CONFIG = YAML.load_file File.join(File.dirname(__FILE__), 'app_config.yml')
 module Wincent
   class Application < Rails::Application
     config.active_record.observers = :comment_observer, :issue_observer
-    config.autoload_paths += %W( #{config.root}/app/sweepers )
+    config.autoload_paths += %W(
+      #{config.root}/app/observers
+      #{config.root}/app/sweepers
+    )
     config.encoding = 'utf-8'
     config.filter_parameters += [ :passphrase ]
     config.time_zone = 'UTC'
