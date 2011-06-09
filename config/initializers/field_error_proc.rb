24704b1fb85a7952ade88a078b5d04ec66a017dc
Wincent Colaiuta <win@wincent.com>
2011-06-08 22:53:02 -0700

    Rails 3.1.0.rc3: can no longer mutate an ActiveSupport::SafeBuffer
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- config/initializers/field_error_proc.rb
+++ config/initializers/field_error_proc.rb
@@ -2,13 +2,13 @@
 # makes it harder to consistently style forms. Instead of adding a new wrapper
 # element, we add the "field_with_errors" class to the existing tag.
 # We'll be called once for the label and once for the form field.
-# The parsing is brittle in the sense that it depends on Rails not ever passing
-# us any malformed tags.
 ActionView::Base.field_error_proc = Proc.new do |html_tag, instance_tag|
   error_class = 'field_with_errors'
-  if html_tag.sub!(/\A(<[^>]+ class=["'])/, '\1 ' + error_class)
-    html_tag.html_safe
-  else
-    html_tag.sub(/\A(<\w+)/, '\1 class="' + error_class + '"').html_safe
-  end
+  html_tag = html_tag.to_str # can't sub! on ActiveSupport::SafeBuffer
+
+  # first try appending to existing class attribute,
+  # otherwise fallback to adding new class attribute
+  html_tag.sub!(/\A(<[^>]+ class=["'])/, '\1 ' + error_class) ||
+    html_tag.sub!(/\A(<\w+)/, '\1 class="' + error_class + '"')
+  html_tag.html_safe
 end
