fd50a62a4100a804cddd632995678b6d23137dd4
Wincent Colaiuta <win@wincent.com>
2011-06-17 18:56:50 -0700

    Make issues#show use comments partial
    We're also enhancing the partial a little bit here, extracting the logic
    used to deterimine the class into a helper. Note that we add the
    "private" class here when appropriate, despite the fact that the
    comments partial may end up being rendered in page-cached pages (note
    that pages which have special content when viewed by an admin are not
    page-cached, so we need not worry about stuff getting inappropriately
    cached).
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -2,4 +2,12 @@ module CommentsHelper
   def button_to_moderate_comment_as_ham comment
     button_to_moderate_model_as_ham comment, comment_path(comment)
   end
+
+  def comment_class comment
+    if comment.user && comment.user.superuser?
+      'admin'
+    else
+      cycle('even', 'odd')
+    end + (comment.public? ? '' : ' private')
+  end
 end
