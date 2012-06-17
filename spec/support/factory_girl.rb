055bf741daf8e6cac088e6294516ea68bccfd71c
Wincent Colaiuta <win@wincent.com>
2012-06-16 22:44:15 -0700

    Silence a bunch of Factory Girl warnings
    Looks like they don't want us using the old DSL any more. Not only has
    the core API changed, by all the syntax shims are deprecated.
    Nevertheless, I have too many specs invested in the old style, so
    changing to the more verbose "blessed" syntax isn't a practical option
    right now.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -46,33 +46,33 @@ end
 # not just "create", and we also want the option of customising the returned
 # instance via a block.
 class ActiveRecord::Base
-  module FactoryGirl
-    module Forwarder
-      def self.delegate method, model, attributes, &block
-        instance = Factory.send method, model.to_s.underscore.to_sym, attributes
-        yield instance if block_given?
-        instance
-      end
+  class << self
+    # Wrapper for FactoryGirl.build
+    def make(overrides = {}, &block)
+      instance = FactoryGirl.build name.underscore, overrides
+      yield instance if block_given?
+      instance
     end
-  end
 
-  # Wrapper for Factory.build
-  def self.make attributes = {}, &block
-    FactoryGirl::Forwarder.delegate :build, self, attributes, &block
-  end
+    # Wrapper for FactoryGirl.create
+    def make!(overrides = {}, &block)
+      instance = FactoryGirl.create name.underscore, overrides
+      yield instance if block_given?
+      instance
+    end
 
-  # Wrapper for Factory.create
-  def self.make! attributes = {}, &block
-    FactoryGirl::Forwarder.delegate :create, self, attributes, &block
-  end
+    # Wrapper for FactoryGirl.attributes_for
+    def valid_attributes(overrides = {}, &block)
+      attrs = FactoryGirl.attributes_for name.underscore, overrides
+      attrs = yield attrs if block_given?
+      attrs
+    end
 
-  # Wrapper for Factory.attributes_for
-  def self.valid_attributes attributes = {}, &block
-    FactoryGirl::Forwarder.delegate :attributes_for, self, attributes, &block
-  end
-
-  # Wrapper for Factory.stub
-  def self.stub attributes = {}, &block
-    FactoryGirl::Forwarder.delegate :stub, self, attributes, &block
+    # Wrapper for FactoryGirl.build_stubbed
+    def stub(overrides = {}, &block)
+      instance = FactoryGirl.build_stubbed name.underscore, overrides
+      yield instance if block_given?
+      instance
+    end
   end
 end
