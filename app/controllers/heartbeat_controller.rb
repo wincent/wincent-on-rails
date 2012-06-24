70c62db36b673a1657eb9b6f42f641a87c04574d
Wincent Colaiuta <win@wincent.com>
2012-06-24 13:44:03 -0700

    Update a stale comment
    Mongrel is dead. Long live Unicorn.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/controllers/heartbeat_controller.rb
+++ app/controllers/heartbeat_controller.rb
@@ -1,6 +1,5 @@
 class HeartbeatController < ApplicationController
-  # we don't want Monit filling up the logs with noise
-  # (4 requests, 1 for each mongrel, every 30 seconds)
+  # we don't want Monit filling up the logs with noise;
   # not sure why, but this method cannot be protected or private
   # (due to routing, however, this action can never be hit anyway)
   def logger
