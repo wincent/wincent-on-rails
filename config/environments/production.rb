ee65e5bc41014a95e88e79a396b4e5af50570eaf
Wincent Colaiuta <win@wincent.com>
2012-10-06 01:38:37 -0700

    Configure Amazon CloudFront static asset hosts
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,15 @@
 Wincent::Application.configure do
+  STATIC_ASSET_HOSTS = [
+    'd1qyd8e11adcyw.cloudfront.net',  # cdn01-wincent.com
+    'd3fuzw120je1e7.cloudfront.net',  # cdn02-wincent.com
+    'dhnj09q3kacqc.cloudfront.net',   # cdn03-wincent.com
+    'd8c1zvn3ij304.cloudfront.net',   # cdn04-wincent.com
+  ]
+
+  config.action_controller.asset_host = proc do |source|
+    'https://' + STATIC_ASSET_HOSTS[source.hash % STATIC_ASSET_HOSTS.size]
+  end
+
   config.action_controller.perform_caching  = true
   config.action_dispatch.x_sendfile_header  = 'X-Accel-Redirect'
   config.action_mailer.delivery_method      = :sendmail
