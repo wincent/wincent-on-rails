a5f17852859c560a4e5b2629856cf99670d28130
Wincent Colaiuta <win@wincent.com>
2013-03-02 08:40:31 -0800

    Replace `#cycle` with equivalent nth-child selectors
    This reduces the amount of code we have to run, the amount of markup we
    have to shift over the wire, and in general makes things nicer.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,15 +1,14 @@
 module CommentsHelper
-  def button_to_moderate_comment_as_ham comment
+  def button_to_moderate_comment_as_ham(comment)
     button_to_moderate_model_as_ham comment, polymorphic_path([comment.commentable, comment])
   end
 
   # Return an appropriate class for commentish (a comment or topic
-  # instance), based on whether it is in an odd or even row, belongs
-  # to a superuser, or is private.
-  def comment_class commentish
-    style = 'comment ' + cycle('even', 'odd')
-    style += ' admin' if commentish.user.try(:superuser?)
-    style += ' private' unless commentish.public?
-    style
+  # instance), based on whether it belongs to a superuser or is private.
+  def comment_class(commentish)
+    'comment'.tap do |css_class|
+      css_class << ' admin' if commentish.user.try(:superuser?)
+      css_class << ' private' unless commentish.public?
+    end
   end
 end
