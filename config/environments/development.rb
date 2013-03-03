f4c1207ce52463f0e759b1ee85ea69d1a62cac80
Wincent Colaiuta <win@wincent.com>
2013-03-03 00:56:09 -0800

    Force Compass to store generated sprites centrally
    This was horribly fiddly to do, and changes to the config file wouldn't
    take effect even on restarting the server. I had to touch the
    _display.scss file in order to finally trick Compass/Sass into
    re-spriting the images and confirming that this technique actually
    works (at least in dev); I hope it works in prod.
    
    See also:
    
      https://wincent.com/issues/2068
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,4 +9,9 @@ Wincent::Application.configure do
   config.cache_classes                                    = false
   config.consider_all_requests_local                      = true
   config.whiny_nils                                       = true
+
+  # prevent Compass from littering all over app/assets/images; confine the
+  # mess to a .gitignore-able subdirectory
+  config.compass.generated_images_dir = 'app/assets/images/sprites'
+  config.assets.paths << Rails.root + 'app/assets/images/sprites'
 end
