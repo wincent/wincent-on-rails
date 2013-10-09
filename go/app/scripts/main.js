3da49bf9ba8fb44c8a48f10e07419455920e1b61
Wincent Colaiuta <win@wincent.com>
2013-10-08 19:16:42 -0700

    Add go subdirectory files
    I am exploring the (probably crazy) idea of a full rewrite.
    
    These files were created with:
    
      brew install node
      npm install -g yo grunt bower generator-backbone
      yo backbone wor # accept proposal to use Twitter Bootstrap for Sass
    
    And:
    
      grunt compass # to compile Sass files
      grunt server # to run local development webserver
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ go/app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global go, $*/
+
+
+window.go = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    go.init();
+});
