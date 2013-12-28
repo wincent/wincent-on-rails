d4e7f6076d2309a49625271c51673de478eaca5b
Wincent Colaiuta <win@wincent.com>
2013-12-28 15:12:17 -0800

    Fix visual glitch on deleting last tag pill
    The widget was visibly shrinking with the removal of the last pill; add
    a margin to the hidden input to ensure that it maintains the widget's
    vertical height even when it is the only thing present.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- issues/react-page/src/elements/TagInput/TagInputStyleRules.js
+++ issues/react-page/src/elements/TagInput/TagInputStyleRules.js
@@ -4,8 +4,9 @@ var ReactStyle = require("ReactStyle");
 
 var TagInputStyleRules = ReactStyle.create({
   '.tagInput': {
-    border:        0,
-    textTransform: 'lowercase' // cheaper to do it in CSS than JS
+    border        : 0,
+    margin        : '5px 0',    // matches TagPill border + vertical margins
+    textTransform : 'lowercase' // cheaper to do it in CSS than JS
   },
   '.tagInput:focus': {
     outline: 0
