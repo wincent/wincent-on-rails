16e3b882a3a10204db82c829b4ebd8eb24edec9c
Wincent Colaiuta <win@wincent.com>
2013-12-28 15:14:28 -0800

    Convert another color to use hsl()
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- issues/react-page/src/elements/TagPill/TagPillStyleRules.js
+++ issues/react-page/src/elements/TagPill/TagPillStyleRules.js
@@ -5,7 +5,7 @@ var ReactStyle = require("ReactStyle");
 var TagPillStyleRules = ReactStyle.create({
   '.tagPill' : {
     background          : 'hsl(230, 21.4%, 94.5%)', // #eeeff4
-    border              : '1px solid #3b5999',
+    border              : '1px solid hsl(220.9,44.3%,41.6%)', // #3b5999
     borderRadius        : '2px',
     display             : 'inline-block',
     margin              : '2px',
