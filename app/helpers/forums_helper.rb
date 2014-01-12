42ee4633a3e0c16d1f6a0ce4d8db1c8e455c2af8
Wincent Colaiuta <win@wincent.com>
2014-01-11 23:00:08 -0500

    Apply minor formatting updates to the ForumsHelper
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/helpers/forums_helper.rb
+++ app/helpers/forums_helper.rb
@@ -3,7 +3,7 @@ module ForumsHelper
   # last_active_user_id, and last_active_user_display_name
   def link_to_user_for_topic topic
     # we're creating a temporary User object here, but it beats hitting the database
-    link_to_user(topic.last_active_user_id.nil? ? nil : User.new(:display_name => topic.last_active_user_display_name))
+    link_to_user(topic.last_active_user_id.nil? ? nil : User.new(display_name: topic.last_active_user_display_name))
   end
 
   # again we rely on a custom field to be set up in the Forums#index action: last_topic_id
