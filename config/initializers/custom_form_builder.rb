b28edd3458154cd9525f3f6bf3b82e7f9429e8b4
Wincent Colaiuta <win@wincent.com>
2014-01-15 13:47:49 -0500

    Suppress custom form builder on issues#show
    These ajax elements are inside a form, but they don't need the extra
    markup that the custom form build provides, so go back to the standard
    one.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- config/initializers/custom_form_builder.rb
+++ config/initializers/custom_form_builder.rb
@@ -7,13 +7,13 @@ ActiveSupport.on_load(:action_view) do
   # http://calvinconaway.com/2011/11/08/how-to-set-a-default-form-builder-in-rails-3-1-while-letting-it-be-autoloaded/
   ::ActionView::Helpers::FormHelper.class_eval do
     def form_for_with_custom(record, options = {}, &block)
-      options[:builder] = CustomFormBuilder
+      options[:builder] ||= CustomFormBuilder
       form_for_without_custom(record, options, &block)
     end
     alias_method_chain :form_for, :custom
 
     def fields_for_with_custom(record_name, record_object = nil, fields_options = {}, &block)
-      fields_options[:builder] = CustomFormBuilder
+      fields_options[:builder] ||= CustomFormBuilder
       fields_for_without_custom(record_name, record_object, fields_options, &block)
     end
     alias_method_chain :fields_for, :custom
