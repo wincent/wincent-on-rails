2c6178631f2e4bce5823f3a2d20207cdecc1588c
Wincent Colaiuta <win@wincent.com>
2014-01-15 01:15:34 -0500

    Revamp issues form styling
    This is the first redesigned form (note: all others are horribly broken
    with the removal of the default form styles from _display.scss).
    
    This is inspired by the Causes form design. I am not convinced that it's
    the best design (takes up a lot of vertical space), but I wanted to
    commit it anyway as a snapshot of what it might look like.
    
    Should improve/implement:
    
      - focus styling for text inputs and textareas
      - think about reducing vertical space by grouping narrow fields onto
        rows (eg. two selects next to one another, on wide screens)
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- lib/custom_form_builder.rb
+++ lib/custom_form_builder.rb
@@ -1,5 +1,46 @@
 class CustomFormBuilder < ActionView::Helpers::FormBuilder
-  def label(*args)
-    super
+  UNWRAPPAPLE_HELPERS = %i[hidden_field fields_for label]
+  WRAPPABLE_HELPERS   = field_helpers - UNWRAPPAPLE_HELPERS
+  ADDITIONAL_HELPERS  = %i[select]
+
+  # Overwrite the wrappable helper methods (text_field, select etc) to produce a
+  # method that generates markup like this:
+  #
+  #   <div class="field-row">
+  #     <label for="issue_summary">
+  #       <div class="label-text">Summary</div>
+  #       <aside class="annotation">enter as much detail as possible</aside)
+  #     </label>
+  #     <!-- superclass's original input here -->
+  #   </div>
+  #
+  (WRAPPABLE_HELPERS + ADDITIONAL_HELPERS).each do |name|
+    define_method(name) do |attr, *args|
+      options    = args.extract_options!
+      annotation = annotation(options.delete(:annotation))
+      label_text = label_text(options.delete(:label) || attr.to_s.humanize)
+
+      @template.content_tag(:div, class: 'field-row') do
+        @template.safe_join([
+          @template.content_tag(:label, label_text),
+          super(attr, *args, options),
+          annotation,
+        ], '')
+      end
+    end
+  end
+
+private
+
+  def label_text(label_text)
+    @template.content_tag(:div, label_text, class: 'label-text')
+  end
+
+  def annotation(annotations)
+    return '' unless annotations
+
+    @template.safe_join(Array(annotations).map do |annotation|
+      @template.content_tag(:aside, annotation, class: 'annotation')
+    end, ' ')
   end
 end
