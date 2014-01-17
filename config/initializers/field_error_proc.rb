3ec5637a29d280ef37cd24ad19489a18e54d27df
Wincent Colaiuta <win@wincent.com>
2014-01-17 00:34:38 -0500

    Robustify field_error_proc
    I could have gone with something simple like:
    
      http://stackoverflow.com/a/8380400
    
    But that is just a little too sloppy. For example, I could break it with
    content like:
    
      <textarea>
      class="yes!
      </textarea>
    
    But still, those ugly regexen deserve to be ripped out. They are hard to
    read. So, let's justify Nokogiri's ongoing presence in the Gemfile (it
    has been dead since we removed that Twitter code a few days ago), let's
    use it to robustly parse the HTML tag and rebuild it with the desired
    class attributes.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- config/initializers/field_error_proc.rb
+++ config/initializers/field_error_proc.rb
@@ -1,14 +1,17 @@
 # We don't want fields with validation errors wrapped in a "div" as this
 # makes it harder to consistently style forms. Instead of adding a new wrapper
 # element, we add the "field_with_errors" class to the existing tag.
-# We'll be called once for the label and once for the form field.
 ActionView::Base.field_error_proc = -> (html_tag, instance_tag) {
-  error_class = 'field_with_errors'
-  html_tag = html_tag.to_str # can't sub! on ActiveSupport::SafeBuffer
+  error_class = 'field-with-errors'
+  fragment    = Nokogiri::HTML.fragment(html_tag)
+  element     = fragment.children.first
+  klass       = element['class']
 
-  # first try appending to existing class attribute,
-  # otherwise fallback to adding new class attribute
-  html_tag.sub!(/\A(<[^>]+ class=["'])/, %Q[\\1#{error_class} ]) ||
-    html_tag.sub!(/\A(<\w+)/, %Q[\\1 class="#{error_class}"])
-  html_tag.html_safe
+  if klass
+    element['class'] = klass + " #{error_class}" unless klass == error_class
+  else
+    element['class'] = error_class
+  end
+
+  fragment.to_s.html_safe
 }
