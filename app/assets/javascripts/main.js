441295e2350dd8460494eba6d157c3a07be06f55
Wincent Colaiuta <win@wincent.com>
2014-04-19 23:53:14 -0700

    Update Browserify bundling strategy
    The `-r` switches mean that I can do:
    
      $ = require('jquery')
    
    anywhere and have it work.
    
    Eventually, all will make sense...
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -5,14 +5,16 @@
 'use strict';
 
 window.Wincent = {};
-Wincent.Menu = require('./Menu');
-Wincent.Spinner = require('./Spinner');
 Wincent.Ajax = require('./Ajax');
 Wincent.Git = require('./Git');
+Wincent.Menu = require('./Menu');
+Wincent.Spinner = require('./Spinner');
+Wincent.TagWidget = require('./TagWidget');
 
 // once all is done, these won't be global
 window.$ = require('jquery');
 window._ = require('underscore');
+window.React = require('react/addons');
 
 // initializers:
 Wincent.Ajax.init();
