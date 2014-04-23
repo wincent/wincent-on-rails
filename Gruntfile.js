c0ee583f3c371155a60a32e36f483ed33cef647c
Wincent Colaiuta <win@wincent.com>
2014-04-23 07:11:13 -0700

    Set up Grunt to build server-side modules
    Installed grunt-react with:
    
      npm install grunt-react --save-dev
    
    Now you can create a server build with:
    
      grunt
    
    or:
    
      npm run serverify
    
    More tweaks to come, but this basically works.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-react');
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    react: {
+      files: {
+        expand: true,
+        cwd: 'app/assets/javascripts',
+        src: ['**/*.js'],
+        dest: 'server/build',
+        ext: '.js',
+        options: {
+          harmony: true
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['react']);
+};
