51baf3b333a819105457e77035b2b0f37e998d4b
Greg Hurrell <greg@hurrell.net>
2015-03-28 17:12:45 -0700

    Apply minor tweak to users table migration before running it
    We want future rows to default to 1, but old rows to be set to 0 to
    start with.

--- db/migrate/20150328224301_add_hash_version_to_users.rb
+++ db/migrate/20150328224301_add_hash_version_to_users.rb
@@ -1,5 +1,6 @@
 class AddHashVersionToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :hash_version, :integer, default: 0
+    add_column :users, :hash_version, :integer, default: 1
+    User.update_all 'hash_version = 0'
   end
 end
