9d42b0860ed176f0ed4a2cc1c0b086bd8abbcf74
Greg Hurrell <greg@hurrell.net>
2015-09-25 07:23:50 -0700

    Make issues pages more suitable for static mirroring

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,31 +1,18 @@
 class IssuesController < ApplicationController
-  before_filter     :find_product, only: :index
-  before_filter     :find_issue_awaiting_moderation, only: :show
-  before_filter     :prepare_issue_for_search, only: %i[index search]
-
-  acts_as_sortable  by: %i[public kind id product_id summary status updated_at],
-                    default: :updated_at,
-                    descending: true
+  before_filter :find_product, only: :index
+  before_filter :find_issue_awaiting_moderation, only: :show
+  before_filter :flash_deprecation_notice
+  before_filter :set_layout_options
 
   def index
-    # set up options and add possible params that can be used to limit the scope of the search
     options = default_access_options # defined in ApplicationController
-    add_kind_scope_condition options
-    add_status_scope_condition options
-    add_product_scope_condition options
-
-    # TODO: lots of string concatenation here; replace with relational algebra
     issues = Issue.where(options)
 
-    # NOTE: have an N + 1 issue here (for each product we get the product info)
-    # can't just :include => :product here because that will introduce an ambiguous "updated_at" column
-    # thanks to acts_as_sortable (will need to update acts as sortable)
     @paginator = RestfulPaginator.new params, issues.count, issues_path
 
     # Rails BUG #5060: must call to_a here so that empty? will work as expected
-    @issues = issues.order(sort_options).limit(@paginator.limit).
+    @issues = issues.order('id DESC').limit(@paginator.limit).
       offset(@paginator.offset).to_a
-
   end
 
   def show
@@ -41,33 +28,22 @@ class IssuesController < ApplicationController
     end
   end
 
-  def search
-    if params[:issue]
-      issues      = Issue.search default_access_options, params[:issue]
-      @paginator  = Paginator.new params, issues.count, search_issues_path
+private
 
-      # to_a() call here is to work around Rails BUG #5060
-      #   https://rails.lighthouseapp.com/projects/8994/tickets/5060
-      @issues     = issues.limit(@paginator.limit).offset(@paginator.offset).order(sort_options).to_a
-      render 'issues/search/create'
-    else
-      render 'issues/search/new'
-    end
+  def flash_deprecation_notice
+    flash.now[:notice] =
+      'You are viewing an historical archive of past issues. ' +
+      'Please report new issues to the appropriate project issue tracker on ' +
+      '<a href="https://github.com/wincent?tab=repositories">GitHub</a>.'
   end
 
-private
+  def set_layout_options
+    @suppress_session_links = true
+    @suppress_menu = true
+  end
 
   def find_product
     @product = Product.find_by_name(params[:product]) if params[:product]
-  end
-
-  # This simplifies our search form, and allows it to "remember" search params
-  # in case the user wants to modify an existing search.
-  def prepare_issue_for_search
-    options = params[:issue] || {}
-    options[:status] = nil unless options.has_key?(:status) # suppress default
-    options[:kind] = nil unless options.has_key?(:kind)     # suppress default
-    @issue = Issue.new options
   end
 
   def find_issue_awaiting_moderation
@@ -76,24 +52,6 @@ private
     else
       @issue = Issue.find params[:id]
     end
-  end
-
-  def add_kind_scope_condition options
-    if params[:kind]
-      key = params[:kind].gsub(' ', '_').downcase.to_sym
-      options << " AND kind = #{Issue::KIND[key]}" if Issue::KIND.key? key
-    end
-  end
-
-  def add_status_scope_condition options
-    if params[:status]
-      key = params[:status].gsub(' ', '_').downcase.to_sym
-      options << " AND status = #{Issue::STATUS[key]}" if Issue::STATUS.key? key
-    end
-  end
-
-  def add_product_scope_condition options
-    options << " AND product_id = #{@product.id}" if @product
   end
 
   def record_not_found
--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -1,23 +1,4 @@
 module IssuesHelper
-  def scope_info
-    scopes = []
-    scopes << "product: #{params[:product]}" if params[:product]
-    scopes << "kind: #{params[:kind].pluralize}" if params[:kind]
-    scopes << "status: #{params[:status]}" if params[:status]
-    "Currently showing only issues with #{scopes.join(', ')}" unless scopes.empty?
-  end
-
-  def search_info
-    unless (issue = params[:issue]).blank?
-      criteria = []
-      criteria << "product: #{Product.find(issue[:product_id]).name}" unless issue[:product_id].blank?
-      criteria << "kind: #{Issue.string_for_kind issue[:kind].to_i}" unless issue[:kind].blank?
-      criteria << "status: #{Issue.string_for_status issue[:status].to_i}" unless issue[:status].blank?
-      criteria << "matching text: #{issue[:summary]}" unless issue[:summary].blank?
-      "Currently showing only issues with #{criteria.join(', ')}" unless criteria.empty?
-    end
-  end
-
   def issue_tooltip issue
     "#{issue.kind_string} \##{issue.id}: #{issue.summary}"
   end
@@ -33,14 +14,6 @@ module IssuesHelper
     if issue
       link_to 'next &raquo;'.html_safe, issue_path(issue),
         :title => issue_tooltip(issue)
-    end
-  end
-
-  def link_to_product_issues product
-    if product
-      link_to product.name, issues_path(:product => product.name)
-    else
-      'no product'
     end
   end
 
